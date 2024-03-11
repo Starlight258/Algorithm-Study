e6ceed36b0367d0791a27b99c0c22e4cec3b8837
Mint <78211281+Starlight258@users.noreply.github.com>
2024-03-11 21:20:12 +0900

    Update 이진 변환 반복하기.java

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
@@ -2,36 +2,25 @@ import java.util.ArrayList;
 
 class Solution {
     public int[] solution(String s) {
-        ArrayList<Integer> answer = new ArrayList<Integer>();
+        int[] answer = new int[2];
         int cnt = 0;
         int numCnt = 0;
         while (s.length()>1){
             // 0 제거하기
-            cnt += s.length(); // 0,1
+            cnt += s.length(); 
             s = s.replaceAll("0", "");
-            
-            if (s.contains("0")){
-                while (s.contains("0")){
-                    s = s.replaceFirst("0", "");
-                    cnt++;
-                }
-            }
-            cnt -= s.length(); // 1
+            cnt -= s.length();
            
             // 2진법으로 표현하기
             int num = s.length();
             StringBuffer numStr = new StringBuffer("");
             s = Integer.toBinaryString(num);
-            // while (num != 0){
-            //     numStr.append(num % 2);
-            //     num /= 2;
-            // }
-            // s = numStr.reverse().toString();
             numCnt++;
         }
-        answer.add(numCnt);
-        answer.add(cnt); 
         
-         return answer.stream().mapToInt(x->x).toArray();
+        answer[0] = numCnt;
+        answer[1] = cnt;
+        
+         return answer;
     }
 }
