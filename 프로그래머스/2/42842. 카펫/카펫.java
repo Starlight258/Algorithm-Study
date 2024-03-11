41b5ea0f462432fc5b50299ca557474ddd3c5e47
Mint <78211281+Starlight258@users.noreply.github.com>
2024-03-11 20:01:53 +0900

    [level 2] Title: 카펫, Time: 0.03 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int[] answer = new int[2];
+        for (int i=1;i<=brown;i++){
+            if (yellow%i==0){
+                int a = i; // 세로
+                int b = yellow / i; // 가로
+                
+                int brownNum = a * 2 + b * 2 + 4;
+                if (brown == brownNum){
+                    answer[0] = b + 2;
+                    answer[1] = a + 2;
+                    break;
+                }
+            }
+        }
+        return answer;
+    }
+}
