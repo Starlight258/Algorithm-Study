79c38451f60147fe6805e9c305b78ea7af9b742f
Mint <78211281+Starlight258@users.noreply.github.com>
2024-03-13 22:33:16 +0900

    [level 1] Title: [PCCE 기출문제] 10번 / 데이터 분석, Time: 17.24 ms, Memory: 95.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/250121. ［PCCE 기출문제］ 10번 ／ 데이터 분석/［PCCE 기출문제］ 10번 ／ 데이터 분석.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+
+
+class Solution {
+    public int getIndex(String columnName){
+        switch(columnName){
+            case "code": 
+                return 0;
+            case "date":
+                return 1;
+            case "maximum":
+                return 2;
+            case "remain":
+                return 3;
+            default: 
+                return 0;
+        }
+    }
+    public int[][] solution(int[][] data, String ext, int val_ext, String sort_by) {
+        int extIndex = getIndex(ext);
+        int sortByIndex = getIndex(sort_by);
+        
+        return Arrays.stream(data)
+            .filter(row -> row[extIndex] < val_ext)
+            .sorted((row1, row2) -> Integer.compare(row1[sortByIndex], row2[sortByIndex]))
+            .toArray(int[][]::new);
+    }
+}
