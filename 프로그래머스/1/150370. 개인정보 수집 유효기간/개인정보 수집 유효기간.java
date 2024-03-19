a045f375074193dfb29bba6f3307c789e259863e
Mint <78211281+Starlight258@users.noreply.github.com>
2024-03-19 16:51:10 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 4.40 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -2,14 +2,16 @@ import java.util.HashMap;
 import java.util.ArrayList;
 
 class Solution {
+    public int getDate(String day){
+        String[] dayList = day.split("\\.");
+        int dayNum = Integer.parseInt(dayList[0]) * 28*12;
+        dayNum += Integer.parseInt(dayList[1]) * 28;
+        dayNum += Integer.parseInt(dayList[2]);
+        return dayNum;
+    }
     public int[] solution(String today, String[] terms, String[] privacies) {
         ArrayList<Integer> answer = new ArrayList<Integer>();
-        int todayNum = 0;
-        
-        String[] t = today.split("\\.");
-        todayNum += Integer.parseInt(t[0]) * 28*12;
-        todayNum += Integer.parseInt(t[1]) * 28;
-        todayNum += Integer.parseInt(t[2]);
+        int todayNum = getDate(today);
         
         HashMap<String, Integer> mp = new HashMap<>();
         for (int i=0;i<terms.length;i++){
@@ -18,12 +20,10 @@ class Solution {
         }
         
         for (int i=0;i<privacies.length;i++){
-            int dayNum = 0;
-            String[] s = privacies[i].split("\\.");
-            dayNum += Integer.parseInt(s[0]) * 28*12;
-            dayNum += Integer.parseInt(s[1]) * 28;
-            dayNum += Integer.parseInt(s[2].substring(0,2));
-            int addDay = mp.get(s[2].substring(3)+"");
+            String[] s = privacies[i].split(" ");
+            int dayNum = getDate(s[0]);
+            
+            int addDay = mp.get(s[1]);
             dayNum += addDay;
             if (dayNum <= todayNum){
                 answer.add(i+1);
