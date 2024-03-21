20ffd574e4ec546065362066399642ffbdad45e4
Mint <78211281+Starlight258@users.noreply.github.com>
2024-03-22 00:12:09 +0900

    [level 2] Title: 타겟 넘버, Time: 0.41 ms, Memory: 76.3 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,18 +1,19 @@
 class Solution {
     int answer = 0;
-
-    public int solution(int[] numbers, int target) {
-        dfs(numbers, 0, target, 0);
-
-        return answer;
-    }
-
-    public void dfs(int[] numbers, int depth, int target, int sum){
-        if(depth == numbers.length){ 
-            if(target == sum) answer++;
-        } else {
-            dfs(numbers, depth + 1, target, sum + numbers[depth]); 
-            dfs(numbers, depth + 1, target, sum - numbers[depth]); 
+    int[] mark = {1, -1};
+    void dfs(int[] numbers, int target, int idx, int sum){
+        if (sum == target && idx == numbers.length-1){
+            answer++;
+            return;
+        } else if (idx >= numbers.length-1) return;
+        for (int i=0;i<2;i++){
+            dfs(numbers, target, idx+1, sum+mark[i]*numbers[idx+1]);
         }
+        
+    }
+    public int solution(int[] numbers, int target) {
+        dfs(numbers, target, 0, numbers[0]);
+        dfs(numbers, target, 0, numbers[0]*-1);
+        return answer;
     }
 }
