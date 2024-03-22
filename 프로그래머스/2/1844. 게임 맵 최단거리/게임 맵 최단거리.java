ea47a3600591311f4b39b9dbcc3915da5cb8ea50
Mint <78211281+Starlight258@users.noreply.github.com>
2024-03-22 23:52:42 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 4.02 ms, Memory: 52.6 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,38 +1,44 @@
 import java.util.*;
-class pNode { 
-    int x;
+class Pair { 
     int y;
-    pNode (int y, int x){
+    int x;
+    
+    Pair (int y, int x){
         this.y = y;
         this.x = x;
     }
 }
 class Solution {
+    int dy[] = {-1, 0, 1, 0};
+    int dx[] = {0, 1, 0, -1};
+    Queue<Pair> stk = new LinkedList<>();
+    int[][] visited = new int[104][104];
+    
     public int solution(int[][] maps) {
-        int answer = 0;
-        int dy[] = {-1, 0, 1, 0};
-        int dx[] = {0, 1, 0, -1};
-        Queue<pNode> stk = new LinkedList<>();
-        int n = maps.length; //y
-        int m = maps[0].length; //x
-        int[][] visited = new int[n][m];
+        int n = maps.length; 
+        int m = maps[0].length; 
         visited[0][0] = 1;
-        stk.offer(new pNode(0, 0));
+        stk.offer(new Pair(0, 0));
+        
         while (!stk.isEmpty()){
-            int x = stk.peek().x;
-            int y = stk.peek().y;
-            stk.poll();
-            answer++;
+            Pair here = stk.poll();
+            int y = here.y;
+            int x = here.x;
+            if (y==n-1 && x==m-1){
+                return visited[n-1][m-1];
+            }
+            
             for (int i=0;i<4;i++){
                 int ny = y + dy[i];
                 int nx = x + dx[i];
                 if (ny<0 || ny>=n || nx<0 || nx>=m) continue;
                 if (visited[ny][nx] == 0 && maps[ny][nx]==1){
-                    stk.offer(new pNode(ny, nx));
+                    stk.offer(new Pair(ny, nx));
                     visited[ny][nx] = visited[y][x] + 1;
                 } 
             }
         }
-        return visited[n-1][m-1]==0 ? -1 : visited[n-1][m-1];
+        
+        return -1;
     }
 }
