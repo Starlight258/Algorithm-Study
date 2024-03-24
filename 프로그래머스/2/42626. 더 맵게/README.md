c22b4af1c709e8fbcee2045b4ee2d77f54740026
Mint <78211281+Starlight258@users.noreply.github.com>
2024-03-24 23:44:21 +0900

    [level 2] Title: 더 맵게, Time: 1712.54 ms, Memory: 122 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/README.md
+++ 프로그래머스/2/42626. 더 맵게/README.md
@@ -1,10 +1,10 @@
 # [level 2] 더 맵게 - 42626 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42626?language=java) 
 
 ### 성능 요약
 
-메모리: 123 MB, 시간: 1636.48 ms
+메모리: 122 MB, 시간: 1712.54 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 24일 23:37:03
+2024년 03월 24일 23:44:20
 
 ### 문제 설명
 
--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -6,16 +6,14 @@ class Solution {
         for (int i=0;i<scoville.length;i++){
             pq.offer(scoville[i]);
         }
+        
         while (pq.size()>1 && pq.peek()<K){
-            int a = pq.poll();            
+            int a = pq.poll();
             int b = pq.poll();
-
+            
             int newScoville = a + b*2;
             pq.offer(newScoville);
             answer++;
-            if (pq.peek()>=K){
-                return answer;
-            }
         }
         
         return pq.peek()>=K ? answer : -1;
