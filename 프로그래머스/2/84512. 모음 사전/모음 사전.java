b7e3e2001e505c6934db1cbca614c4090e5f0469
Mint <78211281+Starlight258@users.noreply.github.com>
2024-03-25 20:26:06 +0900

    [level 2] Title: 모음 사전, Time: 15.97 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,29 +1,6 @@
 class Solution {
-    char[] w = {'A', 'E', 'I', 'O', 'U'};
-    int cnt = 0;
-    boolean exitFlag = false;
-    public int dfs(String makingWord, String word, int depth){
-        if (makingWord.equals(word)){
-            exitFlag = true;
-            return cnt;
-        } 
-        cnt++;
-        if (makingWord.length() >= 5) {
-            return -1;
-        }
-        for (int i=0;i<5;i++){
-            dfs(makingWord + w[i], word, depth+1);
-            if (makingWord.length()>=5) break;
-            if (exitFlag) break;
-        }
-        return cnt;
-    }
-    
     public int solution(String word) {
         int answer = 0;
-        answer = dfs("", word, 0);
-        
         return answer;
-        
     }
 }
