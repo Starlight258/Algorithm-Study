a28c3b980caa64dd9a5aa285cecf1ef8226efd52
Mint <78211281+Starlight258@users.noreply.github.com>
2024-03-31 19:28:04 +0900

    [level 1] Title: 모의고사, Time: 3.42 ms, Memory: 82.3 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -2,19 +2,17 @@ import java.util.*;
 class Solution {
     public int[] solution(int[] answer) {
         ArrayList<Integer> answerList = new ArrayList<>();
-        int p1[] = {1,2,3,4,5};
-        int p2[] = {2,1,2,3,2,4,2,5};
-        int p3[] = {3,3,1,1,2,2,4,4,5,5};
+        int pattern[][] = {
+            {1,2,3,4,5},
+            {2,1,2,3,2,4,2,5},
+            {3,3,1,1,2,2,4,4,5,5}
+        };
         int[] cnt = new int[3];
         for (int i=0;i<answer.length;i++){
-            if (answer[i] == p1[i % p1.length]){
-                cnt[0]++;
-            } 
-            if(answer[i] == p2[i % p2.length]){
-                cnt[1]++;
-            }
-            if (answer[i] == p3[i % p3.length]){
-                cnt[2]++;
+            for (int j=0;j<3;j++){
+                if (answer[i] == pattern[j][i % pattern[j].length]){
+                    cnt[j]++;
+                }
             }
         }
         // 3 원소 크기 비교
