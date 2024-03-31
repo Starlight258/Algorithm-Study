cbe61f8b04a3ab8f78c3336ab20ff3167f12eab4
Mint <78211281+Starlight258@users.noreply.github.com>
2024-03-31 19:18:43 +0900

    [level 1] Title: 모의고사, Time: 2.75 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -5,30 +5,20 @@ class Solution {
         int p1[] = {1,2,3,4,5};
         int p2[] = {2,1,2,3,2,4,2,5};
         int p3[] = {3,3,1,1,2,2,4,4,5,5};
-        int[] pos = new int[3];
         int[] cnt = new int[3];
-        int cnt1=0, cnt2=0, cnt3 = 0;
         for (int i=0;i<answer.length;i++){
-            pos[0] = pos[0]>=p1.length ? 0 : pos[0];
-            pos[1] = pos[1]>=p2.length ? 0 : pos[1];
-            pos[2] = pos[2]>=p3.length ? 0 : pos[2];
- 
-            if (answer[i] == p1[pos[0]]){
+            if (answer[i] == p1[i % p1.length]){
                 cnt[0]++;
             } 
-            if(answer[i] == p2[pos[1]]){
+            if(answer[i] == p2[i % p2.length]){
                 cnt[1]++;
             }
-            if (answer[i] == p3[pos[2]]){
+            if (answer[i] == p3[i % p3.length]){
                 cnt[2]++;
             }
-            pos[0]++; pos[1]++; pos[2]++;
         }
         // 3 원소 크기 비교
-        int maxCnt = 0;
-        for (int i=0;i<3;i++){
-            maxCnt = Math.max(maxCnt, cnt[i]);
-        }
+        int maxCnt = Math.max(cnt[0], Math.max(cnt[1], cnt[2]));
         for (int i=0;i<3;i++){
             if (maxCnt == cnt[i]){
                 answerList.add(i+1);
