051ebe4f6723c96fd9455906dbe460fa14a6ee11
Mint <78211281+Starlight258@users.noreply.github.com>
2024-04-06 00:42:45 +0900

    [level 3] Title: N으로 표현, Time: 0.02 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -1,39 +1,30 @@
 import java.util.*;
-
 class Solution {
     public int solution(int N, int number) {
-        if (N == number) {
-            return 1;
+        if (N==number) return 1;
+        // 1. N이 커질때 가능한 숫자 생성하기
+        ArrayList<HashSet<Integer>> list = new ArrayList<HashSet<Integer>>();
+        for (int i=0;i<9;i++){
+            list.add(new HashSet<Integer>());
         }
-
-        // dp[i]는 N을 i번 사용하여 만들 수 있는 수의 집합
-        List<Set<Integer>> dp = new ArrayList<>();
-        for (int i = 0; i < 9; i++) {
-            dp.add(new HashSet<>());
-        }
-        dp.get(1).add(N);
-
-        for (int i = 2; i <= 8; i++) {
-            dp.get(i).add(Integer.parseInt(String.valueOf(N).repeat(i))); // N을 i번 이어붙인 수 추가
-
-            for (int j = 1; j < i; j++) {
-                for (int x : dp.get(j)) {
-                    for (int y : dp.get(i - j)) {
-                        dp.get(i).add(x + y);
-                        dp.get(i).add(x - y);
-                        dp.get(i).add(x * y);
-                        if (y != 0) {
-                            dp.get(i).add(x / y);
-                        }
+        list.get(1).add(N);
+        for (int i=2;i<=8;i++){ 
+            list.get(i).add(Integer.parseInt(String.valueOf(N).repeat(i)));
+            // 2. 숫자 만들기 
+            for (int j=1;j<i;j++){
+                for (int x:list.get(j)){
+                    for (int y:list.get(i-j)){
+                        list.get(i).add(x+y);
+                        list.get(i).add(x-y);
+                        list.get(i).add(x*y);
+                        if (y!=0) list.get(i).add(x/y);
                     }
                 }
             }
-
-            if (dp.get(i).contains(number)) {
+            if (list.get(i).contains(number)){
                 return i;
             }
         }
-
         return -1;
     }
 }
