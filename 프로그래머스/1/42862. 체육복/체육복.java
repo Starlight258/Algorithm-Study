953c541070104c73575af9c49b6d0e330cb1b3e8
Mint <78211281+Starlight258@users.noreply.github.com>
2024-04-06 22:04:59 +0900

    [level 1] Title: 체육복, Time: 0.37 ms, Memory: 69.1 MB -BaekjoonHub

---
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+        int answer = n - lost.length;
+        int[] check = new int[n+1];
+        Arrays.sort(lost);
+        for (int i=0;i<lost.length;i++){
+            check[lost[i]] = -1;
+        }
+        for (int i=0;i<reserve.length;i++){
+            check[reserve[i]] += 1;   
+            if (check[reserve[i]] == 0) answer++;
+        }
+        
+        for (int i=0;i<lost.length;i++){
+            int l = lost[i];
+            if (check[l]==0) continue;
+            if (l-1 >=0 && check[l-1] > 0){
+                check[l-1] = 0;
+                answer++;
+            } 
+            else if (l+1 <=n && check[l+1]>0){
+                check[l+1] = 0;
+                answer++;
+            }
+        }
+        return answer;
+    }
+}
