6c3b65495a7004b7a5817c63d2c8aea979d5e214
Mint <78211281+Starlight258@users.noreply.github.com>
2024-04-06 21:20:36 +0900

    [level 3] Title: 등굣길, Time: 0.66 ms, Memory: 52.3 MB -BaekjoonHub

--- 프로그래머스/3/42898. 등굣길/등굣길.java
+++ 프로그래머스/3/42898. 등굣길/등굣길.java
@@ -1,7 +1,7 @@
 class Solution {
     public int solution(int m, int n, int[][] puddles) {
         int answer = 0;
-        int dp[][] = new int[n+1][m+1];
+        int dp[][] = new int[n][m];
         dp[0][0] = 1;
         for (int i=0;i<puddles.length;i++){
             dp[puddles[i][1]-1][puddles[i][0]-1] = -1;
