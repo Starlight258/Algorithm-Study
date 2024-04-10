09d614e5ce5acc9ce0e074022ad22787cab4684a
Mint <78211281+Starlight258@users.noreply.github.com>
2024-04-10 12:12:54 +0900

    [level 2] Title: 혼자서 하는 틱택토, Time: 0.11 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/2/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.java
+++ 프로그래머스/2/160585. 혼자서 하는 틱택토/혼자서 하는 틱택토.java
@@ -1,15 +1,13 @@
 class Solution {
-    int checkWin(Character[][] mp, char c){
-        // 가로
+    boolean checkWin(Character[][] mp, char c){
         int cnt = 0;
         for (int i=0;i<3;i++){
+            // 가로
             if (mp[i][0] == c && mp[i][0] == mp[i][1] && mp[i][1]==mp[i][2]){
                 cnt++;
             }
-        }
-        // 세로
-        for (int i=0;i<3;i++){
-            if (mp[0][i] == c && mp[0][i] == mp[1][i] && mp[1][i]==mp[2][i]){
+            // 세로
+             if (mp[0][i] == c && mp[0][i] == mp[1][i] && mp[1][i]==mp[2][i]){
                 cnt++;
             }
         }
@@ -17,8 +15,8 @@ class Solution {
         if (mp[0][0] == c && mp[0][0] == mp[1][1] && mp[1][1]==mp[2][2]) cnt++;
         if (mp[2][0] == c && mp[2][0] == mp[1][1] && mp[1][1] == mp[0][2]) cnt++;
         
-        if (cnt>=1) return 1; // 우승함
-        return 0; // 우승 안함
+        if (cnt>=1) return true;
+        return false;
     }
     
     public int solution(String[] board) {
@@ -34,21 +32,24 @@ class Solution {
             }
         }
         if(ocnt==0 && xcnt==0) return 1;
-        // 1번
-        if (ocnt < xcnt || ocnt-xcnt>1) return 0;
+        // 1번 실수
         if ((ocnt+xcnt) % 2 ==0 && ocnt!=xcnt){
             return 0;
         }
-        // 2번
-        int c1 = checkWin(mp, 'O');
-        int c2 = checkWin(mp, 'X');
-        if (c1==1 && c2==1) return 0; // 둘 다 우승
+        if ((ocnt+xcnt) % 2 ==1 && ocnt-xcnt!=1){
+            return 0;
+        }
+        
+        // 2번 실수
+        boolean c1 = checkWin(mp, 'O');
+        boolean c2 = checkWin(mp, 'X');
+        if (c1 && c2) return 0; // 둘 다 우승
         // 한명만 우승(1)했거나 둘 다 우승안했을 경우(0)
-        if (c1==1){ // O 우승
+        if (c1){ // O 우승
             if (ocnt<=xcnt) return 0;
         }
-        if (c2==1){ // X 우승
-            if (ocnt>xcnt) return 0;
+        if (c2){ // X 우승
+            if (ocnt!=xcnt) return 0;
         }
         // 둘 다 우승안했을 경우
         return answer;
