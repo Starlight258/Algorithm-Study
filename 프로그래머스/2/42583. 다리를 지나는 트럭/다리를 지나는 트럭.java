ec2bf310084c94ef635869eea180a4c30b37f466
Mint <78211281+Starlight258@users.noreply.github.com>
2024-04-11 10:15:26 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 0.17 ms, Memory: 75.6 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -1,24 +1,28 @@
 import java.util.*;
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
-        Queue<Integer> queue = new LinkedList<Integer>();
-        for (int i=0;i<bridge_length;i++) queue.offer(0);
-        int tPos = 0; // 트럭
-        int time = 0;
-        int available = weight;
-        while (tPos<truck_weights.length){
-            time++;
-            available += queue.poll();
-            if (available >= truck_weights[tPos]){
-                available -= truck_weights[tPos];
-                queue.offer(truck_weights[tPos]);
-                tPos++;
-            } else queue.offer(0);
-            if (tPos == truck_weights.length){
-                time += bridge_length;
-            }
-            
+        int answer = 0;
+        int currentWeight=0;
+        int pos = 0;
+        Queue<Integer> bridge = new LinkedList<>();
+        // 다리 초기화 (1초에 1만큼 움직일 수 있다. 무게를 저장한다.)
+        for(int i=0;i<bridge_length;i++){
+            bridge.offer(0);
         }
-        return time;
+        // 다리 건너기 시작한다.
+        while (pos < truck_weights.length){
+            currentWeight -= bridge.poll();
+            if (truck_weights[pos] + currentWeight <= weight){
+                currentWeight += truck_weights[pos];
+                bridge.offer(truck_weights[pos]);
+                pos++;
+            } else{
+                bridge.offer(0);
+            }
+            answer++; // 시간 흐름
+        }
+        
+        
+        return answer + bridge_length;
     }
 }
