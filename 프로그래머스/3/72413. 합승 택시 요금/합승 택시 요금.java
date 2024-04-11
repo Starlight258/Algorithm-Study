115167583c304d6751106f263e76ec23e02778b1
Mint <78211281+Starlight258@users.noreply.github.com>
2024-04-11 14:27:18 +0900

    [level 3] Title: 합승 택시 요금, Time: 3.55 ms, Memory: 52 MB -BaekjoonHub

--- 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
+++ 프로그래머스/3/72413. 합승 택시 요금/합승 택시 요금.java
@@ -1,32 +1,58 @@
 import java.util.*;
 class Solution {
-    public int solution(int n, int s, int a, int b, int[][] fares) {
-        int answer = 0;
-        int INF = 987654321;
-        int[][] graph = new int[n+1][n+1];
-        for (int i=1;i<=n;i++){
-            Arrays.fill(graph[i], INF);
+    class Node implements Comparable<Node>{
+        int index;
+        int dist;
+        Node(int index, int dist){
+            this.index = index;
+            this.dist = dist;
         }
-        for (int i=1;i<=n;i++){
-            graph[i][i] = 0;
+        public int compareTo(Node n){
+            return this.dist - n.dist;
         }
-        for (int i=0;i<fares.length;i++){
-            graph[fares[i][0]][fares[i][1]] = fares[i][2];
-            graph[fares[i][1]][fares[i][0]] = fares[i][2];
-        }
-        
-        for (int k=1;k<=n;k++){
-            for (int i=1;i<=n;i++){
-                for (int j=1;j<=n;j++){
-                    graph[i][j] = Math.min(graph[i][j], graph[i][k]+graph[k][j]);
+    }
+    ArrayList<ArrayList<Node>> graph = new ArrayList<ArrayList<Node>>();
+    int[] dijkstra(int start, int n){
+        int[] d = new int[n+1];
+        Arrays.fill(d, Integer.MAX_VALUE);
+        PriorityQueue<Node> pq = new PriorityQueue<Node>();
+        pq.offer(new Node(start, 0));
+        d[start] = 0;
+        while (!pq.isEmpty()){
+            Node node = pq.poll();
+            int dist = node.dist;
+            int now = node.index;
+            if (d[now] < dist) continue;
+            for (int i=0;i<graph.get(now).size();i++){
+                int cost = d[now] + graph.get(now).get(i).dist;
+                if (cost < d[graph.get(now).get(i).index]){
+                    d[graph.get(now).get(i).index] = cost;
+                    pq.offer(new Node(graph.get(now).get(i).index, cost));
                 }
             }
         }
-        answer = graph[s][a] + graph[s][b];
+        return d;
+    }
+    
+    public int solution(int n, int s, int a, int b, int[][] fares) {
+        int answer = 0;
+        int INF = 987654321;
+        for (int i=0;i<=n;i++){
+            graph.add(new ArrayList<Node>());
+        }
+        for (int i=0;i<fares.length;i++){
+            graph.get(fares[i][0]).add(new Node(fares[i][1], fares[i][2]));
+            graph.get(fares[i][1]).add(new Node(fares[i][0], fares[i][2]));
+        }
+        
+        int[] fromS = dijkstra(s, n);
+        int[] fromA = dijkstra(a, n);
+        int[] fromB = dijkstra(b, n);
+        
+        answer = fromS[a] + fromS[b];
         for (int i=1;i<=n;i++){
             if (i==s) continue;
-            if (graph[s][i] == INF || graph[i][a] == INF || graph[i][b]==INF) continue;
-            answer = Math.min(answer, graph[s][i] + graph[i][a] + graph[i][b]);
+            answer = Math.min(answer, fromS[i] + fromA[i] + fromB[i]);
         }
         return answer;
     }
