968fdeb83dabda1ae590ac169316546e9a28b931
Mint <78211281+Starlight258@users.noreply.github.com>
2024-04-11 21:24:11 +0900

    [level 3] Title: 가장 먼 노드, Time: 58.68 ms, Memory: 115 MB -BaekjoonHub

--- 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
+++ 프로그래머스/3/49189. 가장 먼 노드/가장 먼 노드.java
@@ -1,39 +1,46 @@
 import java.util.*;
 class Solution {
-    public int solution(int n, int[][] edge) {
-        ArrayList<ArrayList<Integer>> graph = new ArrayList<ArrayList<Integer>>();
-        int d[] = new int[n+1];
-        Arrays.fill(d, Integer.MAX_VALUE);
-        for (int i=0;i<=n;i++) graph.add(new ArrayList<Integer>());
-        for (int i=0;i<edge.length;i++){
-            graph.get(edge[i][0]).add(edge[i][1]);
-            graph.get(edge[i][1]).add(edge[i][0]);
-        }
-        
-        PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
-        pq.offer(1);
-        d[1] = 0;
-        while (!pq.isEmpty()){
-            int now = pq.poll();
-            for (int i=0;i<graph.get(now).size();i++){
-                int cost = d[now] + 1;
-                if (cost < d[graph.get(now).get(i)]){
-                    d[graph.get(now).get(i)] = cost;
-                    pq.offer(graph.get(now).get(i));
+    int[] visited;
+    ArrayList<ArrayList<Integer>> list = new ArrayList<ArrayList<Integer>>();
+    void bfs(int n){
+        Queue<Integer> q = new LinkedList<Integer>();
+        visited = new int[n+1];
+        q.offer(1);
+        visited[1] = 1;
+        while (!q.isEmpty()){
+            int now = q.poll();
+            for (int i=0;i<list.get(now).size();i++){
+                int next = list.get(now).get(i);
+                if (visited[next] == 0){
+                    visited[next] = visited[now] + 1;
+                    q.offer(next);
                 }
             }
+            
         }
+    }
+        
+    public int solution(int n, int[][] edge) {
+        for (int i=0;i<=n;i++){
+            list.add(new ArrayList<Integer>());
+        }
+        for (int i=0;i<edge.length;i++){
+            list.get(edge[i][0]).add(edge[i][1]);
+            list.get(edge[i][1]).add(edge[i][0]);
+        }
+        bfs(n);
         int maxDist = 0;
-        int cnt=1;
+        int cnt = 0;
         for (int i=1;i<=n;i++){
-            if (maxDist < d[i]){
-                cnt = 0;
-                maxDist = d[i];
-            }
-            if (maxDist == d[i]){
+            if (maxDist < visited[i]){
+                maxDist = visited[i];
+                cnt = 1;
+            } 
+            else if (maxDist == visited[i]){
                 cnt++;
             }
         }
+        
         return cnt;
     }
 }
