91ad2b2fa96cf289ba961d08ea6f3dc2485e88c1
Mint <78211281+Starlight258@users.noreply.github.com>
2024-04-13 11:49:05 +0900

    [level 3] Title: 단어 변환, Time: 0.04 ms, Memory: 74.7 MB -BaekjoonHub

---
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -0,0 +1,35 @@
+class Solution {
+    boolean[] visited = new boolean[54];
+    boolean checkOne(String a, String b){
+        int cnt = 0;
+        for (int i=0;i<a.length();i++){
+            if (a.charAt(i) != b.charAt(i)) cnt++;
+            if (cnt>1) return false;
+        }
+        return true;
+    }
+    int dfs(int changeCnt, int now, String target, String[] words){
+        visited[now] = true;
+        int returnValue = Integer.MAX_VALUE;
+        if (target.equals(words[now])) return changeCnt;
+        if (changeCnt>= words.length) return 0;
+        for (int i=0;i<words.length;i++){
+            if (!visited[i] && checkOne(words[i], words[now])){
+                visited[i] = true;
+                int value = dfs(changeCnt+1, i, target, words);
+                returnValue = Math.min(returnValue, value);
+                visited[i] = false;
+            }
+        }
+        return returnValue;
+    }
+    public int solution(String begin, String target, String[] words) {
+        int answer = Integer.MAX_VALUE;
+        for (int i=0;i<words.length;i++){
+            if (checkOne(begin, words[i])){
+                answer = Math.min(answer, dfs(1, i, target, words));
+            }
+        }
+        return answer;
+    }
+}
