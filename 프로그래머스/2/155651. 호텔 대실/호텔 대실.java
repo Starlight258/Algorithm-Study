c3d0ecc677d8dc907a59698a5dfa3911ea98c27a
Mint <78211281+Starlight258@users.noreply.github.com>
2024-04-17 00:06:46 +0900

    [level 2] Title: 호텔 대실, Time: 5.53 ms, Memory: 85.4 MB -BaekjoonHub

--- 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
+++ 프로그래머스/2/155651. 호텔 대실/호텔 대실.java
@@ -18,7 +18,6 @@ class Solution {
         time += Integer.parseInt(splitTime[1]);
         return time;
     }
-
     public int solution(String[][] book_time) {
         ArrayList<Reservation> list = new ArrayList<Reservation>();
         for (String[] bt:book_time){
@@ -27,22 +26,14 @@ class Solution {
             list.add(new Reservation(startTime, endTime));
         }
         Collections.sort(list);
+        
         PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
-        int cnt = 0;
         for (Reservation r:list){
-            if (pq.isEmpty()){
-                pq.add(r.end + 10);
-                cnt++;
-            } 
-            else if (!pq.isEmpty() && pq.peek() > r.start){
-                pq.add(r.end+10);
-                cnt++;
-            }
-            else {
+            if (!pq.isEmpty() && pq.peek() <= r.start){
                 pq.poll();
-                pq.add(r.end+10);
             }
+            pq.add(r.end+10);
         }
-        return cnt;
+        return pq.size();
     }
 }
