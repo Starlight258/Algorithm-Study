d7bcd1f980b3611aded571a9c411980d1c7057d6
Mint <78211281+Starlight258@users.noreply.github.com>
2024-04-18 15:33:11 +0900

    [level 2] Title: 광물 캐기, Time: 0.49 ms, Memory: 65.5 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
@@ -2,31 +2,35 @@ import java.util.*;
 class Solution {
     public int solution(int[] picks, String[] minerals) {
         int answer = 0;
-        HashMap<String, Integer> hm = new HashMap<>();
-        hm.put("diamond", 0);
-        hm.put("iron", 1);
-        hm.put("stone", 2);
          int[][] fatigue = {
             {1,1,1},
             {5,1,1},
             {25,5,1}
         };
         //1. 광물 5개씩 자르기
-        int[][] mineralCnt = new int[minerals.length/5+1][3];
+        int[][] mineralCnt = new int[(minerals.length+4)/5][3];
         int available = picks[0]*5+picks[1]*5+picks[2]*5;
-        for (int i=0;i<minerals.length;i++){
-            if (i>=available) break;
-            mineralCnt[i/5][hm.get(minerals[i])]++;
-        }
-        Arrays.sort(mineralCnt, new Comparator<int[]>(){
-            public int compare(int[] a, int[] b){
-                if (a[0] == b[0] && a[1] == b[1]){
-                    return b[2] - a[2]; 
-                } else if (a[0] == b[0]){
-                    return b[1] - a[1];
-                }
-                return b[0] - a[0];
+        
+        for (int i=0;i<Math.min(available, minerals.length);i++){
+            switch (minerals[i]){
+                case "diamond":
+                    mineralCnt[i/5][0]++;
+                    break;
+                case "iron":
+                    mineralCnt[i/5][1]++;
+                    break;
+                case "stone":
+                    mineralCnt[i/5][2]++;
+                    break;
             }
+        }
+        Arrays.sort(mineralCnt, (a, b) -> {
+            if (a[0] == b[0] && a[1] == b[1]){
+                return b[2] - a[2]; 
+            } else if (a[0] == b[0]){
+                return b[1] - a[1];
+            }
+            return b[0] - a[0];
         });
         // 곡괭이 고르기
         int pickPos = 0;
@@ -38,12 +42,13 @@ class Solution {
         }
         for (int[] minCnt:mineralCnt){
             for (int i=0;i<3;i++){
-                answer += minCnt[i]*fatigue[pickPos][i];                
-            }
-            picks[pickPos]--;
-            while (picks[pickPos]<=0){
-                pickPos++;
-                if (pickPos>=picks.length) return answer;
+                if (picks[i]>0){
+                    answer += minCnt[0]*fatigue[i][0];
+                    answer += minCnt[1]*fatigue[i][1];
+                    answer += minCnt[2]*fatigue[i][2];
+                    picks[i]--;
+                    break;
+                }
             }
         }
      
