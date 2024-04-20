26c28bbaeb6386d04e04e0ca8a2f0bd587b03f66
Mint <78211281+Starlight258@users.noreply.github.com>
2024-04-20 13:55:46 +0900

    [level 2] Title: 두 원 사이의 정수 쌍, Time: 9.50 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.java
+++ 프로그래머스/2/181187. 두 원 사이의 정수 쌍/두 원 사이의 정수 쌍.java
@@ -1,23 +1,15 @@
 import java.util.*;
 class Solution {
-    long onCircle = 0;
-    long calculateNum(int r){
-        long result = 0;
-        for (int x=0;x<r;x++){
-            double y = Math.sqrt(Math.pow(r, 2)- Math.pow(x, 2));
-            if (y - (int) y == 0.0){
-                onCircle++; 
-            }  
-            result += (int) y;
-        }
-        return result;
-    } 
     public long solution(int r1, int r2) {
         long answer = 0;
-        answer += calculateNum(r2);
-        onCircle = 0;
-        answer -= calculateNum(r1);
-        answer += onCircle;
+        for (int x=0;x<r2;x++){
+            double y1 = Math.sqrt((long)r2 * r2 - (long)x * x);
+            double y2 = r1 > x ? Math.sqrt((long)r1 * r1 - (long) x * x) : 0;
+            answer += (int) y1 - (int) y2;
+            if (r1 > x && y2 - (int) y2 == 0.0){
+                answer++; 
+            }  
+        }
         return answer * 4;
     }
 }
