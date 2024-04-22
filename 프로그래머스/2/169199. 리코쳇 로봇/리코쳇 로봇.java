c1d34fc496985ef35bc5085318912fa0f294273c
Mint <78211281+Starlight258@users.noreply.github.com>
2024-04-22 23:53:59 +0900

    [level 2] Title: 리코쳇 로봇, Time: 0.70 ms, Memory: 72.2 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
@@ -1,72 +1,76 @@
 import java.util.*;
-
 class Solution {
-    static int n, m, sx, sy, ex, ey;
-    static char[][] map;
-    static boolean[][] visit;
-    static int[] dx = {1, -1, 0, 0};
-    static int[] dy = {0, 0, 1, -1};
-
-    public static int bfs(){
-        Queue<int[]> q = new LinkedList<int[]>();
-        visit[sx][sy] = true;
-        q.add(new int[]{sx, sy, 0});
-
-        while(!q.isEmpty()){
-            int[] point = q.poll();
-
-            int x = point[0];
-            int y = point[1];
-            int dist = point[2];
-
-            if(x==ex && y==ey) return dist;
-
-            for(int i=0;i<4;i++){
-                int nextX = x;
-                int nextY = y;
-
-                for(int k=1;k<=Math.max(n, m);k++){
-                    nextX+=dx[i];
-                    nextY+=dy[i];
-
-                    if(nextX<0 || nextX>=n || nextY<0 || nextY>=m || map[nextX][nextY] =='D') {
-                        nextX-=dx[i];
-                        nextY-=dy[i];
+    class Node {
+        int y;
+        int x;
+        Node(int y, int x){
+            this.y = y;
+            this.x = x;
+        }
+    }
+    int dy[] = {-1, 0, 1, 0};
+    int dx[] = {0, 1, 0, -1};
+    int[][] mp;
+    int[][] visited;
+    void bfs(int y, int x, int goalY, int goalX){
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(new Node(y, x));
+        visited[y][x] = 1;
+        while (!queue.isEmpty()){
+            Node here = queue.poll();
+            y = here.y;
+            x = here.x;
+            if (y==goalY && x==goalX) break;
+            int fy = y;
+            int fx = x;
+            for (int i=0;i<4;i++){
+                int ny = y;
+                int nx = x;
+                while (true){
+                    int nextY = ny + dy[i];
+                    int nextX = nx + dx[i];
+                    if (nextY<0||nextX<0||nextY>=mp.length||nextX>=mp[0].length){
                         break;
-                    }
+                    } 
+                    if (mp[nextY][nextX]==1){
+                        break;
+                    } 
+                    ny = nextY;
+                    nx = nextX;
                 }
-
-                if(visit[nextX][nextY]) continue;
-
-                visit[nextX][nextY] = true;
-                q.add(new int[]{nextX, nextY, dist+1});
+                if (visited[ny][nx]==0){
+                    queue.offer(new Node(ny, nx));
+                    visited[ny][nx] = visited[y][x]+1;
+                }
             }
         }
-
-        return -1;
     }
-
     public int solution(String[] board) {
-        n = board.length;
-        m = board[0].length();
-
-        map = new char[n][m];
-        visit = new boolean[n][m];
-
-        for(int i=0;i<n;i++) {
-            map[i] = board[i].toCharArray();
-            for(int j=0;j<m;j++) {
-                if(map[i][j] == 'R'){
-                    sx = i;
-                    sy = j;
+        int answer = 0;
+        mp = new int[board.length][board[0].length()];
+        visited = new int[board.length][board[0].length()];
+        // 1. 입력받기
+        int startX = 0;
+        int startY = 0;
+        int goalY = 0;
+        int goalX = 0;
+        for (int i=0;i<board.length;i++){
+            for (int j=0;j<board[i].length();j++){
+                if (board[i].charAt(j) == 'D'){
+                    mp[i][j] = 1;
                 }
-                else if(map[i][j] == 'G'){
-                    ex = i;
-                    ey = j;
+                if (board[i].charAt(j) == 'G'){
+                    goalY = i;
+                    goalX = j;
                 }
-            }    
+                if (board[i].charAt(j) == 'R'){
+                    startY = i;
+                    startX = j;
+                }
+            }
         }
-
-        return bfs();
+        //2. bfs 수행
+        bfs(startY, startX, goalY, goalX);
+        return visited[goalY][goalX]==0 ? -1 : visited[goalY][goalX]-1;
     }
 }
