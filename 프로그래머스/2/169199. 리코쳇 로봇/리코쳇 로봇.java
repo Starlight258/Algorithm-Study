f50cc98085417d0dbc7726a209941ac33791d64a
Mint <78211281+Starlight258@users.noreply.github.com>
2024-04-23 11:12:48 +0900

    [level 2] Title: 리코쳇 로봇, Time: 0.80 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
@@ -8,10 +8,11 @@ class Solution {
             this.x = x;
         }
     }
+    int n, m, sx, sy, gx, gy;
+    char[][] mp;
+    int[][] visited;
     int dy[] = {-1, 0, 1, 0};
     int dx[] = {0, 1, 0, -1};
-    int[][] mp;
-    int[][] visited;
     void bfs(int y, int x, int goalY, int goalX){
         Queue<Node> queue = new LinkedList<>();
         queue.add(new Node(y, x));
@@ -20,14 +21,14 @@ class Solution {
             Node here = queue.poll();
             y = here.y;
             x = here.x;
-            if (y==goalY && x==goalX) break;
+            if (y==gy && x==gx) break;
             for (int i=0;i<4;i++){
                 int nextY = y;
                 int nextX = x;
-                while (true){
+                for (int j=0;j<Math.max(n, m);j++){
                     nextY += dy[i];
                     nextX += dx[i];
-                    if (nextY<0||nextX<0||nextY>=mp.length||nextX>=mp[0].length||mp[nextY][nextX]==1){
+                    if (nextY<0||nextX<0||nextY>=mp.length||nextX>=mp[0].length||mp[nextY][nextX]=='D'){
                         nextY -= dy[i];
                         nextX -= dx[i];
                         break;
@@ -42,30 +43,26 @@ class Solution {
     }
     public int solution(String[] board) {
         int answer = 0;
-        mp = new int[board.length][board[0].length()];
-        visited = new int[board.length][board[0].length()];
+        n = board.length;
+        m = board[0].length();
+        mp = new char[n][m];
+        visited = new int[n][m];
         // 1. 입력받기
-        int startX = 0;
-        int startY = 0;
-        int goalY = 0;
-        int goalX = 0;
         for (int i=0;i<board.length;i++){
+            mp[i] = board[i].toCharArray();
             for (int j=0;j<board[i].length();j++){
-                if (board[i].charAt(j) == 'D'){
-                    mp[i][j] = 1;
-                }
                 if (board[i].charAt(j) == 'G'){
-                    goalY = i;
-                    goalX = j;
+                    gy = i;
+                    gx = j;
                 }
                 if (board[i].charAt(j) == 'R'){
-                    startY = i;
-                    startX = j;
+                    sy = i;
+                    sx = j;
                 }
             }
         }
         //2. bfs 수행
-        bfs(startY, startX, goalY, goalX);
-        return visited[goalY][goalX]==0 ? -1 : visited[goalY][goalX]-1;
+        bfs(sy, sx, gy, gx);
+        return visited[gy][gx]==0 ? -1 : visited[gy][gx]-1;
     }
 }
