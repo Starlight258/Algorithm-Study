67e945f16174c3015681d8a9d66951b47b0d203a
Mint <78211281+Starlight258@users.noreply.github.com>
2024-04-29 00:28:41 +0900

    [level 2] Title: 숫자 변환하기, Time: 24.39 ms, Memory: 83.7 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
@@ -1,49 +1,22 @@
-// class Solution {
-//     public int solution(int x, int y, int n) {
-//         int answer = 0;
-//         int dp[] = new int[y+1];
-//         dp[x] = 0;
-        
-//         for (int i=x;i<=y;i++){
-//             if (dp[i/2]>0 && i%2==0) dp[i] = Math.min(dp[i], dp[i/2] + 1);
-//             if (dp[i/3]>0 && i%3==0) dp[i] = Math.min(dp[i], dp[i/3]+1);
-//             if (i-n>=0 && i>=x && dp[i-n]>0) dp[i] = Math.min(dp[i], dp[i-n]+1);
-//         }
-                                         
-//         return dp[y] == Integer.MAX_VALUE ? -1 : dp[y];
-//     }
-// }
-import java.util.ArrayDeque;
-import java.util.Queue;
-
+import java.util.*;
 class Solution {
-
-    private static final int MAX_VALUE = 1_000_000;
-    private final int[] visitCount = new int[MAX_VALUE + 1];
-    private final Queue<Integer> queue = new ArrayDeque<>();
-
     public int solution(int x, int y, int n) {
-        return bfs(x, y, n);
-    }
-
-    private int bfs(int source, int target, int n) {
-        queue.add(source);
-        while (!queue.isEmpty()) {
-            int value = queue.poll();
-            if (value == target) {
-                return visitCount[value];
-            }
-            addQueue(target, value, value + n);
-            addQueue(target, value, value * 2);
-            addQueue(target, value, value * 3);
+        int answer = 0;
+        int dp[] = new int[1000001];
+        
+        Arrays.fill(dp, Integer.MAX_VALUE);
+        
+        dp[x] = 0;
+        if (x*2 < 1000001) dp[x*2] = 1;
+        if (x*3 < 1000001) dp[x*3] = 1;
+        if(x+n < 1000001) dp[x+n] = 1;
+        
+        for (int i=x;i<1000001;i++){
+            if (i*2<1000001 && dp[i] !=Integer.MAX_VALUE) dp[i*2] = Math.min(dp[i*2], dp[i] + 1);
+            if (i*3<1000001 && dp[i] != Integer.MAX_VALUE) dp[i*3] = Math.min(dp[i*3], dp[i]+1);
+            if (i+n<1000001 && dp[i] != Integer.MAX_VALUE) dp[i+n] = Math.min(dp[i+n], dp[i]+1);
         }
-        return -1;
-    }
-
-    private void addQueue(int target, int value, int nextValue) {
-        if (nextValue <= target && visitCount[nextValue] == 0) {
-            queue.add(nextValue);
-            visitCount[nextValue] = visitCount[value] + 1;
-        }
+                                         
+        return dp[y] == Integer.MAX_VALUE ? -1 : dp[y];
     }
 }
