ce7bf5b15e1fea45975c429e739b3fe5528057db
Mint <78211281+Starlight258@users.noreply.github.com>
2024-04-29 01:08:47 +0900

    [level 2] Title: 숫자 변환하기, Time: 16.08 ms, Memory: 98 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
@@ -1,22 +1,25 @@
 import java.util.*;
 class Solution {
     public int solution(int x, int y, int n) {
-        int answer = 0;
-        int dp[] = new int[1000001];
+        final int MAX_VALUE = 1000001;
         
+        int dp[] = new int[MAX_VALUE];        
         Arrays.fill(dp, Integer.MAX_VALUE);
-        
         dp[x] = 0;
-        if (x*2 < 1000001) dp[x*2] = 1;
-        if (x*3 < 1000001) dp[x*3] = 1;
-        if(x+n < 1000001) dp[x+n] = 1;
-        
-        for (int i=x;i<1000001;i++){
-            if (i*2<1000001 && dp[i] !=Integer.MAX_VALUE) dp[i*2] = Math.min(dp[i*2], dp[i] + 1);
-            if (i*3<1000001 && dp[i] != Integer.MAX_VALUE) dp[i*3] = Math.min(dp[i*3], dp[i]+1);
-            if (i+n<1000001 && dp[i] != Integer.MAX_VALUE) dp[i+n] = Math.min(dp[i+n], dp[i]+1);
+                                
+        for (int i=x;i<=y;i++){
+            if (dp[i] == Integer.MAX_VALUE) continue;
+            if (i*2<MAX_VALUE){
+                dp[i*2] = Math.min(dp[i*2], dp[i] + 1);
+            } 
+            if (i*3<MAX_VALUE){
+                dp[i*3] = Math.min(dp[i*3], dp[i] + 1); 
+            } 
+            if (i+n<MAX_VALUE){
+                dp[i+n] = Math.min(dp[i+n], dp[i] + 1);  
+            } 
         }
                                          
-        return dp[y] == Integer.MAX_VALUE ? -1 : dp[y];
+        return dp[y]==Integer.MAX_VALUE ? -1 : dp[y];
     }
 }
