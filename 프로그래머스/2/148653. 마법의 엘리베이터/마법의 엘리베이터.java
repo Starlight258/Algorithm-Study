dac7a9bae02984ee87a7c46558cc049ffa4858a4
Mint <78211281+Starlight258@users.noreply.github.com>
2024-04-30 01:01:09 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.02 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
@@ -1,23 +1,20 @@
 class Solution {
     public int solution(int storey) {
         int answer = 0;
-
-        while(storey > 0){
-            int n = storey % 10;
+        int currentDigit = 0;
+        
+        while (storey > 0){
+            currentDigit = storey%10;
             storey /= 10;
-            // 5일 경우
-            if(n == 5){
-                answer += 5;
-                if((storey%10) >= 5){
-                    storey++;
-                }
-            }
-            // 5보다 클 경우
-            else if(n > 5){
-                answer += (10-n);
+            
+            if (currentDigit<5){
+                answer += currentDigit;
+            } else if (currentDigit>5){
+                answer += (10-currentDigit);
                 storey++;
-            } else { // 5보다 작을 경우
-                answer += n;
+            } else {
+                answer += 5;
+                if ((storey%10) >= 5) storey++;                
             }
         }
         return answer;
