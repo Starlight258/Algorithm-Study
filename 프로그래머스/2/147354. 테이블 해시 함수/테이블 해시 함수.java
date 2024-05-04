0a368d4913e0b900141d99ae29bf65a107d00bff
Mint <78211281+Starlight258@users.noreply.github.com>
2024-05-04 23:47:24 +0900

    [level 2] Title: 테이블 해시 함수, Time: 1.20 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.java
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.java
@@ -3,32 +3,19 @@ class Solution {
     public int solution(int[][] data, int col, int row_begin, int row_end) {
         int answer = 0;
         
-        col-=1;
-        row_begin--;
-        row_end--;
-        
-        final int colFinal = col;
         Arrays.sort(data, (d1, d2) -> {
-            if (d1[colFinal] == d2[colFinal]){
+            if (d1[col-1] == d2[col-1]){
                 return d2[0] - d1[0];
             }
-            return d1[colFinal] - d2[colFinal];
+            return d1[col-1] - d2[col-1];
         });
-        
-        int[] result = new int[row_end-row_begin+1];
-        
-        for (int i=row_begin;i<=row_end;i++){
+                
+        for (int i=row_begin-1;i<row_end;i++){
+            int sum = 0;
             for (int j=0;j<data[i].length;j++){
-                result[i-row_begin] += data[i][j] % (i+1);
+                sum += data[i][j] % (i+1);
             }
-        }
-        
-        answer = result[0];
-        
-        if (result.length==0) return answer;
-        
-        for (int i=1;i<result.length;i++){
-            answer ^= result[i];
+            answer ^= sum;
         }
         
         return answer;
