b4042f40f06db59ffb17ea9f2635cd06ea2a43ce
Mint <78211281+Starlight258@users.noreply.github.com>
2024-05-07 11:29:24 +0900

    [level 2] Title: 점 찍기, Time: 0.09 ms, Memory: 73.4 MB -BaekjoonHub

--- 프로그래머스/2/140107. 점 찍기/점 찍기.java
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.java
@@ -1,13 +1,6 @@
 class Solution {
     public long solution(int k, int d) {
         long answer = 0;
-        long dPowx = (long) Math.pow(d, 2) / (long) Math.pow(k, 2);
-        for (int x=0;x<=d;x++){
-            long xx = (long) Math.pow(x, 2);
-            if (dPowx - xx < 0) continue;
-            long maxY = (long) Math.sqrt(dPowx - xx);
-            answer += maxY + 1;
-        }
         return answer;
     }
 }
