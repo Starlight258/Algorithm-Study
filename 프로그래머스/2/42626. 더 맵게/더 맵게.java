57af8fab4c5854a993410245f4fc84a00a6316ac
Mint <78211281+Starlight258@users.noreply.github.com>
2024-05-08 11:07:15 +0900

    [level 2] Title: 더 맵게, Time: 1708.59 ms, Memory: 124 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -6,16 +6,14 @@ class Solution {
         for (int s:scoville){
             pq.add(s);
         }
-        while (pq.size()>=2){
+        while (!pq.isEmpty()){
             int e1 = pq.poll();
-            if (e1 >= K) break;
+            if (e1 >= K) return answer;
+            if (pq.size()==0) return -1; 
             int e2 = pq.poll();
             int sc = e1 + e2 * 2;
             pq.offer(sc);
             answer++;
-        }
-        if (!pq.isEmpty()){
-            if (pq.poll() >= K) return answer; 
         }
         return -1;
     }
