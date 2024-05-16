e3f51980b5c2e3aa84b970fd3c49df1cda2a0969
Mint <78211281+Starlight258@users.noreply.github.com>
2024-05-16 16:27:40 +0900

    [level 2] Title: 조이스틱, Time: 0.04 ms, Memory: 68.3 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/조이스틱.java
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -1,79 +1,25 @@
-// import java.util.*;
-// class Solution {
-//     public int solution(String name) {
-//         int answer = 0;
-//         int pos= 0;
-//         int n = name.length();
-//         int cnt = 0;
-//         PriorityQueue<Integer> spq = new PriorityQueue<>();
-//         PriorityQueue<Integer> gpq = new PriorityQueue<>(Collections.reverseOrder());
-        
-//         // 오른쪽 이동
-//         // 바꾸어야 할 알파벳 위치 저장
-//         for (int i=0;i<n;i++){
-//             if (name.charAt(i) != 'A'){
-//               spq.offer(i);  
-//               gpq.offer(i);  
-//                 cnt++;
-//             } 
-//         }
-//         while (cnt>0){
-//             // position에서 가장 가까운 것부터 이동
-//             int idx = 0;
-//             if (spq.isEmpty()){
-//                 idx = gpq.poll();
-//                 answer += Math.min(n - idx + pos, idx-pos);
-//                 answer += Math.min('Z'-name.charAt(idx) +1, 26 - ('Z'-name.charAt(idx)+1));
-//                 cnt--;
-//                 pos = idx;
-//                 continue;
-//             }
-//             if (gpq.isEmpty()){
-//                 idx = spq.poll();
-//                 answer += Math.min(n - idx + pos, idx - pos);
-//                 answer += Math.min('Z'-name.charAt(idx) +1, 26 - ('Z'-name.charAt(idx)+1));
-//                 cnt--;
-//                 pos = idx;
-//                 continue;
-//             }
-//             int s = Math.min(n - spq.peek() + pos ,spq.peek() - pos);
-//             int g = Math.min(n - gpq.peek() + pos, gpq.peek() - pos);
-//             if (s<=g) idx = spq.poll();
-//             else idx = gpq.poll();
-//             answer += Math.min(s, g);
-//             answer += Math.min('Z'-name.charAt(idx) +1, 26 - ('Z'-name.charAt(idx)+1));
-//             cnt--;
-//             pos = idx;
-//         }
-
-//         return answer;
-//     }
-// }
-
-class Solution {
+public class Solution {
     public int solution(String name) {
-        int answer = 0;
-        int n = name.length();
-        int minMove = n - 1;
+        int alpha = 0;
+        int minMove = name.length() - 1;
 
-        for (int i = 0; i < n; i++) {
-            // 위, 아래로 이동하는 경우
-            char c = name.charAt(i);
-            int diff = c - 'A';
-            answer += Math.min(diff, 26 - diff);
+        for (int cursor = 0; cursor < name.length(); cursor++) {
+            int nextCursor = cursor + 1;
 
-            // 왼쪽, 오른쪽으로 이동하는 경우
-            int nextIdx = i + 1;
-            while (nextIdx < n && name.charAt(nextIdx) == 'A') {
-                nextIdx++;
+            while (nextCursor < name.length() && name.charAt(nextCursor) == 'A') {
+                nextCursor++;
             }
 
-            int moveLeft = i;
-            int moveRight = n - nextIdx;
-            minMove = Math.min(minMove, moveLeft + moveRight + Math.min(moveLeft, moveRight));
+            int toBack = name.length() - nextCursor;
+
+            // 앞으로만 직진할 때, 현재 위치에서 뒤로 이동(nextCursor까지), 먼저 뒤로 갔다가(nextCursor까지) 현재 위치로 이동
+            minMove = Math.min(minMove, cursor * 2 + toBack);
+            minMove = Math.min(minMove, toBack * 2 + cursor);
+
+            int code = name.charAt(cursor) - 65;
+            alpha += Math.min(code, 26 - code);
         }
 
-        answer += minMove;
-        return answer;
+        return alpha + minMove;
     }
 }
