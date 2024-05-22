f6afd0dd5f47b898f99c90bd5d7bf64a65dd7b4c
Mint <78211281+Starlight258@users.noreply.github.com>
2024-05-22 10:21:29 +0900

    [level 2] Title: 영어 끝말잇기, Time: 0.21 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
+++ 프로그래머스/2/12981. 영어 끝말잇기/영어 끝말잇기.java
@@ -1,23 +1,10 @@
-import java.util.*;
 class Solution {
     public int[] solution(int n, String[] words) {
-        int[] answer = new int[2];
+        int[] answer = {};
 
-        List<String> list = new ArrayList<>();
-        list.add(words[0]);
-        char lastWord = words[0].charAt(words[0].length()-1);
-        for (int i=1;i<words.length;i++){
-            String word = words[i];
-            
-            if (lastWord != word.charAt(0) || word.length()==1 || list.contains(word)){
-                answer[0] = i % n + 1;
-                answer[1] = i / n + 1;
-                return answer;
-            }
-            list.add(word);
-            lastWord = word.charAt(word.length()-1);
-        }
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다. 
+        System.out.println("Hello Java");
 
-        return new int[]{0,0};
+        return answer;
     }
 }
