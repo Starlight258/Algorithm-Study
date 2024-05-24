4d7ce5cbd0eb1dbd223d936d2d85ee17e1f47b9e
Mint <78211281+Starlight258@users.noreply.github.com>
2024-05-24 14:29:57 +0900

    [Gold V] Title: 보물섬, Time: 544 ms, Memory: 295384 KB -BaekjoonHub

--- 백준/Gold/2589. 보물섬/보물섬.java
+++ 백준/Gold/2589. 보물섬/보물섬.java
@@ -1,24 +1,21 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.LinkedList;
-import java.util.List;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
     static int n, m;
-    static int[][] map;
-    static List<Integer[]> land = new ArrayList<>();
+    static char[][] map;
     static int max = 0;
 
     static int[] dy = {-1, 0, 1, 0};
     static int[] dx = {0, 1, 0, -1};
-    static int[][] visited;
 
     static void bfs(int y, int x) {
+        int[][] visited = new int[n][m];
         Queue<Integer[]> queue = new LinkedList<>();
         queue.add(new Integer[]{y, x});
         visited[y][x] = 1;
@@ -33,13 +30,7 @@ public class Main {
                 int ny = y + dy[i];
                 int nx = x + dx[i];
 
-                if (ny < 0 || nx < 0 || ny >= n || nx >= m) {
-                    continue;
-                }
-                if (visited[ny][nx] != 0) {
-                    continue;
-                }
-                if (map[ny][nx] == 0) {
+                if (ny < 0 || nx < 0 || ny >= n || nx >= m || visited[ny][nx] != 0 || map[ny][nx] == 'W') {
                     continue;
                 }
 
@@ -54,28 +45,18 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        map = new int[n][m]; // W: 0, L: 1
+        map = new char[n][m]; // W: 0, L: 1
 
-        String line;
         for (int i = 0; i < n; i++) {
-            line = br.readLine();
-            for (int j = 0; j < m; j++) {
-                if (line.charAt(j) == 'L') {
-                    map[i][j] = 1;
-                    land.add(new Integer[]{i, j});
-                } else {
-                    map[i][j] = 0;
-                }
-            }
+            map[i] = br.readLine().toCharArray();
         }
 
-        visited = new int[n][m];
-        max = 0;
-
-        for (Integer[] integers : land) {
-            Arrays.stream(visited)
-                    .forEach(arr -> Arrays.fill(arr, 0));
-            bfs(integers[0], integers[1]);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (map[i][j] == 'L') {
+                    bfs(i, j);
+                }
+            }
         }
 
         System.out.println(max - 1);
