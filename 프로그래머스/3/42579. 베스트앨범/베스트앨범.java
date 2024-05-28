82aa381b1d60e84677e4a1f9e397c0f0a408f72d
Mint <78211281+Starlight258@users.noreply.github.com>
2024-05-28 10:21:11 +0900

    [level 3] Title: 베스트앨범, Time: 15.30 ms, Memory: 71.2 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -2,29 +2,28 @@ import java.util.*;
 class Solution {
     public int[] solution(String[] genres, int[] plays) {
         List<Integer> answer = new ArrayList<>();
-        
         // 1. 저장하기
-        Map<String, Integer> mp = new HashMap<>(); // 장르별 저장횟수
-        Map<String, List<Integer>> imp = new HashMap<>(); // 장르별 인덱스
-        for (int i=0;i<genres.length;i++){
-            mp.put(genres[i], mp.getOrDefault(genres[i], 0) + plays[i]);
-            imp.computeIfAbsent(genres[i], k->new ArrayList<>()).add(i);
+        // 장르별 재생횟수 저장
+        Map<String, Integer> mp1 = new HashMap<>();
+        // 장르별 노래 인덱스 저장
+        Map<String, List<Integer>> mp2 = new HashMap<>();
+        for (int i=0;i<plays.length;i++){
+            mp1.put(genres[i], mp1.getOrDefault(genres[i], 0) + plays[i]);
+            mp2.computeIfAbsent(genres[i], k -> new ArrayList<>()).add(i);
         }
         
-        // 재생 횟수 많은 장르부터 정렬
-        List<String> sortedGenres = new LinkedList<>(mp.keySet());
-        Collections.sort(sortedGenres, (l1, l2) -> mp.get(l2) - mp.get(l1));
+        //2. 장르별 총 재생횟수 내림차순 정렬하기
+        List<String> sortedGenres = new ArrayList<>(mp1.keySet());
+        sortedGenres.sort((g1, g2) -> mp1.get(g2) - mp1.get(g1));
         
-        // 장르별 재생횟수 + 고유번호 낮은 순으로 정렬
+        // 해당 장르의 재생횟수별로 노래 정렬하기
         for (String genre:sortedGenres){
-            List<Integer> indexes = imp.get(genre);
-            indexes.sort((a,b) -> plays[b] - plays[a]);
+            List<Integer> indexes = mp2.get(genre);
+            indexes.sort((i1, i2) -> plays[i2] -plays[i1]);
             
             answer.add(indexes.get(0));
             if (indexes.size()>1) answer.add(indexes.get(1));
         }
-        
-        
         return answer.stream().mapToInt(x->x).toArray();
     }
 }
