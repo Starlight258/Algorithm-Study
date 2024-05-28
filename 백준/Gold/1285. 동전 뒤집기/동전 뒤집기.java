ffe17c4a816a5566ef1b16d2781019ebfb4a65a4
Mint <78211281+Starlight258@users.noreply.github.com>
2024-05-29 00:52:40 +0900

    [Gold I] Title: 동전 뒤집기, Time: 584 ms, Memory: 15764 KB -BaekjoonHub

---
+++ 백준/Gold/1285. 동전 뒤집기/동전 뒤집기.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.StringReader;
+
+public class Main {
+
+    static final int INF = 987654321;
+    static int n, ret = INF, sum;
+    static int[] a = new int[21];
+
+    static void go(int depth){
+        if (depth == n + 1) {
+            sum = 0;
+            // 각 열에 대해 T 개수 구하기
+            for (int i = 1; i <= (1 << n - 1); i *= 2) {
+                int cnt = 0;
+                for (int j = 1; j <= n; j++) {
+                    if ((a[j] & i) != 0) {
+                        cnt++;
+                    }
+                }
+                sum += Math.min(cnt, n - cnt);
+            }
+            ret = Math.min(sum, ret);
+            return;
+        }
+
+        a[depth] = ~a[depth];
+        go(depth + 1);
+        a[depth] = ~a[depth];
+        go(depth + 1);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        String line;
+        for (int i = 1; i <= n; i++) {
+            line = br.readLine();
+            int value = 1;
+            for (char c : line.toCharArray()) {
+                if (c == 'H') {
+                    a[i] |= value;
+                }
+                value *= 2;
+            }
+        }
+
+        // 완전 탐색 수행
+        go(1);
+
+        // 정답 출력
+        System.out.println(ret);
+    }
+}
