77ec695e16b154a94fea0566462ffe3d2b829046
Mint <78211281+Starlight258@users.noreply.github.com>
2024-05-30 10:20:14 +0900

    [level 2] Title: 방문 길이, Time: 1.30 ms, Memory: 76 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/README.md
+++ 프로그래머스/2/49994. 방문 길이/README.md
@@ -1,6 +1,6 @@
 # [level 2] 방문 길이 - 49994 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49994#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/49994) 
 
 ### 성능 요약
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 30일 10:19:28
+2024년 05월 30일 10:20:12
 
 ### 문제 설명
 
--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -1,53 +1,6 @@
 class Solution {
-    
-    boolean[][][] visited;
-    int answer;
-    
-    void check(int y, int x, int ny, int nx, int d){
-        if (!visited[y][x][d] || !visited[ny][nx][3-d]){
-            visited[y][x][d]=true;
-            visited[ny][nx][3-d] = true;
-            answer++;
-        }
-    }
-    
     public int solution(String dirs) {
-        answer = 0;
-        visited = new boolean[11][11][4]; // y, x, 방향(u:0,d:1,l:2,r:3)
-        int y =5, x = 5;
-        
-        for (String d:dirs.split("")){
-            int ny = y, nx = x;
-            switch(d){
-                case "U": 
-                    if (y+1<=10){
-                        ny++;
-                        check(y, x, ny, nx, 0);
-                    } 
-                    break;
-                case "D":
-                    if (y-1>=0){
-                        ny--;
-                        check(y, x, ny, nx, 3);
-                    } 
-                    break;
-                case "R":
-                    if (x+1<=10){
-                        nx++;
-                        check(y, x, ny, nx, 1);
-                    } 
-                    break;
-                case "L":
-                    if (x-1>=0){
-                        nx--;
-                        check(y, x, ny, nx, 2);
-                    } 
-                    break;
-            }
-            y = ny;
-            x = nx;
-        }
-        
+        int answer = 0;
         return answer;
     }
 }
