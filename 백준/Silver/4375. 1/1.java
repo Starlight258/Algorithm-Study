889c07f8d001fd3769ce9da4fce0e729eb1c924e
Mint <78211281+Starlight258@users.noreply.github.com>
2024-06-10 20:31:34 +0900

    [Silver III] Title: 1, Time: 120 ms, Memory: 13980 KB -BaekjoonHub

---
+++ 백준/Silver/4375. 1/1.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] arg) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input;
+        int num;
+        int remainder;
+        int count;
+        while ((input = br.readLine()) != null) {
+            num = Integer.parseInt(input);
+            remainder = 1;
+            count = 1;
+            while (remainder % num != 0) {
+                remainder = (remainder * 10 + 1) % num;
+                count++;
+            }
+            System.out.println(count);
+        }
+    }
+}
