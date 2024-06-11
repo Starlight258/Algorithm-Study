cf6f3141081ae6800b3e197186286f4283ef484b
Mint <78211281+Starlight258@users.noreply.github.com>
2024-06-11 14:37:06 +0900

    [Gold II] Title: 새로운 게임 2, Time: 228 ms, Memory: 17088 KB -BaekjoonHub

--- 백준/Gold/17837. 새로운 게임 2/새로운 게임 2.java
+++ 백준/Gold/17837. 새로운 게임 2/새로운 게임 2.java
@@ -4,122 +4,107 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
+import java.util.StringTokenizer;
 
-public class Main{
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+        int[][] map = new int[n + 1][n + 1];
+        List<Integer>[][] list = new List[n + 1][n + 1];
 
-    public static void main(String[]args)throws IOException{
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
-        String[]tmp = reader.readLine().split(" ");
-
-        int N = Integer.parseInt(tmp[0]);
-        int K = Integer.parseInt(tmp[1]);
-        int[][]board = new int[N+1][N+1];
-        List<Integer>[][]list = new List[N+1][N+1];
-
-        // 1 ~ N 칸으로 시작
-        for(int i = 1; i<=N; i++) {
-            String[]mapString = reader.readLine().split(" ");
-
-            for(int j = 1; j<=N; j++) {
-                list[i][j]= new ArrayList<>();
-                board[i][j]= Integer.parseInt(mapString[j-1]);
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 1; j <= n; j++) {
+                list[i][j] = new ArrayList<>();
+                map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        // 말이 현재 있는 위치 (행, 열, 방향)
-        int[][]horse = new int[K+1][3]; // (i, j) 방향
+        int[][] horse = new int[k + 1][3];
+        for (int i = 1; i <= k; i++) {
+            st = new StringTokenizer(br.readLine());
+            horse[i][0] = Integer.parseInt(st.nextToken()); // y
+            horse[i][1] = Integer.parseInt(st.nextToken()); // x
+            horse[i][2] = Integer.parseInt(st.nextToken()); // dir
 
-        for(int i = 1; i<=K; i++) {
-            String[]t = reader.readLine().split(" ");
-            int r = Integer.parseInt(t[0]);
-            int c = Integer.parseInt(t[1]);
-
-            horse[i][0]= r;
-            horse[i][1]= c;
-            horse[i][2]= Integer.parseInt(t[2]);
-
-            list[r][c].add(i);
+            list[horse[i][0]][horse[i][1]].add(i); // 지도에 말 저장
         }
 
-
-        // 1 = 오른쪽, 2 = 왼쪽, 3 = 위, 4 = 아래
-        int[]dr ={0, 0, 0, -1, 1};
-        int[]dc ={0, 1, -1, 0, 0};
+        // 이동
+        int[] dy = {0, 0, 0, -1, 1}; // 0, 1(오른쪽),2(왼쪽),3(위쪽),4(아래쪽)
+        int[] dx = {0, 1, -1, 0, 0};
 
         int count = 0;
-        while(count < 1000) {
-            count++;
+        while (count++ < 1000) {
 
-            // K개의 말의 이동
-            for(int i = 1; i<=K; i++) {
+            // 말 움직이기
+            for (int i = 1; i <= k; i++) {
                 int r = horse[i][0];
                 int c = horse[i][1];
                 int dir = horse[i][2];
 
-                int nr = r + dr[dir];
-                int nc = c + dc[dir];
+                int nr = r + dy[dir];
+                int nc = c + dx[dir];
 
-                // 다음으로 이동할 수 있을 때
-                if(nr <= N && nr >= 1 && nc >= 1 && nc <= N) {
-                    if(board[nr][nc]== 0) {
+                if (nr <= n && nr >= 1 && nc >= 1 && nc <= n) {
+
+                    if (map[nr][nc] == 0) { // 흰색
+                        // 위에 말까지 함께 옮기기
                         int index = list[r][c].indexOf(i);
-
                         list[nr][nc].addAll(list[r][c].subList(index, list[r][c].size()));
-
                         list[r][c] = list[r][c].subList(0, index);
 
-                        horse[i][0]= nr;
-                        horse[i][1]= nc;
-
-                        // 이전의 말도 위치 갱신하기 
-                        for(Integer h : list[nr][nc]) {
-                            horse[h][0]= nr;
-                            horse[h][1]= nc;
+                        // 말 위치 업데이트
+                        horse[i][0] = nr;
+                        horse[i][1] = nc;
+                        for (Integer h : list[nr][nc]) {
+                            horse[h][0] = nr;
+                            horse[h][1] = nc;
                         }
-
-                    }else if(board[nr][nc]== 1) {
+                    } else if (map[nr][nc] == 1) { // 빨간색
+                        // 말 reverse해서 옮기기
                         int index = list[r][c].indexOf(i);
                         List<Integer> subList = list[r][c].subList(index, list[r][c].size());
                         Collections.reverse(subList);
 
                         list[nr][nc].addAll(subList);
                         list[r][c] = list[r][c].subList(0, index);
-
-                        for(Integer h : list[nr][nc]) {
-                            horse[h][0]= nr;
-                            horse[h][1]= nc;
+                        for (Integer h : list[nr][nc]) {
+                            horse[h][0] = nr;
+                            horse[h][1] = nc;
                         }
-
-                    } else if(board[nr][nc] == 2) {
+                    } else if (map[nr][nc] == 2) {
                         int new_dir = dir % 2 == 0 ? dir - 1 : dir + 1;
                         horse[i][2] = new_dir;
 
-                        if(r + dr[new_dir]< 1 || r + dr[new_dir]>= N+1 || c + dc[new_dir]< 1 || c + dc[new_dir]>= N+1) {
+                        if (r + dy[new_dir] < 1 || r + dy[new_dir] >= n + 1 || c + dx[new_dir] < 1
+                                || c + dx[new_dir] >= n + 1) {
                             continue;
                         }
 
-                        if(board[r + dr[new_dir]][c + dc[new_dir]]!= 2) {
+                        if (map[r + dy[new_dir]][c + dx[new_dir]] != 2) {
                             i--;
                         }
                     }
 
-                    if(list[nr][nc].size()> 3) {
+                    // 말이 4개 이상 쌓이는 경우 종료
+                    if (list[nr][nc].size() > 3) {
                         System.out.println(count);
                         return;
                     }
-                }else {
-                    int new_dir = dir % 2 == 0 ? dir - 1 : dir + 1;
-                    horse[i][2]= new_dir;
+                } else {
+                    int newDir = dir % 2 == 0 ? dir - 1 : dir + 1;
+                    horse[i][2] = newDir;
 
-                    if(board[r + dr[new_dir]][c + dc[new_dir]]!= 2) {
+                    if (map[r + dy[newDir]][c + dx[newDir]] != 2) {
                         i--;
                     }
                 }
-
             }
         }
         System.out.println(-1);
     }
-
 }
