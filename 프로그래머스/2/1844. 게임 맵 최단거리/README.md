45bf9946882e5a752d37713c1f789816b0e17305
Mint <78211281+Starlight258@users.noreply.github.com>
2024-06-12 20:52:30 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 5.31 ms, Memory: 52.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/README.md
+++ 프로그래머스/2/1844. 게임 맵 최단거리/README.md
@@ -1,10 +1,10 @@
 # [level 2] 게임 맵 최단거리 - 1844 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/1844) 
 
 ### 성능 요약
 
-메모리: 52.6 MB, 시간: 4.02 ms
+메모리: 52.3 MB, 시간: 5.31 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 22일 23:52:40
+2024년 06월 12일 20:52:27
 
 ### 문제 설명
 
--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,44 +1,29 @@
 import java.util.*;
-class Pair { 
-    int y;
-    int x;
-    
-    Pair (int y, int x){
-        this.y = y;
-        this.x = x;
-    }
-}
 class Solution {
-    int dy[] = {-1, 0, 1, 0};
-    int dx[] = {0, 1, 0, -1};
-    Queue<Pair> stk = new LinkedList<>();
-    int[][] visited = new int[104][104];
-    
+    int dy[]={-1,0,1,0};
+    int dx[]={0,1,0,-1};
     public int solution(int[][] maps) {
-        int n = maps.length; 
-        int m = maps[0].length; 
+        int answer = 0;
+        int[][] visited = new int[maps.length][maps[0].length];
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{0,0});
         visited[0][0] = 1;
-        stk.offer(new Pair(0, 0));
-        
-        while (!stk.isEmpty()){
-            Pair here = stk.poll();
-            int y = here.y;
-            int x = here.x;
-            if (y==n-1 && x==m-1){
-                return visited[n-1][m-1];
-            }
-            
+        while(!q.isEmpty()){
+            int[] cur = q.poll();
+            int y = cur[0];
+            int x = cur[1];
             for (int i=0;i<4;i++){
-                int ny = y + dy[i];
-                int nx = x + dx[i];
-                if (ny<0 || ny>=n || nx<0 || nx>=m) continue;
-                if (visited[ny][nx] == 0 && maps[ny][nx]==1){
-                    stk.offer(new Pair(ny, nx));
-                    visited[ny][nx] = visited[y][x] + 1;
-                } 
+                int ny = y+dy[i];
+                int nx = x+dx[i];
+                if(ny<0||nx<0||ny>=maps.length||nx>=maps[0].length) continue;
+                if (visited[ny][nx]==0 && maps[ny][nx]==1){
+                    visited[ny][nx] = visited[y][x]+1;
+                    q.offer(new int[]{ny, nx});
+                }
             }
+            answer = visited[maps.length-1][maps[0].length-1];
+            answer= answer==0?-1:answer;
         }
-        
-        return -1;
+        return answer;
     }
 }
