a2a097a09f9aee632176274303496e401b6b7436
Mint <78211281+Starlight258@users.noreply.github.com>
2024-06-12 22:28:08 +0900

    [level 1] Title: 신고 결과 받기, Time: 0.68 ms, Memory: 66.4 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -2,20 +2,18 @@ import java.util.*;
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
         Map<String, Integer> name = new HashMap<>();
-        Set<String> reportSet = new HashSet<>();
         Map<Integer, List<Integer>> reportIdx = new HashMap<>();
         for (int i=0;i<id_list.length;i++){
             name.put(id_list[i], i);
             reportIdx.computeIfAbsent(i, l->new ArrayList<>());
         }
         for (String rp:report){
-            reportSet.add(rp);
-        }
-        for (String rp:reportSet){
             String[] split = rp.split(" ");
             String from = split[0];
             String to = split[1];
-            reportIdx.get(name.get(to)).add(name.get(from));
+            if (!reportIdx.get(name.get(to)).contains(name.get(from))){
+                reportIdx.get(name.get(to)).add(name.get(from));
+            }
         }
         // 메일 횟수
         int[] mail = new int[id_list.length];
