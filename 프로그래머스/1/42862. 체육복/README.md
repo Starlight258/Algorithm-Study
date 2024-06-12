833fa61ca4cca1b1e2e99284f1d852d2a7396d46
Mint <78211281+Starlight258@users.noreply.github.com>
2024-06-12 21:10:53 +0900

    [level 1] Title: 체육복, Time: 0.59 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/README.md
+++ 프로그래머스/1/42862. 체육복/README.md
@@ -1,10 +1,10 @@
 # [level 1] 체육복 - 42862 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862?itm_content=course14743) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42862) 
 
 ### 성능 요약
 
-메모리: 69.1 MB, 시간: 0.37 ms
+메모리: 74.2 MB, 시간: 0.59 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 04월 06일 22:05:15
+2024년 06월 12일 21:10:51
 
 ### 문제 설명
 
--- 프로그래머스/1/42862. 체육복/체육복.java
+++ 프로그래머스/1/42862. 체육복/체육복.java
@@ -1,27 +1,31 @@
 import java.util.*;
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
-        int answer = n - lost.length;
-        int[] check = new int[n+1];
-        Arrays.sort(lost);
-        Arrays.sort(reserve);
+        int answer = 0;
+        int[] arr = new int[n];
         for (int i=0;i<lost.length;i++){
-            check[lost[i]] = -1;
+            arr[lost[i]-1] = -1;
         }
         for (int i=0;i<reserve.length;i++){
-            check[reserve[i]] += 1;   
-            if (check[reserve[i]] == 0) answer++;
+            arr[reserve[i]-1]++;
         }
+        //2. 체육복 빌려주기
+        Arrays.sort(lost);
         for (int i=0;i<lost.length;i++){
-            int l = lost[i];
-            if (l-1 >=0 && check[l-1] > 0){
-                check[l-1] = 0;
-                answer++;
-            } 
-            else if (l+1 <n && check[l+1]>0){
-                check[l+1] = 0;
-                answer++;
+            int l = lost[i]-1;
+            if (arr[l]!=-1) continue;
+            if (l>0 && arr[l-1]>0){
+                arr[l-1]--;
+                arr[l]++;
             }
+            else if (l+1<n&&arr[l+1]>0){
+                arr[l+1]--;
+                arr[l]++;
+            }
+        }
+        //3. 정답
+        for (int a:arr){
+            if (a>=0) answer++;
         }
         return answer;
     }
