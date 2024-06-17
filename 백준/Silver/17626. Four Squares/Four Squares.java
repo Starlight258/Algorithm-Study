1921bc234d5fc0bb56f1ba2dbe4ea7501134f48f
Mint <78211281+Starlight258@users.noreply.github.com>
2024-06-17 12:51:36 +0900

    [Silver III] Title: Four Squares, Time: 204 ms, Memory: 18016 KB -BaekjoonHub

---
+++ 백준/Silver/17626. Four Squares/Four Squares.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = Integer.parseInt(sc.nextLine());
+
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, 50001);
+        dp[0] = 0;
+        dp[1] = 1;
+        for (int i = 2; i <= n; i++) {
+            for (int j = 1; j * j <= i; j++) {
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
+            }
+        }
+
+        System.out.println(dp[n]);
+    }
+}
