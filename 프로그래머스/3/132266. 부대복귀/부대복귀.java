1994a68eefc781dead7815dea9108ecf5aa75413
Mint <78211281+Starlight258@users.noreply.github.com>
2024-06-20 21:09:09 +0900

    [level 3] Title: 부대복귀, Time: 56.10 ms, Memory: 129 MB -BaekjoonHub

--- 프로그래머스/3/132266. 부대복귀/부대복귀.java
+++ 프로그래머스/3/132266. 부대복귀/부대복귀.java
@@ -1,52 +1,53 @@
 import java.util.*;
 class Solution {
     class Node implements Comparable<Node>{
-        int idx, cost;
+        int idx;
+        int cost;
         Node (int idx, int cost){
             this.idx = idx;
             this.cost = cost;
         }
         public int compareTo(Node n){
-            return this.cost - n.cost; // 비용 오름차순 
+            return this.cost - n.cost;
         }
     }
     public int[] solution(int n, int[][] roads, int[] sources, int destination) {
         int[] answer = new int[sources.length];
-        List<List<Integer>> list = new ArrayList<>();
+        
+        List<List<Integer>> edges = new ArrayList<>();
         for (int i=0;i<=n;i++){
-            list.add(new ArrayList<>());
+            edges.add(new ArrayList<>());
         }
         for (int[] road:roads){
-            list.get(road[0]).add(road[1]);
-            list.get(road[1]).add(road[0]);
+            edges.get(road[0]).add(road[1]);
+            edges.get(road[1]).add(road[0]);
         }
-        
         int[] dist = new int[n+1];
         for (int i=0;i<=n;i++){
             dist[i] = Integer.MAX_VALUE;
         }
-        // 우선순위 큐
+        // 다익스트라 알고리즘
         PriorityQueue<Node> pq = new PriorityQueue<>();
         pq.add(new Node(destination, 0));
-        dist[destination]=0;
+        dist[destination] = 0;
         
         while (!pq.isEmpty()){
             Node cur = pq.poll();
+            
             if (dist[cur.idx] < cur.cost) continue;
-        
-            for (Integer next:list.get(cur.idx)){
-                if (dist[next] > cur.cost+1){
+            
+            for (Integer next:edges.get(cur.idx)){
+                if (dist[next] > cur.cost + 1){
                     dist[next] = cur.cost + 1;
                     pq.offer(new Node(next, dist[next]));
                 }
             }
         }
         
-        // 최단 거리 구하기
+        // source만큼 정답 구하기
         for (int i=0;i<sources.length;i++){
             answer[i] = dist[sources[i]] == Integer.MAX_VALUE ? -1 : dist[sources[i]];
         }
-        
         return answer;
     }
 }
