10f377ff2ea584a24f1102c1e8899c6bad2f35fe
Mint <78211281+Starlight258@users.noreply.github.com>
2024-06-25 09:41:39 +0900

    [level 2] Title: 리코쳇 로봇, Time: 0.81 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
@@ -1,6 +1,10 @@
 import java.util.*;
+
 class Solution {
-    class Node {
+    static char[][] graph;
+    static int[] dy = {-1,0,1,0};
+    static int[] dx = {0,1,0,-1};
+    class Node{
         int y;
         int x;
         Node(int y, int x){
@@ -8,61 +12,54 @@ class Solution {
             this.x = x;
         }
     }
-    int n, m, sx, sy, gx, gy;
-    char[][] mp;
-    int[][] visited;
-    int dy[] = {-1, 0, 1, 0};
-    int dx[] = {0, 1, 0, -1};
-    void bfs(int y, int x, int goalY, int goalX){
-        Queue<Node> queue = new LinkedList<>();
-        queue.add(new Node(y, x));
-        visited[y][x] = 1;
-        while (!queue.isEmpty()){
-            Node here = queue.poll();
-            y = here.y;
-            x = here.x;
-            if (y==gy && x==gx) break;
+    public int bfs(Node start, Node goal){
+        Queue<Node> q = new LinkedList<>();
+        q.add(start);
+        int[][] visited = new int[graph.length][graph[0].length];
+        visited[start.y][start.x] = 1;
+        // bfs 수행
+        while (!q.isEmpty()){
+            Node cur = q.poll();
             for (int i=0;i<4;i++){
-                int nextY = y;
-                int nextX = x;
-                for (int j=0;j<Math.max(n, m);j++){
-                    nextY += dy[i];
-                    nextX += dx[i];
-                    if (nextY<0||nextX<0||nextY>=mp.length||nextX>=mp[0].length||mp[nextY][nextX]=='D'){
-                        nextY -= dy[i];
-                        nextX -= dx[i];
-                        break;
-                    } 
+                int y = cur.y;
+                int x = cur.x;
+                if (graph[y][x] == 'G') break;
+                while (true){
+                    y += dy[i];
+                    x += dx[i];
+                    if (y<0||x<0||y>=graph.length||x>=graph[0].length) break;
+                    if (graph[y][x] == 'D') break;
                 }
-                if (visited[nextY][nextX]==0){
-                    queue.offer(new Node(nextY, nextX));
-                    visited[nextY][nextX] = visited[y][x]+1;
+                y -= dy[i];
+                x -= dx[i];
+                if (visited[y][x]==0){
+                    visited[y][x] = visited[cur.y][cur.x]+1;
+                    q.add(new Node(y, x));
                 }
             }
         }
+        return visited[goal.y][goal.x] - 1;
     }
     public int solution(String[] board) {
         int answer = 0;
-        n = board.length;
-        m = board[0].length();
-        mp = new char[n][m];
-        visited = new int[n][m];
-        // 1. 입력받기
+        graph = new char[board.length][];
         for (int i=0;i<board.length;i++){
-            mp[i] = board[i].toCharArray();
+            graph[i] = board[i].toCharArray();
+        }
+        Node start = new Node(0, 0);
+        Node goal = new Node(0, 0);
+        for (int i=0;i<board.length;i++){
             for (int j=0;j<board[i].length();j++){
-                if (board[i].charAt(j) == 'G'){
-                    gy = i;
-                    gx = j;
+                if (graph[i][j] == 'R'){
+                    start = new Node(i, j);
                 }
-                if (board[i].charAt(j) == 'R'){
-                    sy = i;
-                    sx = j;
+                if (graph[i][j] == 'G'){
+                    goal = new Node(i, j);
                 }
             }
         }
-        //2. bfs 수행
-        bfs(sy, sx, gy, gx);
-        return visited[gy][gx]==0 ? -1 : visited[gy][gx]-1;
+        //1. bfs 수행
+        answer = bfs(start, goal);
+        return answer;
     }
 }
