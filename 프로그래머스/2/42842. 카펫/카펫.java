f542035586dbf4f1c21a8771ee0e846b024f5f70
Mint <78211281+Starlight258@users.noreply.github.com>
2024-06-25 13:42:28 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 84.7 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -2,13 +2,9 @@ class Solution {
     public int[] solution(int brown, int yellow) {
         //1. 갈색 블록으로 경우의 수 구하기
         int sum = (brown+4)/2;
-        System.out.println(sum);
         for (int a=1;a<=sum/2;a++){
             int b = sum - a;
             if ((a-2) * (b-2) == yellow){
-                if (a>=b){
-                    return new int[]{a,b};
-                }
                 return new int[]{b,a};
             }
         }
