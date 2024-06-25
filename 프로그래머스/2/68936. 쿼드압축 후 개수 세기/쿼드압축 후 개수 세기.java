6d05b5fda8e29931868ec5c215ec25828917c57a
Mint <78211281+Starlight258@users.noreply.github.com>
2024-06-25 13:22:56 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 4.36 ms, Memory: 97.9 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
@@ -1,11 +1,11 @@
 class Solution {
-    static int[] dy = {0,0,1,1};
-    static int[] dx = {0,1,0,1};
     static int[] answer = new int[2];
+    
     public int[] solution(int[][] arr) {
         checkArea(arr, arr.length, 0, 0);
         return answer;
     }
+    
     void checkArea(int[][] arr, int width, int y, int x){
         if (isMatched(arr, width, y, x)){
             answer[arr[y][x]]++;
@@ -16,6 +16,7 @@ class Solution {
         checkArea(arr, width/2, y, x+width/2);
         checkArea(arr, width/2, y+width/2, x+width/2);
     }
+    
     boolean isMatched(int[][] arr, int width, int y, int x){
         int e = arr[y][x];
         for (int i=y;i<y+width;i++){
