c616c110d68658194f65f688c66d210a359e45cd
Mint <78211281+Starlight258@users.noreply.github.com>
2024-06-25 15:46:40 +0900

    [level 2] Title: H-Index, Time: 0.44 ms, Memory: 71.1 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,23 +1,21 @@
 import java.util.*;
 class Solution {
-    public int solution(int[] citations) {
-        int answer = 0;
-        Arrays.sort(citations);
-        int h=0;
-        while (h>=0 && h<=citations[citations.length-1]){
-            int cnt = 0;
-            for (int i=0;i<citations.length;i++){
-                if(h<=citations[i]){
-                    cnt++;
+    public int solution(int[] ct) {
+        //1. 정렬
+        Arrays.sort(ct);
+        //2. h-index 찾기
+        int h = ct[ct.length-1];
+        while (h>0){
+            int count=0;
+            for (int i=0;i<ct.length;i++){
+                if (ct[i]>=h){
+                    count++;
                 }
             }
-            if (cnt>=h){
-                if(cnt < answer) break;
-                answer = h;
-            }
-            h++;
-        }
+            if (count>=h) return h;
+            h--;
+         }
         
-        return answer;
+        return 0;
     }
 }
