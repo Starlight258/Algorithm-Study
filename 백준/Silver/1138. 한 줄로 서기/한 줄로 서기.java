e9dc6e95c7333a52c7b28707607aaf2b28b56ee4
Mint <78211281+Starlight258@users.noreply.github.com>
2024-07-04 01:23:17 +0900

    [Silver II] Title: 한 줄로 서기, Time: 112 ms, Memory: 14300 KB -BaekjoonHub

--- 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.java
+++ 백준/Silver/1138. 한 줄로 서기/한 줄로 서기.java
@@ -7,7 +7,6 @@ import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        //1. 입력받기
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
         int[] arr = new int[n];
@@ -15,13 +14,15 @@ public class Main {
         for (int i = 0; i < n; i++) {
             arr[i] = Integer.parseInt(st.nextToken());
         }
-        //2. 줄 서기
+
         List<Integer> line = new ArrayList<>();
         for (int i = arr.length - 1; i >= 0; i--) {
             line.add(arr[i], i + 1);
         }
+
         for (Integer integer : line) {
             System.out.print(integer + " ");
         }
+
     }
 }
