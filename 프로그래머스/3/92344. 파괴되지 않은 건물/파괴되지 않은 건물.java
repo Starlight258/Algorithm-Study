30a2e7f9f8610673e687928bc088676fe1fdc85e
Mint <78211281+Starlight258@users.noreply.github.com>
2024-07-05 10:35:22 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 45.70 ms, Memory: 218 MB -BaekjoonHub

---
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
@@ -0,0 +1,67 @@
+class Solution {
+    int[][] durability;
+    public int solution(int[][] board, int[][] skills) {
+        int answer = 0;
+        durability = new int[board.length+1][board[0].length+1];
+        for (int[] skill:skills){
+            check(durability, skill);
+        }
+        int height = board.length-1;
+        int width = board[0].length-1;
+        calculate(durability, board);
+        answer = findSafeBuilding(board);
+        return answer;
+    }
+    public void check(int[][] d, int[] skill){
+        int type = skill[0];
+        int r1 = skill[1];
+        int c1 = skill[2];
+        int r2 = skill[3];
+        int c2 = skill[4];
+        int degree = skill[5];
+        if (type==1){ // 적(-)
+            d[r1][c1] -= degree;
+            d[r1][c2+1] += degree;
+            d[r2+1][c1] += degree;
+            d[r2+1][c2+1] -= degree;
+        } else { // 아군(+)
+            d[r1][c1] += degree;
+            d[r1][c2+1] -= degree;
+            d[r2+1][c1] -= degree;
+            d[r2+1][c2+1] += degree;
+        }
+    }
+    public void calculate(int[][] d, int[][] board){
+        int height = board.length-1;
+        int width = board[0].length-1;
+        // 아래쪽으로 가기
+        for (int c=0;c<=width+1;c++){
+            for (int r=0;r<=height;r++){
+                d[r+1][c] += d[r][c];
+            }
+        }
+        // 왼쪽으로 가기
+        for (int r=0;r<=height+1;r++){
+            for (int c=0;c<=width;c++){
+                d[r][c+1] += d[r][c];
+            }
+        }
+        // 원본 배열에 더하기
+        for (int r=0;r<=height;r++){
+            for (int c=0;c<=width;c++){
+                board[r][c] += d[r][c];
+            }
+        }
+    }
+    public int findSafeBuilding(int[][] board){
+        int cnt = 0;
+        for (int[] b:board){
+            for (int e:b){
+                if (e>0){
+                    cnt++;
+                }
+            }
+        }
+        return cnt;
+    }
+}
