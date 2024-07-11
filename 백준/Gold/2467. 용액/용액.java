e6daccaca0553f2a96a5432d39c22c332b66e04a
Mint <78211281+Starlight258@users.noreply.github.com>
2024-07-11 14:13:46 +0900

    [Gold V] Title: 용액, Time: 332 ms, Memory: 30676 KB -BaekjoonHub

--- 백준/Gold/2467. 용액/용액.java
+++ 백준/Gold/2467. 용액/용액.java
@@ -1,36 +1,41 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        // 1. 입력받기
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[] number = new int[n];
+        int[] arr = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            number[i] = Integer.parseInt(st.nextToken());
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-
+        // 2. 투포인터
+        Arrays.sort(arr);
         int left = 0;
-        int right = n-1;
-        int[] pair = new int[2];
-        int answer = 2000000001;
-
-        while (left<right){
-            int sum = number[left] + number[right];
-            if (Math.abs(sum) < answer){
-                answer = Math.abs(sum);
-                pair[0] = number[left];
-                pair[1] = number[right];
+        int right = n - 1;
+        int minTotal = Integer.MAX_VALUE;
+        int[] values = new int[2];
+        while (left < right) {
+            int sum = arr[left] + arr[right];
+            if (Math.abs(sum) <= minTotal) {
+                minTotal = Math.abs(sum);
+                values = new int[]{left, right};
+                if (sum == 0) {
+                    break;
+                }
             }
-            if (sum==0) break;
-            if (sum < 0){
+            if (sum < 0) {
                 left++;
-            } else right--;
-        }
+            } else {
+                right--;
+            }
 
-        System.out.println(pair[0] + " " + pair[1]);
+        }
+        System.out.println(arr[values[0]] + " " + arr[values[1]]);
     }
 }
