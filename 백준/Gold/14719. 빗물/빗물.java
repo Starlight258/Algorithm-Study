4346350855ef709da352b39892505f79a835dd88
Mint <78211281+Starlight258@users.noreply.github.com>
2024-07-18 15:52:38 +0900

    [Gold V] Title: 빗물, Time: 136 ms, Memory: 14380 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.java
+++ 백준/Gold/14719. 빗물/빗물.java
@@ -5,7 +5,6 @@ import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        //1. 입력받기
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int h = Integer.parseInt(st.nextToken());
@@ -16,25 +15,17 @@ public class Main {
             arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        //2. 빗물 구하기
         int answer = 0;
         for (int i = 1; i < w; i++) {
-            // left max
-            int leftMax = 0;
-            for (int j = 0; j < i; j++) {
+            int leftMax = 0, rightMax = 0;
+            for (int j = 0; j <= i; j++) {
                 leftMax = Math.max(leftMax, arr[j]);
             }
-            // right max
-            int rightMax = 0;
-            for (int j = i + 1; j < w; j++) {
+            for (int j = i; j < w; j++) {
                 rightMax = Math.max(rightMax, arr[j]);
             }
-            int pole = Math.min(leftMax, rightMax);
-            if (pole > arr[i]) {
-                answer += pole - arr[i];
-            }
+            answer += Math.min(leftMax, rightMax) - arr[i];
         }
-
         System.out.println(answer);
     }
 }
