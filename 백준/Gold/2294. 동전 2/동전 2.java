2e85774d88b62c4e77e68d4b11574ce02310ea23
Mint <78211281+Starlight258@users.noreply.github.com>
2024-07-24 12:40:12 +0900

    [Gold V] Title: 동전 2, Time: 136 ms, Memory: 14268 KB -BaekjoonHub

--- 백준/Gold/2294. 동전 2/동전 2.java
+++ 백준/Gold/2294. 동전 2/동전 2.java
@@ -6,31 +6,24 @@ import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        //1. 입력받기
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
-        int[] coins = new int[n];
-        for (int i = 0; i < n; i++) {
-            coins[i] = Integer.parseInt(br.readLine());
-        }
-        //2. dp 배열 생성
+
+        //2. dp 수행하기
         int[] dp = new int[k + 1];
-        Arrays.fill(dp, 10001);
+        Arrays.fill(dp, k + 1);
+
         dp[0] = 0;
-        for (int i = 0; i < coins.length; i++) {
-            for (int j = coins[i]; j <= k; j++) {
-                if (j - coins[i] >= 0) {
-                    dp[j] = Math.min(dp[j], dp[j - coins[i]] + 1);
-                }
+        for (int i = 0; i < n; i++) {
+            int coin = Integer.parseInt(br.readLine());
+            for (int j = coin; j <= k; j++) {
+                dp[j] = Math.min(dp[j], dp[j - coin] + 1);
             }
         }
-        //3. 결과 출력
-        if (dp[k] == 10001) {
-            System.out.println(-1);
-        } else {
-            System.out.println(dp[k]);
-        }
+
+        //3. 정답 출력
+        System.out.println(dp[k] > k ? -1 : dp[k]);
     }
 }
