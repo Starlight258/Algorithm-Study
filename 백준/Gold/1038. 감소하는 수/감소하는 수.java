9d871c33b9face785efde0f88370c1b6197d821d
Mint <78211281+Starlight258@users.noreply.github.com>
2024-07-24 13:41:04 +0900

    [Gold V] Title: 감소하는 수, Time: 132 ms, Memory: 14308 KB -BaekjoonHub

---
+++ 백준/Gold/1038. 감소하는 수/감소하는 수.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class Main {
+
+    static List<Long> list;
+
+    private static void dfs(long n) {
+        list.add(n);
+        long next = n % 10;
+        if (next == 0) {
+            return;
+        }
+
+        for (long i = next - 1; i >= 0; i--) {
+            long nextNum = n * 10 + i;
+            dfs(nextNum);
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        if (n <= 10) {
+            System.out.println(n);
+            return;
+        } else if (n >= 1023) {
+            System.out.println(-1);
+            return;
+        }
+
+        list = new ArrayList<>();
+        for (int i = 0; i <= 9; i++) {
+            dfs(i);
+        }
+
+        Collections.sort(list);
+        System.out.println(list.get(n));
+    }
+}
