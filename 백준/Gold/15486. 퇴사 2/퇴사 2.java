0a47098500de3d832bd0b2c035a42de14cd3c243
Mint <78211281+Starlight258@users.noreply.github.com>
2024-08-01 15:35:25 +0900

    [Gold V] Title: 퇴사 2, Time: 884 ms, Memory: 339716 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.java
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.java
@@ -1,43 +1,32 @@
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Main {
-    static class Pair{
-        int time;
-        int price;
-        Pair(int time, int price){
-            this.time = time;
-            this.price = price;
-        }
-    }
-    static int[] dp;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        ArrayList<Pair> planList = new ArrayList<>();
+        int[][] profits = new int[n + 1][2];
         StringTokenizer st;
-        for (int i=0;i<n;i++){
+        for (int i = 1; i <= n; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            planList.add(new Pair(a, b));
+            profits[i] = new int[]{a, b};
         }
-        dp = new int[n+1]; // 1~n
-        for (int i=0;i<n;i++){
-            Pair p = planList.get(i);
-            if (i+p.time <= n){
-                dp[i + p.time] = Math.max(dp[i + p.time], dp[i] + p.price);
+
+        //2. dp 수행
+        int[] dp = new int[n + 2];
+        for (int i = 1; i <= n; i++) {
+            int t = profits[i][0];
+            int p = profits[i][1];
+            if (i + t <= n + 1) {
+                dp[i + t] = Math.max(dp[i + t], dp[i] + p);
             }
-            dp[i+1] = Math.max(dp[i+1], dp[i]);
+            dp[i + 1] = Math.max(dp[i + 1], dp[i]);
         }
-        int answer = 0;
-        for (int i=1;i<=n;i++){
-            answer = Math.max(answer, dp[i]);
-        }
-        System.out.println(dp[n]);
+
+        System.out.println(dp[n + 1]);
     }
 }
