fdad30b038c5996e69713a1d56327b22367bb085
Mint <78211281+Starlight258@users.noreply.github.com>
2024-08-08 15:07:00 +0900

    [Gold IV] Title: 두 동전, Time: 100 ms, Memory: 14404 KB -BaekjoonHub

--- 백준/Gold/16197. 두 동전/두 동전.java
+++ 백준/Gold/16197. 두 동전/두 동전.java
@@ -8,82 +8,84 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
+
+    static int n;
+    static int m;
+    static char[][] graph;
     static int[] dy = {-1, 0, 1, 0};
     static int[] dx = {0, 1, 0, -1};
-    static int n, m;
     static List<int[]> coins;
-    static char[][] arr;
-    static boolean[][][][] visited;
 
-    public static int bfs() {
+    private static int bfs() {
         Queue<int[]> q = new LinkedList<>();
-        q.add(new int[]{coins.get(0)[0], coins.get(0)[1], coins.get(1)[0], coins.get(1)[1], 0});
+        q.offer(new int[]{coins.get(0)[0], coins.get(0)[1], coins.get(1)[0], coins.get(1)[1], 0});
+        boolean[][][][] visited = new boolean[n][m][n][m];
+        visited[coins.get(0)[0]][coins.get(0)[1]][coins.get(1)[0]][coins.get(1)[1]] = true;
+
         while (!q.isEmpty()) {
-            int y1 = q.peek()[0];
-            int x1 = q.peek()[1];
-            int y2 = q.peek()[2];
-            int x2 = q.peek()[3];
-            int cnt = q.peek()[4];
-            q.poll();
-            if (cnt >= 10) {
-                return -1;
+            int[] cur = q.poll();
+            int y1 = cur[0];
+            int x1 = cur[1];
+            int y2 = cur[2];
+            int x2 = cur[3];
+            int count = cur[4];
+            if (count >= 10) {
+                continue;
             }
             for (int i = 0; i < 4; i++) {
+                boolean fall1 = false;
+                boolean fall2 = false;
+
                 int ny1 = y1 + dy[i];
                 int nx1 = x1 + dx[i];
-                int ny2 = y2 + dy[i];
-                int nx2 = x2 + dx[i];
-                boolean isFall1 = false;
-                boolean isFall2 = false;
-                if (ny1 < 0 || ny1 >= n || nx1 < 0 || nx1 >= m) {
-                    isFall1 = true;
-                }
-                if (ny2 < 0 || ny2 >= n || nx2 < 0 || nx2 >= m) {
-                    isFall2 = true;
-                }
-                if (isFall1 && isFall2) {
-                    continue;
-                } else if ((!isFall1 && isFall2) || (isFall1 && !isFall2)) {
-                    return cnt + 1;
-                }
-                if (arr[ny1][nx1] == '#') {
+                if (ny1 < 0 || nx1 < 0 || ny1 >= n || nx1 >= m) {
+                    fall1 = true;
+                } else if (graph[ny1][nx1] == '#') {
                     ny1 = y1;
                     nx1 = x1;
                 }
-                if (arr[ny2][nx2] == '#') {
+                int ny2 = y2 + dy[i];
+                int nx2 = x2 + dx[i];
+                if (ny2 < 0 || nx2 < 0 || ny2 >= n || nx2 >= m) {
+                    fall2 = true;
+                } else if (graph[ny2][nx2] == '#') {
                     ny2 = y2;
                     nx2 = x2;
                 }
-                if (visited[ny1][nx1][ny2][nx2]) {
+                if (fall1 && fall2) {
                     continue;
+                } else if (!fall1 && !fall2) {
+                    if (visited[ny1][nx1][ny2][nx2]) {
+                        continue;
+                    }
+                    q.offer(new int[]{ny1, nx1, ny2, nx2, count + 1});
+                    visited[ny1][nx1][ny2][nx2] = true;
+                } else {
+                    return count + 1;
                 }
-                q.add(new int[]{ny1, nx1, ny2, nx2, cnt + 1});
-                visited[ny1][nx1][ny2][nx2] = true;
             }
         }
         return -1;
     }
 
     public static void main(String[] args) throws IOException {
-        //1. 입력받기
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        arr = new char[n][m];
-        visited = new boolean[n][m][n][m];
+
+        graph = new char[n][m];
         coins = new ArrayList<>();
         for (int i = 0; i < n; i++) {
+            String line = br.readLine();
             for (int j = 0; j < m; j++) {
-                arr[i][j] = (char) br.read();
-                if (arr[i][j] == 'o') {
+                graph[i][j] = line.charAt(j);
+                if (graph[i][j] == 'o') {
                     coins.add(new int[]{i, j});
                 }
             }
-            br.readLine();
         }
 
-        //2. bfs
         System.out.println(bfs());
     }
 }
