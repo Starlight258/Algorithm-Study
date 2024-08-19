c3271d7b7fa06279ec823aeb7672a765a10f2e49
Mint <78211281+Starlight258@users.noreply.github.com>
2024-08-19 14:03:42 +0900

    [Gold V] Title: 퇴사 2, Time: 712 ms, Memory: 333392 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.java
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.java
@@ -4,29 +4,37 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+    static class Load {
+        int t;
+        int p;
+
+        public Load(final int t, final int p) {
+            this.t = t;
+            this.p = p;
+        }
+    }
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[][] profits = new int[n + 1][2];
+        Load[] times = new Load[n + 1];
         StringTokenizer st;
         for (int i = 1; i <= n; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            profits[i] = new int[]{a, b};
+            times[i] = new Load(a, b);
         }
 
-        //2. dp 수행
         int[] dp = new int[n + 2];
         for (int i = 1; i <= n; i++) {
-            int t = profits[i][0];
-            int p = profits[i][1];
-            if (i + t <= n + 1) {
-                dp[i + t] = Math.max(dp[i + t], dp[i] + p);
+            dp[i] = Math.max(dp[i - 1], dp[i]);
+            Load time = times[i];
+            if (i + time.t <= n + 1) {
+                dp[i + time.t] = Math.max(dp[i + time.t], dp[i] + time.p);
             }
-            dp[i + 1] = Math.max(dp[i + 1], dp[i]);
         }
 
-        System.out.println(dp[n + 1]);
+        System.out.println(Math.max(dp[n], dp[n + 1]));
     }
 }
