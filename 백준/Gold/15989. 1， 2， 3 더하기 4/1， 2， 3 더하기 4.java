8f93f85248c931b60b904be8c2c821e3c00960d3
Mint <78211281+Starlight258@users.noreply.github.com>
2024-08-20 12:07:51 +0900

    [Gold V] Title: 1, 2, 3 더하기 4, Time: 120 ms, Memory: 14588 KB -BaekjoonHub

--- 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.java
+++ 백준/Gold/15989. 1， 2， 3 더하기 4/1， 2， 3 더하기 4.java
@@ -3,22 +3,21 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+
+    public static final int MAX = 10001;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
 
-        // 2. dp 배열 만들기
-        int[] cases = new int[]{1, 2, 3};
-        int[] dp = new int[10001];
+        int[] dp = new int[MAX];
         dp[0] = 1;
-        for (int i = 0; i < cases.length; i++) {
-            for (int j = cases[0]; j <= 10000; j++) {
-                if (j - cases[i] >= 0) {
-                    dp[j] += dp[j - cases[i]];
-                }
+        for (int i = 1; i <= 3; i++) {
+            for (int j = i; j < MAX; j++) {
+                dp[j] += dp[j - i];
             }
         }
-
+        
+        int t = Integer.parseInt(br.readLine());
         while (t-- > 0) {
             int n = Integer.parseInt(br.readLine());
             System.out.println(dp[n]);
