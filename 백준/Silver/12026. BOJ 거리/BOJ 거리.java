2a81298d6bc9f3676faa991c61b2e66d6a58f99f
Mint <78211281+Starlight258@users.noreply.github.com>
2024-08-21 14:08:33 +0900

    [Silver I] Title: BOJ 거리, Time: 124 ms, Memory: 14328 KB -BaekjoonHub

--- 백준/Silver/12026. BOJ 거리/BOJ 거리.java
+++ 백준/Silver/12026. BOJ 거리/BOJ 거리.java
@@ -5,19 +5,24 @@ import java.util.Arrays;
 
 public class Main {
 
-    public static final int MAX = 1000000;
-
     static int n;
     static String line;
     static int[] dp;
+    static int MAX = 1000000;
+
+    private static void updateDp(final int idx, final char c) {
+        for (int i = idx + 1; i < line.length(); i++) {
+            if (line.charAt(i) == c) {
+                dp[i] = Math.min(dp[i], dp[idx] + (int) Math.pow((i - idx), 2));
+            }
+        }
+    }
 
     public static void main(String[] args) throws IOException {
-        // 1. 입력받기
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
         line = br.readLine();
 
-        //2. 에너지
         dp = new int[n];
         Arrays.fill(dp, MAX);
         dp[0] = 0;
@@ -25,26 +30,14 @@ public class Main {
         for (int i = 0; i < line.length(); i++) {
             char c = line.charAt(i);
             if (c == 'B') {
-                makeDp(i, 'O');
+                updateDp(i, 'O');
             } else if (c == 'O') {
-                makeDp(i, 'J');
+                updateDp(i, 'J');
             } else {
-                makeDp(i, 'B');
+                updateDp(i, 'B');
             }
         }
 
-        if (dp[line.length() - 1] == MAX) {
-            System.out.println(-1);
-        } else {
-            System.out.println(dp[line.length() - 1]);
-        }
-    }
-
-    private static void makeDp(int index, char c) {
-        for (int i = index + 1; i < line.length(); i++) {
-            if (line.charAt(i) == c) {
-                dp[i] = Math.min(dp[i], dp[index] + (int) Math.pow(i - index, 2));
-            }
-        }
+        System.out.println(dp[n - 1] == MAX ? -1 : dp[n - 1]);
     }
 }
