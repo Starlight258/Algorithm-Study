a8613782d99a99973e60f53c55637ebac610b885
Mint <78211281+Starlight258@users.noreply.github.com>
2024-08-21 15:24:32 +0900

    [Gold V] Title: 1학년, Time: 100 ms, Memory: 14124 KB -BaekjoonHub

--- 백준/Gold/5557. 1학년/1학년.java
+++ 백준/Gold/5557. 1학년/1학년.java
@@ -6,39 +6,38 @@ import java.util.StringTokenizer;
 public class Main {
 
     static int n;
-    static int[] numbers;
+    static int[] nums;
     static long[][] dp;
+    static long answer = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
-        numbers = new int[n];
-
+        nums = new int[n];
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            numbers[i] = Integer.parseInt(st.nextToken());
+            nums[i] = Integer.parseInt(st.nextToken());
         }
-
         dp = new long[n][21];
-        dp[0][numbers[0]] = 1;
 
-        int plus;
-        int minus;
-        for (int i = 1; i < n - 1; i++) {
-            for (int j = 0; j <= 20; j++) {
-                if (dp[i - 1][j] != 0) {
-                    plus = j + numbers[i];
-                    minus = j - numbers[i];
-                    if (plus >= 0 && plus <= 20) {
-                        dp[i][plus] += dp[i - 1][j];
-                    }
-                    if (minus >= 0 && minus <= 20) {
-                        dp[i][minus] += dp[i - 1][j];
-                    }
+        dp[0][nums[0]] = 1;
+
+        for (int i = 1; i < n - 1; i++) { // 인덱스
+            for (int j = 0; j <= 20; j++) { // 합
+                if (dp[i - 1][j] == 0) {
+                    continue;
+                }
+                int plus = j + nums[i];
+                int minus = j - nums[i];
+                if (plus >= 0 && plus <= 20) {
+                    dp[i][plus] += dp[i - 1][j];
+                }
+                if (minus >= 0 && minus <= 20) {
+                    dp[i][minus] += dp[i - 1][j];
                 }
             }
         }
 
-        System.out.println(dp[n - 2][numbers[n - 1]]);
+        System.out.println(dp[n - 2][nums[n - 1]]);
     }
 }
