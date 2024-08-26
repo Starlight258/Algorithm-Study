744e277fe61dff7f832939e1b3f70e8badeaccad
Mint <78211281+Starlight258@users.noreply.github.com>
2024-08-26 13:32:11 +0900

    [Gold IV] Title: 괄호, Time: 124 ms, Memory: 14308 KB -BaekjoonHub

--- 백준/Gold/10422. 괄호/괄호.java
+++ 백준/Gold/10422. 괄호/괄호.java
@@ -6,23 +6,20 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
-
-        // dp
+        // dp 배열 만들기
         long[] dp = new long[5001];
         dp[0] = 1;
         dp[2] = 1;
-
         for (int i = 2; i < 2501; i++) {
             for (int j = 0; j < i; j++) {
-                dp[i * 2] += dp[j * 2] * dp[(i - 1 - j) * 2];
-                dp[i * 2] %= 1000000007L;
+                dp[i * 2] += dp[j * 2] * dp[(i - j - 1) * 2];
+                dp[i * 2] %= 1_000_000_007L;
             }
         }
 
-        // 정답 출력
-        for (int i = 0; i < t; i++) {
-            int n = Integer.parseInt(br.readLine());
-            System.out.println(dp[n]);
+        while (t-- > 0) {
+            int number = Integer.parseInt(br.readLine());
+            System.out.println(dp[number]);
         }
     }
 }
