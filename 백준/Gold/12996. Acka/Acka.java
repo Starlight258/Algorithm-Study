030150ed68f442c0b55d1fc35c0eb9ba28be7c60
Mint <78211281+Starlight258@users.noreply.github.com>
2024-08-27 14:24:43 +0900

    [Gold III] Title: Acka, Time: 264 ms, Memory: 35940 KB -BaekjoonHub

--- 백준/Gold/12996. Acka/Acka.java
+++ 백준/Gold/12996. Acka/Acka.java
@@ -8,57 +8,56 @@ public class Main {
 
     static final long MOD = 1_000_000_007;
 
-    static long[][][][] dp;
-    static int n;
-    static int d;
-    static int k;
-    static int h;
+    static int s;
+    static int a;
+    static int b;
+    static int c;
+    static long dp[][][][]; // 곡수, a,b,c
 
-    private static long solve(int n, int d, int k, int h) {
-        if (n == 0) {
-            return (d == 0 && k == 0 && h == 0) ? 1 : 0;
+    private static long dfs(int s, int a, int b, int c) {
+        if (s == 0) {
+            return (a == 0 && b == 0 && c == 0) ? 1 : 0; // 모두 사용
         }
 
-        if (d < 0 || k < 0 || h < 0) {
+        if (a < 0 || b < 0 || c < 0) {
             return 0;
         }
 
-        if (dp[n][d][k][h] != -1) {
-            return dp[n][d][k][h];
+        if (dp[s][a][b][c] != -1) {
+            return dp[s][a][b][c];
         }
 
         long result = 0;
-        for (int i = 0; i <= 1; i++) { // 부르느냐 부르지 않느냐 경우 구함
+        for (int i = 0; i <= 1; i++) {
             for (int j = 0; j <= 1; j++) {
-                for (int l = 0; l <= 1; l++) {
-                    if (i + j + l > 0) { // 세명 중 한명은 불러야함
-                        result += solve(n - 1, d - i, k - j, h - l);
+                for (int k = 0; k <= 1; k++) {
+                    if (i + j + k > 0) {
+                        result += dfs(s - 1, a - i, b - j, c - k);
                     }
                 }
             }
         }
 
-        return dp[n][d][k][h] = result % MOD;
+        return dp[s][a][b][c] = result % MOD;
     }
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        n = Integer.parseInt(st.nextToken());
-        d = Integer.parseInt(st.nextToken());
-        k = Integer.parseInt(st.nextToken());
-        h = Integer.parseInt(st.nextToken());
+        s = Integer.parseInt(st.nextToken());
+        a = Integer.parseInt(st.nextToken());
+        b = Integer.parseInt(st.nextToken());
+        c = Integer.parseInt(st.nextToken());
+        dp = new long[s + 1][a + 1][b + 1][c + 1];
 
-        dp = new long[n + 1][d + 1][k + 1][h + 1]; // 곡수, 각각의 곡 수
-
-        for (int i = 0; i <= n; i++) {
-            for (int j = 0; j <= d; j++) {
-                for (int l = 0; l <= k; l++) {
-                    Arrays.fill(dp[i][j][l], -1);
+        for (int i = 0; i <= s; i++) {
+            for (int j = 0; j <= a; j++) {
+                for (int k = 0; k <= b; k++) {
+                    Arrays.fill(dp[i][j][k], -1);
                 }
             }
         }
 
-        System.out.println(solve(n, d, k, h));
+        System.out.println(dfs(s, a, b, c));
     }
 }
