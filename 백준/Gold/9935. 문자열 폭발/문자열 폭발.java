33d991eafb0ca6d765829380e7cdab494f08f377
Mint <78211281+Starlight258@users.noreply.github.com>
2024-09-09 15:03:25 +0900

    [Gold IV] Title: 문자열 폭발, Time: 364 ms, Memory: 82540 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.java
@@ -2,19 +2,17 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Main {
+public class Main{
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
+        String words = br.readLine();
         String bomb = br.readLine();
 
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < str.length(); i++) {
-            sb.append(str.charAt(i));
+        for (int i = 0; i < words.length(); i++) {
+            sb.append(words.charAt(i));
 
-            if (sb.length() >= bomb.length() &&
-                    sb.substring(sb.length() - bomb.length())
-                            .equals(bomb)) {
+            if (sb.length() >= bomb.length() && sb.substring(sb.length() - bomb.length()).equals(bomb)) {
                 sb.delete(sb.length() - bomb.length(), sb.length());
             }
         }
@@ -22,4 +20,3 @@ public class Main {
         System.out.println(sb.length() == 0 ? "FRULA" : sb.toString());
     }
 }
-
