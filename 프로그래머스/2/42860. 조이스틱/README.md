e5ba54d193cb92af16c3eac19d61da973ed1d736
Mint <78211281+Starlight258@users.noreply.github.com>
2024-09-19 13:28:18 +0900

    [level 2] Title: 조이스틱, Time: 0.09 ms, Memory: 79.5 MB -BaekjoonHub

--- 프로그래머스/2/42860. 조이스틱/README.md
+++ 프로그래머스/2/42860. 조이스틱/README.md
@@ -1,10 +1,10 @@
 # [level 2] 조이스틱 - 42860 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42860) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42860?language=java) 
 
 ### 성능 요약
 
-메모리: 75 MB, 시간: 0.04 ms
+메모리: 79.5 MB, 시간: 0.09 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 16일 16:57:40
+2024년 09월 19일 13:28:16
 
 ### 문제 설명
 
--- 프로그래머스/2/42860. 조이스틱/조이스틱.java
+++ 프로그래머스/2/42860. 조이스틱/조이스틱.java
@@ -1,25 +1,17 @@
-public class Solution {
+class Solution {
     public int solution(String name) {
-        int alpha = 0;
-        int minMove = name.length() - 1;
-
-        for (int cursor = 0; cursor < name.length(); cursor++) {
-            int nextCursor = cursor + 1;
-
-            while (nextCursor < name.length() && name.charAt(nextCursor) == 'A') {
-                nextCursor++;
+        int answer = 0;
+        int n = name.length();
+        int leftRight = n-1;
+        for (int i=0;i<n;i++){
+            char goal = name.charAt(i);
+            answer += Math.min(goal-'A', 'Z'-goal+1);
+            int next = i+1;
+            while (next<n && name.charAt(next) == 'A'){
+                next++;
             }
-
-            int toBack = name.length() - nextCursor;
-
-            // 앞으로만 직진할 때, 현재 위치에서 뒤로 이동(nextCursor까지), 먼저 뒤로 갔다가(nextCursor까지) 현재 위치로 이동
-            minMove = Math.min(minMove, cursor * 2 + toBack);
-            minMove = Math.min(minMove, toBack * 2 + cursor);
-
-            int code = name.charAt(cursor) - 65;
-            alpha += Math.min(code, 26 - code);
+            leftRight = Math.min(leftRight, i + n - next + Math.min(i, n - next));
         }
-
-        return alpha + minMove;
+        return answer + leftRight;
     }
 }
