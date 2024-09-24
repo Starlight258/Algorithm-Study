185a49254b78c8ea8d1f7a10c4627e95948a070b
Mint <78211281+Starlight258@users.noreply.github.com>
2024-09-24 12:42:56 +0900

    [level 2] Title: 도넛과 막대 그래프, Time: 36.77 ms, Memory: 196 MB -BaekjoonHub

--- 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.java
+++ 프로그래머스/2/258711. 도넛과 막대 그래프/도넛과 막대 그래프.java
@@ -2,53 +2,28 @@ import java.util.*;
 class Solution {
     public int[] solution(int[][] edges) {
         int[] answer = {};
-        int n = 1;
-        int startPoint = 1;
-        // 출발점 찾기
-        for (int[] edge:edges){
-            n = Math.max(n, Math.max(edge[0], edge[1]));
-        }
-        // 정점별 indegree 수 구하기
-        int[] indegree = new int[n+1];
-        int[] outdegree = new int[n+1];
+        int n = 1_000_000;
+        int startPoint = 0;
+        int[] indegree = new int[n];
+        int[] outdegree = new int[n];
         
-        // 총 그래프 수
         for (int[] edge:edges){
-            outdegree[edge[0]]++;
-            indegree[edge[1]]++;
-        }
-        for (int i=1;i<=n;i++){
-            if (outdegree[i]>=2 && indegree[i]==0){
-                startPoint = i;
-                break;
-            }
-        }
-        // 개수 구하기
-        boolean[] notUsed = new boolean[n+1];
-        Arrays.fill(notUsed, true);
-        int total = outdegree[startPoint];
-        for (int[] edge:edges){
-            if (edge[0] == startPoint){
-                indegree[edge[1]]--;
-            }
-            notUsed[edge[0]] = false;
-            notUsed[edge[1]] = false;
+            outdegree[edge[0]-1]++;
+            indegree[edge[1]-1]++;
         }
         
         int stick = 0, eight = 0;
-        for (int i=1;i<=n;i++){
-            if (notUsed[i]) continue;
-            if (indegree[i]==2 && outdegree[i]==2){
-                eight++;
-                continue;
-            } 
-            if (outdegree[i] == 0){
+        for (int i=0;i<n;i++){
+            if (outdegree[i]>=2){
+                if (indegree[i]==0){
+                    startPoint = i;                
+                } else eight++;
+            } else if (indegree[i]>0 && outdegree[i] == 0){
                 stick++;
-                continue;
             }
         }
-       
-        answer = new int[]{startPoint, total-stick-eight, stick, eight};
+      
+        answer = new int[]{startPoint+1, outdegree[startPoint]-stick-eight, stick, eight};
         return answer;
     }
 }
