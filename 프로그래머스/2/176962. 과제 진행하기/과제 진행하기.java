4781264daef157d14ba34ae16af11ae62ee1f41c
Mint <78211281+Starlight258@users.noreply.github.com>
2024-09-26 14:42:01 +0900

    [level 2] Title: 과제 진행하기, Time: 32.24 ms, Memory: 80.9 MB -BaekjoonHub

--- 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.java
+++ 프로그래머스/2/176962. 과제 진행하기/과제 진행하기.java
@@ -1,44 +1,54 @@
 import java.util.*;
 class Solution {
-    class Assignment{
-        String name;
-        int start;
-        int playTime;
-        Assignment(String name, int start, int playTime){
-            this.name = name;
-            this.start = start;
-            this.playTime = playTime;
-        }
-    }
-    int calculateTime(String time){
-        String[] splitTime = time.split(":");
-        return Integer.parseInt(splitTime[0]) * 60 + Integer.parseInt(splitTime[1]);
-    }
     public String[] solution(String[][] plans) {
-        List<String> answer = new ArrayList<String>();
-        List<Assignment> list = new ArrayList<Assignment>();
+        // 정렬하기
+        Arrays.sort(plans, (String[] p1, String[] p2) -> convertToMinutes(p1[1]) - convertToMinutes(p2[1]));
+        List<String> list = new ArrayList<>();
+        Stack<int[]> stk = new Stack<>();
         for (int i=0;i<plans.length;i++){
             String[] plan = plans[i];
-            list.add(new Assignment(plan[0], calculateTime(plan[1]), Integer.parseInt(plan[2])));
-        }
-        Collections.sort(list, (a1, a2)-> a1.start - a2.start);
-        Stack<Assignment> stk = new Stack<>();
-        for (int pos=0;pos<list.size();pos++){
-            System.out.println(pos +"," + list.size());
-            while (!stk.isEmpty() && pos<list.size()){
-                Assignment a1 = stk.peek();
-                int currentTime = a1.start;
-                System.out.println(a1.name);
-                if (a1.start + a1.playTime <=list.get(pos).start){
-                    answer.add(a1.name);
-                    stk.pop();
-                } else {
-                    a1.playTime = list.get(pos+1).start - a1.start;
-                    break;
+            int now = convertToMinutes(plans[i][1]);
+            int minutes = Integer.parseInt(plan[2]);
+            
+            int nextStartTime=0;
+            if (i == plans.length-1){
+                nextStartTime = Integer.MAX_VALUE;
+            } else {
+                nextStartTime = convertToMinutes(plans[i+1][1]);                
+            }
+            
+            int endTime = now + minutes;
+            if (endTime > nextStartTime){
+                stk.add(new int[]{i, endTime - nextStartTime}); // 잠시 멈춘 과제
+            } else {
+                int remain = nextStartTime - endTime;
+                list.add(plans[i][0]);
+                if (endTime == nextStartTime){
+                    continue;
+                } else { // 시간 남으면 가장 최근에 멈춘 과제 수행하기
+                    while (true){
+                        if (stk.isEmpty() || remain<=0){
+                            break;
+                        }
+                        int[] recent = stk.pop();
+                        if (recent[1] > remain){
+                            stk.push(new int[]{recent[0], recent[1]-remain});
+                            break;
+                        } else {
+                            list.add(plans[recent[0]][0]);
+                            remain -= recent[1];
+                        }    
+                    }
                 }
             }
-            stk.push(list.get(pos));
         }
-        return answer.toArray(new String[0]);
+        
+        return list.toArray(new String[0]);
+    }
+    private int convertToMinutes(String time){
+        String[] splitedTime = time.split(":");
+        int h = Integer.parseInt(splitedTime[0]);
+        int m = Integer.parseInt(splitedTime[1]);
+        return h * 60 + m;
     }
 }
