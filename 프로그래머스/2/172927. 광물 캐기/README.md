0843570bd928aba867cdb7d67a9280386dd0209c
Mint <78211281+Starlight258@users.noreply.github.com>
2024-09-26 15:38:46 +0900

    [level 2] Title: 광물 캐기, Time: 1.60 ms, Memory: 92.5 MB -BaekjoonHub

--- 프로그래머스/2/172927. 광물 캐기/README.md
+++ 프로그래머스/2/172927. 광물 캐기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 광물 캐기 - 172927 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/172927) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/172927#) 
 
 ### 성능 요약
 
-메모리: 70.1 MB, 시간: 0.49 ms
+메모리: 92.5 MB, 시간: 1.60 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 03일 10:50:57
+2024년 09월 26일 15:38:43
 
 ### 문제 설명
 
--- 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
+++ 프로그래머스/2/172927. 광물 캐기/광물 캐기.java
@@ -1,55 +1,62 @@
 import java.util.*;
+
 class Solution {
+    int[][] fatigue = {
+        {1,1,1},
+        {5,1,1},
+        {25,5,1}
+    };
     public int solution(int[] picks, String[] minerals) {
-        int[][] fatigue = {
-            {1,1,1},
-            {5,1,1},
-            {25,5,1}
-        };
-        int pickNum = 0;
-        for(int i=0;i<picks.length;i++){
-            pickNum += picks[i];
+        int answer = 0;
+        List<int[]> list = new ArrayList<>();
+        int totalPicks = 0;
+        for (int pick:picks){
+            totalPicks += pick;
         }
-        // 1. 5개씩 쪼개기
-        int n = Math.min(pickNum*5, minerals.length);
-        int[][] mineralDivision = new int[n/5+1][3];
-        for (int i=0;i<n;i++){
-            if (minerals[i].equals("diamond")){
-                mineralDivision[i/5][0]++;
-            }
-            if (minerals[i].equals("iron")){
-                mineralDivision[i/5][1]++;
-            }
-            if (minerals[i].equals("stone")){
-                mineralDivision[i/5][2]++;
-            }
-        }
-        // 2. 정렬하기
-        Arrays.sort(mineralDivision, new Comparator<int[]>(){
-            public int compare(int[] o1, int[] o2){
-                if (o1[0]==o2[0]){
-                    if (o1[1]==o2[1]){
-                        return o2[2] - o1[2];
-                    }
-                    return o2[1]-o1[1];
+        for (int i=0;i<minerals.length/5+1 && i<totalPicks;i++){
+            int dia = 0, iron = 0, stone = 0;
+            for (int j=0;j<5 && i*5+j<minerals.length;j++){
+                switch(minerals[i*5+j]){
+                    case "diamond":
+                        dia++;
+                        break;
+                    case "iron":
+                        iron++;
+                        break;
+                    case "stone":
+                        stone++;
+                        break;
                 }
-                return o2[0] - o1[0];
+            }
+            list.add(new int[]{dia, iron, stone});
+        }
+        // 리스트 정렬하기
+        Collections.sort(list, new Comparator<int[]>(){
+            public int compare(int[] a, int[] b){
+                if (a[0] == b[0]){
+                    if (a[1] == b[1]){
+                        return Integer.compare(b[2], a[2]);
+                    }
+                    return Integer.compare(b[1], a[1]);
+                }
+                return Integer.compare(b[0], a[0]);
             }
         });
         
-        //3. 피로도 구하기
-        int totalFatigue = 0;
+        // 곡괭이로 깨뜨리기
         int pos = 0;
         for (int i=0;i<picks.length;i++){
-            while (picks[i]>0 && pos<n/5+1){
-                totalFatigue += fatigue[i][0] * mineralDivision[pos][0];
-                totalFatigue += fatigue[i][1] * mineralDivision[pos][1];
-                totalFatigue += fatigue[i][2] * mineralDivision[pos][2];
-                picks[i]--;
-                pos++;
-            }
+            int pick = picks[i];
+            while (pick>0){
+                if (pos>=list.size()) break;
+                answer += fatigue[i][0] * list.get(pos)[0];
+                answer += fatigue[i][1] * list.get(pos)[1];
+                answer += fatigue[i][2] * list.get(pos)[2];
+                pos++; 
+                pick--;
+            }  
         }
-        
-        return totalFatigue;
+    
+        return answer;
     }
 }
