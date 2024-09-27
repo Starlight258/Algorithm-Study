daf1b06020b9b19dd8e8d9c4880653a4bd9aec65
Mint <78211281+Starlight258@users.noreply.github.com>
2024-09-27 13:23:24 +0900

    [level 2] Title: 리코쳇 로봇, Time: 3.84 ms, Memory: 73.9 MB -BaekjoonHub

--- 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
+++ 프로그래머스/2/169199. 리코쳇 로봇/리코쳇 로봇.java
@@ -1,65 +1,52 @@
 import java.util.*;
 
 class Solution {
-    static char[][] graph;
-    static int[] dy = {-1,0,1,0};
-    static int[] dx = {0,1,0,-1};
-    class Node{
-        int y;
-        int x;
-        Node(int y, int x){
-            this.y = y;
-            this.x = x;
-        }
-    }
-    public int bfs(Node start, Node goal){
-        Queue<Node> q = new LinkedList<>();
-        q.add(start);
-        int[][] visited = new int[graph.length][graph[0].length];
-        visited[start.y][start.x] = 1;
-        // bfs 수행
+    private static int[] dy = {-1,0,1,0};
+    private static int[] dx = {0,1,0,-1};
+    
+    private static int bfs(String[] board, int[] start, int[] dest){
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{start[0], start[1], 1});
+        int n = board.length, m = board[0].length();
+        int[][] visited = new int[n][m];
+        visited[start[0]][start[1]] = 1;
+        
         while (!q.isEmpty()){
-            Node cur = q.poll();
+            int[] cur = q.poll();
+            int y = cur[0], x= cur[1];
             for (int i=0;i<4;i++){
-                int y = cur.y;
-                int x = cur.x;
-                if (graph[y][x] == 'G') break;
+                int ny = y, nx = x;
                 while (true){
-                    y += dy[i];
-                    x += dx[i];
-                    if (y<0||x<0||y>=graph.length||x>=graph[0].length) break;
-                    if (graph[y][x] == 'D') break;
+                    ny += dy[i];
+                    nx += dx[i];
+                    if (ny<0||nx<0||ny>=n|nx>=m || board[ny].charAt(nx) == 'D'){
+                        ny -= dy[i];
+                        nx -= dx[i];
+                        break;
+                    } 
                 }
-                y -= dy[i];
-                x -= dx[i];
-                if (visited[y][x]==0){
-                    visited[y][x] = visited[cur.y][cur.x]+1;
-                    q.add(new Node(y, x));
+                if (ny==dest[0] && nx == dest[1]){
+                    return cur[2];
                 }
-            }
+                if (visited[ny][nx]>0) continue;
+                visited[ny][nx] = 1;
+                q.add(new int[]{ny, nx, cur[2]+1});
+            } 
         }
-        return visited[goal.y][goal.x] - 1;
+        return -1;
     }
     public int solution(String[] board) {
         int answer = 0;
-        graph = new char[board.length][];
-        for (int i=0;i<board.length;i++){
-            graph[i] = board[i].toCharArray();
-        }
-        Node start = new Node(0, 0);
-        Node goal = new Node(0, 0);
+        int[] start = {}; int[] dest = {};
         for (int i=0;i<board.length;i++){
             for (int j=0;j<board[i].length();j++){
-                if (graph[i][j] == 'R'){
-                    start = new Node(i, j);
-                }
-                if (graph[i][j] == 'G'){
-                    goal = new Node(i, j);
+                if (board[i].charAt(j) == 'R'){
+                    start = new int[]{i, j};
+                } else if (board[i].charAt(j) == 'G'){
+                    dest = new int[]{i, j};
                 }
             }
         }
-        //1. bfs 수행
-        answer = bfs(start, goal);
-        return answer;
+        return bfs(board, start, dest);
     }
 }
