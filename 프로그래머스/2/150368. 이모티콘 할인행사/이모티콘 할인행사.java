d57735c6aa544a0f70381eae40bb409821141fe6
Mint <78211281+Starlight258@users.noreply.github.com>
2024-10-01 14:53:41 +0900

    [level 2] Title: 이모티콘 할인행사, Time: 40.82 ms, Memory: 77.4 MB -BaekjoonHub

--- 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.java
+++ 프로그래머스/2/150368. 이모티콘 할인행사/이모티콘 할인행사.java
@@ -1,45 +1,50 @@
 import java.util.*;
 
 class Solution {
-    private int[] rate = {90, 80, 70, 60};
-    private int totalSub = 0;
-    private int totalCost = 0;
+    private static final int[] DISCOUNT_RATES = {10, 20, 30, 40};
+    private int maxSubscribers = 0;
+    private int maxRevenue = 0;
     
-    private void dfs(int depth, int[] cost, int[][] users, int[] emoticons){
+    public int[] solution(int[][] users, int[] emoticons) {
+        dfs(0, new int[emoticons.length], users, emoticons);
+        return new int[]{maxSubscribers, maxRevenue};
+    }
+    
+    private void dfs(int depth, int[] discounts, int[][] users, int[] emoticons){
         if (depth == emoticons.length){
-            int tsub = 0;
-            int tcost = 0;
-           for (int i=0;i<cost.length;i++){
-                if (cost[i] >= users[i][1]){
-                    tsub++;
-                } else tcost += cost[i];
-            } 
-            if (totalSub <= tsub){
-                if (totalSub==tsub && totalCost >= tcost){
-                } else {
-                    totalSub = tsub;
-                    totalCost = tcost;
-                }
-            }
+            calculateResult(discounts, users, emoticons);
             return;
         }
-        for (int i=0;i<rate.length;i++){
-            int[] prev = new int[users.length];
-            for (int j=0;j<users.length;j++){
-                prev[j] = cost[j];
-                if (users[j][0] <= (100-rate[i])){
-                    cost[j] += emoticons[depth] * ((double)rate[i]/100);  
-                }
-            }
-            dfs(depth+1, cost, users, emoticons);
-             for (int j=0;j<users.length;j++){
-                cost[j] = prev[j];
-             }
+        
+        for (int rate : DISCOUNT_RATES){
+            discounts[depth] = rate;
+            dfs(depth+1, discounts, users, emoticons);
         }
     }
     
-    public int[] solution(int[][] users, int[] emoticons) {
-        dfs(0, new int[users.length], users, emoticons);
-        return new int[]{totalSub, totalCost};
+    private void calculateResult(int[] discounts, int[][] users, int[] emoticons){
+        int subscribers = 0;
+        int revenue = 0;
+        
+        for (int[] user:users){
+            int userMinDiscount = user[0];
+            int userMaxCost = user[1];
+            int totalCost = 0;
+            
+            for (int i=0;i<emoticons.length;i++){
+                if (discounts[i]>= userMinDiscount){
+                    totalCost += emoticons[i] * (100-discounts[i])/100;
+                }
+            }
+            if (totalCost >= userMaxCost){
+                subscribers++;
+            } else {
+                revenue += totalCost;
+            }
+        }
+        if (subscribers > maxSubscribers || (subscribers==maxSubscribers && revenue >= maxRevenue)){
+            maxSubscribers = subscribers;
+            maxRevenue = revenue;
+        }
     }
 }
