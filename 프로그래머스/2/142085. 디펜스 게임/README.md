6f515fce45d7b3990cbdbfc99a52c45272cc1de7
Mint <78211281+Starlight258@users.noreply.github.com>
2024-10-02 14:49:18 +0900

    [level 2] Title: 디펜스 게임, Time: 58.65 ms, Memory: 126 MB -BaekjoonHub

--- 프로그래머스/2/142085. 디펜스 게임/README.md
+++ 프로그래머스/2/142085. 디펜스 게임/README.md
@@ -1,10 +1,10 @@
 # [level 2] 디펜스 게임 - 142085 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/142085#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/142085) 
 
 ### 성능 요약
 
-메모리: 69.3 MB, 시간: 1.75 ms
+메모리: 126 MB, 시간: 58.65 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 06일 00:30:46
+2024년 10월 02일 14:49:16
 
 ### 문제 설명
 
--- 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
+++ 프로그래머스/2/142085. 디펜스 게임/디펜스 게임.java
@@ -1,25 +1,29 @@
 import java.util.*;
 class Solution {
     public int solution(int n, int k, int[] enemy) {
+        int answer = 0;
+        if (enemy.length<=k) return enemy.length;
         
-        PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
-        int sum = 0;        
-        
+        PriorityQueue<Integer> pq = new PriorityQueue<>((a,b)-> Integer.compare(a, b));
+        int death = n;
         for (int i=0;i<enemy.length;i++){
             if (i<k){
                 pq.offer(enemy[i]);
+                answer++;
                 continue;
             }
-
-            if (enemy[i]>pq.peek()){
-                sum += pq.poll();
-                pq.offer(enemy[i]);                
-            } else sum += enemy[i];
-            
-            if (sum > n){
+            if (!pq.isEmpty() && pq.peek() < enemy[i]){
+                death-= pq.poll();
+                pq.offer(enemy[i]);
+            } else {
+                death-= enemy[i];
+            }
+            if (death==0) return i+1;
+            if (death<0){
                 return i;
             }
+            answer++;
         }
-        return enemy.length;
+        return answer;
     }
 }
