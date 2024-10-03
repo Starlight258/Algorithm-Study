023fc085cc668fa77b6e6867567fd751e289125f
Mint <78211281+Starlight258@users.noreply.github.com>
2024-10-03 12:15:50 +0900

    [level 2] Title: 테이블 해시 함수, Time: 11.39 ms, Memory: 154 MB -BaekjoonHub

--- 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.java
+++ 프로그래머스/2/147354. 테이블 해시 함수/테이블 해시 함수.java
@@ -1,21 +1,28 @@
 import java.util.*;
+
 class Solution {
     public int solution(int[][] data, int col, int row_begin, int row_end) {
         int answer = 0;
-        
-        Arrays.sort(data, (d1, d2) -> {
-            if (d1[col-1] == d2[col-1]){
-                return d2[0] - d1[0];
+        row_begin--; row_end--;
+        // 정렬
+        Arrays.sort(data, new Comparator<int[]>(){
+            public int compare(int[] a, int[] b){
+                if (a[col-1] == b[col-1]){
+                    return Integer.compare(b[0], a[0]);
+                }
+                return Integer.compare(a[col-1], b[col-1]);
             }
-            return d1[col-1] - d2[col-1];
         });
-                
-        for (int i=row_begin-1;i<row_end;i++){
-            int sum = 0;
-            for (int j=0;j<data[i].length;j++){
-                sum += data[i][j] % (i+1);
+  
+        // row_begin~row_end에 대해 각 Si 구하기
+        int[] s = new int[data.length];
+        for (int r=row_begin;r<=row_end;r++){
+            int si = 0;
+            for (int idx=0;idx<data[row_begin].length;idx++){
+                si += data[r][idx] % (r+1);
             }
-            answer ^= sum;
+            s[r] = si;
+            answer ^= si;
         }
         
         return answer;
