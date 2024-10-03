fddd661a2c09703b5e8a6bb3bb3436ee6b9387a9
Mint <78211281+Starlight258@users.noreply.github.com>
2024-10-03 12:37:22 +0900

    [level 2] Title: 귤 고르기, Time: 126.86 ms, Memory: 92.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/138476. 귤 고르기/귤 고르기.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int k, int[] tangerine) {
+        int answer = 0;
+        // 종류별 개수 세기
+        Map<Integer, Integer> mp = new HashMap<>();
+        for (int t:tangerine){
+            mp.put(t, mp.getOrDefault(t, 0) + 1);
+        }
+        // 개수별 내림차순 정렬
+        List<Map.Entry<Integer, Integer>> entryList = new ArrayList<>(mp.entrySet());
+        Collections.sort(entryList, (a, b) -> b.getValue().compareTo(a.getValue()));
+        // 귤 담기
+        for (Map.Entry<Integer, Integer> e : entryList){
+            k -= e.getValue();
+            answer++;
+            if (k<=0) break;
+        }
+
+        return answer;
+    }
+}
