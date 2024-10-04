0182ddbe2bb24aea5916d0e1a0e3e7301cb3df1c
Mint <78211281+Starlight258@users.noreply.github.com>
2024-10-04 13:56:50 +0900

    [level 2] Title: 롤케이크 자르기, Time: 22.32 ms, Memory: 138 MB -BaekjoonHub

--- 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
+++ 프로그래머스/2/132265. 롤케이크 자르기/롤케이크 자르기.java
@@ -1,47 +1,31 @@
-import java.util.*;
-
 class Solution {
-    public int solution(int[] toppings) {
+    public int solution(int[] topping) {
         int answer = 0;
-        int[] sortAsc = new int[toppings.length];
-        Map<Integer, Integer> toppingAsc = new HashMap<>();
-        int[] sortDesc = new int[toppings.length];
-        Map<Integer, Integer> toppingDesc = new HashMap<>();
-        // 오름차순
-        toppingAsc.put(toppings[0], 1);
-        sortAsc[0] = 1;
-        for (int i=1;i<toppings.length;i++){
-            if (toppingAsc.getOrDefault(toppings[i], 0) == 0){
-                toppingAsc.put(toppings[i], 1);
-                sortAsc[i] = sortAsc[i-1]+1;
-            } else {
-                sortAsc[i] = sortAsc[i-1];
-            }
+        // 투포인터 사용하기
+        int[] left = new int[100001], right = new int[100001];
+        
+        // 맨 처음에는 오른쪽에 모든 토핑 두기
+        for (int i : topping){
+            right[i]++;
         }
-        // 내림차순
-        toppingDesc.put(toppings[toppings.length-1], 1);
-        sortDesc[toppings.length-1] = 1;
-        for (int i=toppings.length-2;i>=0;i--){
-            if (toppingDesc.getOrDefault(toppings[i], 0) == 0){
-                toppingDesc.put(toppings[i], 1);
-                sortDesc[i] = sortDesc[i+1]+1;
-            } else {
-                sortDesc[i] = sortDesc[i+1];
-            }
+        // 전체 토핑 수 계산하기
+        int ls = 0, rs = 0;
+        for (int r: right){
+            rs += r== 0 ? 0 : 1;
         }
-        // 개수 세기
-        boolean isContinuous = false;
-        for (int i=1;i<toppings.length;i++){
-            if (sortAsc[i] - sortDesc[i] <= 1){ // 흐름이 역전될때
-                if (sortAsc[i-1] == sortDesc[i]){
-                    answer++;
-                }
-            } 
+        // 토핑 왼쪽으로 옮기기
+        for (int i: topping){
+            // 오른쪽에서 토핑 하나 제거
+            right[i]--;
+            if (right[i]==0) rs--;
+            if (left[i]==0) ls++;
+            left[i]++;
+            if (ls == rs) answer++;
+            if (ls>rs) break;
         }
         return answer;
     }
 }
 
-// 토핑 종류를 공평하게 분배하기
-// 순서O, 토핑 종류가 같도록 나누기
-// 0 ~ i-1번째까지의 토핑수 = i번째부터 끝까지의 토핑수
+// left[i] = 왼쪽에 i 종류의 토핑 수
+// right[i] = 오른쪽에 i 종류의 토핑 수
