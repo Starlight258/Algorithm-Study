7888e09792215206df6f281643e1d1970995ec3c
Mint <78211281+Starlight258@users.noreply.github.com>
2024-10-04 12:50:24 +0900

    [level 2] Title: 점 찍기, Time: 16.28 ms, Memory: 85 MB -BaekjoonHub

--- 프로그래머스/2/140107. 점 찍기/점 찍기.java
+++ 프로그래머스/2/140107. 점 찍기/점 찍기.java
@@ -3,16 +3,12 @@ class Solution {
         long answer = 0;
         // d^2 / k^2
         long sum = (long)((Math.pow(d, 2) / Math.pow(k, 2)));
-        int maxAB = (int) Math.sqrt(sum);
-        for (int a=0;a<=maxAB;a++){
+        int maxA = (int) Math.sqrt(sum);
+        for (int a=0;a<=maxA;a++){
             int maxB = (int) Math.sqrt(sum - Math.pow(a, 2));
             answer += maxB+1;
         }
         return answer;
-    }
-    private boolean checkDistance(int x, int y, long sum){
-        long distance = (long)(Math.pow(x, 2)+ Math.pow(y, 2));
-        return distance <= sum;
     }
 }
 
@@ -23,4 +19,4 @@ class Solution {
 // 거리가 d를 넘는 위치는 X
 // 점이 총 몇개 찍히는지?
 // 최대 k : d/a, d/b , a^2+b^2=d
-// 모든 점에 대해 반복하면서 거리 구하고 개수 세기
+// a를 0부터 증가시키면서 b의 최대값 구하기
