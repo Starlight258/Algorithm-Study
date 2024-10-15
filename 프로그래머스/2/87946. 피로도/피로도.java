debfabde55c826fffa0a10af9a8b7179d37f73f1
Mint <78211281+Starlight258@users.noreply.github.com>
2024-10-15 11:04:20 +0900

    [level 2] Title: 피로도, Time: 3.43 ms, Memory: 70.3 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,23 +1,32 @@
+import java.util.*;
+
 class Solution {
-    int answer = 0;
-    boolean[] visited;
-    void play(int depth, int fatigue, int[][] dungeons){
-        answer = Math.max(answer, depth);
+    private int n;
+    private boolean[] visited;
+    private int answer = 0;
+    public int solution(int k, int[][] dungeons) {
+        n = dungeons.length;
+        visited = new boolean[n];
+        dfs(0, 0, k, dungeons);
+        
+        return answer;
+    }
+    private void dfs(int index, int count, int fatigue, int[][] dungeons){
+        answer = Math.max(answer, count);
+        
         for (int i=0;i<dungeons.length;i++){
-            if (!visited[i]){
+            if (visited[i]) continue;
+            if (fatigue >= dungeons[i][0]){
                 visited[i] = true;
-                if (dungeons[i][0] <= fatigue){
-                    play(depth+1, fatigue - dungeons[i][1], dungeons);                    
-                }
+                dfs(i, count+1, fatigue-dungeons[i][1], dungeons);
                 visited[i] = false;
             }
         }
     }
-    public int solution(int k, int[][] dungeons) {
-        visited = new boolean[dungeons.length];
-   
-        play(0, k, dungeons);
-        
-        return answer;
-    }
 }
+
+// XX 게임 - 피로도 시스템
+// 일정 피로도를 사용해서 던전 탐험
+// 최소 필요 피로도, 소모 피로도
+// 탐험할 수 있는 최대 던전 수 구하기
+// dp가 아닌 이유 : 순서가 정해지지 않았기 때문 -> 모든 순서 고려 : 완전탐색
