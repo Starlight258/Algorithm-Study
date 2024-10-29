d2918feb1dc1eb461103555bfbcf84538c52790b
Mint <78211281+Starlight258@users.noreply.github.com>
2024-10-29 12:37:40 +0900

    [level 2] Title: 모음 사전, Time: 10.33 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,27 +1,27 @@
 import java.util.*;
 
 class Solution {
-    static List<String> list;
-    static String [] words = {"A", "E", "I", "O", "U"};
-    public int solution(String word) {
-        int answer = 0;
-        list = new ArrayList<>();
-        dfs("", 0);
-        int size = list.size();
-        for (int i = 0; i < size; i++) {
-            if (list.get(i).equals(word)) {
-                answer = i;
-                break;
-            }
+    private final Character[] alphabet = {'A', 'E', 'I', 'O', 'U'};
+    private int count = 0;
+    private boolean isTerminated = false;
+    
+    private void dfs(StringBuilder sb, String word){
+        if (sb.length()>5) return;
+        if (isTerminated) return;
+        if (sb.toString().equals(word)){
+            isTerminated = true;
+            return;
         }
-        return answer;
+        count++;
+        for (int i=0;i<alphabet.length;i++){
+            sb.append(alphabet[i]);
+            dfs(sb, word);
+            sb.deleteCharAt(sb.length()-1);
+        }
     }
-
-    static void dfs(String str, int len) {
-        list.add(str);
-        if (len == 5) return;
-        for (int i = 0; i < 5; i++) {
-            dfs(str + words[i], len + 1);
-        }
+    
+    public int solution(String word) {
+        dfs(new StringBuilder(), word);
+        return count;
     }
 }
