472b4e0794bc034eb3fd553f5c2acc39db6a17cb
Mint <78211281+Starlight258@users.noreply.github.com>
2024-10-30 12:15:37 +0900

    [level 2] Title: 이진 변환 반복하기, Time: 40.64 ms, Memory: 88.4 MB -BaekjoonHub

--- 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
+++ 프로그래머스/2/70129. 이진 변환 반복하기/이진 변환 반복하기.java
@@ -2,16 +2,14 @@ class Solution {
     public int[] solution(String s) {
         int round = 0;
         int totalCount = 0;
+        int size = 0;
+        int count = 0;
         while (!s.equals("1")){
             round++;
-            int count = 0;
-            //1. 0의 개수 세기
-            for (char c:s.toCharArray()){
-                if (c=='0') count++;
-            }
-            totalCount += count;
-            //2. 1의 개수로 이진수 만들기
-            s = Integer.toBinaryString(s.length()-count);
+            size = s.length();
+            s = s.replaceAll("0", "");
+            totalCount += size-s.length();
+            s = Integer.toBinaryString(s.length());
         }
         return new int[]{round, totalCount};
     }
