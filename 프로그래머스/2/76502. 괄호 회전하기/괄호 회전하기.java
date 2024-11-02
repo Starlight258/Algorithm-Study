3ed3a45d60474e638e7023e4d7d719e1960719ff
Mint <78211281+Starlight258@users.noreply.github.com>
2024-11-02 23:54:35 +0900

    [level 2] Title: 괄호 회전하기, Time: 118.26 ms, Memory: 94.4 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -1,29 +1,31 @@
-import java.util.*;
-class Solution {
-    public int solution(String s) {
-        // 1. 가능한 모든 문자열 구하기
+import java.util.Stack;
+
+public class Solution {
+    static public int solution(String s) {
         int answer = 0;
-        for (int i=0;i<s.length();i++){
-            if (check(s)) answer++;
-            s = s.substring(1) + s.charAt(0);
+
+        for (int i = 0; i < s.length(); i++) {
+            Stack<Character> stack = new Stack<>();
+            String str = s.substring(i, s.length()) + s.substring(0, i);
+            for (int j = 0; j < str.length(); j++) {
+                char c = str.charAt(j);
+                if (stack.isEmpty()) {
+                    stack.push(c);
+                } else if (c == ')' && stack.peek() == '(') {
+                    stack.pop();
+                } else if (c == '}' && stack.peek() == '{') {
+                    stack.pop();
+                } else if (c == ']' && stack.peek() == '[') {
+                    stack.pop();
+                } else {
+                    stack.push(c);
+                }
+            }
+            if (stack.isEmpty()) {
+                answer++;
+            }
         }
+
         return answer;
-    }
-    public boolean check(String s){
-        int count = 0;
-        Stack<Character> stk = new Stack<>();
-        for (char c:s.toCharArray()){
-            if (c=='(' || c=='{'||c=='[') stk.push(c);
-            else if (!stk.isEmpty() && (checkOtherSide(c) == stk.peek())){
-                stk.pop();
-            } else return false;
-        }
-        if (!stk.isEmpty()) return false;
-        return true;
-    }
-    char checkOtherSide(char c){
-        if (c==']') return '[';
-        else if (c=='}') return '{';
-        else return '(';
     }
 }
