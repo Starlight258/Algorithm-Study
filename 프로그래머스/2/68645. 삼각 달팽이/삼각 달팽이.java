a8aa9c9aae14350a23790fa6ed296cbcf4a585ed
Mint <78211281+Starlight258@users.noreply.github.com>
2024-11-14 02:33:21 +0900

    [level 2] Title: 삼각 달팽이, Time: 17.78 ms, Memory: 139 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -1,42 +1,33 @@
-import java.util.*;
 class Solution {
     public int[] solution(int n) {
-        ArrayList<Integer> answer = new ArrayList<Integer>();
-        int[][] graph = new int[n][n];
-        int num = 0;
-        int nextY = 0;
-        int nextX = 0;
-        while (true){
-            // 1. 아래로 내려가기
-            while (nextY<n && graph[nextY][nextX]==0){
-                graph[nextY][nextX] = ++num;
-                nextY++;
-            }
-            nextY--; nextX++;
-            
-            // 2. 오른쪽으로 쭉 가기
-            while (nextX<n && graph[nextY][nextX]==0){
-                graph[nextY][nextX] = ++num;
-                nextX++;
-            }
-            nextY--; nextX-=2;
-            
-            // 3. 대각선으로 올라가기
-            while (nextY>=0 && nextX>=0 && graph[nextY][nextX]==0){
-                graph[nextY][nextX] = ++num;
-                nextY--; nextX--;
-            }
-            nextY+=2; nextX++;
-            
-            if (nextY>=n||nextX>=n||nextY<0||nextX<0) break;
-            if (graph[nextY][nextX]>0) break;
-        }
-        // 정답 출력
-        for (int i=0;i<n;i++){
-            for (int j=0;j<=i;j++){
-                answer.add(graph[i][j]);
+        int[] answer = new int[(n*(n+1))/2];
+        int[][] matrix = new int[n][n];
+
+        int x = -1, y = 0;
+        int num = 1;
+
+        for (int i = 0; i < n; ++i) {
+            for (int j = i; j < n; ++j) {
+                if (i % 3 == 0) {
+                    ++x;
+                } else if (i % 3 == 1) {
+                    ++y;
+                } else if (i % 3 == 2) {
+                    --x;
+                    --y;
+                }
+                matrix[x][y] = num++;
             }
         }
-        return answer.stream().mapToInt(x->x).toArray();
+
+        int k = 0;
+        for(int i = 0; i < n; ++i) {
+            for(int j = 0; j < n; ++j) {
+                if(matrix[i][j] == 0) break;
+                answer[k++] = matrix[i][j];
+            }
+        }
+
+        return answer;
     }
 }
