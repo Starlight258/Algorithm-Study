8621959b91a3bf1a2fc28f740bc9c6cf8583995c
Mint <78211281+Starlight258@users.noreply.github.com>
2024-11-14 16:11:25 +0900

    [level 2] Title: 삼각 달팽이, Time: 48.76 ms, Memory: 141 MB -BaekjoonHub

--- 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
+++ 프로그래머스/2/68645. 삼각 달팽이/삼각 달팽이.java
@@ -1,33 +1,29 @@
+import java.util.*;
+
 class Solution {
     public int[] solution(int n) {
-        int[] answer = new int[(n*(n+1))/2];
-        int[][] matrix = new int[n][n];
-
-        int x = -1, y = 0;
-        int num = 1;
-
-        for (int i = 0; i < n; ++i) {
-            for (int j = i; j < n; ++j) {
-                if (i % 3 == 0) {
-                    ++x;
-                } else if (i % 3 == 1) {
-                    ++y;
-                } else if (i % 3 == 2) {
-                    --x;
-                    --y;
+        List<Integer> answers = new ArrayList<>();
+        int[][] squares = new int[n][n];
+        int num=1;
+        int x = 0, y = -1;
+        for (int i=0;i<n;i++){
+            for (int j=i;j<n;j++){
+                if (i%3==0){
+                    y++;
+                } else if (i%3==1){
+                    x++;
+                } else {
+                    y--; x--;
                 }
-                matrix[x][y] = num++;
+                squares[y][x] = num++;
             }
         }
-
-        int k = 0;
-        for(int i = 0; i < n; ++i) {
-            for(int j = 0; j < n; ++j) {
-                if(matrix[i][j] == 0) break;
-                answer[k++] = matrix[i][j];
+        for (int i=0;i<n;i++){
+            for (int j=0;j<n;j++){
+                if (squares[i][j]==0) break;
+                answers.add(squares[i][j]);
             }
         }
-
-        return answer;
+        return answers.stream().mapToInt(i->i).toArray();
     }
 }
