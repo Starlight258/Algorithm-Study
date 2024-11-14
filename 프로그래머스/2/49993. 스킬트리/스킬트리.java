e4e7da277e90d1ce55ea4b62d387ceff767d60d3
Mint <78211281+Starlight258@users.noreply.github.com>
2024-11-14 20:50:58 +0900

    [level 2] Title: 스킬트리, Time: 10.89 ms, Memory: 83.7 MB -BaekjoonHub

--- 프로그래머스/2/49993. 스킬트리/스킬트리.java
+++ 프로그래머스/2/49993. 스킬트리/스킬트리.java
@@ -1,27 +1,9 @@
-import java.util.*;
 class Solution {
     public int solution(String skill, String[] skill_trees) {
         int answer = 0;
-        //1. map에 저장
-        Map<Character, Integer> mp = new HashMap<>();
-        for (int i=0;i<skill.length();i++){
-            mp.put(skill.charAt(i), i);
-        }
-        //2. 순서 확인
         for (String tree:skill_trees){
-            int pos=0;
-            boolean flag = false;
-            for (Character c:tree.toCharArray()){
-                int e = mp.getOrDefault(c, -1);
-                if (e != -1){
-                    if (pos== e) pos++;
-                    else {
-                        flag = true;
-                        break;
-                    }
-                }
-            }
-            if (!flag) answer++;
+            String concat = tree.replaceAll("[^"+skill+"]", "");
+            if (skill.startsWith(concat)) answer++;
         }
         return answer;
     }
