799f1411422a4a2826ed48d209b30575bdef7045
Mint <78211281+Starlight258@users.noreply.github.com>
2024-11-14 20:00:20 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 18.71 ms, Memory: 112 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
@@ -1,31 +1,39 @@
 class Solution {
-    static int[] answer = new int[2];
     
-    public int[] solution(int[][] arr) {
-        checkArea(arr, arr.length, 0, 0);
-        return answer;
-    }
-    
-    void checkArea(int[][] arr, int width, int y, int x){
-        if (isMatched(arr, width, y, x)){
-            answer[arr[y][x]]++;
-            return;
-        }
-        checkArea(arr, width/2, y, x);
-        checkArea(arr, width/2, y+width/2, x);
-        checkArea(arr, width/2, y, x+width/2);
-        checkArea(arr, width/2, y+width/2, x+width/2);
-    }
-    
-    boolean isMatched(int[][] arr, int width, int y, int x){
-        int e = arr[y][x];
-        for (int i=y;i<y+width;i++){
-            for (int j=x;j<x+width;j++){
-                if (e!=arr[i][j]){
-                    return false;
+    private void check(int[][] arr, int n, int[] answer, int startY, int startX){
+        boolean isSame = true;
+        int sum = 0;
+        int val = arr[startY][startX];
+        for (int i=0;i<n;i++){
+            for (int j=0;j<n;j++){
+                if (arr[startY+i][startX+j] != val) {
+                    isSame = false;
+                    break;
                 }
+                sum += val;
+            }
+            if (!isSame){
+                break;
             }
         }
-        return true;
+        if (isSame){
+            if(sum==0) answer[0]++;
+            else answer[1]++;
+            return;
+        }
+        
+        n /=2;
+        check(arr, n, answer, startY, startX);
+        check(arr, n, answer, startY, startX+n);
+        check(arr, n, answer, startY+n, startX);
+        check(arr, n, answer, startY+n, startX+n);
+    }
+    
+    public int[] solution(int[][] arr) {
+        int[] answer = new int[2];
+        int n = arr.length;
+        int[][] counts = new int[n][n];
+        check(arr, n, answer, 0, 0);
+        return answer;
     }
 }
