8b2a868a0cb54779f76d0403788f03e9ccb70c0d
Mint <78211281+Starlight258@users.noreply.github.com>
2024-11-15 17:08:18 +0900

    [level 2] Title: 괄호 변환, Time: 25.87 ms, Memory: 81.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/60058. 괄호 변환/괄호 변환.java
@@ -0,0 +1,61 @@
+import java.util.*;
+
+class Solution {
+    public String solution(String p) {
+        return steps(p);
+    }
+    
+    private String steps(String p){
+        if (p.isEmpty() || isCorrect(p)) return p;
+        String u = "", v = "";
+        for (int i=1;i<=p.length();i++){
+            u = p.substring(0, i);
+            v = p.substring(i);
+            if (isBalanced(u) && isBalanced(v)){
+                String result = steps(v);
+                if (isCorrect(u)){
+                    return u + result;
+                } else {
+                    return "(" + result + ")" + trans(u);
+                }
+            }
+        }
+        return "";
+    }
+    
+    private String trans(String u){
+        u = u.substring(1, u.length()-1);
+        String result = "";
+        for (char c:u.toCharArray()){
+            if (c=='(') result += ')';
+            else result += '(';
+        }
+        return result;
+    }
+    
+    // 균형잡힌 괄호 문자열 체크함수
+    private boolean isBalanced(String p){
+        int leftCount = 0; int rightCount = 0;
+        for (char c:p.toCharArray()){
+            if (c == '(') leftCount++;
+            else rightCount++;
+        }
+        return leftCount == rightCount;
+    }
+    
+    // 올바른 괄호 문자열 체크함수
+    private boolean isCorrect(String p){
+        if (!isBalanced(p)) return false;
+        Stack<Character> stk = new Stack<>();
+        for (char pc:p.toCharArray()){
+            if (pc=='('){
+                stk.push(pc);
+            } else {
+                if (!stk.empty()){
+                    stk.pop();
+                } else return false;
+            }
+        }
+        return stk.isEmpty();
+    }
+}
