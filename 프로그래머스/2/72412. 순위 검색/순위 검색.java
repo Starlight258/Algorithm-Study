13a1bd574c45f7bd4c9eb35756e78d3cd7059180
Mint <78211281+Starlight258@users.noreply.github.com>
2024-11-18 13:39:34 +0900

    [level 2] Title: 순위 검색, Time: 1231.48 ms, Memory: 323 MB -BaekjoonHub

--- 프로그래머스/2/72412. 순위 검색/순위 검색.java
+++ 프로그래머스/2/72412. 순위 검색/순위 검색.java
@@ -1,67 +1,51 @@
 import java.util.*;
+
 class Solution {
-    public int[] solution(String[] info, String[] query) {
-        int[] answer = new int[query.length];
-        
-        // 해당 info에 대해 가능한 모든 조건에 점수 저장
-        Map<String, List<Integer>> infoMap = new HashMap<>();
-        for (int i=0;i<info.length;i++){
-            String[] split = info[i].split(" ");
-            String lang = split[0], part = split[1], career = split[2], food = split[3];
-            int score = Integer.parseInt(split[4]);
-            
-            for (int a=0;a<2;a++){
-                for (int b=0;b<2;b++){
-                    for (int c=0;c<2;c++){
-                        for (int d=0;d<2;d++){
-                            StringBuilder sb = new StringBuilder();
-                            sb.append(a == 0 ? "-" : lang);
-                            sb.append(b == 0 ? "-" : part);
-                            sb.append(c == 0 ? "-" : career);
-                            sb.append(d == 0 ? "-" : food);
-                            String key = sb.toString();
-                            infoMap.computeIfAbsent(key, k-> new ArrayList<>()).add(score);
-                         }
-                    }
-                }
-            }
+    public int[] solution(String[] infos, String[] queries) {
+        List<Integer> answers = new ArrayList<>();
+        Map<String, List<Integer>> map = new HashMap<>();
+        for (String info: infos){
+            String[] split = info.split(" ");
+            makeAllCases(0, "", split, map);
         }
-        
-        // 점수 정렬 (이진탐색을 위함)
-        for (List<Integer> list:infoMap.values()){
-            Collections.sort(list);
+         for (Map.Entry<String, List<Integer>> entry:map.entrySet()){
+            Collections.sort(entry.getValue());
         }
-        
-        // 쿼리 조건 저장
-        String[] querySplit = new String[query.length];
-        int[] queryScore = new int[query.length];
-        
-        for (int i=0;i<query.length;i++){            
-            String[] sq = query[i].split(" and ");
-            String key = String.join("", sq[0], sq[1], sq[2]);
-            String[] subCond = sq[3].split(" ");
-            key += subCond[0];
-            querySplit[i] = key;
-            queryScore[i] = Integer.parseInt(subCond[1]);
+        // score 개수구하기
+        for (String query:queries){
+            query = query.replaceAll(" and ", " ");
+            String[] split = query.split(" ");
+            String key = (split[0]+split[1]+split[2]+split[3]);
+            List<Integer> list = map.getOrDefault(key, new ArrayList<>());
+            int number = list.size() - findScores(list, Integer.parseInt(split[4]));
+            answers.add(number);
         }
-        
-        // 조건에 해당하는 점수 리스트에 대해 이진 탐색 수행
-        for (int i=0;i<query.length;i++){
-            List<Integer> list = infoMap.getOrDefault(querySplit[i], new ArrayList<>());
-            
-            int left = 0;
-            int right = list.size();
-            
-            while (left<right){
-                int mid = (left + right) / 2;
-                if (list.get(mid) >= queryScore[i]){
-                    right = mid;
-                } else left = mid + 1;
-            }
-            
-            answer[i] = list.size() - left;
-        }
-      
-        return answer;
+        return answers.stream().mapToInt(x->x).toArray();
     }
+    
+    private int findScores(List<Integer> scores, int score){
+         int left = 0, right = scores.size();
+        
+        while (left < right) {
+            int mid = (left + right) / 2;
+            if (scores.get(mid) >= score) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        
+        return left;
+    }
+    
+    private void makeAllCases(int depth, String key, String[] split, Map<String, List<Integer>> map){
+        if (depth==4){
+            map.computeIfAbsent(key, k -> new ArrayList<>()).add(Integer.parseInt(split[4]));
+            return;
+        }
+        
+        makeAllCases(depth+1, key + split[depth], split, map);
+        makeAllCases(depth+1, key+"-", split, map);
+    }
+    
 }
