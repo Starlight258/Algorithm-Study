520f1ed68a33dd4b167c90072ddc16ea6f52b97c
Mint <78211281+Starlight258@users.noreply.github.com>
2024-11-18 14:38:52 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 퍼즐 게임 챌린지, Time: 34.26 ms, Memory: 140 MB -BaekjoonHub

--- 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
+++ 프로그래머스/2/340212. ［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지/［PCCP 기출문제］ 2번 ／ 퍼즐 게임 챌린지.java
@@ -1,29 +1,34 @@
 class Solution {
     public int solution(int[] diffs, int[] times, long limit) {
         int answer = Integer.MAX_VALUE;
-        int left = 1, right = 100000; // level
-        while (left <= right){
-            int mid = left + (right-left)/2;
-            int prev = 0;
-            long result = 0;
-            for (int i=0;i<diffs.length;i++){
-                if (diffs[i]<=mid){
-                    result += times[i];
-                } else if (diffs[i]>mid){
-                    result += (diffs[i]-mid)*(times[i]+prev)+times[i];
-                }
-                
-                if (result>limit) break;
-                prev = times[i];
-            }
-            if (result <= limit){
-                answer = Math.min(answer, mid);
-                right = mid - 1;
-            } else {
-                left = mid + 1;
-            }
+        int[] prefix = new int[diffs.length+1];
+        for (int i=0;i<times.length;i++){
+            prefix[i+1] = prefix[i] + times[i]; 
         }
         
+        // 이진탐색
+        int left = 1; int right = 100_000;
+        while (left<=right){
+            int mid = (left + right)/2;
+            if (calculateTime(diffs, prefix, limit, mid)){
+                right = mid-1; // 최소시간 구하기
+                answer = Math.min(answer, mid);
+            } else left = mid+1;
+        }
         return answer;
+    }
+    
+    private boolean calculateTime(int[] diffs, int[] prefix, long limit, int level){
+        long total = 0;
+        for (int i=0;i<diffs.length;i++){
+            int diff = diffs[i];
+            if (diff<=level){
+                total += prefix[i+1] - prefix[i];
+            } else {
+                if (i==0) total += (diff-level) * prefix[i+1] + prefix[i+1];
+                else total += (diff-level) * (prefix[i+1]-prefix[i-1]) + (prefix[i+1]-prefix[i]);
+            }
+        }
+        return total <= limit;
     }
 }
