6d442667475dfd788982731235a1c1f0a6ffa52d
Mint <78211281+Starlight258@users.noreply.github.com>
2024-11-19 15:29:53 +0900

    [level 2] Title: 후보키, Time: 50.62 ms, Memory: 91.4 MB -BaekjoonHub

--- 프로그래머스/2/42890. 후보키/후보키.java
+++ 프로그래머스/2/42890. 후보키/후보키.java
@@ -1,71 +1,65 @@
 import java.util.*;
+
 class Solution {
-    
-    List<String> candi = new ArrayList<>();
-    
-    void dfs(boolean[] visited, int start, int depth, int limit, String[][] relation){
-        if (depth == limit){
-            // key 저장하기
-            List<Integer> list = new ArrayList<>();
-            String key = "";
-            for (int i=0;i<visited.length;i++){
-                if (visited[i]){
-                    key += String.valueOf(i); // 인덱스로 저장
-                    list.add(i);
+    public int solution(String[][] relations) {
+        int answer = 0;
+        // key 만들기
+        List<List<Integer>> keys = makeKeys(relations[0].length);
+
+        // 개수 세기
+        List<List<Integer>> realKeys = new ArrayList<>();
+        for (List<Integer> key:keys){
+            boolean isValid = true;
+            for (List<Integer> realKey:realKeys){
+                if (key.containsAll(realKey)){
+                    isValid = false;
+                    break;
                 }
             }
-            
-            // 유일성 검사하기
-            Map<String, Integer> map = new HashMap<>();
-            
-            for (int i=0;i<relation.length;i++){
-                String s = "";
-                for (Integer j:list){
-                    s += relation[i][j]; // key부분 가져오기
+            if (!isValid) continue;
+            Set<String> combination = new HashSet<>();
+            for (String[] relation:relations){
+                String temp = "";
+                for (Integer k:key){
+                    temp += relation[k];
                 }
-                
-                if (map.containsKey(s)){ // 중복될 경우 리턴
-                    return;
-                } else{
-                    map.put(s, 0);
-                }
+                combination.add(temp);
             }
-            
-            // 최소성 검사하기
-            for (String c:candi){
-                int count = 0;
-                for (int i=0;i<list.size();i++){
-                    String subKey = String.valueOf(list.get(i));
-                    if (c.contains(subKey)){
-                        count++;
-                    }
-                }
-                if (count==c.length()) return; //최소성 만족X
+            if (combination.size()==relations.length){
+                realKeys.add(key);
+                answer++;
             }
-            candi.add(key);
-            
+        }
+        return answer;
+    }
+    private List<List<Integer>> makeKeys(int n){
+        List<List<Integer>> keys = new ArrayList<>();
+        List<Integer> key = new ArrayList<>();
+        for (int i=0;i<n;i++){
+            key.add(i);
+        }
+        boolean visited[] = new boolean[n];
+        for (int i=0;i<key.size();i++){
+            combination(keys, key, visited, 0, 0, i+1);
+        }
+        
+        return keys;
+    }
+    private void combination(List<List<Integer>> keys, List<Integer> key, boolean[] visited, int start, int depth, int r){
+        if (depth==r){
+            List<Integer> temp = new ArrayList<>();
+            for (int i=0;i<key.size();i++){
+                if (visited[i]) temp.add(key.get(i));
+            }
+            keys.add(temp);
             return;
         }
-        
-        for (int i=start;i<visited.length;i++){
-            if (visited[i]) continue;
-            
-            visited[i] = true;
-            dfs(visited, i+1, depth+1, limit, relation); 
-            visited[i] = false;
+        for (int i=start;i<key.size();i++){
+            if (!visited[i]){
+                visited[i] = true;
+                combination(keys, key, visited, start+1, depth+1, r);
+                visited[i] = false;
+            }
         }
-    }
-    
-    public int solution(String[][] relation) {
-        int answer = 0;
-        
-        // 완전탐색 수행
-        for (int i=0;i<relation[0].length;i++){
-            boolean[] visited = new boolean[relation[0].length];
-            dfs(visited, 0, 0, i+1, relation);
-        }
-        
-        answer = candi.size();
-        return answer;
     }
 }
