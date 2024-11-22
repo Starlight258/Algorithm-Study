a1d82cd5a7047067ec8d1468b1f6e1e6b58e98b1
Mint <78211281+Starlight258@users.noreply.github.com>
2024-11-22 12:23:42 +0900

    [level 2] Title: [1차] 캐시, Time: 48.36 ms, Memory: 119 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.java
@@ -3,12 +3,12 @@ import java.util.*;
 class Solution {
     public int solution(int CACHESIZE, String[] CITIES) {
         int answer = 0;
-        Set<String> cities = new HashSet<>();
         LinkedList<String> used = new LinkedList<>();
         if (CACHESIZE==0) return CITIES.length * 5;
+        
         for (int i=0;i<CITIES.length;i++){
             String city = CITIES[i].toLowerCase();
-            if (cities.contains(city)){
+            if (used.contains(city)){
                 used.remove(city);
                 used.addLast(city);
                 answer += 1;
@@ -16,10 +16,8 @@ class Solution {
             }
             if (used.size()>=CACHESIZE){
                 String oldCity = used.get(0);
-                cities.remove(oldCity);
                 used.remove(oldCity);
             } 
-            cities.add(city);
             used.addLast(city);
             answer += 5;
         }
