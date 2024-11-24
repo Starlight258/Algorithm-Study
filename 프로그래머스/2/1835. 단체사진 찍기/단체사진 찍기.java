6962c49c635935b55a2589e418586255d524dd70
Mint <78211281+Starlight258@users.noreply.github.com>
2024-11-25 00:08:20 +0900

    [level 2] Title: 단체사진 찍기, Time: 1454.89 ms, Memory: 395 MB -BaekjoonHub

--- 프로그래머스/2/1835. 단체사진 찍기/단체사진 찍기.java
+++ 프로그래머스/2/1835. 단체사진 찍기/단체사진 찍기.java
@@ -1,57 +1,52 @@
 import java.util.*;
-class Solution {
-    static char [] friends;
-    static String [] dt;
-    static int answer;
-    static boolean [] visited;
-    public int solution(int n, String[] data) {
-        dt = data;
-        answer = 0;
-        friends = new char[]{'A', 'C', 'F', 'J', 'M', 'N', 'R', 'T'};
-        visited = new boolean[8];
 
-        dfs("",0);
+class Solution {
+    private final String FRIENDS = "ACFJMNRT";
+    private final List<String> cases = new ArrayList<>();
+    private int answer = 0;
+    private String[] dt;
+    private final boolean[] visited = new boolean[8];
+    
+    public int solution(int n, String[] datas) {
+        dt = datas;
+        dfs(0, "");
         return answer;
     }
-    static void dfs(String line, int depth)
-    {
-        if(depth == 8)
-        {
-            if(check(line)) answer++;
-            return;
-        }
-
-        for(int i = 0; i < 8; ++i)
-        {
-            if(!visited[i])
-            {
-                visited[i] = true;
-                dfs(line+friends[i],depth+1);
-                visited[i] = false;
-            }
-        }
-    }
-    static boolean check(String line)
-    {
-        for(String cond : dt)
-        {
-            int diff = (Math.abs(line.indexOf(cond.charAt(0)) - line.indexOf(cond.charAt(2))))-1;
-            char sign = cond.charAt(3);
-            int val = cond.charAt(4)-'0';
-
-            if(sign == '=')
-            {
-                if(diff != val) return false;
-            }
-            else if(sign == '>')
-            {
-                if(diff <= val) return false;
-            }
-            else
-            {
-                if(diff >= val) return false;
+    
+    private boolean check(String text){
+        for (String data : dt){
+            char person1 = data.charAt(0);
+            char person2 = data.charAt(2);
+            int diff = Math.abs(text.indexOf(person1) - text.indexOf(person2))-1;
+            int distance = data.charAt(4) - '0';
+            switch (data.charAt(3)){
+                case '=':
+                    if (diff!=distance) return false;
+                    break;
+                case '<':
+                    if (diff>=distance) return false;
+                    break;
+                case '>':
+                    if (diff<=distance) return false;
+                    break;
             }
         }
         return true;
+    }
+    
+    private void dfs(int depth, String temp){
+        if (depth == 8){
+            if (check(temp)){
+                answer++;
+            }
+            return;
+        }
+        for (int i=0;i<8;i++){
+            if (!visited[i]){
+                visited[i] = true;
+                dfs(depth+1, temp+FRIENDS.charAt(i));
+                visited[i] = false;
+            }
+        }
     }
 }
