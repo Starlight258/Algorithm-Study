12cb476e0f03811106f2a9bc3f32a57f48eeab90
Mint <78211281+Starlight258@users.noreply.github.com>
2024-11-26 18:05:13 +0900

    [Gold II] Title: 아맞다우산, Time: 108 ms, Memory: 14640 KB -BaekjoonHub

--- 백준/Gold/17244. 아맞다우산/아맞다우산.java
+++ 백준/Gold/17244. 아맞다우산/아맞다우산.java
@@ -13,23 +13,24 @@ public class Main {
     static final char START = 'S';
     static final char MUST_HAVE = 'X';
     static final char WALL = '#';
+    static final int[] dy = {-1, 0, 1, 0};
+    static final int[] dx = {0, 1, 0, -1};
+
+    static final ArrayList<Node>[] adjacent = new ArrayList[6];
+
     static int[][] map;
-
-    static final int[][] dxDy = {{-1, 0,}, {0, -1}, {1, 0}, {0, 1}};
-
-    static final ArrayList<Edge>[] adjacentList = new ArrayList[6];
+    static int min = Integer.MAX_VALUE;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
         StringTokenizer mapInfo = new StringTokenizer(br.readLine());
 
-        int number = 0;
+        int itemNumber = 0;
         int M = Integer.parseInt(mapInfo.nextToken());
         int N = Integer.parseInt(mapInfo.nextToken());
 
         for (int i = 0; i < 6; i++) {
-            adjacentList[i] = new ArrayList<>();
+            adjacent[i] = new ArrayList<>();
         }
         map = new int[N][M];
         for (int i = 0; i < N; i++) {
@@ -37,30 +38,28 @@ public class Main {
             for (int j = 0; j < M; j++) {
                 map[i][j] = input.charAt(j);
                 if (map[i][j] == MUST_HAVE) {
-                    map[i][j] = ++number;
+                    map[i][j] = ++itemNumber; // 물건 1~6까지 번호 지정
                 }
             }
         }
         initEdge(N, M);
-        solution(number, 0, 0, new boolean[6], 0);
+        solution(itemNumber, 0, 0, new boolean[6], 0);
         System.out.println(min);
     }
 
-    static int min = Integer.MAX_VALUE;
-
-    static void solution(int totalCount, int index, int length, boolean[] use, int to) {
-        if (totalCount == index) {
-            for (Edge edge : adjacentList[to]) {
-                if (edge.to == 6) {
+    static void solution(int totalCount, int depth, int length, boolean[] use, int to) {
+        if (depth == totalCount) {
+            for (Node edge : adjacent[to]) {
+                if (edge.to == 6) { // 출구 더하기
                     min = Math.min(length + edge.weight, min);
                     return;
                 }
             }
         }
-        for (Edge edge : adjacentList[to]) {
+        for (Node edge : adjacent[to]) { // 인접 위치로 이동(경유지 거치면서 거리 구하기)
             if (edge.to != 6 && !use[edge.to]) {
                 use[edge.to] = true;
-                solution(totalCount, index + 1, length + edge.weight, use, edge.to);
+                solution(totalCount, depth + 1, length + edge.weight, use, edge.to);
                 use[edge.to] = false;
             }
         }
@@ -69,40 +68,42 @@ public class Main {
     static void initEdge(int N, int M) {
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < M; j++) {
-                if (map[i][j] == START) {
+                if (map[i][j] == START) { // 시작점에서의 모든 거리
                     bfs(new Point(j, i), N, M, 0);
-                } else if (map[i][j] >= 1 && map[i][j] <= 5) {
+                } else if (map[i][j] >= 1 && map[i][j] <= 5) { // 경로 위치에서의 모든 거리
                     bfs(new Point(j, i), N, M, map[i][j]);
                 }
             }
         }
     }
 
-    static void bfs(Point startPoint, int N, int M, int start) {
-        Queue<Point> points = new LinkedList<>();
-        points.add(startPoint);
+    static void bfs(Point startPoint, int N, int M, int index) {
+        Queue<Point> queue = new LinkedList<>();
         boolean[][] visited = new boolean[N][M];
+        queue.add(startPoint);
         visited[startPoint.y][startPoint.x] = true;
         int length = 0;
-        while (!points.isEmpty()) {
-            int size = points.size();
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
             for (int i = 0; i < size; i++) {
-                Point temp = points.poll();
-                for (int[] move : dxDy) {
-                    int X = temp.x + move[0];
-                    int Y = temp.y + move[1];
-                    if (!visited[Y][X]) {
-                        if (map[Y][X] >= 1 && map[Y][X] <= 5) {
-                            visited[Y][X] = true;
-                            adjacentList[start].add(new Edge(map[Y][X], length + 1));
-                            points.add(new Point(X, Y));
-                        } else if (map[Y][X] == EXIT) {
-                            visited[Y][X] = true;
-                            adjacentList[start].add(new Edge(6, length + 1));
-                        } else if (map[Y][X] != WALL) {
-                            visited[Y][X] = true;
-                            points.add(new Point(X, Y));
-                        }
+                Point temp = queue.poll();
+                for (int d = 0; d < 4; d++) {
+                    int Y = temp.y + dy[d];
+                    int X = temp.x + dx[d];
+                    if (visited[Y][X]) {
+                        continue;
+                    }
+                    if (map[Y][X] >= 1 && map[Y][X] <= 5) { // 경유점
+                        visited[Y][X] = true;
+                        adjacent[index].add(new Node(map[Y][X], length + 1));
+                        queue.add(new Point(X, Y));
+                    } else if (map[Y][X] == EXIT) { // 나가는 위치
+                        visited[Y][X] = true;
+                        adjacent[index].add(new Node(6, length + 1));
+                    } else if (map[Y][X] != WALL) { // 벽
+                        visited[Y][X] = true;
+                        queue.add(new Point(X, Y));
                     }
                 }
             }
@@ -110,11 +111,11 @@ public class Main {
         }
     }
 
-    static class Edge {
+    static class Node {
         int to;
         int weight;
 
-        public Edge(int to, int weight) {
+        public Node(int to, int weight) {
             this.to = to;
             this.weight = weight;
         }
