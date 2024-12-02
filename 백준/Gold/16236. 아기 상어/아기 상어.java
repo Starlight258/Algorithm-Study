02934b06aa000b1aa947102f0c23cf1779893064
Mint <78211281+Starlight258@users.noreply.github.com>
2024-12-02 17:36:49 +0900

    [Gold III] Title: 아기 상어, Time: 112 ms, Memory: 14676 KB -BaekjoonHub

--- 백준/Gold/16236. 아기 상어/아기 상어.java
+++ 백준/Gold/16236. 아기 상어/아기 상어.java
@@ -5,79 +5,84 @@ import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        // 1. 입력받기
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
 
-        int[][] map = new int[n][n];
-        int sy = 0, sx = 0;
-        int[] cur = new int[2];
-        StringTokenizer st;
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < n; j++) {
+    private static int N;
+    private static int[][] map;
+    private static final int dy[] = {-1, 0, 1, 0};
+    private static final int dx[] = {0, 1, 0, -1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        map = new int[N][N];
+        int[] start = null;
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
                 if (map[i][j] == 9) {
-                    cur[0] = i;
-                    cur[1] = j;
+                    start = new int[]{i, j, 0};
                     map[i][j] = 0;
                 }
             }
         }
+        // simulation
+        System.out.println(simulation(start));
+    }
 
-        // 2. bfs 수행
-        int[] dy = {-1, 0, 1, 0};
-        int[] dx = {0, 1, 0, -1};
-        int size = 2;
-        int eat = 0;
-        int move = 0;
+    private static int simulation(int[] start) {
+        int sharkSize = 2;
+        int numberOfFishEaten = 0;
+        int time = 0;
 
         while (true) {
-            boolean[][] visited = new boolean[n][n];
-            PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) ->
-                    o1[2] != o2[2] ? Integer.compare(o1[2], o2[2])
-                            : (o1[0] != o2[0] ? Integer.compare(o1[0], o2[0])
-                                    : Integer.compare(o1[1], o2[1]))
-            );
+            boolean[][] visited = new boolean[N][N];
+            PriorityQueue<int[]> pq = new PriorityQueue<>((o1, o2) -> { // y, x, 시간
+                if (o1[2] != o2[2]) {
+                    return o1[2] - o2[2];
+                }
+                if (o1[0] != o2[0]) {
+                    return o1[0] - o2[0];
+                }
+                return o1[1] - o2[1];
+            });
 
-            pq.offer(new int[]{cur[0], cur[1], 0});
-            visited[cur[0]][cur[1]] = true;
-            boolean ck = false;
-
+            pq.offer(start);
+            visited[start[0]][start[1]] = true;
+            boolean isChecked = false;
             while (!pq.isEmpty()) {
-                cur = pq.poll();
-                int y = cur[0];
-                int x = cur[1];
-                int dist = cur[2];
-
-                if (map[y][x] != 0 && map[y][x] < size) {
+                start = pq.poll();
+                int y = start[0];
+                int x = start[1];
+                int distance = start[2];
+                if (map[y][x] != 0 && map[y][x] < sharkSize) {
+                    numberOfFishEaten++;
+                    time += distance;
+                    isChecked = true;
                     map[y][x] = 0;
-                    eat++;
-                    move += cur[2];
-                    ck = true;
+                    start = new int[]{y, x, 0};
                     break;
                 }
-
                 for (int i = 0; i < 4; i++) {
                     int ny = y + dy[i];
                     int nx = x + dx[i];
-                    if (ny < 0 || nx < 0 || ny >= n || nx >= n) {
+                    if (ny < 0 || nx < 0 || ny >= N || nx >= N || visited[ny][nx]) {
                         continue;
                     }
-                    if (!visited[ny][nx] && map[ny][nx] <= size) {
-                        pq.offer(new int[]{ny, nx, dist + 1});
+                    if (map[ny][nx] <= sharkSize) {
+                        pq.offer(new int[]{ny, nx, distance + 1});
                         visited[ny][nx] = true;
                     }
                 }
             }
-            if (!ck) break;
-            if (eat == size) {
-                size++;
-                eat = 0;
+            if (numberOfFishEaten == sharkSize) {
+                sharkSize++;
+                numberOfFishEaten = 0;
+            }
+            if (!isChecked) {
+                break;
             }
         }
-
-        System.out.println(move);
+        return time;
     }
 }
