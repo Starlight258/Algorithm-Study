335747ddba5c6758cbcc1da3f6ea0d4fecc2765e
Mint <78211281+Starlight258@users.noreply.github.com>
2024-12-04 19:18:03 +0900

    [Gold V] Title: 공주님을 구해라!, Time: 144 ms, Memory: 15412 KB -BaekjoonHub

--- 백준/Gold/17836. 공주님을 구해라！/공주님을 구해라！.java
+++ 백준/Gold/17836. 공주님을 구해라！/공주님을 구해라！.java
@@ -1,93 +1,92 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.Comparator;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    private static final String FAIL = "Fail";
-    private static int n, m, t;
-    private static int[][] board;
-    private static boolean[][][] visited;
-    private static int[] dx = {0, 1, 0, -1};
-    private static int[] dy = {1, 0, -1, 0};
+	static int[] dx= {0,1,0,-1};
+	static int[] dy= {1,0,-1,0};
+	static int N,M,T;
+	static String[][] map;
+	static boolean[][] visit;
+	static PriorityQueue<int[]>q;
+	static int answer=0;
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
+		String s=br.readLine();
+		StringTokenizer st=new StringTokenizer(s);
+		StringBuilder sb=new StringBuilder();
+		
+		N=Integer.parseInt(st.nextToken());
+		M=Integer.parseInt(st.nextToken());
+		T=Integer.parseInt(st.nextToken());
+	
+		q=new PriorityQueue<>(new Comparator<>() {
 
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
+			@Override
+			public int compare(int[] o1, int[] o2) {
+				 return o1[2]-o2[2];
+			}
+			
+		});
+		map=new String[N][M];
+		visit=new boolean[N][M];
+		
+		for(int i=0;i<N;i++) {
+			s=br.readLine();
+			
+			map[i]=s.split(" ");
+		}
+		visit[0][0]=true;
+		q.add(new int[] {0,0,0,0});
+		bfs(0,0,0);
+		
+		
+		if(answer==0)
+		System.out.println("Fail");
+		else System.out.println(answer);
+	
+	}
+	static void bfs(int x, int y, int weapon) {
 
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
-        t = Integer.parseInt(st.nextToken());
-        board = new int[n][m];
-        for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < m; j++) {
-                board[i][j] = Integer.parseInt(String.valueOf(st.nextToken()));
-            }
-        }
+		while(!q.isEmpty()) {
+			
+			int[] qq=q.poll();
+	
+			if(qq[2]>T) break;
+			
+	
+			if(qq[0]==N-1 && qq[1]==M-1) {
+				answer=qq[2];
+				break;
+			}
+			
+		 	for(int i=0;i<4;i++) {
+				int nx=qq[0]+dx[i];
+				int ny=qq[1]+dy[i];
+				
+				if(nx<0 || nx>=N || ny<0 || ny>=M ) continue;
+				
+		
+				if(map[nx][ny].equals("1") || visit[nx][ny]) continue;
+		
+				
+				visit[nx][ny]=true;
+				if(map[nx][ny].equals("2")) { 
+					visit[nx][ny]=true;
+					qq[2]+=((N-1-nx) + (M-1-ny)); 
+					nx=N-1; ny=M-1;
+					qq[3]=1;
 
-        visited = new boolean[n][m][2];
-        int result = bfs(0, 0);
-        if (result == -1) {
-            System.out.println(FAIL);
-        } else {
-            System.out.println(result);
-        }
-    }
-
-    public static int bfs(int x, int y) {
-        Queue<Node> q = new LinkedList<>();
-        q.offer(new Node(x, y, 0, false));
-        visited[x][y][0] = true;
-
-        while (!q.isEmpty()) {
-            Node current = q.poll();
-
-            if (current.count > t) {
-                break;
-            }
-            if (current.x == n - 1 && current.y == m - 1) {
-                return current.count;
-            }
-
-            for (int i = 0; i < 4; i++) {
-                int nx = current.x + dx[i];
-                int ny = current.y + dy[i];
-                if (nx >= 0 && ny >= 0 && nx < n && ny < m) {
-                    if (!current.isGram) { //그람 없음
-                        if (!visited[nx][ny][0] && board[nx][ny] == 0) {
-                            q.offer(new Node(nx, ny, current.count + 1, current.isGram));
-                            visited[nx][ny][0] = true;
-                        } else if (!visited[nx][ny][0] && board[nx][ny] == 2) {
-                            q.offer(new Node(nx, ny, current.count + 1, true));
-                            visited[nx][ny][0] = true;
-                        }
-                    } else { //그람 있음
-                        if (!visited[nx][ny][1]) {
-                            q.offer(new Node(nx, ny, current.count + 1, current.isGram));
-                            visited[nx][ny][1] = true;
-                        }
-                    }
-                }
-            }
-        }
-        return -1;
-    }
-
-    public static class Node {
-        int x;
-        int y;
-        int count;
-        boolean isGram;
-
-        public Node(int x, int y, int count, boolean isGram) {
-            this.x = x;
-            this.y = y;
-            this.count = count;
-            this.isGram = isGram;
-        }
-    }
+				}	
+			
+				q.add(new int[] {nx,ny,qq[2]+1, qq[3]});
+		}
+			
+		}
+	}
 }
