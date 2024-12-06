cb464981cae21f89140f4d3cd0450646b7e4d848
Mint <78211281+Starlight258@users.noreply.github.com>
2024-12-06 16:33:32 +0900

    [Gold III] Title: 마법사 상어와 토네이도, Time: 444 ms, Memory: 34556 KB -BaekjoonHub

--- 백준/Gold/20057. 마법사 상어와 토네이도/마법사 상어와 토네이도.java
+++ 백준/Gold/20057. 마법사 상어와 토네이도/마법사 상어와 토네이도.java
@@ -1,107 +1,139 @@
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.List;
+import java.util.StringTokenizer;
 
 public class Main {
 
-	static int N;
-	static int[][] A;
-	static int[][][] direct = { { { 1, -1, 2, -2, 0, 1, -1, 1, -1, 0 }, { 1, 1, 0, 0, -2, 0, 0, -1, -1, -1 } },
-			{ { -1, -1, 0, 0, 2, 0, 0, 1, 1, 1 }, { -1, 1, -2, 2, 0, -1, 1, -1, 1, 0 } },
-			{ { 1, -1, 2, -2, 0, 1, -1, 1, -1, 0 }, { -1, -1, 0, 0, 2, 0, 0, 1, 1, 1 } },
-			{ { 1, 1, 0, 0, -2, 0, 0, -1, -1, -1 }, { -1, 1, -2, 2, 0, -1, 1, -1, 1, 0 } } };
+    private static final int[] dy = {0, 1, 0, -1};
+    private static final int[] dx = {-1, 0, 1, 0};
+    private static final List<List<Wind>> winds = List.of(
+            // 왼쪽 방향
+            List.of(
+                    new Wind(-1, 1, 1), new Wind(1, 1, 1),    
+                    new Wind(-2, 0, 2), new Wind(2, 0, 2),     
+                    new Wind(0, -2, 5),                       
+                    new Wind(-1, 0, 7), new Wind(1, 0, 7),     
+                    new Wind(-1, -1, 10), new Wind(1, -1, 10), 
+                    new Wind(0, -1, 0)                        
+            ),
+            // 아래 방향
+            List.of(
+                    new Wind(-1, -1, 1), new Wind(-1, 1, 1),
+                    new Wind(0, -2, 2), new Wind(0, 2, 2),
+                    new Wind(2, 0, 5),
+                    new Wind(0, -1, 7), new Wind(0, 1, 7),
+                    new Wind(1, -1, 10), new Wind(1, 1, 10),
+                    new Wind(1, 0, 0)
+            ),
+            // 오른쪽 방향
+            List.of(
+                    new Wind(-1, -1, 1), new Wind(1, -1, 1),
+                    new Wind(-2, 0, 2), new Wind(2, 0, 2),
+                    new Wind(0, 2, 5),
+                    new Wind(-1, 0, 7), new Wind(1, 0, 7),
+                    new Wind(-1, 1, 10), new Wind(1, 1, 10),
+                    new Wind(0, 1, 0)
+            ),
+            // 위 방향
+            List.of(
+                    new Wind(1, -1, 1), new Wind(1, 1, 1),
+                    new Wind(0, -2, 2), new Wind(0, 2, 2),
+                    new Wind(-2, 0, 5),
+                    new Wind(0, -1, 7), new Wind(0, 1, 7),
+                    new Wind(-1, -1, 10), new Wind(-1, 1, 10),
+                    new Wind(-1, 0, 0)
+            )
+    );
 
-	static int[] percent = { 1, 1, 2, 2, 5, 7, 7, 10, 10 };
-	static int[] dr = { 0, 1, 0, -1 };
-	static int[] dc = { -1, 0, 1, 0 };
-	static int total;
+    private static int n;
+    private static int[][] map;
+    private static int answer = 0;
 
-	public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        map = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < n; j++) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        simulation();
+        System.out.println(answer);
+    }
 
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    private static void simulation() {
+        int y = n / 2, x = n / 2;
+        int count = 0;
+        int move = 1;
+        int d = 0;
+        while (true) {
+            if (count == 2) {
+                count = 0;
+                if (y > 0) {
+                    move++;
+                }
+            }
+            for (int i = 0; i < move; i++) {
+                int ny = y + dy[d];
+                int nx = x + dx[d];
+                if (!isNotInRange(ny, nx) && map[ny][nx] > 0) {
+                    moveSand(ny, nx, d);
+                }
+                y = ny;
+                x = nx;
+                if (y == 0 && x == 0) {
+                    return;
+                }
+            }
+            d = (d + 1) % 4;
+            count++;
+        }
+    }
 
-		N = Integer.parseInt(br.readLine());
-		A = new int[N + 1][N + 1];
+    private static void moveSand(final int ny, final int nx, final int d) {
+        int sand = map[ny][nx];
+        map[ny][nx] = 0;
+        List<Wind> windsEach = winds.get(d);
+        int remaining = sand;
+        for (int i = 0; i < windsEach.size() - 1; i++) {
+            Wind wind = windsEach.get(i);
+            int windY = ny + wind.dy;
+            int windX = nx + wind.dx;
+            int calculateSand = (int) (sand * (wind.rate / 100.0));
+            remaining -= calculateSand;
+            if (isNotInRange(windY, windX)) {
+                answer += calculateSand;
+            } else {
+                map[windY][windX] += calculateSand;
+            }
+        }
+        Wind wind = windsEach.get(windsEach.size() - 1);
+        int remainingY = ny + wind.dy;
+        int remainingX = nx + wind.dx;
+        if (isNotInRange(remainingY, remainingX)) {
+            answer += remaining;
+        } else {
+            map[remainingY][remainingX] += remaining;
+        }
+    }
 
-		for (int i = 1; i <= N; i++) {
-			String[] input = br.readLine().split(" ");
-			for (int j = 1; j <= N; j++) {
-				A[i][j] = Integer.parseInt(input[j - 1]);
-			}
-		}
+    private static boolean isNotInRange(int ny, int nx) {
+        return ny < 0 || nx < 0 || ny >= n || nx >= n;
+    }
 
-		solve();
-		bw.write(total + "\n");
-		bw.flush();
+    static class Wind {
+        int dy;
+        int dx;
+        int rate;
 
-	}
-
-	public static void solve() {
-
-		int cnt = 0;
-		int move = 1;
-		int d = 0;
-		int r = N / 2 + 1;
-		int c = N / 2 + 1;
-
-		while (true) {
-
-			if (cnt == 2) {
-				cnt = 0;
-				if (r > 1)
-					move++;
-			}
-
-			for (int i = 0; i < move; i++) {
-
-				int nr = r + dr[d];
-				int nc = c + dc[d];
-
-				if (A[nr][nc] > 0) {
-					moveSand(nr, nc, d);
-				}
-
-				r = nr;
-				c = nc;
-			}
-
-			cnt++;
-			d = (d + 1) % 4;
-
-			if (r == 1 && c == 1)
-				break;
-		}
-
-	}
-
-	public static void moveSand(int r, int c, int d) {
-
-		int rest = A[r][c];
-
-		for (int i = 0; i < 9; i++) {
-			int nr = r + direct[d][0][i];
-			int nc = c + direct[d][1][i];
-
-			int amount = (int) Math.floor(A[r][c] * (double) (percent[i] / 100.0));
-			rest -= amount;
-
-			if ((1 <= nr && nr <= N) && (1 <= nc && nc <= N)) {
-				A[nr][nc] += amount;
-			}
-
-			else
-				total += amount;
-		}
-
-		int nr = r + direct[d][0][9];
-		int nc = c + direct[d][1][9];
-
-		if ((1 <= nr && nr <= N) && (1 <= nc && nc <= N)) {
-			A[nr][nc] += rest;
-			A[r][c] = 0;
-		}
-
-		else
-			total += rest;
-	}
-
+        public Wind(final int dy, final int dx, final int rate) {
+            this.dy = dy;
+            this.dx = dx;
+            this.rate = rate;
+        }
+    }
 }
