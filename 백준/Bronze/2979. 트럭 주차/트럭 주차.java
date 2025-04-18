fe857f68e90df263c50904d74cb921cd83253595
Mint <78211281+Starlight258@users.noreply.github.com>
2025-04-19 00:26:18 +0900

    [Bronze II] Title: 트럭 주차, Time: 100 ms, Memory: 14164 KB -BaekjoonHub

--- 백준/Bronze/2979. 트럭 주차/트럭 주차.java
+++ 백준/Bronze/2979. 트럭 주차/트럭 주차.java
@@ -4,41 +4,38 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int a = Integer.parseInt(st.nextToken());
-        int b = Integer.parseInt(st.nextToken());
-        int c = Integer.parseInt(st.nextToken());
-
-        int[] arr = new int[101];
-        int max = 0;
+        final int a = Integer.parseInt(st.nextToken());
+        final int b = Integer.parseInt(st.nextToken());
+        final int c = Integer.parseInt(st.nextToken());
+        int[] time = new int[101];
         for (int i = 0; i < 3; i++) {
             st = new StringTokenizer(br.readLine());
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
-            max = Math.max(max, end);
-
-            for (int j = start; j < end; j++) {
-                arr[j]++;
+            for (int index = start; index < end; index++) {
+                time[index]++;
             }
         }
 
-        int sum = 0;
-        for (int i = 1; i <= max; i++) {
-            switch (arr[i]) {
+        int answer = 0;
+        for (int t : time) {
+            switch (t) {
                 case 1:
-                    sum += a;
+                    answer += a;
                     break;
                 case 2:
-                    sum += b * 2;
+                    answer += b * 2;
                     break;
                 case 3:
-                    sum += c * 3;
+                    answer += c * 3;
                     break;
+
             }
         }
-
-        System.out.println(sum);
+        System.out.println(answer);
     }
 }
