cdf1b28fe7ab4d2ad259b626cd504fc4f1ab187f
Mint <78211281+Starlight258@users.noreply.github.com>
2025-04-21 13:51:30 +0900

    [Silver III] Title: 피보나치 함수, Time: 120 ms, Memory: 15860 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -1,17 +1,15 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
-        Map<Integer, int[]> dp = new HashMap<>();
-        dp.put(0, new int[]{1, 0});
-        dp.put(1, new int[]{0, 1});
+        int[][] dp = new int[41][2];
+        dp[0][0] = 1;
+        dp[1][1] = 1;
 
         for (int number = 0; number <= 40; number++) {
             fibonacci(number, dp);
@@ -22,16 +20,17 @@ public class Main {
             int[] fibonacci = fibonacci(n, dp);
             System.out.println(fibonacci[0] + " " + fibonacci[1]);
         }
+
     }
 
-    private static int[] fibonacci(final int number, final Map<Integer, int[]> dp) {
-        if (dp.containsKey(number)) {
-            return dp.get(number);
+    private static int[] fibonacci(final int number, final int[][] dp) {
+        if ((dp[number][0] != 0 || dp[number][1] != 0)) {
+            return dp[number];
         }
         int[] first = fibonacci(number - 1, dp);
         int[] second = fibonacci(number - 2, dp);
-        int[] sum = {first[0] + second[0], first[1] + second[1]};
-        dp.put(number, sum);
-        return sum;
+        dp[number][0] = first[0] + second[0];
+        dp[number][1] = first[1] + second[1];
+        return dp[number];
     }
 }
