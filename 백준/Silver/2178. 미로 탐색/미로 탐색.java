e751217632b80d3c916db248012ce02b6297db99
Mint <78211281+Starlight258@users.noreply.github.com>
2025-06-18 11:12:27 +0900

    [Silver I] Title: 미로 탐색, Time: 112 ms, Memory: 14600 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -3,74 +3,58 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.StringTokenizer;
 
 public class Main {
-    static boolean[][] map;
-    static int[][] visited;
-    static int[] dy = {-1, 0, 1, 0};
-    static int[] dx = {0, 1, 0, -1};
 
-    static class Node {
-        int y;
-        int x;
+    private static final Queue<int[]> queue = new LinkedList<>();
+    private static int[][] arr;
+    private static int n;
+    private static int m;
+    private static int[][] visited;
+    private static int[] dy = {-1, 0, 1, 0};
+    private static int[] dx = {0, 1, 0, -1};
 
-        Node(int y, int x) {
-            this.y = y;
-            this.x = x;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] split = br.readLine().split(" ");
+        n = Integer.parseInt(split[0]);
+        m = Integer.parseInt(split[1]);
+        arr = new int[n][m];
+        visited = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            String line = br.readLine();
+            for (int j = 0; j < m; j++) {
+                arr[i][j] = line.charAt(j) - '0';
+            }
         }
+        bfs();
     }
 
-    private static void bfs(int n, int m) {
+    private static void bfs() {
+        queue.add(new int[]{0, 0});
         visited[0][0] = 1;
-        Queue<Node> queue = new LinkedList<>();
-        queue.offer(new Node(0, 0));
 
         while (!queue.isEmpty()) {
-            Node currentNode = queue.poll();
-            int y = currentNode.y;
-            int x = currentNode.x;
-            if (y == n && x == m) {
-                break;
-            }
+            int[] now = queue.poll();
+            int y = now[0];
+            int x = now[1];
 
+            if (y == n - 1 && x == m - 1) {
+                System.out.println(visited[n - 1][m - 1]);
+                return;
+            }
             for (int i = 0; i < 4; i++) {
                 int ny = y + dy[i];
                 int nx = x + dx[i];
-                if (ny < 0 || ny >= n || nx < 0 || nx >= m) {
+                if (ny < 0 || nx < 0 || ny >= n || nx >= m) {
                     continue;
                 }
-                if (visited[ny][nx] == 0 && map[ny][nx]) {
-                    visited[ny][nx] = visited[y][x] + 1;
-                    queue.offer(new Node(ny, nx));
+                if (visited[ny][nx] != 0 || arr[ny][nx] == 0) {
+                    continue;
                 }
+                queue.offer(new int[]{ny, nx});
+                visited[ny][nx] = visited[y][x] + 1;
             }
         }
-
-    }
-
-    public static void main(String[] args) throws IOException {
-        //1. 입력받기
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer stringTokenizer = new StringTokenizer(bufferedReader.readLine());
-        int n = Integer.parseInt(stringTokenizer.nextToken());
-        int m = Integer.parseInt(stringTokenizer.nextToken());
-
-        map = new boolean[n][m];
-        visited = new int[n][m];
-
-        for (int i = 0; i < n; i++) {
-            String line = bufferedReader.readLine();
-            for (int j = 0; j < m; j++) {
-                map[i][j] = line.charAt(j) != '0';
-            }
-        }
-
-        //2. bfs 수행
-        bfs(n, m);
-
-        //3. 정답 출력
-        System.out.println(visited[n - 1][m - 1]);
-
     }
 }
