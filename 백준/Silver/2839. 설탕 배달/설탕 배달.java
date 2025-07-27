f2774124885e4afb42e9bbd8e78c9a44506f239e
Mint <78211281+Starlight258@users.noreply.github.com>
2025-07-28 02:22:22 +0900

    [Silver IV] Title: 설탕 배달, Time: 104 ms, Memory: 14288 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,25 +1,29 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        if (n < 3) {
-            System.out.println(-1);
-            return;
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, -1);
+        dp[3] = 1;
+        if (n >= 5) {
+            dp[5] = 1;
         }
-        int start = n / 5;
-        while (start >= 0) {
-            int remain = n - (start * 5);
-            if (remain % 3 == 0) {
-                System.out.println(start + (remain / 3));
-                return;
+        for (int i = 6; i <= n; i++) {
+            if (dp[i - 3] != -1) {
+                dp[i] = dp[i - 3] + 1;
             }
-            start--;
+            if (dp[i - 5] != -1) {
+                dp[i] = dp[i - 5] + 1;
+            }
         }
-        System.out.println(-1);
+
+        System.out.println(dp[n]);
     }
+
 }
