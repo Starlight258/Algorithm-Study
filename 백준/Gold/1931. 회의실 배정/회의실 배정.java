cbd00f7875e9cb621f962390d345b249e0dba23c
Mint <78211281+Starlight258@users.noreply.github.com>
2025-07-28 21:00:37 +0900

    [Gold V] Title: 회의실 배정, Time: 500 ms, Memory: 45860 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.java
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.java
@@ -8,34 +8,32 @@ import java.util.StringTokenizer;
 public class Main {
 
     public static void main(String[] args) throws IOException {
-        // 1. 입력받기
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(bufferedReader.readLine());
-        List<int[]> times = new ArrayList<>();
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        List<int[]> meetings = new ArrayList<>();
         StringTokenizer st;
         for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(bufferedReader.readLine());
-            int start = Integer.parseInt(st.nextToken());
-            int end = Integer.parseInt(st.nextToken());
-            times.add(new int[]{start, end});
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            meetings.add(new int[]{a, b});
         }
-        // 2. 정렬하기
-        times.sort((o1, o2) -> {
+        meetings.sort((o1, o2) -> {
             if (o1[1] == o2[1]) {
-                return Integer.compare(o1[0], o2[0]);
+                return o1[0] - o2[0];
             }
-            return Integer.compare(o1[1], o2[1]);
+            return o1[1] - o2[1];
         });
-
-        // 3. 반복하기
-        int answer = 0;
-        int now = 0;
-        for (int i = 0; i < times.size(); i++) {
-            if (now <= times.get(i)[0]) {
-                now = times.get(i)[1];
-                answer++;
+        
+        int endTime = 0;
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (endTime <= meetings.get(i)[0]){
+                endTime = meetings.get(i)[1];
+                count++;
             }
         }
-        System.out.println(answer);
+        System.out.println(count);
     }
+
 }
