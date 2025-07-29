7d1a9075fd469c9d1698106cbc9b62296654b5e0
Mint <78211281+Starlight258@users.noreply.github.com>
2025-07-29 22:34:13 +0900

    [Gold IV] Title: N-Queen, Time: 6212 ms, Memory: 14468 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -1,46 +1,44 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 
 public class Main {
-    static int n;
-    static int answer;
-    static ArrayList<Integer> list = new ArrayList<>();
-    static void dfs(int row) {
-        if (row == n) {
+
+    private static int n;
+    private static int[] map;
+    private static int answer = 0;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        map = new int[n];
+
+        nQueenMethod(0);
+        System.out.println(answer);
+    }
+
+    private static void nQueenMethod(final int depth) {
+        if (depth == n) {
             answer++;
             return;
         }
-        for (int col = 0; col < n; col++) {
-            if (check(col)) {
-                list.add(col);
-                dfs(row + 1);
-                list.remove(list.size() - 1);
+        for (int i = 0; i < n; i++) {
+            map[depth] = i;
+            if (canQueen(depth)) {
+                nQueenMethod(depth + 1);
             }
         }
     }
 
-    static boolean check(int col) {
-        int row = list.size();
-        for (int i = 0; i < list.size(); i++) {
-            int c = list.get(i);
-            if (c == col) {
+    private static boolean canQueen(final int depth) {
+        for (int i = 0; i < depth; i++) {
+            if (map[i] == map[depth]) {
                 return false;
             }
-            if (row - i == Math.abs(col - c)) {
+            if (Math.abs(depth - i) == Math.abs(map[depth] - map[i])) {
                 return false;
             }
         }
-
         return true;
-    }
-    public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(bufferedReader.readLine());
-        answer = 0;
-        list = new ArrayList<>(n);
-        dfs(0);
-        System.out.println(answer);
     }
 }
