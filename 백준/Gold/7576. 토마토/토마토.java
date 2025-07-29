422e098ec6bdbcc5bb3729df0eaef1f3c0334e74
Mint <78211281+Starlight258@users.noreply.github.com>
2025-07-29 22:07:51 +0900

    [Gold V] Title: 토마토, Time: 584 ms, Memory: 123860 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -20,28 +20,23 @@ public class Main {
         m = Integer.parseInt(st.nextToken());
         n = Integer.parseInt(st.nextToken());
         boxes = new int[n][m];
+        Queue<int[]> queue = new LinkedList<>();
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < m; j++) {
                 boxes[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-
-        System.out.println(bfsse());
-    }
-
-    private static int bfsse() {
-        int[][] visited = new int[n][m];
-        Queue<int[]> queue = new LinkedList<>();
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
                 if (boxes[i][j] == 1) {
                     queue.offer(new int[]{i, j});
-                    visited[i][j] = 1;
                 }
             }
         }
 
+        System.out.println(bfs(queue));
+    }
+
+    private static int bfs(final Queue<int[]> queue) {
+        int[][] visited = new int[n][m];
+        int answer = 0;
         while (!queue.isEmpty()) {
             int[] cur = queue.poll();
             int y = cur[0];
@@ -52,25 +47,22 @@ public class Main {
                 if (ny < 0 || nx < 0 || ny >= n || nx >= m) {
                     continue;
                 }
-                if (boxes[ny][nx] == 0 & visited[ny][nx] == 0) {
-                    boxes[ny][nx] = 1;
+                if (boxes[ny][nx] == 0) {
                     queue.offer(new int[]{ny, nx});
-                    visited[ny][nx] = visited[y][x] + 1;
+                    boxes[ny][nx] = boxes[y][x] + 1;
+                    answer = Math.max(answer, boxes[ny][nx] - 1);
                 }
             }
         }
 
-        int answer = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
                 if (boxes[i][j] == 0) {
                     return -1;
                 }
-                answer = Math.max(answer, visited[i][j] - 1);
             }
         }
         return answer;
-
     }
 
 }
