8c67ef565b73a91888ba40df6c44238081c33eab
Mint <78211281+Starlight258@users.noreply.github.com>
2025-07-31 03:44:11 +0900

    [Gold V] Title: 평범한 배낭, Time: 144 ms, Memory: 15528 KB -BaekjoonHub

--- 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
+++ 백준/Gold/12865. 평범한 배낭/평범한 배낭.java
@@ -4,29 +4,22 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
-        int[][] bags = new int[n][2];
+        int[] bags = new int[k + 1];
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
-            int a = Integer.parseInt(st.nextToken());
-            int b = Integer.parseInt(st.nextToken());
-            bags[i] = new int[]{a, b};
-        }
-
-        int[] dp = new int[k + 1];
-
-        for (int i = 0; i < bags.length; i++) {
-            int w = bags[i][0];
-            int v = bags[i][1];
+            int w = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
             for (int j = k; j >= w; j--) {
-                dp[j] = Math.max(dp[j], dp[j - w] + v);
+                bags[j] = Math.max(bags[j], bags[j - w] + v);
             }
         }
 
-        System.out.println(dp[k]);
+        System.out.println(bags[k]);
     }
 }
