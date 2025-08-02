16708239684eecccaf6aa9415ff29c4e0aaf4a51
Mint <78211281+Starlight258@users.noreply.github.com>
2025-08-03 01:45:58 +0900

    [Gold IV] Title: 연구소, Time: 412 ms, Memory: 146988 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.java
+++ 백준/Gold/14502. 연구소/연구소.java
@@ -2,103 +2,100 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Arrays;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-    static class Node {
-        int x;
-        int y;
 
-        Node(int y, int x) {
-            this.y = y;
-            this.x = x;
-        }
-    }
-
-    static int[] dy = {-1, 0, 1, 0};
-    static int[] dx = {0, 1, 0, -1};
-
-    static void dfs(int[][] mp, int y, int x, boolean[][] visited) {
-        visited[y][x] = true;
-
-        for (int i = 0; i < 4; i++) {
-            int ny = y + dy[i];
-            int nx = x + dx[i];
-            if (ny < 0 || nx < 0 || ny >= mp.length || nx >= mp[0].length) {
-                continue;
-            }
-            if (mp[ny][nx] == 0 && !visited[ny][nx]) {
-                dfs(mp, ny, nx, visited);
-            }
-        }
-    }
+    private static int n;
+    private static int m;
+    private static int[][] maps;
+    private static List<int[]> areas;
+    private static int[] dy = {-1, 0, 1, 0};
+    private static int[] dx = {0, 1, 0, -1};
+    private static int answer = 0;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken());
-        int m = Integer.parseInt(st.nextToken());
-        int answer = 0;
-
-        int[][] mp = new int[n][m];
-        boolean[][] visited = new boolean[n][m];
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        maps = new int[n][m];
+        areas = new ArrayList<>();
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < m; j++) {
-                mp[i][j] = Integer.parseInt(st.nextToken());
-            }
-        }
-
-        List<Node> emptyList = new ArrayList<>();
-        List<Node> virusList = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (mp[i][j] == 0) {
-                    emptyList.add(new Node(i, j));
-                }
-                if (mp[i][j] == 2) {
-                    virusList.add(new Node(i, j));
+                maps[i][j] = Integer.parseInt(st.nextToken());
+                if (maps[i][j] == 0) {
+                    areas.add(new int[]{i, j});
                 }
             }
         }
 
-        // 3개의 벽을 세울 수 있는 모든 경우의 수
-        for (int i = 0; i < emptyList.size(); i++) {
-            for (int j = i + 1; j < emptyList.size(); j++) {
-                for (int k = j + 1; k < emptyList.size(); k++) {
-                    mp[emptyList.get(i).y][emptyList.get(i).x] = 1;
-                    mp[emptyList.get(j).y][emptyList.get(j).x] = 1;
-                    mp[emptyList.get(k).y][emptyList.get(k).x] = 1;
-
-                    for (Node node : virusList) {
-                        dfs(mp, node.y, node.x, visited);
-                    }
-                    answer = Math.max(answer, countSafety(mp, visited));
-
-                    mp[emptyList.get(i).y][emptyList.get(i).x] = 0;
-                    mp[emptyList.get(j).y][emptyList.get(j).x] = 0;
-                    mp[emptyList.get(k).y][emptyList.get(k).x] = 0;
-                    for (int a = 0; a < n; a++) {
-                        Arrays.fill(visited[a], false);
-                    }
+        int size = areas.size();
+        for (int i = 0; i < size; i++) {
+            int[] node = areas.get(i);
+            maps[node[0]][node[1]] = 1;
+            for (int j = i + 1; j < size; j++) {
+                int[] jNode = areas.get(j);
+                maps[jNode[0]][jNode[1]] = 1;
+                for (int k = j + 1; k < size; k++) {
+                    int[] kNode = areas.get(k);
+                    maps[kNode[0]][kNode[1]] = 1;
+                    bfs();
+                    maps[kNode[0]][kNode[1]] = 0;
                 }
+                maps[jNode[0]][jNode[1]] = 0;
             }
+            maps[node[0]][node[1]] = 0;
         }
-
         System.out.println(answer);
     }
 
-    private static int countSafety(int[][] mp, boolean[][] visited) {
-        int result = 0;
-        for (int i = 0; i < mp.length; i++) {
-            for (int j = 0; j < mp[0].length; j++) {
-                if (mp[i][j] == 0 && !visited[i][j]) {
-                    result++;
+    private static void bfs() {
+        int[][] tmp = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                tmp[i][j] = maps[i][j];
+            }
+        }
+        Queue<int[]> queue = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (tmp[i][j] == 2) {
+                    queue.offer(new int[]{i, j});
                 }
             }
         }
-        return result;
+        boolean[][] visited = new boolean[n][m];
+        while (!queue.isEmpty()) {
+            int[] node = queue.poll();
+            int y = node[0];
+            int x = node[1];
+            for (int i = 0; i < 4; i++) {
+                int ny = y + dy[i];
+                int nx = x + dx[i];
+                if (ny < 0 || nx < 0 || ny >= n || nx >= m) {
+                    continue;
+                }
+                if (tmp[ny][nx] == 0) {
+                    tmp[ny][nx] = 2;
+                    queue.offer(new int[]{ny, nx});
+                    visited[ny][nx] = true;
+                }
+            }
+        }
+
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (tmp[i][j] == 0) {
+                    count++;
+                }
+            }
+        }
+        answer = Math.max(answer, count);
     }
 }
