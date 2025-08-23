bb17ba7cc62f0a5fbd6fba477168125392d3a7ad
Mint <78211281+Starlight258@users.noreply.github.com>
2025-08-24 01:39:41 +0900

    [Silver III] Title: 피보나치 함수, Time: 124 ms, Memory: 15932 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -4,33 +4,33 @@ import java.io.InputStreamReader;
 
 public class Main {
 
+    private static int t;
+    private static int[][] numbers;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int t = Integer.parseInt(br.readLine());
-        int[][] dp = new int[41][2];
-        dp[0][0] = 1;
-        dp[1][1] = 1;
+        t = Integer.parseInt(br.readLine());
+        numbers = new int[41][2];
 
-        for (int number = 0; number <= 40; number++) {
-            fibonacci(number, dp);
+        numbers[0][0] = 1;
+        numbers[1][1] = 1;
+
+        for (int i = 2; i < 41; i++) {
+            fibos(i);
         }
 
         for (int i = 0; i < t; i++) {
             int n = Integer.parseInt(br.readLine());
-            int[] fibonacci = fibonacci(n, dp);
-            System.out.println(fibonacci[0] + " " + fibonacci[1]);
+            System.out.println(numbers[n][0] + " " + numbers[n][1]);
         }
-
     }
 
-    private static int[] fibonacci(final int number, final int[][] dp) {
-        if ((dp[number][0] != 0 || dp[number][1] != 0)) {
-            return dp[number];
+    private static void fibos(final int number) {
+        if (numbers[number][0] != 0 || numbers[number][1] != 0) {
+            return;
         }
-        int[] first = fibonacci(number - 1, dp);
-        int[] second = fibonacci(number - 2, dp);
-        dp[number][0] = first[0] + second[0];
-        dp[number][1] = first[1] + second[1];
-        return dp[number];
+
+        numbers[number][0] = numbers[number - 1][0] + numbers[number - 2][0];
+        numbers[number][1] = numbers[number - 1][1] + numbers[number - 2][1];
     }
 }
