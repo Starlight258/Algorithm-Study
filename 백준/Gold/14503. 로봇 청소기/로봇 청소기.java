1aa7e3c3b75d7367ba77c1826de9a905d1911b8c
Mint <78211281+Starlight258@users.noreply.github.com>
2025-08-26 01:21:44 +0900

    [Gold V] Title: 로봇 청소기, Time: 132 ms, Memory: 14456 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
@@ -4,82 +4,71 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int n;
-    static int m;
-    static int[][] map;
-    static int r;
-    static int c;
-    static int d;
 
-    static int[] dy = {-1, 0, 1, 0};
-    static int[] dx = {0, 1, 0, -1};
+    private static int[] dy = {-1, 0, 1, 0};
+    private static int[] dx = {0, 1, 0, -1};
+
+    private static int n;
+    private static int m;
+    private static int answer;
+    private static int[][] map;
+    //    1
+    // -1   1
+    //   -1
 
     public static void main(String[] args) throws IOException {
-        //1. 입력받기
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        st = new StringTokenizer(br.readLine());
-        r = Integer.parseInt(st.nextToken());
-        c = Integer.parseInt(st.nextToken());
-        d = Integer.parseInt(st.nextToken());
-
         map = new int[n][m];
+        st = new StringTokenizer(br.readLine());
+        int ry = Integer.parseInt(st.nextToken());
+        int rx = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < m; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
             }
         }
-        //2. 현재칸 청소
-        clean();
-        //3. 정답 출력
-        int answer = 0;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < m; j++) {
-                if (map[i][j] == 2) {
-                    answer++;
-                }
-            }
-        }
+        moveRobots(ry, rx, d);
         System.out.println(answer);
     }
 
-    private static void clean() {
-        map[r][c] = 2;
-
-        for (int i = 0; i < 4; i++) {
-            rotate();
-            int ny = r + dy[d];
-            int nx = c + dx[d];
-            if (ny < 0 || nx < 0 || ny >= n || nx >= m) {
-                continue;
+    private static void moveRobots(int ry, int rx, int d) {
+        while (true) {
+            if (map[ry][rx] == 0) {
+                map[ry][rx] = 2;
+                answer++;
             }
-            if (map[ny][nx] == 0) {
-                r = ny;
-                c = nx;
-                clean();
-                return;
+            boolean isDirty = false;
+            for (int i = 0; i < 4; i++) {
+                d = (d + 3) % 4;
+                int ny = ry + dy[d];
+                int nx = rx + dx[d];
+                if (ny < 0 || nx < 0 || ny >= n || nx >= m) {
+                    continue;
+                }
+                if (map[ny][nx] == 0) {
+                    isDirty = true;
+                    ry = ny;
+                    rx = nx;
+                    break;
+                }
+            }
+            if (!isDirty) {
+                int nd = (d + 2) % 4;
+                ry = ry + dy[nd];
+                rx = rx + dx[nd];
+                if (ry < 0 || rx < 0 || ry >= n || rx >= m) {
+                    break;
+                }
+                if (map[ry][rx] == 1) {
+                    break;
+                }
             }
         }
-        goBack();
     }
 
-    private static void goBack() {
-        int ny = r - dy[d];
-        int nx = c - dx[d];
-        if (ny < 0 || nx < 0 || ny >= n || nx >= m) {
-            return;
-        }
-        if (map[ny][nx] != 1) {
-            r = ny;
-            c = nx;
-            clean();
-        }
-    }
-
-    private static void rotate() {
-        d = (d + 3) % 4;
-    }
 }
