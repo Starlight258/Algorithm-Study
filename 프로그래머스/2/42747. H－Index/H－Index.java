f3a16acade72d0e67e1e21ed394d3b8bfde3dde4
Mint <78211281+Starlight258@users.noreply.github.com>
2025-08-27 22:25:12 +0900

    [level 2] Title: H-Index, Time: 4.91 ms, Memory: 75 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -2,20 +2,19 @@ import java.util.*;
 
 class Solution {
     public int solution(int[] citations) {
+        Integer[] a = Arrays.stream(citations).boxed().toArray(Integer[]::new);
         int n = citations.length;
-        int h = n;
-        while (h>0){
-            int count = 0;
-            for (int c:citations){
-                if (c>=h){
-                    count++;
-                }
-            }
-            if (count>=h && n-count<=h){
-                return h;
-            }
-            h--;
+        Arrays.sort(a, Collections.reverseOrder());
+        int h = 0;
+        for (int i=0;i<n;i++){
+            if (a[i] >= i+1){
+                h = i+1;
+            } else break;
         }
+        
         return h;
     }
+    // 0 1 3 5 6
+    // 6 5 3 1 0
+    // 6 5 4 3 2 1 
 }
