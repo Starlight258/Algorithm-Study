2240175fdf6a1e551c70d60344ce0da030c881d5
Mint <78211281+Starlight258@users.noreply.github.com>
2025-08-27 23:04:22 +0900

    [level 3] Title: 베스트앨범, Time: 4.50 ms, Memory: 84 MB -BaekjoonHub

--- 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
+++ 프로그래머스/3/42579. 베스트앨범/베스트앨범.java
@@ -1,29 +1,44 @@
 import java.util.*;
+
 class Solution {
     public int[] solution(String[] genres, int[] plays) {
         List<Integer> answer = new ArrayList<>();
-        // 1. 저장하기
-        // 장르별 재생횟수 저장
-        Map<String, Integer> mp1 = new HashMap<>();
-        // 장르별 노래 인덱스 저장
-        Map<String, List<Integer>> mp2 = new HashMap<>();
-        for (int i=0;i<plays.length;i++){
-            mp1.put(genres[i], mp1.getOrDefault(genres[i], 0) + plays[i]);
-            mp2.computeIfAbsent(genres[i], k -> new ArrayList<>()).add(i);
+        Map<String, Integer> gen = new HashMap<>();
+        Map<String, List<int[]>> songs = new HashMap<>();
+        for (int i=0;i<genres.length;i++){
+            String g = genres[i];
+            gen.put(g, gen.getOrDefault(g, 0) + plays[i]);
+            if (songs.containsKey(g)){
+                List<int[]> s = songs.get(g);
+                s.add(new int[]{i,plays[i]});
+                songs.put(g, s);
+            } else {
+                songs.put(g, new ArrayList<>(List.of(new int[]{i,plays[i]})));           
+            }
+        }
+        // 인기 장르별로 정렬
+        List<Map.Entry<String, Integer>> keys = new LinkedList<>(gen.entrySet());
+        Collections.sort(keys, (o1, o2)-> {
+            return Integer.compare(o2.getValue(), o1.getValue());
+        });
+        for (Map.Entry<String, Integer> k:keys){
+            String genre = k.getKey();
+            // 가장 조회수가 많은 노래 2개 조회
+            List<int[]> song = songs.get(genre);
+            Collections.sort(song, (s1,s2)-> Integer.compare(s2[1], s1[1]));
+            if (song.size()==1){
+                answer.add(song.get(0)[0]);
+            } else {
+                answer.add(song.get(0)[0]);
+                answer.add(song.get(1)[0]);
+            }
         }
         
-        //2. 장르별 총 재생횟수 내림차순 정렬하기
-        List<String> sortedGenres = new ArrayList<>(mp1.keySet());
-        sortedGenres.sort((g1, g2) -> mp1.get(g2) - mp1.get(g1));
         
-        // 해당 장르의 재생횟수별로 노래 정렬하기
-        for (String genre:sortedGenres){
-            List<Integer> indexes = mp2.get(genre);
-            indexes.sort((i1, i2) -> plays[i2] -plays[i1]);
-            
-            answer.add(indexes.get(0));
-            if (indexes.size()>1) answer.add(indexes.get(1));
-        }
         return answer.stream().mapToInt(x->x).toArray();
     }
+    
+    // 1. 장르별 조회수 합 계산 (Map<String, Integer> )
+    // 2. 인기 장르별로 가장 많이 재생된 노래 수록 -> 재생수가 같을 경우 고유번호가 낮은 노래 수록
+    // Map<String, List<Integer>> -> 정렬시 오름차순, 고유번호가 낮은 순으로 정렬
 }
