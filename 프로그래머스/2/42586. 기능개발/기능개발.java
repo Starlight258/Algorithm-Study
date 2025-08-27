5d1e410b36f80bff06760d8494ed85ed35abf1d9
Mint <78211281+Starlight258@users.noreply.github.com>
2025-08-27 17:26:05 +0900

    [level 2] Title: 기능개발, Time: 6.87 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,26 +1,33 @@
 import java.util.*;
+
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        ArrayList<Integer> answer = new ArrayList<Integer>();
-        Queue<Integer> q = new LinkedList<>();
-        // 요일 계산
+        List<Integer> answer = new ArrayList<>();
+        int lastDay = 0;
+        int count = 0;
         for (int i=0;i<progresses.length;i++){
-            int remains = 100 - progresses[i];
-            int day = remains / speeds[i];
-            if ((remains % speeds[i]) != 0){
+            int remain = 100 - progresses[i];
+            int s = speeds[i];
+            int day = remain / s;
+            if (remain % s !=0){
                 day++;
             }
-            q.offer(day);
-        }
-        while (!q.isEmpty()){
-            int n  = 1;
-            int value = q.poll();
-            while (!q.isEmpty() && value >= q.peek()){
-                q.poll();
-                n++;
+            if (i!=0 && lastDay<day) {
+                answer.add(count);
+                count = 1;
+            } else {
+                count++;
             }
-            answer.add(n);
+            lastDay = Math.max(lastDay, day);
+        }
+        if (count!=0){
+            answer.add(count);
         }
         return answer.stream().mapToInt(x->x).toArray();
     }
+    // 3 7
+    // 7 3 9 -> 7 7 9
+    // 5 10 1 1 20 1 -> 5 10 10 10 20 20
+    // 1. 기능 작업 일수 계산
+    // 2. a>b일 경우 a로 count후 answer에 저장
 }
