026b045a0c0bd229b0bb8917a468cc477cdf1646
Mint <78211281+Starlight258@users.noreply.github.com>
2025-08-28 14:11:39 +0900

    [Gold V] Title: 탑, Time: 3276 ms, Memory: 132052 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -5,27 +5,31 @@ import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
-        //1. 입력받기
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[] tower = new int[n + 1];
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 1; i <= n; i++) {
+        int[] tower = new int[n];
+        int[] answer = new int[n];
+        for (int i = 0; i < n; i++) {
             tower[i] = Integer.parseInt(st.nextToken());
         }
-        //2. 스택 이용해서 구하기
+
         Stack<Integer> stk = new Stack<>();
-        for (int i = 1; i <= n; i++) {
+        for (int i = n - 1; i >= 0; i--) {
             while (!stk.isEmpty() && tower[stk.peek()] < tower[i]) {
-                stk.pop();
-            }
-            if (stk.isEmpty()) {
-                System.out.print("0 ");
-            } else {
-                System.out.print(stk.peek() + " ");
+                answer[stk.pop()] = i + 1;
             }
             stk.push(i);
         }
+
+        for (int a : answer) {
+            System.out.print(a + " ");
+        }
     }
+
+    // 6 9 5 7 4
+    // 1. stack에 끝에서부터 타워 인덱스 집어넣기
+    // 2. 현재 타워가 stack.peek()보다 클 경우 : stack.pop() 인덱스에 현재 타워 인덱스 저장
 }
