acd8c2a85782be1c960ffff5444b621a40ac5c84
Mint <78211281+Starlight258@users.noreply.github.com>
2025-08-28 12:23:50 +0900

    [level 2] Title: 주식가격, Time: 77.65 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/2/42584. 주식가격/README.md
+++ 프로그래머스/2/42584. 주식가격/README.md
@@ -1,10 +1,10 @@
 # [level 2] 주식가격 - 42584 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584#) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42584) 
 
 ### 성능 요약
 
-메모리: 63.2 MB, 시간: 25.94 ms
+메모리: 74.7 MB, 시간: 77.65 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 31일 11:16:52
+2025년 08월 28일 12:23:48
 
 ### 문제 설명
 
--- 프로그래머스/2/42584. 주식가격/주식가격.java
+++ 프로그래머스/2/42584. 주식가격/주식가격.java
@@ -1,22 +1,24 @@
 import java.util.*;
+
 class Solution {
     public int[] solution(int[] prices) {
-        int[] answer = new int[prices.length];
+        int n = prices.length;
+        int[] answer = new int[n];
         Stack<Integer> stk = new Stack<>();
-        stk.push(0);
-        for (int i=1;i<prices.length;i++){
-            while (!stk.isEmpty() && prices[i]<prices[stk.peek()]){
-                int pos = stk.pop();
-                answer[pos] = i - pos;
+        for (int i=0;i<prices.length;i++){
+            while (!stk.isEmpty() && prices[stk.peek()] > prices[i]){
+                int prev = stk.pop();
+                answer[prev] = i - prev;
             }
-            
             stk.push(i);
         }
-        for (int i=0;i<answer.length;i++){
-            if (answer[i] == 0){
-                answer[i] = answer.length - i - 1;
-            }
+        while (!stk.isEmpty()){
+            int index = stk.pop();
+            answer[index] = n -1 - index;
         }
         return answer;
     }
+    // 1 2 3 2 3
+    // 감소할 경우 -> 인덱스 차이, pop
+    // 감소하지 않을 경우 -> n-(index+1) = n-1-index
 }
