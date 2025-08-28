506f439e0e24b128288ad3ad7e56e5bff885dac2
Mint <78211281+Starlight258@users.noreply.github.com>
2025-08-28 15:18:37 +0900

    [level 2] Title: 다리를 지나는 트럭, Time: 17.19 ms, Memory: 76.5 MB -BaekjoonHub

--- 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
+++ 프로그래머스/2/42583. 다리를 지나는 트럭/다리를 지나는 트럭.java
@@ -1,28 +1,37 @@
 import java.util.*;
+
 class Solution {
     public int solution(int bridge_length, int weight, int[] truck_weights) {
-        int answer = 0;
-        int currentWeight=0;
-        int pos = 0;
-        Queue<Integer> bridge = new LinkedList<>();
-        // 다리 초기화 (1초에 1만큼 움직일 수 있다. 무게를 저장한다.)
-        for(int i=0;i<bridge_length;i++){
+        int curTime = 0;
+        Deque<Integer> bridge = new ArrayDeque<>();
+        int total = 0;
+        for (int i=0;i<bridge_length;i++){
             bridge.offer(0);
         }
-        // 다리 건너기 시작한다.
-        while (pos < truck_weights.length){
-            currentWeight -= bridge.poll();
-            if (truck_weights[pos] + currentWeight <= weight){
-                currentWeight += truck_weights[pos];
-                bridge.offer(truck_weights[pos]);
-                pos++;
-            } else{
-                bridge.offer(0);
+        int index = 0;
+        int n = truck_weights.length;
+        while (index<n){
+            curTime++;
+            total -= bridge.pollFirst();
+            if (weight - total >= truck_weights[index]){
+                bridge.offerLast(truck_weights[index]);
+                total += truck_weights[index];
+                index++;
+            } else {
+                bridge.offerLast(0);
             }
-            answer++; // 시간 흐름
         }
         
+        while (!bridge.isEmpty() && total != 0){
+            curTime++;        
+            total -= bridge.pollFirst();
+        }
         
-        return answer + bridge_length;
+        return curTime;
     }
+
+    // 1. 다리에 bridge_length만큼 0 채우기
+    // 2. 차가 올라가면 kg 추가
+    // 3. 초마다 pop후, weight - kg >= 새로 추가된 트럭 무게 : 일 경우 kg추가, 그렇지 않을 경우 0 추가
+    // ArrayDeque
 }
