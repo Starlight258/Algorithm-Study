941f009ceb3f78da74f516324b21f16b6aaebdfa
Mint <78211281+Starlight258@users.noreply.github.com>
2025-08-30 02:37:17 +0900

    [Gold III] Title: 최소비용 구하기 2, Time: 496 ms, Memory: 51084 KB -BaekjoonHub

--- 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.java
+++ 백준/Gold/11779. 최소비용 구하기 2/최소비용 구하기 2.java
@@ -3,93 +3,102 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.PriorityQueue;
-import java.util.Stack;
 import java.util.StringTokenizer;
 
 public class Main {
 
-    private static final int INF = 1_000_000_000;
-    private static final List<List<Node>> graph = new ArrayList<>();
-    private static int[] d;
-    private static int[] prevCity;
+    static class Node implements Comparable<Node> {
+        int edge;
+        int cost;
+
+        public Node(final int edge, final int cost) {
+            this.edge = edge;
+            this.cost = cost;
+        }
+
+        @Override
+        public int compareTo(final Node o) {
+            return Integer.compare(cost, o.cost);
+        }
+    }
+
+    private static int n;
+    private static int m;
+    private static int[] dist;
+    private static int[] prev;
+    private static List<List<Node>> nodes;
+    private static int INF = 1_000_000_000;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
+        n = Integer.parseInt(br.readLine());
+        dist = new int[n + 1];
+        prev = new int[n + 1];
+        Arrays.fill(dist, INF);
+        m = Integer.parseInt(br.readLine());
+        nodes = new ArrayList<>();
         for (int i = 0; i <= n; i++) {
-            graph.add(new ArrayList<>());
+            nodes.add(new ArrayList<>());
         }
-        d = new int[n + 1];
-        prevCity = new int[n + 1];
-        Arrays.fill(d, INF);
-        int m = Integer.parseInt(br.readLine());
         StringTokenizer st;
         for (int i = 0; i < m; i++) {
             st = new StringTokenizer(br.readLine());
             int s = Integer.parseInt(st.nextToken());
             int e = Integer.parseInt(st.nextToken());
             int cost = Integer.parseInt(st.nextToken());
-            graph.get(s).add(new Node(e, cost));
+            nodes.get(s).add(new Node(e, cost));
         }
         st = new StringTokenizer(br.readLine());
         int start = Integer.parseInt(st.nextToken());
         int end = Integer.parseInt(st.nextToken());
 
-        // 다익스트라
-        dijkstra(start, end);
+        dijkstraPsS(start);
+        System.out.println(dist[end]);
+
+        int curEdge = end;
+        List<Integer> path = new ArrayList<>();
+        while (curEdge != 0) {
+            path.add(curEdge);
+            curEdge = prev[curEdge];
+        }
+        Collections.reverse(path);
+        System.out.println(path.size());
+        StringBuilder sb = new StringBuilder();
+        for (Integer p : path) {
+            sb.append(p).append(" ");
+        }
+        System.out.println(sb.toString().trim());
     }
 
-    private static void dijkstra(final int start, final int end) {
+    private static void dijkstraPsS(final int start) {
+        dist[start] = 0;
         PriorityQueue<Node> pq = new PriorityQueue<>();
         pq.offer(new Node(start, 0));
-        d[start] = 0;
+
         while (!pq.isEmpty()) {
-            Node wayPoint = pq.poll();
-            int wayPointIndex = wayPoint.index;
-            int wayPointDistance = wayPoint.distance;
-            if (d[wayPointIndex] < wayPointDistance) {
+            Node curNode = pq.poll();
+            int curEdge = curNode.edge;
+            if (dist[curEdge] != curNode.cost) {
                 continue;
             }
-            for (Node destination : graph.get(wayPointIndex)) {
-                int destinationIndex = destination.index;
-                int cost = d[wayPointIndex] + destination.distance;
-                if (d[destinationIndex] > cost) {
-                    d[destinationIndex] = cost;
-                    pq.offer(new Node(destinationIndex, cost));
-                    prevCity[destinationIndex] = wayPointIndex;
+            for (Node next : nodes.get(curEdge)) {
+                int cost = dist[curEdge] + next.cost;
+                int nextEdge = next.edge;
+                if (dist[nextEdge] > cost) {
+                    dist[nextEdge] = cost;
+                    pq.offer(new Node(nextEdge, dist[nextEdge]));
+                    prev[nextEdge] = curEdge;
                 }
             }
         }
-        System.out.println(d[end]);
-        // 거리 역추적
-        Stack<Integer> stk = new Stack<>();
-        int cur = end;
-        stk.add(cur);
-        while (prevCity[cur] != 0) {
-            cur = prevCity[cur];
-            stk.push(cur);
-        }
-        System.out.println(stk.size());
-        while (!stk.isEmpty()) {
-            System.out.print(stk.pop() + " ");
-        }
+
     }
 
-    private static class Node implements Comparable<Node> {
-        private final int index;
-        private final int distance;
-
-        public Node(final int index, final int distance) {
-            this.index = index;
-            this.distance = distance;
-        }
-
-        @Override
-        public int compareTo(final Node o) {
-            return this.distance - o.distance;
-        }
-    }
+    // 다익스트라
+    // int[] dist : INF
+    // if (dist[i] < dist[u]+w) dist[i] = dist[u]+w
 
 }
