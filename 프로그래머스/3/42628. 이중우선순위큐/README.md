02caa67367e99b0ad229d1e33cccfd3856a69a7b
Mint <78211281+Starlight258@users.noreply.github.com>
2025-08-29 22:46:52 +0900

    [level 3] Title: 이중우선순위큐, Time: 98.09 ms, Memory: 138 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/README.md
+++ 프로그래머스/3/42628. 이중우선순위큐/README.md
@@ -1,10 +1,10 @@
 # [level 3] 이중우선순위큐 - 42628 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42628?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42628) 
 
 ### 성능 요약
 
-메모리: 167 MB, 시간: 484.12 ms
+메모리: 138 MB, 시간: 98.09 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 12월 17일 22:25:44
+2025년 08월 29일 22:46:49
 
 ### 문제 설명
 
--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -2,33 +2,34 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] operations) {
-        PriorityQueue<Integer> queue1 = new PriorityQueue<Integer>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        PriorityQueue<Integer> pqReverse = new PriorityQueue<>(Collections.reverseOrder());
         for (String operation:operations){
-            String[] split = operation.split(" ");
-            String command = split[0];
-            int number = Integer.parseInt(split[1]);
-            switch (command){
-                case "I":
-                    queue1.offer(number);
-                    break;
-                case "D":
-                    if (queue1.isEmpty()) continue;
-                    if (number==1){ // 최대값 삭제
-                        PriorityQueue<Integer> queue2 = new PriorityQueue<>(Collections.reverseOrder());
-                        queue2.addAll(queue1);
-                        int num = queue2.poll();
-                        queue1.clear();
-                        queue1.addAll(queue2);
-                    } else { // 최소값 삭제
-                        int num = queue1.poll();
-                    }
+            String[] splited = operation.split(" ");
+            String command = splited[0];
+            int number = Integer.parseInt(splited[1]);
+            if (command.equals("I")){
+                pq.offer(number);
+                pqReverse.offer(number);
+            } else if (command.equals("D")){
+                if (pq.isEmpty()){
+                    continue;
+                }
+                if (number == 1){ // 최댓값 삭제
+                    int n = pqReverse.poll();
+                    pq.remove(n);
+                } else {
+                    int n = pq.poll();
+                    pqReverse.remove(n);
                 }
             }
-        if (queue1.isEmpty()){
+        }
+        if (pq.isEmpty()){
             return new int[]{0,0};
         }
-        int maxNum = Collections.max(queue1);
-        int minNum = Collections.min(queue1);
-        return new int[]{maxNum, minNum};
+        int max = pqReverse.poll();
+        int min = pq.poll();
+        
+        return new int[]{max, min};
     }
 }
