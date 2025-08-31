74e1092a2bc3362e0a068f9bb9e652816d673e35
Mint <78211281+Starlight258@users.noreply.github.com>
2025-08-31 19:06:44 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 104 ms, Memory: 14308 KB -BaekjoonHub

--- 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.java
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.java
@@ -8,47 +8,43 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    private static final int INF = 1_000;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
         int[] dist = new int[101];
-        Arrays.fill(dist, -1);
         int[] link = new int[101];
+        Arrays.fill(link, -1);
         for (int i = 0; i < n + m; i++) {
             st = new StringTokenizer(br.readLine());
             int s = Integer.parseInt(st.nextToken());
             int e = Integer.parseInt(st.nextToken());
             link[s] = e;
         }
+
         Queue<Integer> q = new ArrayDeque<>();
-        dist[1] = 0;
         q.offer(1);
+        dist[1] = 0;
 
         while (!q.isEmpty()) {
             Integer cur = q.poll();
-            if (cur == 100) {
-                break;
-            }
+
             for (int i = 1; i <= 6; i++) {
                 int v = cur + i;
                 if (v > 100) {
                     continue;
                 }
-                if (link[v] != 0) {
-                    v = link[v]; // 텔레포트
+                if (link[v] != -1) {
+                    v = link[v];
                 }
-                if (dist[v] != -1) { // 이미 간 거리는 패스
+                if (dist[v] != 0) {
                     continue;
                 }
-                dist[v] = dist[cur] + 1; // 주사위
+                dist[v] = dist[cur] + 1;
                 q.offer(v);
             }
         }
-
         System.out.println(dist[100]);
     }
 
@@ -56,6 +52,7 @@ public class Main {
     // 6 -> 12 (주사위)
     // 12 -> 98 (사다리)
     // 98 -> 100 (주사위)
+    // -> 주사위 횟수 3번
 
     // 1 -> 6 -> 80
     // 80 -> 86
@@ -65,6 +62,7 @@ public class Main {
 
     // 사다리로 올라갈 수도, 뱀으로 인해 내려갈 수도 있다. (다익스트라 최단 경로 X)
     // 모든 경우 -> 최단거리이므로 bfs?
-    // bfs : 가장 작은 거리부터
-    // dp? : dp[i] = Math.min(dp[i], dp[u]+1);
+    // 1부터 시작 -> +1 ~ +6 까지 dist[i] = dist[u] + 1; 주사위
+    // 사다리 or 뱀으로 이동
+    // 단방향이 아니라 다시 돌아가야하므로 queue 넣기
 }
