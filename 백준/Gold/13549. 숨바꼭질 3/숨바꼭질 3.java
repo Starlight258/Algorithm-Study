81cd3f62acbfdd799e7b372de3e4668086718349
Mint <78211281+Starlight258@users.noreply.github.com>
2025-08-31 23:55:48 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 136 ms, Memory: 16224 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.java
@@ -1,44 +1,49 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
 import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
+
+    private static final int INF = 100_000;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int k = Integer.parseInt(st.nextToken());
-
-        int[] time = new int[100001];
-        Arrays.fill(time, -1);
-
-        Queue<Integer> q = new LinkedList<>();
+        int[] dist = new int[INF + 1];
+        Arrays.fill(dist, INF);
+        Queue<Integer> q = new ArrayDeque<>();
+        dist[n] = 0;
         q.offer(n);
-        time[n] = 0;
-
         while (!q.isEmpty()) {
             int cur = q.poll();
-            if (cur * 2 <= 100000) {
-                if (time[cur * 2] == -1) {
-                    time[cur * 2] = time[cur];
-                    q.offer(cur * 2);
-                } 
+            if (cur == k) {
+                break;
             }
-            for (int next : new int[]{cur - 1, cur + 1}) {
-                if (next < 0 || next > 100000) {
-                    continue;
-                }
-                if (time[next] == -1) {
-                    time[next] = time[cur] + 1;
-                    q.offer(next);
-                }
+            if (dist[cur] == INF) {
+                continue;
+            }
+            if (cur - 1 >= 0 && dist[cur - 1] > dist[cur] + 1) {
+                dist[cur - 1] = dist[cur] + 1;
+                q.offer(cur - 1);
+            }
+            if (cur + 1 <= INF && dist[cur + 1] > dist[cur] + 1) {
+                dist[cur + 1] = dist[cur] + 1;
+                q.offer(cur + 1);
+            }
+            if (cur * 2 <= INF && dist[cur * 2] > dist[cur]) {
+                dist[cur * 2] = dist[cur];
+                q.offer(cur * 2);
             }
         }
-
-        System.out.println(time[k]);
+        System.out.println(dist[k]);
     }
+    // bfs -> 최단거리
+    // +, - 방향 가능 -> 큐 사용
+    // dist
 }
