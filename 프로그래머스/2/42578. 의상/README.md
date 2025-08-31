394c207c98897107daba0eaffd0fa053514a8e82
Mint <78211281+Starlight258@users.noreply.github.com>
2025-08-31 14:52:37 +0900

    [level 2] Title: 의상, Time: 0.14 ms, Memory: 81.4 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/README.md
+++ 프로그래머스/2/42578. 의상/README.md
@@ -1,10 +1,10 @@
 # [level 2] 의상 - 42578 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42578) 
 
 ### 성능 요약
 
-메모리: 74.2 MB, 시간: 0.08 ms
+메모리: 81.4 MB, 시간: 0.14 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 03월 26일 23:52:06
+2025년 08월 31일 14:52:35
 
 ### 문제 설명
 
--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -1,14 +1,22 @@
 import java.util.*;
+
 class Solution {
     public int solution(String[][] clothes) {
-        HashMap<String, Integer> hm = new HashMap<>();
-        for (int i=0;i<clothes.length;i++){
-            hm.put(clothes[i][1], hm.getOrDefault(clothes[i][1], 0) + 1);
+        Map<String, Integer> countMap = new HashMap<>();
+        for (String[] cloth : clothes){
+            String name = cloth[0];
+            String kind = cloth[1];
+            countMap.put(kind, countMap.getOrDefault(kind,0)+1);
         }
         int answer = 1;
-        for (Map.Entry<String, Integer> cur : hm.entrySet()){
-            answer *= (cur.getValue()+1);
-        };
-        return answer-1;
+        for (int v : countMap.values()){
+            answer *= (v+1);
+        }
+        
+        return answer - 1;
     }
+    // 옷 -> 입을 수도 있고, 안입을 수도 있음
+    // 3 * 2 * 2*2 = 24 -1 = 23
+    // Map<String, Integer> 개수 
+    // (개수+1)*(개수+1)*.. - 1(모두 안입은 경우)
 }
