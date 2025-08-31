7c7c65c5492996b613d57d3f17b336b9b55185bc
Mint <78211281+Starlight258@users.noreply.github.com>
2025-08-31 14:15:55 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 88.08 ms, Memory: 97.8 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.java
@@ -1,19 +1,24 @@
 import java.util.*;
+
 class Solution {
     public String solution(String[] participant, String[] completion) {
         Map<String, Integer> mp = new HashMap<>();
-        for (String part : participant){
-            mp.put(part, mp.getOrDefault(part, 0)+1);
+        for (String p:participant){
+            mp.put(p, mp.getOrDefault(p, 0)+1);
         }
-        
-        for (String comp: completion){
-            mp.put(comp, mp.get(comp)-1);
-        }
-        for (Map.Entry<String, Integer> entry:mp.entrySet()){
-            if (entry.getValue()>0){
-                return entry.getKey();
+        for (String c:completion){
+            int count = mp.get(c);
+            if (count==1){
+                mp.remove(c);
+            } else {
+                mp.put(c, count-1);
             }
         }
-        return "";
+        List<String> remains = new ArrayList<>(mp.keySet());
+        return remains.get(0);
     }
+    
+    // Map<String, Integer> 
+    // 완주한 경우 -1, 마지막 동명이인 선수일 경우 remove
+    // -> map을 순회하며 남은 key가 존재할 경우 return
 }
