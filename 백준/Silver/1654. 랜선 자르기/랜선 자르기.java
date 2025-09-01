12e8c27b2d0c344602bce4dd621db1bd8f843fee
Mint <78211281+Starlight258@users.noreply.github.com>
2025-09-01 17:33:57 +0900

    [Silver II] Title: 랜선 자르기, Time: 152 ms, Memory: 16548 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.java
@@ -1,53 +1,44 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
+
+    private static int[] lines;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int k = Integer.parseInt(st.nextToken());
         int n = Integer.parseInt(st.nextToken());
-
-        List<Long> lines = new ArrayList<>();
+        lines = new int[k];
+        long right = 0;
         for (int i = 0; i < k; i++) {
-            lines.add(Long.parseLong(br.readLine()));
+            lines[i] = Integer.parseInt(br.readLine());
+            right = Math.max(lines[i], right);
         }
 
-        System.out.println(getMaxLength(lines, n));
-    }
-
-    public static boolean isPossible(List<Long> lines, long target, int n) {
-        long count = 0;
-        for (Long line : lines) {
-            count += line / target;
-        }
-        return count >= n;
-    }
-
-    public static long getMaxLength(List<Long> lines, int n) {
         long left = 1;
-        long right = 1;
         long answer = 0;
-        for (Long line : lines) {
-            right = Math.max(right, line);
-        }
         while (left <= right) {
-            long mid = (right - left) / 2 + left;
-
-            if (isPossible(lines, mid, n)) {
+            long mid = (left + right) >>> 1;
+            if (cutLine(mid) >= n) {
                 answer = mid;
                 left = mid + 1;
             } else {
                 right = mid - 1;
             }
         }
+        System.out.println(answer);
+    }
 
-        return answer;
+    private static long cutLine(final long mid) {
+        long count = 0;
+        for (int line : lines) {
+            count += line / mid;
+        }
+        return count;
     }
 
 }
