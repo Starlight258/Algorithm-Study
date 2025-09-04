2f409a24d0be185445f0f92227cfe0d3a6bbcf47
Mint <78211281+Starlight258@users.noreply.github.com>
2025-09-05 03:17:16 +0900

    [Silver IV] Title: 수 찾기, Time: 1160 ms, Memory: 53784 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.java
+++ 백준/Silver/1920. 수 찾기/수 찾기.java
@@ -2,25 +2,45 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    private static final int[] days = new int[]{31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+    private static int n;
+    private static int[] numbers;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        String[] split = br.readLine().split(" ");
-        Set<Integer> numbers = Arrays.stream(split).map(Integer::parseInt).collect(Collectors.toSet());
+        n = Integer.parseInt(br.readLine());
+        numbers = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            numbers[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(numbers);
 
         int m = Integer.parseInt(br.readLine());
-        String[] splittedM = br.readLine().split(" ");
-        List<Integer> target = Arrays.stream(splittedM).map(Integer::parseInt).collect(Collectors.toList());
-        for (Integer number : target) {
-            System.out.println(numbers.contains(number) ? 1 : 0);
+        int[] finders = new int[m];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < m; i++) {
+            finders[i] = Integer.parseInt(st.nextToken());
+            System.out.println(findd(finders[i])? 1 : 0);
         }
+    }
+
+    private static boolean findd(final int finder) {
+        int left = 0;
+        int right = n - 1;
+        while (left <= right) {
+            int mid = (left + right) >>> 1;
+            if (numbers[mid] == finder) {
+                return true;
+            } else if (numbers[mid] < finder) {
+                left = mid + 1;
+            } else {
+                right = mid - 1;
+            }
+        }
+        return false;
     }
 }
