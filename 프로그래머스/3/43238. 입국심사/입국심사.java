5db758d1cb9bf1f59da00757c91f2251e6ca9943
Mint <78211281+Starlight258@users.noreply.github.com>
2025-09-07 23:54:08 +0900

    [level 3] Title: 입국심사, Time: 75.53 ms, Memory: 85.7 MB -BaekjoonHub

--- 프로그래머스/3/43238. 입국심사/입국심사.java
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -1,23 +1,36 @@
-import java.util.*;
 class Solution {
     public long solution(int n, int[] times) {
-        Arrays.sort(times);
-        
-        long left = 0;
-        long right = (long) times[0] * n;
-        long mid = 0;
+        long answer = 0;
+        long left = 1;
+        long right = 0;
+        for (int time:times){
+            right = Math.max(time, right);
+        }
+        right *= n;
         while (left <= right){
-            mid = left - (left-right)/2;
-            long count = 0;
-            for (int i=0;i<times.length;i++){
-                count += mid / times[i];
-            }
-            if (count>=n){
-                right = mid-1;
+            long mid = (left + right) >>> 1;
+            if (count(mid, times) >= n){
+                answer = mid;
+                right = mid - 1;
             } else {
                 left = mid + 1;
             }
         }
-        return left;
+        return answer;
     }
+    
+    private long count(long mid, int[] times){
+        long count = 0;
+        for (int time : times){
+            count += mid / time;
+        }
+        return count;
+    }
+    // 7(3) 10(4) 14(5) 20(6) + 10 vs 21 + 7(6)
+    // 28 / 7 = 4;
+    // 28 / 10 = 2;
+    
+    // 30 / 7 = 4
+    // 30 / 10 = 3 
+    
 }
