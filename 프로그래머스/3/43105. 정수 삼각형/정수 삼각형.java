cba600f5cbf108b69bfdd315215750b5a4f304a2
Mint <78211281+Starlight258@users.noreply.github.com>
2025-09-13 01:33:16 +0900

    [level 3] Title: 정수 삼각형, Time: 13.98 ms, Memory: 63.2 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -1,18 +1,49 @@
+import java.util.*;
+
 class Solution {
     public int solution(int[][] triangle) {
         int answer = 0;
-        for (int i=1;i<triangle.length;i++){
-            for (int j=0;j<triangle[i].length;j++){
-                if (j==0) triangle[i][j] += triangle[i-1][j];
-                else if (j==triangle[i].length-1) triangle[i][j] += triangle[i-1][j-1];
-                else {
-                    triangle[i][j] += Math.max(triangle[i-1][j], triangle[i-1][j-1]);
+        int n = triangle.length;
+        int[][] dp = new int[n][n];
+        dp[0][0] = triangle[0][0];
+        
+        for (int i=1;i<n;i++){
+            for (int j=0;j<=i;j++){
+                if (j==0){
+                    dp[i][j] = Math.max(dp[i][j], dp[i-1][j]) + triangle[i][j];
+                    continue;
                 }
+                if (i==j){
+                    dp[i][j] = Math.max(dp[i][j], dp[i-1][j-1]) + triangle[i][j];
+                    continue;
+                }
+                dp[i][j] = Math.max(dp[i][j], dp[i-1][j-1]); 
+                dp[i][j] = Math.max(dp[i][j], dp[i-1][j]);
+                dp[i][j] += triangle[i][j];
             }
         }
-        for (int i=0;i<triangle[triangle.length-1].length;i++){
-            answer = Math.max(triangle[triangle.length-1][i], answer);
+        
+        for (int i=0;i<n;i++){
+            answer = Math.max(dp[n-1][i], answer);
         }
         return answer;
     }
+    
+    // dp[i+1][j] = Math.max(dp[i][j-1], dp[i][j]);
+    // i depth에서 i+1 depth
+    // 1.0 <- 0,0
+    // 1,1 <-0,0
+    // 2,0 <- 1,0
+    // 2,1 <- 1,0, 1,1
+    // 2,2 <- 1,1
+    // 3,0 <- 2,0
+    // 3,1 <- 2,0, 2,1
+    // 3,2 <- 2,1, 2,2
+    // 3,3 <- 2,2
+    // dp[i][j] = dp[i-1][j-1]
+    // 7
+    // 3 8
+    // 8 1 0
+    // 2 7 4 4
+    // 4 5 2 6 5
 }
