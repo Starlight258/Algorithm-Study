36fee76294ef8ee5315f638546de98284bbe5784
Mint <78211281+Starlight258@users.noreply.github.com>
2025-09-14 01:36:13 +0900

    [level 3] Title: N으로 표현, Time: 31.86 ms, Memory: 90.4 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -1,30 +1,43 @@
 import java.util.*;
+
 class Solution {
     public int solution(int N, int number) {
-        if (N==number) return 1;
-        // 1. N이 커질때 가능한 숫자 생성하기
-        ArrayList<HashSet<Integer>> list = new ArrayList<HashSet<Integer>>();
-        for (int i=0;i<9;i++){
-            list.add(new HashSet<Integer>());
-        }
-        list.get(1).add(N);
-        for (int i=2;i<=8;i++){ 
-            list.get(i).add(Integer.parseInt(String.valueOf(N).repeat(i)));
-            // 2. 숫자 만들기 
-            for (int j=1;j<i;j++){
-                for (int x:list.get(j)){
-                    for (int y:list.get(i-j)){
-                        list.get(i).add(x+y);
-                        list.get(i).add(x-y);
-                        list.get(i).add(x*y);
-                        if (y!=0) list.get(i).add(x/y);
+        if (N == number) return 1;
+
+        List<Set<Integer>> dp = new ArrayList<>(9);
+        for (int i = 0; i <= 8; i++) dp.add(new HashSet<>());
+
+        for (int k = 1; k <= 8; k++) {
+            // 1) 이어붙이기 수: N, NN, NNN ...
+            int concat = concatN(N, k);
+            dp.get(k).add(concat);
+            if (concat == number) return k;
+
+            // 2) 분할 i + (k-i) 조합
+            for (int i = 1; i < k; i++) {
+                Set<Integer> A = dp.get(i);
+                Set<Integer> B = dp.get(k - i);
+
+                for (int a : A) {
+                    for (int b : B) {
+                        dp.get(k).add(a + b);
+                        dp.get(k).add(a - b);
+                        dp.get(k).add(b - a);
+                        dp.get(k).add(a * b);
+                        if (b != 0) dp.get(k).add(a / b);
+                        if (a != 0) dp.get(k).add(b / a);
                     }
                 }
             }
-            if (list.get(i).contains(number)){
-                return i;
-            }
+
+            if (dp.get(k).contains(number)) return k;
         }
         return -1;
+    }
+
+    private int concatN(int N, int times) {
+        int val = 0;
+        for (int i = 0; i < times; i++) val = val * 10 + N;
+        return val;
     }
 }
