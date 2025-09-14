26e2a1cc5df410c1ab6b6b865c9e4f2e9f8458dd
Mint <78211281+Starlight258@users.noreply.github.com>
2025-09-15 01:25:54 +0900

    [level 3] Title: N으로 표현, Time: 36.63 ms, Memory: 96 MB -BaekjoonHub

--- 프로그래머스/3/42895. N으로 표현/N으로 표현.java
+++ 프로그래머스/3/42895. N으로 표현/N으로 표현.java
@@ -1,43 +1,53 @@
 import java.util.*;
 
 class Solution {
+    private static int MAX = 8;
+    
     public int solution(int N, int number) {
         if (N == number) return 1;
-
-        List<Set<Integer>> dp = new ArrayList<>(9);
-        for (int i = 0; i <= 8; i++) dp.add(new HashSet<>());
-
-        for (int k = 1; k <= 8; k++) {
-            // 1) 이어붙이기 수: N, NN, NNN ...
-            int concat = concatN(N, k);
+        
+        List<Set<Integer>> dp = new ArrayList<>(MAX+1);
+        for (int i=0;i<=MAX;i++){
+            dp.add(new HashSet<>());
+        }
+        
+        for (int k=1;k<=MAX;k++){
+            int concat = concat(N, k);
+            if (concat == number){
+                return k;
+            }
             dp.get(k).add(concat);
-            if (concat == number) return k;
-
-            // 2) 분할 i + (k-i) 조합
-            for (int i = 1; i < k; i++) {
+            
+            // dp[k] = dp[i] + dp[k-i]
+            for (int i=1;i<k;i++){
                 Set<Integer> A = dp.get(i);
-                Set<Integer> B = dp.get(k - i);
-
-                for (int a : A) {
-                    for (int b : B) {
-                        dp.get(k).add(a + b);
-                        dp.get(k).add(a - b);
-                        dp.get(k).add(b - a);
-                        dp.get(k).add(a * b);
-                        if (b != 0) dp.get(k).add(a / b);
-                        if (a != 0) dp.get(k).add(b / a);
+                Set<Integer> B = dp.get(k-i);
+                
+                for (int a:A){
+                    for (int b:B){
+                        dp.get(k).add(a+b);
+                        dp.get(k).add(a-b);
+                        dp.get(k).add(b-a);
+                        dp.get(k).add(a*b);
+                        if (b!=0) dp.get(k).add(a/b);
+                        if (a!=0) dp.get(k).add(b/a);
                     }
                 }
             }
-
-            if (dp.get(k).contains(number)) return k;
+            
+            if (dp.get(k).contains(number)){
+                return k;
+            }
         }
+                
         return -1;
     }
-
-    private int concatN(int N, int times) {
+    
+    private int concat(int N, int times){
         int val = 0;
-        for (int i = 0; i < times; i++) val = val * 10 + N;
+        for (int i=0;i<times;i++){
+            val = val * 10 + N;
+        }
         return val;
     }
 }
