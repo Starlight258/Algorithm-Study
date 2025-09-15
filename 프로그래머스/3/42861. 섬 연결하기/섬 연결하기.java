5b77543cc0a816a82968a4e3f857c615d97a1af6
Mint <78211281+Starlight258@users.noreply.github.com>
2025-09-16 01:41:17 +0900

    [level 3] Title: 섬 연결하기, Time: 6.96 ms, Memory: 75.5 MB -BaekjoonHub

--- 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
+++ 프로그래머스/3/42861. 섬 연결하기/섬 연결하기.java
@@ -1,54 +1,82 @@
 import java.util.*;
+
 class Solution {
-    int[] parent;
     
-    class Node implements Comparable<Node>{
-        int x;
-        int y;
-        int cost;
-        Node (int x, int y, int cost){
-            this.x = x;
-            this.y = y;
-            this.cost = cost;
+    class Edge implements Comparable<Edge> {
+        int from;
+        int to;
+        int weight;
+        
+        public Edge(int from, int to, int weight){
+            this.from = from;
+            this.to = to;
+            this.weight=weight;
         }
-        public int compareTo(Node n){
-            return Integer.compare(this.cost, n.cost);
+        
+        public int compareTo(final Edge o){
+            return this.weight - o.weight;
         }
     }
-        
+    
     public int solution(int n, int[][] costs) {
         int answer = 0;
-        List<Node> list = new ArrayList<>();
-        for (int[] cost:costs){
-            list.add(new Node(cost[0], cost[1], cost[2]));
+        List<Edge> edges = new ArrayList<>();
+        for (int i=0;i<costs.length;i++){
+            edges.add(new Edge(costs[i][0], costs[i][1], costs[i][2]));
+            edges.add(new Edge(costs[i][1], costs[i][0], costs[i][2]));
         }
-        // 비용 오름차순으로 정렬
-        Collections.sort(list);
-        
-        // 크루스칼
-        parent = new int[n];
-        for (int i=0;i<n;i++){
-            parent[i] = i;
-        }
-        // find 후 사이클 없으면 union
-        for (Node node:list){
-            if (find(node.x) != find(node.y)){
-                union(node.x, node.y);
-                answer += node.cost;
+        Collections.sort(edges);
+        int total = 0;
+        UnionFind unionFind = new UnionFind(n);
+        for (Edge edge:edges){
+            int from = edge.from;
+            int to = edge.to;
+            if (!unionFind.isConnected(from, to)){
+                unionFind.union(from, to);
+                total += edge.weight;
             }
         }
-        return answer;
+        
+        return total;
     }
     
-    public int find(int x){
-        if (parent[x]==x) return x;
-        return parent[x] = find(parent[x]);
+    class UnionFind{
+        
+        int[] parent;
+        
+        public UnionFind(int n){
+            parent = new int[n+1];
+            for (int i=1;i<=n;i++){
+                parent[i]=i;
+            }
+        }
+        
+        public int find(int x){
+            if (parent[x]!=x){
+                return parent[x] = find(parent[x]);
+            }
+            return parent[x];
+        }
+        
+        public boolean union(int x, int y){
+            int rootX = find(x);
+            int rootY = find(y);
+            if (rootX == rootY){
+                return false;
+            }
+            if (rootX != rootY){
+                if (rootX < rootY){
+                    parent[rootY] = rootX;
+                } else {
+                    parent[rootX] = rootY;
+                }
+            }
+            return true;
+        }
+        
+        public boolean isConnected(int x, int y){
+            return find(x) == find(y);
+        }
     }
-    
-    public void union(int x, int y){
-        x = parent[x];
-        y = parent[y];
-        if (x>y) parent[x] = y;
-        else parent[y] = x;
-    }
+    // MST(최소 간선 트리)
 }
