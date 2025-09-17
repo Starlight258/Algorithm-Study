db172ff496fa5004b1e63b52e736ae7fd921dfb8
Mint <78211281+Starlight258@users.noreply.github.com>
2025-09-18 00:46:42 +0900

    [level 2] Title: 배달, Time: 4.90 ms, Memory: 91.7 MB -BaekjoonHub

--- 프로그래머스/2/12978. 배달/배달.java
+++ 프로그래머스/2/12978. 배달/배달.java
@@ -1,48 +1,72 @@
 import java.util.*;
+
 class Solution {
-    class Node implements Comparable<Node>{
-        int idx;
-        int dist;
-        Node(int idx, int dist){
-            this.idx = idx;
-            this.dist = dist;
-        }
-        public int compareTo(Node n){
-            return this.dist - n.dist;
+    
+    class Edge implements Comparable<Edge>{
+        int node;
+        int cost;
+        
+        Edge(int node, int cost){
+            this.node = node;
+            this.cost = cost;
         }
         
+        public int compareTo(Edge edge){
+            return this.cost - edge.cost;
+        }
     }
-    public int solution(int N, int[][] road, int K) {
+    
+    private static int INF = (int) 1e9;
+    
+    public int solution(int N, int[][] roads, int K) {
         int answer = 0;
-        ArrayList<ArrayList<Node>> list = new ArrayList<ArrayList<Node>>();
+        int[] dist = new int[N+1];
+        List<List<Edge>> edges = new ArrayList<>();
+        Arrays.fill(dist, INF);
         for (int i=0;i<=N;i++){
-            list.add(new ArrayList<Node>());
+            edges.add(new ArrayList<>());
         }
-        for (int i=0;i<road.length;i++){
-            list.get(road[i][0]).add(new Node(road[i][1], road[i][2]));
-            list.get(road[i][1]).add(new Node(road[i][0], road[i][2]));
+        for (int[] road : roads){
+            int a = road[0];
+            int b = road[1];
+            int c = road[2];
+            edges.get(a).add(new Edge(b, c));
+            edges.get(b).add(new Edge(a, c));
         }
-        int d[] = new int[N+1];
-        Arrays.fill(d, Integer.MAX_VALUE);
-        d[1] = 0;
-        PriorityQueue<Node> pq = new PriorityQueue<Node>();
-        pq.offer(new Node(1, 0));
+        
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
+        dist[1] = 0;
+        pq.offer(new Edge(1, 0));
+        
         while (!pq.isEmpty()){
-            Node node = pq.poll();
-            int now = node.idx;
-            int dist = node.dist;
-            if (d[now]<dist) continue;
-            for (int i=0;i<list.get(now).size();i++){
-                int cost = d[now] + list.get(now).get(i).dist;
-                if (cost < d[list.get(now).get(i).idx]){
-                    d[list.get(now).get(i).idx] = cost;
-                    pq.offer(new Node(list.get(now).get(i).idx, cost));
+            Edge cur = pq.poll();
+            int curNode = cur.node;
+            int curCost = cur.cost;
+            
+            for (Edge next:edges.get(curNode)){
+                int nextNode = next.node;
+                int nextCost = next.cost;
+                if (dist[nextNode] <= nextCost){
+                    continue;
+                }
+
+                if (dist[nextNode] > dist[curNode] + nextCost){
+                    dist[nextNode] = dist[curNode] + nextCost;
+                    pq.offer(new Edge(nextNode, dist[nextNode]));
                 }
             }
         }
+        
         for (int i=1;i<=N;i++){
-            if (d[i] <= K) answer++;
+            if (dist[i] <= K){
+                answer++;
+            }
         }
         return answer;
     }
+    // 최단거리경로 : 한 지점 : 다익스트라 
+    // int[] dist = INF
+    // 거리별 오름차순 
+    // 처음 경로부터 PriorityQueue< -> 작은 가중치순대로 정렬,
+    // 하나씩 가져오기
 }
