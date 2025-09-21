56396c03431be70e008f96efec007f8029a446bc
Mint <78211281+Starlight258@users.noreply.github.com>
2025-09-22 00:58:41 +0900

    [Gold IV] Title: 호텔, Time: 104 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Gold/1106. 호텔/호텔.java
+++ 백준/Gold/1106. 호텔/호텔.java
@@ -11,36 +11,29 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int c = Integer.parseInt(st.nextToken());
         int n = Integer.parseInt(st.nextToken());
-        int[][] cities = new int[n][2];
+        int max = c + 100;
+        int[] dp = new int[max + 1];
+        Arrays.fill(dp, 1_000_000_000);
+        dp[0] = 0;
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             int cost = Integer.parseInt(st.nextToken());
-            int number = Integer.parseInt(st.nextToken());
-            cities[i] = new int[]{cost, number};
-        }
-
-        int max = c + 100;
-        int INF= 1_000_000_000;
-        int[] minCost = new int[max+1];
-        Arrays.fill(minCost, INF);
-        minCost[0] = 0;
-        for (int[] city : cities) {
-            int cost = city[0];
-            int number = city[1];
-            for (int i = number; i <= max; i += 1) {
-                minCost[i] = Math.min(minCost[i], minCost[i - number] + cost);
+            int customer = Integer.parseInt(st.nextToken());
+            for (int j = customer; j <= max; j++) {
+                dp[j] = Math.min(dp[j], dp[j - customer] + cost);
             }
         }
-        int answer = INF;
+
+        int answer = Integer.MAX_VALUE;
         for (int i = c; i <= max; i++) {
-            answer = Math.min(answer, minCost[i]);
+            answer = Math.min(answer, dp[i]);
         }
         System.out.println(answer);
     }
-    // 9원 -> 3명의 고객이 늘어난다.
-    // 18원 -> 6명의 고객
-    // 27원 -> 9명의 고객
-    // 9의 정수배만큼 고객이 늘어난다.
-    // 12 -> 5 * 2 + 1 * 2
-    // dp[i] = k : k명일때 최소 비용
+    // 9원-> 3명
+    // 18원 -> 6명
+    // 27원 -> 9명
+    // dp[9]=3;
+    // dp[18]=6
+    // dp[12] = dp[10] + dp[2]
 }
