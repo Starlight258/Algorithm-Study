231904b193a9dbed97ecf7311cba264086b0360a
Mint <78211281+Starlight258@users.noreply.github.com>
2025-09-23 01:50:08 +0900

    [Gold IV] Title: 오큰수, Time: 920 ms, Memory: 147952 KB -BaekjoonHub

---
+++ 백준/Gold/17298. 오큰수/오큰수.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        int[] answer = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Stack<Integer> stack = new Stack<>(); // index 저장용
+
+        for (int i = 0; i < n; i++) {
+            while (!stack.isEmpty() && arr[stack.peek()] < arr[i]) {
+                answer[stack.pop()] = arr[i]; // 오큰수 업데이트
+            }
+            stack.push(i);
+        }
+
+        while (!stack.isEmpty()) {
+            answer[stack.pop()] = -1;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i : answer) {
+            sb.append(i).append(" ");
+        }
+
+        System.out.println(sb);
+    }
+}
