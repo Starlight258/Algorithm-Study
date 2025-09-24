63224e99ac65293a716a4131edfcfab963263597
Mint <78211281+Starlight258@users.noreply.github.com>
2025-09-25 01:26:23 +0900

    [Gold III] Title: ACM Craft, Time: 700 ms, Memory: 242876 KB -BaekjoonHub

--- 백준/Gold/1005. ACM Craft/ACM Craft.java
+++ 백준/Gold/1005. ACM Craft/ACM Craft.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Queue;
@@ -9,69 +10,59 @@ import java.util.StringTokenizer;
 
 public class Main {
 
-    static int n;
-    static int k;
-    static int[] times;
-    static List<List<Integer>> adj;
-    static int[] indegree;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
-        StringTokenizer st;
         while (t-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int n = Integer.parseInt(st.nextToken());
+            int k = Integer.parseInt(st.nextToken());
+            int[] times = new int[n];
             st = new StringTokenizer(br.readLine());
-            n = Integer.parseInt(st.nextToken());
-            k = Integer.parseInt(st.nextToken());
-
-            st = new StringTokenizer(br.readLine());
-            times = new int[n + 1];
-            for (int i = 1; i <= n; i++) {
+            for (int i = 0; i < n; i++) {
                 times[i] = Integer.parseInt(st.nextToken());
             }
-
-            adj = new ArrayList<>();
+            List<List<Integer>> buildings = new ArrayList<>();
             for (int i = 0; i <= n; i++) {
-                adj.add(new ArrayList<>());
+                buildings.add(new ArrayList<>());
             }
-
-            indegree = new int[n + 1];
+            int[] indegree = new int[n + 1];
             for (int i = 0; i < k; i++) {
                 st = new StringTokenizer(br.readLine());
-                int a = Integer.parseInt(st.nextToken());
-                int b = Integer.parseInt(st.nextToken());
-                adj.get(a).add(b);
-                indegree[b]++;
+                int s = Integer.parseInt(st.nextToken());
+                int e = Integer.parseInt(st.nextToken());
+                buildings.get(s).add(e);
+                indegree[e]++;
             }
-
             int w = Integer.parseInt(br.readLine());
 
-            topologySort(w);
-        }
-    }
-
-    private static void topologySort(int w) {
-        Queue<Integer> q = new LinkedList<>();
-        int[] result = new int[n + 1]; // 현재 점 기준 최대시간
-
-        for (int i = 1; i <= n; i++) {
-            if (indegree[i] == 0) {
-                q.offer(i);
-                result[i] = times[i];
-            }
-        }
-
-        while (!q.isEmpty()) {
-            int cur = q.poll();
-
-            for (Integer next : adj.get(cur)) {
-                result[next] = Math.max(result[next], result[cur] + times[next]);
-                if (--indegree[next] == 0) {
-                    q.offer(next);
+            int[] answers = new int[n + 1];
+            Queue<Integer> q = new LinkedList<>();
+            for (int i = 1; i <= n; i++) {
+                if (indegree[i] == 0) {
+                    q.offer(i);
+                    answers[i] = times[i - 1];
                 }
             }
-        }
 
-        System.out.println(result[w]);
+            while (!q.isEmpty()) {
+                Integer cur = q.poll();
+                if (cur == w) {
+                    break;
+                }
+                for (Integer next : buildings.get(cur)) {
+                    indegree[next]--;
+                    answers[next] = Math.max(answers[next], answers[cur] + times[next-1]);
+                    if (indegree[next] == 0) {
+                        q.offer(next);
+                    }
+                }
+            }
+            System.out.println(answers[w]);
+        }
     }
+
+    // 순서 존재
+    // indegree[i] ==0 일때 queue에 추가
+    //
 }
