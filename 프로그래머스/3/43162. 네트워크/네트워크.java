1a358df8ec14e958006c74db86618e275b9b18f1
Mint <78211281+Starlight258@users.noreply.github.com>
2025-09-29 01:36:37 +0900

    [level 3] Title: 네트워크, Time: 1.31 ms, Memory: 85.7 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,40 +1,59 @@
 import java.util.*;
 
 class Solution {
+    
+    public class UF {
+        int[] parent;
 
-    static class UF {
-        int[] parent, rank;
-        UF(int n) {
+        public UF(int n){
             parent = new int[n];
-            rank = new int[n];
-            for (int i = 0; i < n; i++) parent[i] = i;
-        }
-        int find(int x) {
-            if (parent[x] != x) parent[x] = find(parent[x]);
-            return parent[x];
-        }
-        void union(int x, int y) {
-            int rx = find(x), ry = find(y);
-            if (rx == ry) return;
-            if (rank[rx] < rank[ry]) parent[rx] = ry;
-            else if (rank[rx] > rank[ry]) parent[ry] = rx;
-            else { parent[ry] = rx; rank[rx]++; }
-        }
-    }
-
-    public int solution(int n, int[][] computers) {
-        UF uf = new UF(n);
-
-        // 인접행렬: 자기 자신 1 포함. 대칭이므로 i<j만 보면 충분.
-        for (int i = 0; i < n; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (computers[i][j] == 1) uf.union(i, j);
+            for (int i=0;i<n;i++){
+                parent[i] = i;
             }
         }
 
-        // 각 노드의 루트를 세어 네트워크 개수 계산
-        Set<Integer> roots = new HashSet<>();
-        for (int i = 0; i < n; i++) roots.add(uf.find(i));
-        return roots.size();
+        public int find(int x){
+            if (parent[x] != x){
+                return parent[x] = find(parent[x]);
+            }
+            return x;
+        }
+
+        public void union(int x, int y){
+            int rootX = find(x);
+            int rootY = find(y);
+            if (rootX == rootY){
+                return;
+            }
+            if (rootX < rootY){
+                parent[rootY] = rootX;
+            } else {
+                parent[rootX] = rootY;
+            }
+        }
     }
+    
+    public int solution(int n, int[][] computers) {
+        UF uf = new UF(n);
+        for (int[] computer:computers){
+            for (int i=0;i<n;i++){
+                if (computer[i]==0){
+                    continue;
+                }
+                for (int j=i+1;j<n;j++){
+                    if (computer[j]==1){
+                        uf.union(i, j);
+                    }
+                }
+            }
+        }
+        
+        Set<Integer> answers = new HashSet<>();
+        for (int i=0;i<n;i++){
+            answers.add(uf.find(i));
+        }
+        return answers.size();
+    }
+    
+    // union find -> 같은 집합에 있을 경우 같은 루트 노드 가진다.
 }
