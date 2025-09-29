9dc13c6ca260b5ee1537407b8af6e263111a3597
Mint <78211281+Starlight258@users.noreply.github.com>
2025-09-30 02:36:23 +0900

    [level 3] Title: 단어 변환, Time: 0.40 ms, Memory: 89 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,46 +1,41 @@
 import java.util.*;
+
 class Solution {
-    public boolean checkWord(String s1, String s2){
-        int cnt = 0;
-        for (int i=0;i<s1.length();i++){
-            if (s1.charAt(i) != s2.charAt(i)) cnt++;
-            if (cnt>1) return false; 
-        }
-        return true;
+    static class Node {
+        String w; int d;
+        Node(String w, int d){ this.w = w; this.d = d; }
     }
+
     public int solution(String begin, String target, String[] words) {
-        int answer = 0;
-        
-        // bfs 수행
-        Queue<Integer> q = new LinkedList<>();
-        int[] visited = new int[words.length];
-        int targetIdx = -1;
-        for (int i=0;i<words.length;i++){
-            if (checkWord(words[i], begin)){
-                q.add(i);
-                visited[i] = 1;
-            }
-            if (words[i].equals(target)){
-                targetIdx = i;
-            }
-        }
-        if (targetIdx<0) return 0;
-        
-        
-        while (!q.isEmpty()){
-            int cur = q.poll();
-            
-            if (cur==targetIdx) break;
-            for (int i=0;i<words.length;i++){
-                if (i==cur) continue;
-                if (visited[i]==0 && checkWord(words[i], words[cur])){
-                    q.add(i);
-                    visited[i] = visited[cur]+1;
+        // (옵션) target 존재 체크
+        boolean hasTarget = false;
+        for (String w : words) if (w.equals(target)) hasTarget = true;
+        if (!hasTarget) return 0;
+
+        boolean[] visited = new boolean[words.length];
+        Queue<Node> q = new ArrayDeque<>();
+        q.offer(new Node(begin, 0));
+
+        while (!q.isEmpty()) {
+            Node cur = q.poll();
+            if (cur.w.equals(target)) return cur.d;
+
+            for (int i = 0; i < words.length; i++) {
+                if (visited[i]) continue;
+                if (isAdj(cur.w, words[i])) {
+                    visited[i] = true;                 // enqueue 시점에 방문 체크
+                    q.offer(new Node(words[i], cur.d + 1));
                 }
             }
         }
-        
-        // 정답        
-        return visited[targetIdx];
+        return 0;
+    }
+
+    private boolean isAdj(String a, String b) {
+        int diff = 0;
+        for (int i = 0; i < a.length(); i++) {
+            if (a.charAt(i) != b.charAt(i) && ++diff > 1) return false;
+        }
+        return diff == 1;
     }
 }
