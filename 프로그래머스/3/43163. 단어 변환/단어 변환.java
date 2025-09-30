55d461f49d7a077456cdd35c2709b24eae7cc58b
Mint <78211281+Starlight258@users.noreply.github.com>
2025-10-01 01:14:24 +0900

    [level 3] Title: 단어 변환, Time: 0.52 ms, Memory: 86.9 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,41 +1,57 @@
 import java.util.*;
 
 class Solution {
-    static class Node {
-        String w; int d;
-        Node(String w, int d){ this.w = w; this.d = d; }
+    class Node {
+        String w;
+        int d;
+        
+        Node(String w, int d){
+            this.w = w;
+            this.d = d;
+        }
     }
-
     public int solution(String begin, String target, String[] words) {
-        // (옵션) target 존재 체크
+        int answer = 0;
         boolean hasTarget = false;
-        for (String w : words) if (w.equals(target)) hasTarget = true;
-        if (!hasTarget) return 0;
-
-        boolean[] visited = new boolean[words.length];
-        Queue<Node> q = new ArrayDeque<>();
-        q.offer(new Node(begin, 0));
-
-        while (!q.isEmpty()) {
-            Node cur = q.poll();
-            if (cur.w.equals(target)) return cur.d;
-
-            for (int i = 0; i < words.length; i++) {
-                if (visited[i]) continue;
-                if (isAdj(cur.w, words[i])) {
-                    visited[i] = true;                 // enqueue 시점에 방문 체크
-                    q.offer(new Node(words[i], cur.d + 1));
-                }
+        for (String word:words){
+            if (word.equals(target)){
+                hasTarget = true;
+                break;
             }
         }
-        return 0;
-    }
-
-    private boolean isAdj(String a, String b) {
-        int diff = 0;
-        for (int i = 0; i < a.length(); i++) {
-            if (a.charAt(i) != b.charAt(i) && ++diff > 1) return false;
+        Queue<Node> queue = new ArrayDeque<>();
+        queue.offer(new Node(begin, 0));
+        boolean[] visited = new boolean[words.length];
+        while (!queue.isEmpty()){
+            Node node = queue.poll();
+            if (node.w.equals(target)){
+                return node.d;
+            }
+            for (int i=0;i<words.length;i++){
+                if (visited[i]) continue;
+                String word = words[i];
+                if (isAdj(node.w, word)){
+                  queue.offer(new Node(word, node.d+1));
+                  visited[i] = true;
+              }
+            }
         }
-        return diff == 1;
+        
+        return answer;
+    }
+    private boolean isAdj(String w1, String w2){
+        int diff = 0;
+        for (int i=0;i<w1.length();i++){
+            if (w1.charAt(i) != w2.charAt(i)){
+                diff++;
+            }
+            if (diff>2){
+                return false;
+            }
+        }
+        if (diff==1){
+            return true;
+        }
+        return false;
     }
 }
