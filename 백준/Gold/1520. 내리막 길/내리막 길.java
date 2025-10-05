2952877eb4d38b716c7280721e796e62092ba7a4
Mint <78211281+Starlight258@users.noreply.github.com>
2025-10-06 01:42:19 +0900

    [Gold III] Title: 내리막 길, Time: 348 ms, Memory: 36816 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.java
+++ 백준/Gold/1520. 내리막 길/내리막 길.java
@@ -1,57 +1,55 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
-public class Main {
+class Main {
 
-    private static int[] dy = {-1, 0, 1, 0};
-    private static int[] dx = {0, 1, 0, -1};
-    private static int[][] map;
-    private static int[][] dp;
-    private static int m;
-    private static int n;
+    static int N, M;
+    static int[][] map;
+    static int[][] dp;
+    static int[] dy = {-1, 0, 1, 0};
+    static int[] dx = {0, 1, 0, -1};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        m = Integer.parseInt(st.nextToken());
-        n = Integer.parseInt(st.nextToken());
-        map = new int[m][n];
-        for (int i = 0; i < m; i++) {
+        M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        map = new int[M][N];
+        dp = new int[M][N];
+
+        for (int i = 0; i < M; i++) {
             st = new StringTokenizer(br.readLine());
-            for (int j = 0; j < n; j++) {
+            for (int j = 0; j < N; j++) {
                 map[i][j] = Integer.parseInt(st.nextToken());
+                dp[i][j] = -1;
             }
-        }
-        dp = new int[m][n];
-        for (int i = 0; i < m; i++) {
-            Arrays.fill(dp[i], -1);
         }
 
         System.out.println(dfs(0, 0));
     }
 
-    private static int dfs(final int y, final int x) {
-        if (y == m - 1 && x == n - 1) {
+    private static int dfs(int y, int x) {
+        if (y == M - 1 && x == N - 1) {
             return 1;
         }
+
         if (dp[y][x] != -1) {
             return dp[y][x];
         }
+
         dp[y][x] = 0;
         for (int i = 0; i < 4; i++) {
             int ny = y + dy[i];
             int nx = x + dx[i];
-            if (ny < 0 || nx < 0 || ny >= m || nx >= n) {
+            if (ny < 0 || nx < 0 || ny >= M | nx >= N) {
                 continue;
             }
-            if (map[y][x] > map[ny][nx]) {
+            if (map[ny][nx] < map[y][x]) {
                 dp[y][x] += dfs(ny, nx);
             }
         }
         return dp[y][x];
     }
-
 }
