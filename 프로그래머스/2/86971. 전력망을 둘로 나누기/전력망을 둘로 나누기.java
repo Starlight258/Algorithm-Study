71f45e3beaade4e9214d961869acf244ce3681a7
Mint <78211281+Starlight258@users.noreply.github.com>
2025-10-07 01:52:48 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 4.36 ms, Memory: 82.5 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -1,44 +1,40 @@
 import java.util.*;
 
 class Solution {
-    List<List<Integer>> list;
-    int min;
-    
     public int solution(int n, int[][] wires) {
-        list = new ArrayList<>(n+1);
-        min = Integer.MAX_VALUE;
-        for (int i=0;i<=n;i++){
-            list.add(new ArrayList<>());
-        }
+        int answer = n;
+        
+        Map<Integer, List<Integer>> mp = new HashMap<>();
         for (int[] wire:wires){
-            list.get(wire[0]).add(wire[1]);
-            list.get(wire[1]).add(wire[0]);
+            mp.computeIfAbsent(wire[0], k -> new ArrayList<>()).add(wire[1]);
+            mp.computeIfAbsent(wire[1], k -> new ArrayList<>()).add(wire[0]);
         }
-        // 완전탐색
+        // 하나의 간선 골라서 간선 탐색하기 
+        // k개, n-k 개
         for (int[] wire:wires){
-            int v1 = wire[0];
-            int v2 = wire[1];
-            
-            list.get(v1).remove(Integer.valueOf(v2));
-            list.get(v2).remove(Integer.valueOf(v1));
+            int a = wire[0];
+            int b = wire[1];
             boolean[] visited = new boolean[n+1];
-            int cnt = dfs(visited, 1);
-            min = Math.min(Math.abs((n-cnt)-cnt), min);
-            list.get(v1).add(v2);
-            list.get(v2).add(v1);
+            // a 기준으로 순회
+            visited[b] = true;
+            int count = dfs(a, mp, n, visited);
+            int diff = Math.abs(count - (n-count));
+            answer = Math.min(diff, answer);
         }
         
-        return min;
+        return answer;
     }
-    int dfs(boolean[] visited, int v){
-        visited[v] = true;
-       int cnt = 1;
+    
+    int dfs(int cur, Map<Integer, List<Integer>> mp, int n, boolean[] visited){
+        visited[cur] = true;
+        int count = 1;
         
-        for (int next:list.get(v)){
-            if (!visited[next]){
-                cnt += dfs(visited, next);
-            }
+        for (Integer next:mp.get(cur)){
+            if (visited[next]){
+                continue;
+            }    
+            count += dfs(next, mp, n, visited);    
         }
-        return cnt;
+        return count;
     }
 }
