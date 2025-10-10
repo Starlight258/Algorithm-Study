e672d360c1ebb40dd7663f402cab5280d418442b
Mint <78211281+Starlight258@users.noreply.github.com>
2025-10-11 02:33:32 +0900

    [Silver I] Title: 단지번호붙이기, Time: 116 ms, Memory: 14324 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.java
@@ -2,66 +2,62 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class Main {
 
-    static int n;
-    static int[][] graph;
-    static boolean[][] visited;
-    static int[] dy = {-1, 0, 1, 0};
-    static int[] dx = {0, 1, 0, -1};
+    private static final int[] dy = {-1, 0, 1, 0};
+    private static final int[] dx = {0, 1, 0, -1};
 
-    private static int dfs(int y, int x) {
+    private static int n;
+    private static int[][] map;
+    private static boolean[][] visited;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        n = Integer.parseInt(br.readLine());
+        map = new int[n][n];
+        visited = new boolean[n][n];
+        for (int i = 0; i < n; i++) {
+            String line = br.readLine();
+            for (int j = 0; j < n; j++) {
+                map[i][j] = line.charAt(j) - '0';
+            }
+        }
+
+        List<Integer> answers = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                if (visited[i][j]) {
+                    continue;
+                }
+                if (map[i][j] == 1) {
+                    int count = dfs(i, j);
+                    answers.add(count);
+                }
+            }
+        }
+        answers.sort(Integer::compareTo);
+
+        System.out.println(answers.size());
+        for (Integer answer : answers) {
+            System.out.println(answer);
+        }
+    }
+
+    private static int dfs(final int y, final int x) {
         int count = 1;
         visited[y][x] = true;
-
         for (int i = 0; i < 4; i++) {
             int ny = y + dy[i];
             int nx = x + dx[i];
             if (ny < 0 || nx < 0 || ny >= n || nx >= n) {
                 continue;
             }
-            if (graph[ny][nx] == 0 || visited[ny][nx]) {
-                continue;
+            if (!visited[ny][nx] && map[ny][nx] == 1) {
+                count += dfs(ny, nx);
             }
-            count += dfs(ny, nx);
         }
-
         return count;
-    }
-
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        n = Integer.parseInt(br.readLine());
-        graph = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            String line = br.readLine();
-            for (int j = 0; j < n; j++) {
-                graph[i][j] = line.charAt(j) - '0';
-            }
-        }
-        visited = new boolean[n][n];
-
-        //2. dfs 수행
-        int answer = 0;
-        List<Integer> homes = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; j < n; j++) {
-                if (!visited[i][j] && graph[i][j] == 1) {
-                    answer++;
-                    homes.add(dfs(i, j));
-                }
-            }
-        }
-
-        //3. 출력
-        System.out.println(answer);
-
-        Collections.sort(homes);
-        for (Integer home : homes) {
-            System.out.println(home);
-        }
     }
 }
