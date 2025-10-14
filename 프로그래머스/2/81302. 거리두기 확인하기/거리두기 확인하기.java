709817e33295294dceac11225bb608361653f751
Mint <78211281+Starlight258@users.noreply.github.com>
2025-10-15 00:09:30 +0900

    [level 2] Title: 거리두기 확인하기, Time: 0.10 ms, Memory: 84.2 MB -BaekjoonHub

--- 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
+++ 프로그래머스/2/81302. 거리두기 확인하기/거리두기 확인하기.java
@@ -1,85 +1,102 @@
 import java.util.*;
 
 class Solution {
-    private int[] dy = {-1,0,1,0};
-    private int[] dx = {0,1,0,-1};
+    
+    private static int[] dy = {-1,0,1,0};
+    private static int[] dx = {0,1,0,-1};
+    
+    private static int[] diay = {1, -1, 1, -1};
+    private static int[] diax = {1, -1, -1, 1};
+    
     private char[][] p;
     
     public int[] solution(String[][] places) {
         int n = places.length;
         int[] answer = new int[n];
-        // 배열
-        p = new char[5][5];
         
-        for (int c=0;c<n;c++){
-             for (int i=0;i<5;i++){
+        for (int cur=0;cur<n;cur++){
+            String[] place = places[cur];
+            p = new char[5][5];
+            for (int i=0;i<5;i++){
                 for (int j=0;j<5;j++){
-                    p[i][j] = places[c][i].charAt(j);
+                    p[i][j] = place[i].charAt(j);
                 }
             }
-            
-            boolean isZero = false;
+                    
+            // 확인하기
+            // 거리 +1
+            boolean hasNear = false;
             for (int i=0;i<5;i++){
                 for (int j=0;j<5;j++){
                     if (p[i][j]=='P'){
-                        if (!bfs(i, j)){
-                            isZero = true;
+                        if (findNear(i, j) || findNextNear(i,j) || findNextNear(i,j ) || findDiagonal(i, j)){
+                            hasNear = true;
                             break;
-                        }
-                    }
-                    if (isZero){
-                        break;
+                        };
+                        
                     }
                 }
+                if (hasNear){
+                    answer[cur] = 0;
+                    break;
+                }
             }
-            if (isZero){
-                answer[c] = 0;
-            } else {
-                answer[c] = 1;
+            if (!hasNear){
+                answer[cur] = 1;
             }
         }
-       
-        
         return answer;
     }
     
-    private boolean bfs(int sy, int sx){
-        
-        Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[]{sy,sx});
-    
-        int[][] visited = new int[5][5];
-        visited[sy][sx] = 1;
-        
-        // bfs
-        while (!queue.isEmpty()){
-            int[] node = queue.poll();
-            int y = node[0];
-            int x = node[1];
-            if (visited[y][x]>2){
-                continue;
-            }
-            
-            for (int i=0;i<4;i++){
-                int ny = y + dy[i];
-                int nx = x + dx[i];
-                
-                if (ny<0||nx<0||ny>=5||nx>=5){
-                    continue;
-                }
-                if (visited[ny][nx]>0 || p[ny][nx]=='X'){
-                    continue;
-                }
-                if (p[ny][nx]=='P'){
-                    return false; // 거리두기 안지킴
-                }
-                visited[ny][nx] = visited[y][x]+1;
-                queue.offer(new int[]{ny,nx});
+    private boolean findNear(int y, int x){
+        for (int i=0;i<4;i++){
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+            if (ny<0||nx<0||ny>=5||nx>=5) continue;
+            if (p[ny][nx] == 'P'){
+                return true;
             }
         }
-        return true;
+        return false;
     }
-    // 모든 좌표 bfs
-    // 빈 테이블로만 이동 가능
-    // 응시자마다 돌아가면서 순회 -> 거리 2 초과시 종료 -> 찾으면 0
+    
+     
+    private boolean findNextNear(int y, int x){
+        for (int i=0;i<4;i++){
+            int ny = y + dy[i] * 2;
+            int nx = x + dx[i] * 2;
+            int my = y + dy[i];
+            int mx = x + dx[i];
+            if (ny<0||nx<0||ny>=5||nx>=5) continue;
+            if (p[ny][nx] == 'P' && p[my][mx]=='O'){
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    private boolean findDiagonal(int y, int x){
+        for (int i=0;i<4;i++){
+            int ny = y + diay[i];
+            int nx = x + diax[i];
+            int my = y + diay[i];
+            int mx = x;
+            int oy = y;
+            int ox = x + diax[i];
+            if (ny<0||nx<0||ny>=5||nx>=5) continue;
+            if (p[ny][nx] == 'P' && (p[oy][ox]=='O' || p[my][mx]=='O')){
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    // 거리두기 1
+    // (r,c) -> (r+1, c), (r-1,c), (r, c+1), (r, c-1)
+    // PXP
+    // 파티션을 사이에 두고 앉은 경우 -> 거리 두기
+    // P X
+    // X P 
+    // (r,c) - (r+1, c+1), (r-1, c-1), (r-1, c+1), (r+1, c-1) 
+    
 }
