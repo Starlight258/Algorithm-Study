be43f904aac370c10d1260a00a91eb7f91b4ef7b
Mint <78211281+Starlight258@users.noreply.github.com>
2025-10-15 01:41:00 +0900

    [level 2] Title: 문자열 압축, Time: 5.85 ms, Memory: 98.1 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -1,24 +1,51 @@
+import java.util.*;
+
 class Solution {
     public int solution(String s) {
-        int answer = s.length();
-        //1. 문자열 쪼개기
-        for (int i=1;i<=s.length();i++){ // 쪼갤 단어 개수
-            StringBuffer result = new StringBuffer();
-            String prev = s.substring(0,i);
-            int count = 1;
-            for (int j=i;j<s.length();j+=i){ // 인덱스
-                String split = s.substring(j, Math.min(j + i, s.length()));  
-                if (prev.equals(split)){
-                    count++;
+        int n = s.length();
+        int answer = n;
+        List<String> str = new ArrayList<>();
+        for (int c=1; c<=n/2; c++){
+            // 자르기
+            str = new ArrayList<>();
+            for (int i=0;i<n;i+=c){
+                if (i+c>=n){
+                    str.add(s.substring(i)); 
                 } else {
-                    result.append(count>1?count:"").append(prev);
-                    count=1;
-                    prev = split;
+                    str.add(s.substring(i, i+c)); 
                 }
             }
-            result.append(count>1?count:"").append(prev);
-            answer = Math.min(answer, result.length());
+            
+            // 같은 경우 count 추가하기
+            String prev = "";
+            int count = 1;
+            StringBuilder sb = new StringBuilder();
+            boolean hasAppend = false;
+            for (String word:str){
+                if (word.equals(prev)){
+                    count++;
+                    hasAppend = false;
+                } else {
+                    if (count!=1){
+                        sb.append(count);
+                    }
+                    sb.append(prev);
+                    hasAppend = true;
+                    count=1;
+                }
+                prev = word;
+            }
+
+            if (count!=1){
+                sb.append(count);
+            }
+            sb.append(prev);
+
+            answer = Math.min(answer, sb.length());
         }
+        
         return answer;
     }
+    // 1~n/2 글자까지 쪼개서 count
+    // count+글자.. 가장 짧은 것의 길이
 }
