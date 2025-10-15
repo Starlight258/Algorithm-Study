7da15979c9db5def31df374a2981fc377e683ce1
Mint <78211281+Starlight258@users.noreply.github.com>
2025-10-16 01:21:48 +0900

    [level 2] Title: 괄호 변환, Time: 3.11 ms, Memory: 87.3 MB -BaekjoonHub

--- 프로그래머스/2/60058. 괄호 변환/괄호 변환.java
+++ 프로그래머스/2/60058. 괄호 변환/괄호 변환.java
@@ -1,60 +1,85 @@
 import java.util.*;
 
 class Solution {
+    
+    private static int n = 0;
+    private static int endIndex = 0;
+    
     public String solution(String p) {
-        return steps(p);
+        String answer = "";
+        if (p.isEmpty() || isCorrect(p)){
+            return p;
+        }
+        
+        // 2. 문자열 w를 두 "균형잡힌 괄호 문자열" u, v로 분리합니다. 단, u는 "균형잡힌 괄호 문자열"로 더 이상 분리할 수 없어야 하며, v는 빈 문자열이 될 수 있습니다. 
+        // (())()
+        // (()) / ()
+        // (())() / 
+        n = p.length();
+        int lastIndex = seperate(n, p);
+        String u = p.substring(0, lastIndex+1);
+        String v = p.substring(lastIndex+1);
+        // 3. 문자열 u가 "올바른 괄호 문자열" 이라면 문자열 v에 대해 1단계부터 다시 수행합니다. 
+        // 3-1. 수행한 결과 문자열을 u에 이어 붙인 후 반환합니다. 
+        if (isCorrect(u)){
+            return u + solution(v);
+        } else {
+            return processFour(u, v);
+        }
     }
     
-    private String steps(String p){
-        if (p.isEmpty() || isCorrect(p)) return p;
-        for (int i=1;i<=p.length();i++){
-            String u = p.substring(0, i);
-            String v = p.substring(i);
-            if (isBalanced(u) && isBalanced(v)){
-                String result = steps(v);
-                if (isCorrect(u)){
-                    return u + result;
-                } else {
-                    return "(" + result + ")" + trans(u);
-                }
+    private int seperate(int n, String p){
+        int count = 0;
+        int lc = 0, rc = 0;
+        boolean hasExit = false;
+        for (int i=0;i<p.length();i++){
+            char c = p.charAt(i);
+            if (c == '('){
+                lc++;
+            } else if (c == ')'){
+                rc++;
+            }
+            if (lc == rc){
+                return i;
             }
         }
-        return "";
+        return n-1;
     }
     
-    private String trans(String u){
-        u = u.substring(1, u.length()-1);
-        String result = "";
+    
+    // 균형 잡힌 괄호 문자열 : '(' 개수와 ')' 개수가 같음 
+    // 올바른 괄호 문자열 : '(' 개수와 ')' 괄호의 짝이 모두 맞음
+    // "(()))(" : 각 3개, 올바른 괄호 문자열은 아님
+    // 올바른 괄호 문자열
+    private boolean isCorrect(String u){
+        Deque<Character> deque = new ArrayDeque<>();
         for (char c:u.toCharArray()){
-            if (c=='(') result += ')';
-            else result += '(';
-        }
-        return result;
-    }
-    
-    // 균형잡힌 괄호 문자열 체크함수
-    private boolean isBalanced(String p){
-        int leftCount = 0; int rightCount = 0;
-        for (char c:p.toCharArray()){
-            if (c == '(') leftCount++;
-            else rightCount++;
-        }
-        return leftCount == rightCount;
-    }
-    
-    // 올바른 괄호 문자열 체크함수
-    private boolean isCorrect(String p){
-        if (!isBalanced(p)) return false;
-        Stack<Character> stk = new Stack<>();
-        for (char pc:p.toCharArray()){
-            if (pc=='('){
-                stk.push(pc);
+            if (c == '('){
+                deque.add(c);                
             } else {
-                if (!stk.empty()){
-                    stk.pop();
-                } else return false;
+                if (deque.isEmpty()){
+                    return false;
+                }
+                deque.pop();
             }
+            
         }
-        return stk.isEmpty();
+        if (!deque.isEmpty()){
+            return false;
+        }
+        return true;
+    }
+    
+    private String processFour(String u, String v){
+        StringBuilder sb = new StringBuilder("(");
+        sb.append(solution(v)).append(")");
+        int length = u.length();
+        String nu = u.substring(1, length-1);
+        nu = nu.replace("(", "0");
+        nu = nu.replace(")", "1");
+        nu = nu.replace("0", ")");
+        nu = nu.replace("1", "(");
+        sb.append(nu);
+        return sb.toString();
     }
 }
