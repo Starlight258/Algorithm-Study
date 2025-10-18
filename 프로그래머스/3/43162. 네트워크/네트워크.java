a798bc9b7c9acbb20828f90d09fba66c7de6e938
Mint <78211281+Starlight258@users.noreply.github.com>
2025-10-19 03:41:38 +0900

    [level 3] Title: 네트워크, Time: 0.74 ms, Memory: 83 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -2,58 +2,53 @@ import java.util.*;
 
 class Solution {
     
-    public class UF {
+    class UF {
+        
         int[] parent;
-
-        public UF(int n){
+        
+        UF (int n){
             parent = new int[n];
             for (int i=0;i<n;i++){
                 parent[i] = i;
             }
         }
-
-        public int find(int x){
-            if (parent[x] != x){
-                return parent[x] = find(parent[x]);
-            }
-            return x;
-        }
-
-        public void union(int x, int y){
-            int rootX = find(x);
-            int rootY = find(y);
-            if (rootX == rootY){
-                return;
-            }
-            if (rootX < rootY){
-                parent[rootY] = rootX;
-            } else {
-                parent[rootX] = rootY;
-            }
-        }
-    }
-    
-    public int solution(int n, int[][] computers) {
-        UF uf = new UF(n);
-        for (int[] computer:computers){
-            for (int i=0;i<n;i++){
-                if (computer[i]==0){
-                    continue;
-                }
-                for (int j=i+1;j<n;j++){
-                    if (computer[j]==1){
-                        uf.union(i, j);
-                    }
+        
+        public void union(int a, int b){
+            int pa = find(a);
+            int pb = find(b);
+            if (pa != pb){
+                if (pa<pb){
+                    parent[pb] = pa;
+                } else {
+                    parent[pa] = pb;
                 }
             }
         }
         
-        Set<Integer> answers = new HashSet<>();
-        for (int i=0;i<n;i++){
-            answers.add(uf.find(i));
+        private int find(int node){
+            if (parent[node]!=node){
+                return parent[node] = find(parent[node]);
+            }
+            
+            return node;
         }
-        return answers.size();
     }
-    
-    // union find -> 같은 집합에 있을 경우 같은 루트 노드 가진다.
+    public int solution(int n, int[][] computers) {
+        int answer = 0;
+        UF uf = new UF(n);
+        for (int i=0;i<n;i++){
+            for (int j=0;j<n;j++){
+                if (computers[i][j]==1){
+                     uf.union(i, j);
+                }
+            }
+        }
+                
+        Set<Integer> set = new HashSet<>();
+        for (int i=0;i<n;i++){
+            set.add(uf.find(i));
+        }
+        
+        return set.size();
+    }
 }
