d4600755bb9c41e387d92e426d30b1c2b0e6826f
Mint <78211281+Starlight258@users.noreply.github.com>
2025-10-20 02:15:22 +0900

    [level 3] Title: 여행경로, Time: 0.88 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,29 +1,32 @@
 import java.util.*;
 
 class Solution {
+    
     public String[] solution(String[][] tickets) {
+        
         List<String> answer = new ArrayList<>();
-        Map<String, PriorityQueue<String>> map = new HashMap<>();
+        Map<String, PriorityQueue<String>> q = new HashMap<>();
+        
         for (String[] ticket:tickets){
-            map.computeIfAbsent(ticket[0], k -> new PriorityQueue<String>()).offer(ticket[1]);
+            q.computeIfAbsent(ticket[0], k -> new PriorityQueue<>())
+                .offer(ticket[1]);        
         }
         
         Stack<String> stk = new Stack<>();
         stk.push("ICN");
-        while (!stk.isEmpty()){
+        
+        while (!stk.isEmpty()){     
             String cur = stk.peek();
-            PriorityQueue<String> queue = map.get(cur);
-            if (queue != null && !queue.isEmpty()){
-                stk.push(queue.poll());
+            
+            if (q.containsKey(cur) && !q.get(cur).isEmpty()) {
+                 stk.push(q.get(cur).poll());   
             } else {
                 answer.add(stk.pop());
             }
         }
         
         Collections.reverse(answer);
-        return answer.toArray(String[]::new);
+        
+        return answer.toArray(new String[0]);
     }
-    // 큐로 진행, 우선순위 큐 : Map<String, PriorityQueue<String>> map
-    // 큐에 없을 때까지 진행
-    // 모든 항공권을 사용해야하므로 Stack 먼저 집어넣기 Stack<String>
 }
