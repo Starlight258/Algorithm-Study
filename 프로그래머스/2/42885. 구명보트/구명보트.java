e22dd25b1de64ba202d00c045fc09b33266ba5f3
Mint <78211281+Starlight258@users.noreply.github.com>
2025-10-29 02:41:47 +0900

    [level 2] Title: 구명보트, Time: 13.52 ms, Memory: 58.2 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.java
+++ 프로그래머스/2/42885. 구명보트/구명보트.java
@@ -1,19 +1,26 @@
 import java.util.*;
+
 class Solution {
     public int solution(int[] people, int limit) {
         int answer = 0;
         Arrays.sort(people);
-        int fpos = 0; int lpos = people.length -1;
-        while (fpos<lpos){
-            int remain = limit - people[fpos];
-            if (remain>=people[lpos]) {
-                fpos++; lpos--;
-            } else { // 20,30,50,70, 90
-                lpos--; // 30, 30, 30 // 90
-            }
+        int n = people.length;
+        int left = 0, right = n-1;
+        while (left <= right){
+            if (people[left] + people[right] <=limit){
+                answer++;
+                left++; right--;
+                continue;
+            } 
             answer++;
-            if (fpos == lpos) answer++;
+            right--;
         }
+        
         return answer;
     }
+    // 20 50 50 70 80
+    // (20 + 80) (50, 50) (70)
+    // (20 + 50) (50) (70) (80)
+    // a + b = limit -> b = limit - a
+    // 작은 것부터 순회 돌면서 limit - a인 가장 큰 숫자끼리 visited, count
 }
