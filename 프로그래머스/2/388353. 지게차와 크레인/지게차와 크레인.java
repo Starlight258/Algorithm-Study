c35f4a3c803d68046a2d7f5a7911e423e3f73a7c
Mint <78211281+Starlight258@users.noreply.github.com>
2025-10-30 21:42:49 +0900

    [level 2] Title: 지게차와 크레인, Time: 45.87 ms, Memory: 79.2 MB -BaekjoonHub

--- 프로그래머스/2/388353. 지게차와 크레인/지게차와 크레인.java
+++ 프로그래머스/2/388353. 지게차와 크레인/지게차와 크레인.java
@@ -1,8 +1,7 @@
 import java.util.*;
 
 class Solution {
-    
-    class Node{
+    class Node {
         int y;
         int x;
         Node(int y, int x){
@@ -13,92 +12,93 @@ class Solution {
     
     private static int[] dy = {-1,0,1,0};
     private static int[] dx = {0,1,0,-1};
+    
+    private char[][] map;
     private int n;
     private int m;
-    private int answer = 0;
-    private char[][] arr;
+    private int answer;
     
     public int solution(String[] storage, String[] requests) {
+        answer = 0;
+        // 1. 패딩하기
         n = storage.length;
         m = storage[0].length();
-        // 0. 패딩하기
-        arr = new char[n+2][m+2];
+        map = new char[n+2][m+2];
         
         for (int i=0;i<n+2;i++){
-            Arrays.fill(arr[i], 'o'); // air
+            Arrays.fill(map[i], 'o');
         }
         for (int i=0;i<n;i++){
             char[] s = storage[i].toCharArray();
-            System.arraycopy(s, 0, arr[i+1], 1, m);
+            System.arraycopy(s, 0, map[i+1], 1, m);
         }
         n+=2;
         m+=2;
         
-        // 요청 확인하기
+        // 2. 명령어 확인하기
         for (String request:requests){
-            int size = request.length();
+            int length = request.length();
             char c = request.charAt(0);
-            if (size==1){
-                contactAir(arr, c);
-            } else if(size==2){
-                if (request.charAt(0) != request.charAt(1)){
-                    continue;
-                }
-                visitAll(arr, c);
-            } else {
-                continue;
-            }
+            if (length == 1){
+                bfs(c);    
+            } else if (length == 2){
+                if (c != request.charAt(1)) continue;
+                bfsAll(c);
+            } 
+            
         }
-        return (n-2)*(m-2) - answer;
+        
+        return (n-2) * (m-2) - answer;
     }
     
-    private void contactAir(char[][] arr, char target){
+    private void bfs(char c){
         Queue<Node> q = new ArrayDeque<>();
         boolean[][] visited = new boolean[n][m];
         q.offer(new Node(0, 0));
         visited[0][0] = true;
-        
         while (!q.isEmpty()){
             Node node = q.poll();
             int y = node.y;
             int x = node.x;
             for (int i=0;i<4;i++){
-                int ny = y + dy[i];
+                int ny = y+ dy[i];
                 int nx = x + dx[i];
                 if (ny<0||nx<0||ny>=n||nx>=m || visited[ny][nx]) continue;
-                char c = arr[ny][nx];
-                if (c!= target && c!='o') continue;
-                if (c == 'o'){
+                char cur = map[ny][nx];
+                if (!(cur == 'o' || cur==c)){
+                    continue;
+                }
+                if (map[ny][nx] == 'o'){
                     q.offer(new Node(ny,nx));
+                } 
+                if (map[ny][nx] == c){
+                    answer++;
                 }
                 visited[ny][nx] = true;
             }
         }
         
-        // 방문한 곳 o로 표시
+        // 'o'로 표시
         for (int i=0;i<n;i++){
             for (int j=0;j<m;j++){
-                if (visited[i][j] && arr[i][j] != 'o'){
-                    arr[i][j] = 'o';
+                if (visited[i][j] && map[i][j] != 'o'){
+                    map[i][j] = 'o';
+                }
+            }
+        }
+    }
+    
+    private void bfsAll(char c){
+        for (int i=0;i<n;i++){
+            for (int j=0;j<m;j++){
+                if (map[i][j] == c){
+                    map[i][j] = 'o';
                     answer++;
                 }
             }
         }
     }
     
-    private void visitAll(char[][] arr, char c){
-        for (int i=0;i<n;i++){
-            for (int j=0;j<m;j++){
-                if (arr[i][j] == c){
-                    answer++;
-                    arr[i][j] = 'o';
-                }
-            }
-        }
-    }
-    
-    // 1. 패딩하기 -> 표시하기
-    // 2. bfs로 표시하기 
-    // 3. 
-    
+    // 1. 외부에서 접근 가능하도록 하기 -> 패딩하기
+    // 2. bfs, -> o인 부분만 계속 queue에 집어넣기, 그 외는 answer++;
 }
