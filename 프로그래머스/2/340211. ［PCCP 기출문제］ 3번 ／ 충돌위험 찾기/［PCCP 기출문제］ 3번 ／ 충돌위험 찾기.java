e7e87e29d632b03f3f4cf13d3c09a80bec934962
Mint <78211281+Starlight258@users.noreply.github.com>
2025-11-01 16:00:00 +0900

    [level 2] Title: [PCCP 기출문제] 3번 / 충돌위험 찾기, Time: 479.04 ms, Memory: 307 MB -BaekjoonHub

--- 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.java
+++ 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.java
@@ -1,80 +1,101 @@
 import java.util.*;
 
 class Solution {
-    class Point {
-        int x, y;
-        Point(int x, int y) {
-            this.x = x;
+    
+    private int[][] visited;
+    private int answer = 0;
+    
+    class Node{
+        
+        int y;
+        int x;
+        int robotIndex;
+        int routeIndex = 0;
+        
+        Node (int y, int x, int robotIndex){
             this.y = y;
+            this.x = x;
+            this.robotIndex = robotIndex;
         }
         
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            Point point = (Point) o;
-            return x == point.x && y == point.y;
-        }
-        
-        @Override
-        public int hashCode() {
-            return Objects.hash(x, y);
+        void moveNext(int destY, int destX){
+            if (destY == y){
+                // x 이동
+                if (destX < x){
+                    this.x--;
+                } else {
+                    this.x++;
+                }
+                return;
+            } 
+            // y 이동 
+            if (destY < y){
+                this.y--;
+            } else {
+                this.y++;
+            }   
         }
     }
-
     public int solution(int[][] points, int[][] routes) {
-        int answer = 0;
-        Map<Integer, List<Point>> map = new HashMap<>(); // 시간별 로봇 위치 저장
-
-        for (int i = 0; i < routes.length; i++) { // 로봇의 개수
-            int level = 1; // 시간
-            for (int j = 1; j < routes[i].length; j++) { // 경로 개수
-                int currentX = points[routes[i][j - 1] - 1][0];
-                int currentY = points[routes[i][j - 1] - 1][1];
-                int targetX = points[routes[i][j] - 1][0];
-                int targetY = points[routes[i][j] - 1][1];
-                
-                while (currentX != targetX) {
-                    List<Point> pList = map.getOrDefault(level, new ArrayList<>());
-                    pList.add(new Point(currentX, currentY));
-                    map.put(level++, pList); // 덮어쓰기
-
-                    if (currentX > targetX) 
-                        currentX--;
-                    else 
-                        currentX++;
-                }
-
-                while (currentY != targetY) {
-                    List<Point> pList = map.getOrDefault(level, new ArrayList<>());
-                    pList.add(new Point(currentX, currentY));
-                    map.put(level++, pList);
-
-                    if (currentY > targetY) 
-                        currentY--;
-                    else 
-                        currentY++;
-                }
-                if(j == routes[i].length -1){
-                    List<Point> pList = map.getOrDefault(level, new ArrayList<>());
-                    pList.add(new Point(currentX, currentY));
-                    map.put(level, pList); 
-                }
-            }
+        // 초마다 수행
+        Deque<Node> deque = new ArrayDeque<>();
+        int robotSize = routes.length; // 로봇 개수
+        visited = new int[101][101];
+        for (int i=0;i<robotSize;i++){
+            int[] startPoint = points[routes[i][0]-1];
+            deque.offer(new Node(startPoint[0], startPoint[1], i));
+            visited[startPoint[0]] [startPoint[1]]++;
         }
-        // 겹치는 곳 찾기
-        for (Map.Entry<Integer, List<Point>> entry : map.entrySet()) {
-            List<Point> list = entry.getValue();
-            Map<Point, Integer> pmap = new HashMap<>(); // 충돌 횟수 구하기
-            for (Point p : list) {
-                pmap.put(p, pmap.getOrDefault(p, 0) + 1);
+        checkConflict();
+        
+        while (!deque.isEmpty()){
+            Iterator<Node> iter = deque.iterator();
+            visited = new int[101][101];
+            
+            while (iter.hasNext()){
+                Node cur = iter.next();
+                int routeIndex = cur.routeIndex; // 현재 포인트 번호                
+                int robotIndex = cur.robotIndex; // 로봇 번호
+                int[] dest = routes[robotIndex]; // 포인트
+                if (routeIndex == dest.length-1){
+                    iter.remove();
+                    continue;
+                }
+
+                int destY = points[dest[routeIndex+1]-1][0];
+                int destX = points[dest[routeIndex+1]-1][1];
+                cur.moveNext(destY, destX);
+                visited[cur.y][cur.x]++;
+                if (cur.y==destY && cur.x == destX){
+                    cur.routeIndex++;
+                }
+
             }
-            for (Map.Entry<Point, Integer> pentry : pmap.entrySet()) {
-                if (pentry.getValue() > 1) {
+            checkConflict();
+        }
+        
+        return answer;
+    }
+    
+    private void checkConflict(){
+        for (int i=0;i<=100;i++){
+            for (int j=0;j<=100;j++){
+                if (visited[i][j] > 1){
                     answer++;
                 }
             }
         }
-        return answer;
     }
+    
+    // n개의 포인트
+    // 운송 경로 : m개의 포인트 순서대로 방문
+    // 로봇 x대, 0초에 동시에 출발, 1초마다 상하좌우 한칸 이동
+    // 최단경로로 이동 우선순위 : (r 이동 > c 이동)
+    // 충돌 상황 : 같은 좌표에 로봇이 2대 이상 모임
+    // routes : [출발 포인트 번호, 도착 포인트 번호]
+    // 100 * 100 = 10_000, 최대 100의 로봇 -> 최단거리는 하나 (100번 수행)
+    // 최단 거리 : bfs X, r 이동 후 -> c 이동하면 됨
+    // 초마다 움직이면서 업데이트
+    // 구현문제
 }
+// 10_000_000
