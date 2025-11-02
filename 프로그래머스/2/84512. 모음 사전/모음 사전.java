8e34919b8b45b73daf15ddcd9bbacfc557a624a0
Mint <78211281+Starlight258@users.noreply.github.com>
2025-11-03 01:24:36 +0900

    [level 2] Title: 모음 사전, Time: 2.99 ms, Memory: 80.9 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,27 +1,33 @@
 import java.util.*;
 
 class Solution {
-    private final Character[] alphabet = {'A', 'E', 'I', 'O', 'U'};
-    private int count = 0;
-    private boolean isTerminated = false;
     
-    private void dfs(StringBuilder sb, String word){
-        if (sb.length()>5) return;
-        if (isTerminated) return;
-        if (sb.toString().equals(word)){
-            isTerminated = true;
+    private char[] vowels = new char[]{'A', 'E', 'I', 'O', 'U'};
+    private int answer = 0;
+    private boolean isFound = false;
+    
+    public int solution(String word) {        
+        findWord(new StringBuilder(), word);
+        return answer-1;
+    }
+    
+    private void findWord(StringBuilder cur, String word){
+        if (isFound || cur.length()>5){
             return;
         }
-        count++;
-        for (int i=0;i<alphabet.length;i++){
-            sb.append(alphabet[i]);
-            dfs(sb, word);
-            sb.deleteCharAt(sb.length()-1);
+        answer++;
+        if (word.equals(cur.toString())){
+            isFound = true;
         }
+
+        for (int i=0;i<vowels.length;i++){
+            cur.append(vowels[i]);
+            findWord(cur, word);
+            cur.deleteCharAt(cur.length()-1);
+        }
+        
     }
     
-    public int solution(String word) {
-        dfs(new StringBuilder(), word);
-        return count;
-    }
+    
+    // 6*6*6*6*6 = 36 * 36 * 6 = 7200 쯤 ? 완전탐색
 }
