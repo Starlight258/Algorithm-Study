89f38226b89673fafb3cb971201a72d81adf55d0
Mint <78211281+Starlight258@users.noreply.github.com>
2025-11-04 01:26:52 +0900

    [level 2] Title: 숫자 변환하기, Time: 26.79 ms, Memory: 78.4 MB -BaekjoonHub

--- 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
+++ 프로그래머스/2/154538. 숫자 변환하기/숫자 변환하기.java
@@ -1,25 +1,34 @@
 import java.util.*;
+
 class Solution {
     public int solution(int x, int y, int n) {
-        final int MAX_VALUE = 1000001;
-        
-        int dp[] = new int[MAX_VALUE];        
+        int max = 1_000_000;
+        int[] dp = new int[max+1];
         Arrays.fill(dp, Integer.MAX_VALUE);
         dp[x] = 0;
-                                
         for (int i=x;i<=y;i++){
-            if (dp[i] == Integer.MAX_VALUE) continue;
-            if (i*2<MAX_VALUE){
-                dp[i*2] = Math.min(dp[i*2], dp[i] + 1);
-            } 
-            if (i*3<MAX_VALUE){
-                dp[i*3] = Math.min(dp[i*3], dp[i] + 1); 
-            } 
-            if (i+n<MAX_VALUE){
-                dp[i+n] = Math.min(dp[i+n], dp[i] + 1);  
-            } 
+            if (dp[i] == Integer.MAX_VALUE){
+                continue;
+            }
+            if (i+n <= max){
+                dp[i+n] = Math.min(dp[i+n], dp[i]+1);
+            }
+            if (i*2 <= max){
+                dp[i*2] = Math.min(dp[i*2], dp[i]+1);
+            }
+            if (i*3 <= max){
+                dp[i*3] = Math.min(dp[i*3], dp[i]+1);
+            }
         }
-                                         
-        return dp[y]==Integer.MAX_VALUE ? -1 : dp[y];
+        
+        if (dp[y] == Integer.MAX_VALUE){
+            return -1;
+        }
+        return dp[y];
     }
+    // 자연수 x -> y로 변환
+    // x + n, x*2, x*3
+    // x -> y로 변환하는 필요한 최소 연산 횟수
+    // dp[i] = x에서 i를 만들기 위한 최소 연산 횟수 -> dp
+    // dp[i] = Math.min(dp[i], dp[i+n]);
 }
