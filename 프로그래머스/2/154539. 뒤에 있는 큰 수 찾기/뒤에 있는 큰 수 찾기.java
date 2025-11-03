a404d26ae5edcf8ea32241790d111e2520a09062
Mint <78211281+Starlight258@users.noreply.github.com>
2025-11-04 00:43:08 +0900

    [level 2] Title: 뒤에 있는 큰 수 찾기, Time: 84.51 ms, Memory: 220 MB -BaekjoonHub

--- 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.java
+++ 프로그래머스/2/154539. 뒤에 있는 큰 수 찾기/뒤에 있는 큰 수 찾기.java
@@ -1,24 +1,38 @@
 import java.util.*;
+
 class Solution {
-    class Num{
+    class Node{
+        int index;
         int number;
-        int idx;
-        Num(int number, int idx){
+        
+        Node (int index, int number){
+            this.index = index;
             this.number = number;
-            this.idx = idx;
         }
     }
     public int[] solution(int[] numbers) {
-        int[] answer = new int[numbers.length];
+        int n = numbers.length;
+        int[] answer = new int[n];
         Arrays.fill(answer, -1);
-        Stack<Num> stk = new Stack<>();
-        for (int i=0;i<numbers.length;i++){
-            while (!stk.isEmpty() && stk.peek().number < numbers[i]){
-                Num e = stk.pop();
-                answer[e.idx] = numbers[i];
+        // 스택 저장
+        Stack<Node> stk = new Stack<>();
+        
+        // 순회하면서 뒷 큰수 구하기
+        for (int i=0;i<n;i++){
+            int cur = numbers[i];
+            while (!stk.isEmpty() && stk.peek().number < cur){
+                Node node = stk.pop();
+                answer[node.index] = cur;
             }
-            stk.push(new Num(numbers[i], i));
+            stk.push(new Node(i, cur));
         }
+        
         return answer;
     }
+    // 뒷 큰 수 : 자신보다 크면서 뒤에서 가장 가까이 있는 수
+    // result : -1로 초기화
+    // stack에서 
+    // if (stk.peek() <= cur) : pop + 해당 인덱스에 cur 저장 
+    // else : push
+    // 스택에 모든 수 저장: numbers 하나씩 돌면서 number < stk.peek()이면 result에 저장, 그렇지 않으면 poll(), 원소가 없을 경우 -1
 }
