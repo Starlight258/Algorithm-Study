2126b04aa91c03b0b19d3fd8ae1ccf6004c965f6
Mint <78211281+Starlight258@users.noreply.github.com>
2025-11-04 22:15:09 +0900

    [level 2] Title: [PCCP 기출문제] 2번 / 석유 시추, Time: 58.76 ms, Memory: 70.8 MB -BaekjoonHub

--- 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
+++ 프로그래머스/2/250136. ［PCCP 기출문제］ 2번 ／ 석유 시추/［PCCP 기출문제］ 2번 ／ 석유 시추.java
@@ -1,61 +1,99 @@
 import java.util.*;
 
 class Solution {
-
-    int n, m;
-    int[] dy = {-1,0,1,0};
-    int[] dx = {0,1,0,-1};
-    boolean[][] visited;
-    int[] amounts;
-
     
-    public int solution(int[][] land) {
-        int answer = 0;
-        n = land.length;
-        m = land[0].length;
-        visited = new boolean[n][m];
-        amounts = new int[m];
+    private final static int[] dy = {-1,0,1,0};
+    private final static int[] dx = {0,1,0,-1};
+    
+    private boolean[][] visited;
+    private int answer = 0;
+    private int n = 0;
+    private int m = 0;
+    private int minm = 0;
+    private int maxm = 0;
+    private int[] lines;
+    
+    public int solution(int[][] land) {        
+        n = land.length; // 세로 길이 5
+        m = land[0].length; // 가로 길이 8
+        lines = new int[501];
+        visited = new boolean[501][501];
+        minm = m-1;
+        maxm = 0;
         
         for (int i=0;i<n;i++){
+            int count = 0;
             for (int j=0;j<m;j++){
-                if (land[i][j]>0 && !visited[i][j]){
-                    bfs(i, j, land);
+                if (visited[i][j] || land[i][j]==0) continue;
+                if (land[i][j]==1){
+                    count = bfs(land, i, j);     
                 }
-            }
+                for (int k=minm;k<=maxm;k++){
+                    lines[k] += count;
+                }
+                minm = m-1;
+                maxm = 0;
+            }            
         }
-        int maxIdx = 0;
-        for (int i=0;i<m;i++){
-            if (amounts[maxIdx] < amounts[i]){
-                maxIdx = i;
-            }
+        
+        for (int i=0;i<m;i++) {
+            answer = Math.max(answer, lines[i]);
         }
-        return amounts[maxIdx];
+      
+        return answer;
     }
     
-    private void bfs(int y, int x, int[][] land){
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{y, x});
-        visited[y][x] = true;
-        int maxX = 0, minX = m, count=0;
-        while (!queue.isEmpty()){
-            int[] cur = queue.poll();
-            y = cur[0];
-            x = cur[1];
-            count++;
-            minX = Math.min(minX, x);
-            maxX = Math.max(maxX, x);
+    private int bfs(int[][] land, int startY, int startX){
+        Queue<int[]> q = new ArrayDeque<>();
+        q.offer(new int[]{startY, startX});
+        visited[startY][startX] = true;
+        int count = 1;
+        
+        while (!q.isEmpty()){
+            int[] node = q.poll();
+            int y = node[0];
+            int x = node[1];
+            maxm = Math.max(maxm, x);
+            minm = Math.min(minm, x);
+            
             for (int i=0;i<4;i++){
                 int ny = y + dy[i];
                 int nx = x + dx[i];
-                if (ny<0||nx<0||ny>=n||nx>=m || visited[ny][nx] || land[ny][nx]==0){
+                if (ny<0||nx<0||ny>=n||nx>=m){
+                    continue;
+                }
+                if (visited[ny][nx] || land[ny][nx]==0){
                     continue;
                 }
                 visited[ny][nx] = true;
-                queue.add(new int[]{ny, nx});
+                count++;
+                q.offer(new int[]{ny, nx});
             }
         }
-        for (int j=minX;j<=maxX;j++){
-            amounts[j] += count;
-        }
+        return count;
     }
+    
+    private int dfs(int y, int x, int[][] land){
+        int count = 1;
+        visited[y][x] = true;
+        maxm = Math.max(maxm, x);
+        minm = Math.min(minm, x);
+        
+        for (int i=0;i<4;i++){
+            int ny = y + dy[i];
+            int nx = x + dx[i];
+            if (ny<0||nx<0||ny>=n||nx>=m) continue;
+            if (visited[ny][nx] || land[ny][nx] == 0) continue;
+            if (land[ny][nx]==1){
+                count += dfs(ny, nx, land);
+            }
+        }
+        return count;
+    }
+    // 세로 길이 n, 가로 길이 m
+    // 가장 많은 석유를 뽑을 수 있는 시추관의 위치
+    // 500 * 500 * 500 = 250_000
+    // 가로 (y, x) -> (0, 0) ~ (0, n-1) : 한칸씩 시추함
+    // (0,0) (1,0)~(4,0)
+    // 그냥 완전 탐색해서 
 }
