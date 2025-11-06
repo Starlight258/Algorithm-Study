f067a2a9572d3e1492391771bd52427269183276
Mint <78211281+Starlight258@users.noreply.github.com>
2025-11-07 02:52:58 +0900

    [level 2] Title: 괄호 회전하기, Time: 29.70 ms, Memory: 92.7 MB -BaekjoonHub

--- 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
+++ 프로그래머스/2/76502. 괄호 회전하기/괄호 회전하기.java
@@ -1,31 +1,35 @@
-import java.util.Stack;
+import java.util.*;
 
-public class Solution {
-    static public int solution(String s) {
+class Solution {
+
+    public int solution(String s) {
         int answer = 0;
-
         for (int i = 0; i < s.length(); i++) {
-            Stack<Character> stack = new Stack<>();
-            String str = s.substring(i, s.length()) + s.substring(0, i);
-            for (int j = 0; j < str.length(); j++) {
-                char c = str.charAt(j);
-                if (stack.isEmpty()) {
-                    stack.push(c);
-                } else if (c == ')' && stack.peek() == '(') {
-                    stack.pop();
-                } else if (c == '}' && stack.peek() == '{') {
-                    stack.pop();
-                } else if (c == ']' && stack.peek() == '[') {
-                    stack.pop();
-                } else {
-                    stack.push(c);
-                }
-            }
-            if (stack.isEmpty()) {
-                answer++;
+            String rotated = s.substring(i) + s.substring(0, i);
+            if (isValid(rotated)) answer++;
+        }
+        return answer;
+    }
+
+    private boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c == '{' || c == '[') {
+                stack.push(c);
+            } else {
+                if (stack.isEmpty()) return false;
+                char top = stack.pop();
+                if (!matches(top, c)) return false;
             }
         }
 
-        return answer;
+        return stack.isEmpty();
+    }
+
+    private boolean matches(char open, char close) {
+        return (open == '(' && close == ')') ||
+               (open == '{' && close == '}') ||
+               (open == '[' && close == ']');
     }
 }
