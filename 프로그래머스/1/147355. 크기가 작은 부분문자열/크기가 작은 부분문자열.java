7fdeca6c36d51c9745928393c9c71d31c5839c3e
Mint <78211281+Starlight258@users.noreply.github.com>
2025-11-10 01:15:10 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 1.78 ms, Memory: 83.1 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.java
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.java
@@ -1,14 +1,31 @@
+
 class Solution {
     public int solution(String t, String p) {
         int answer = 0;
-        int length = p.length();
-        long pNum = Long.parseLong(p);
-        for (int i=0; i<=t.length()-length;i++){
-            long tNum = Long.parseLong(t.substring(i, i+length));
-            if (pNum >= tNum){
-                answer++;
+        int n = p.length();
+        int left = 0;
+        while (left+n<=t.length()){
+            boolean isAnswer = true;
+            for (int i=0;i<n;i++){
+                if (t.charAt(left+i) > p.charAt(i)){
+                    isAnswer = false;
+                    break;
+                }
+                if (t.charAt(i+left) < p.charAt(i)){
+                    break;
+                }
+                if (t.charAt(i+left) == p.charAt(i)){
+                    continue;
+                } 
             }
+            if (isAnswer){
+              answer++;
+            } 
+            left++;
         }
         return answer;
     }
+    // t, p
+    // t에서 p와 길이가 같은 부분문자열중에서 <= p : 횟수 
+    // 최대 10_000번 반복 -> 구현
 }
