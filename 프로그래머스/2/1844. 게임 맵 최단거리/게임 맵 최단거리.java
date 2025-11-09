ceb9e954bc63454c15fc61741d20fa10d0810723
Mint <78211281+Starlight258@users.noreply.github.com>
2025-11-10 01:21:48 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 8.01 ms, Memory: 55.2 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -6,38 +6,33 @@ class Solution {
     private static int[] dx = {0,1,0,-1};
     
     public int solution(int[][] maps) {
-        Deque<int[]> q = new ArrayDeque<>();
+        int answer = 0;
+        int n = maps.length;
+        int m = maps[0].length;
+        Queue<int[]> q = new ArrayDeque<>();
         q.offer(new int[]{0,0});
-        int maxY = maps.length;
-        int maxX = maps[0].length;
-        int[][] visited = new int[maxY][maxX];
-        visited[0][0] = 1;
-        if (maps[0][0] == 0){
-            return -1;
-        }
+        int[][] visited = new int[n][m];
         
         while (!q.isEmpty()){
             int[] node = q.poll();
             int y = node[0];
             int x = node[1];
-            if (y==maxY-1 && x==maxX-1){
-                break;
-            }
-            
             for (int i=0;i<4;i++){
                 int ny = y + dy[i];
                 int nx = x + dx[i];
-                if (ny<0||nx<0||ny>=maxY||nx>=maxX){
-                    continue;
-                }
-                if (visited[ny][nx] > 0 || maps[ny][nx] == 0){
-                    continue;
-                }
-                visited[ny][nx] = visited[y][x]+1;
+                if (ny<0||nx<0||ny>=n||nx>=m) continue;
+                if (visited[ny][nx]>0) continue;
+                if (maps[ny][nx]==0) continue;
                 q.offer(new int[]{ny, nx});
+                visited[ny][nx] = visited[y][x]+1;
             }
         }
         
-        return visited[maxY-1][maxX-1] == 0 ? -1 : visited[maxY-1][maxX-1];
+        if (visited[n-1][m-1]==0){
+            return -1;
+        }
+        
+        return visited[n-1][m-1]+1;
     }
+    // 최단경로 : bfs
 }
