722dd435d0b0606be8ddf6159b287dbf29b1eb17
Mint <78211281+Starlight258@users.noreply.github.com>
2025-11-10 16:49:58 +0900

    [level 3] Title: 네트워크, Time: 0.29 ms, Memory: 80.1 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -1,26 +1,31 @@
 class Solution {
+    
     private boolean[] visited;
+    
     public int solution(int n, int[][] computers) {
         int answer = 0;
+        n = computers.length;
         visited = new boolean[n];
         for (int i=0;i<n;i++){
-            if (!visited[i]){
-                dfs(i, computers, n);
-                answer++;
+            if (visited[i]){
+                continue;
             }
+            visited[i] = true;
+            dfs(i, computers, n);
+            answer++;
         }
+        
         return answer;
     }
     
     void dfs(int node, int[][] computers, int n){
-        for (int i=0;i<n;i++){
-            int computer =  computers[node][i];
-            if (computer==0 || visited[i]){
-                continue;
-            } 
+        for (int i=0;i<computers[node].length;i++){
+            if (visited[i] || computers[node][i]==0) continue;
             visited[i] = true;
             dfs(i, computers, n);
         }
     }
-    // dfs로 풀기
+    // 네트워크 
+    // dfs 
+    // connection 개수
 }
