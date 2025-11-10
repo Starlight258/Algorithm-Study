3e1505e8b3b7ada7c543bb03f299e600b1ad55b2
Mint <78211281+Starlight258@users.noreply.github.com>
2025-11-10 16:25:01 +0900

    [level 3] Title: 정수 삼각형, Time: 10.60 ms, Memory: 61.8 MB -BaekjoonHub

--- 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -1,39 +1,38 @@
-import java.util.*;
-
 class Solution {
+    
+    private int n;
+    
     public int solution(int[][] triangle) {
-        int answer = 0;
-        int n = triangle.length;
-
+        n = triangle.length;
+        
         for (int i=1;i<n;i++){
-            triangle[i][0] += triangle[i-1][0];
-            triangle[i][i] += triangle[i-1][i-1];
-            for (int j=1;j<i;j++){
-                triangle[i][j] += Math.max(triangle[i-1][j-1], triangle[i-1][j]);
+            for (int j=0;j<=i;j++){
+                if (j==0){
+                    triangle[i][j] += triangle[i-1][j];
+                    continue;
+                }
+                if (j==i){
+                    triangle[i][j] += triangle[i-1][j-1];
+                    continue;
+                }
+                triangle[i][j] += Math.max(triangle[i-1][j], triangle[i-1][j-1]);
             }
         }
         
+        int answer = 0;
         for (int i=0;i<n;i++){
-            answer = Math.max(triangle[n-1][i], answer);
+            answer = Math.max(answer, triangle[n-1][i]);
         }
         return answer;
     }
     
-    // dp[i+1][j] = Math.max(dp[i][j-1], dp[i][j]);
-    // i depth에서 i+1 depth
-    // 1.0 <- 0,0
-    // 1,1 <-0,0
-    // 2,0 <- 1,0
-    // 2,1 <- 1,0, 1,1
-    // 2,2 <- 1,1
-    // 3,0 <- 2,0
-    // 3,1 <- 2,0, 2,1
-    // 3,2 <- 2,1, 2,2
-    // 3,3 <- 2,2
-    // dp[i][j] = dp[i-1][j-1]
-    // 7
-    // 3 8
-    // 8 1 0
+    // 거쳐간 숫자의 최대값
+    // 7(0,0)
+    // 3(1,0) 8(1,1)
+    // 8(2,0) 1(2,1) 0(2,2)
     // 2 7 4 4
     // 4 5 2 6 5
+    // 완전탐색 -> dfs 돌면서 끝 도착하면 최대값 return
+    // 두번째 부터 시작, 가장 왼쪽이거나 가장 오른쪽은 update가 안됨
+    // t[y][x] = Math.max(t[y-1][x-1], t[y-1][x]);
 }
