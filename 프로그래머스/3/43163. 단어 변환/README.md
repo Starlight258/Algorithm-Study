eb7be0e5ae3e77cf7287b499e2b57416d5095bbd
Mint <78211281+Starlight258@users.noreply.github.com>
2025-11-10 12:11:45 +0900

    [level 3] Title: 단어 변환, Time: 0.61 ms, Memory: 90.4 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/README.md
+++ 프로그래머스/3/43163. 단어 변환/README.md
@@ -1,10 +1,10 @@
 # [level 3] 단어 변환 - 43163 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43163) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43163#) 
 
 ### 성능 요약
 
-메모리: 86.9 MB, 시간: 0.52 ms
+메모리: 90.4 MB, 시간: 0.61 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 10월 01일 01:14:19
+2025년 11월 10일 12:11:42
 
 ### 문제 설명
 
--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,57 +1,67 @@
 import java.util.*;
 
 class Solution {
-    class Node {
-        String w;
-        int d;
-        
-        Node(String w, int d){
-            this.w = w;
-            this.d = d;
-        }
-    }
+    
+    private int n;
+    private int answer;
+    private boolean[] visited;
+    private boolean hasAnswer = false;
+    
     public int solution(String begin, String target, String[] words) {
-        int answer = 0;
-        boolean hasTarget = false;
-        for (String word:words){
-            if (word.equals(target)){
-                hasTarget = true;
-                break;
-            }
-        }
-        Queue<Node> queue = new ArrayDeque<>();
-        queue.offer(new Node(begin, 0));
-        boolean[] visited = new boolean[words.length];
-        while (!queue.isEmpty()){
-            Node node = queue.poll();
-            if (node.w.equals(target)){
-                return node.d;
-            }
-            for (int i=0;i<words.length;i++){
-                if (visited[i]) continue;
-                String word = words[i];
-                if (isAdj(node.w, word)){
-                  queue.offer(new Node(word, node.d+1));
-                  visited[i] = true;
-              }
-            }
+        n = words.length;
+        answer = n;
+        visited = new boolean[n];
+        List<String> list = new ArrayList<>();
+             
+        dfs(begin, list, 0, target, words);
+        
+        if (!hasAnswer){
+            return 0;
         }
         
         return answer;
     }
-    private boolean isAdj(String w1, String w2){
-        int diff = 0;
-        for (int i=0;i<w1.length();i++){
-            if (w1.charAt(i) != w2.charAt(i)){
-                diff++;
-            }
-            if (diff>2){
-                return false;
+    
+    private void dfs(String cur, List<String> ways, int start, String target, String[] words){
+        if (ways.size()==n){
+            return;
+        }
+        if (cur.equals(target)){
+            answer = Math.min(answer, ways.size());
+            hasAnswer = true;
+            return;
+        }
+        for (int i=0;i<words.length;i++){
+            String next = words[i];
+            if (visited[i]) continue;
+            // begin -> words 돌면서 한단어 차이나면 그 다음 depth로
+            if (isDiffOne(cur, next)){
+                ways.add(cur);
+                visited[i]= true;
+                dfs(next, ways, i+1, target, words);
+                ways.remove(ways.size()-1);   
+                visited[i]= false;
             }
         }
-        if (diff==1){
-            return true;
-        }
-        return false;
     }
+    
+    private boolean isDiffOne(String a, String b){
+        char[] ar = a.toCharArray();
+        char[] br = b.toCharArray();
+        int count = 0;
+        for (int i=0;i<ar.length;i++){
+            if (ar[i] != br[i]){
+                count++;
+                if (count>1) break;
+            }
+        }
+        if (count!=1) return false;
+        return true;
+    }
+        
+    // begin -> target : 가장 짧은 변환 과정
+    // 50P50 = 너무 많음, 근데 백트래킹이라면 괜찮을지도 ? 
+    // begin -> words 돌면서 한단어 차이나면 그 다음 depth로
+    // target 찾으면 단계 반환
+    // 가장 적은 단계 횟수 return -> 완전 탐색
 }
