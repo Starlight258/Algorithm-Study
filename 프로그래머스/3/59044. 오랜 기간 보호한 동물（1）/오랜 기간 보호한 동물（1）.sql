a02a1f29e982988bf3097dfd317c23b7633e86b7
Mint <78211281+Starlight258@users.noreply.github.com>
2025-11-11 04:03:08 +0900

    [level 3] Title: 오랜 기간 보호한 동물(1), Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
+++ 프로그래머스/3/59044. 오랜 기간 보호한 동물（1）/오랜 기간 보호한 동물（1）.sql
@@ -1,18 +1,19 @@
--- 코드를 입력하세요
-SELECT ins.NAME, ins.DATETIME
-FROM ANIMAL_INS ins
-LEFT JOIN ANIMAL_OUTS outs ON ins.ANIMAL_ID = outs.ANIMAL_ID
-WHERE outs.ANIMAL_ID IS NULL
-ORDER BY ins.DATETIME
-LIMIT 3;
+# SELECT ins.NAME, ins.DATETIME
+# FROM ANIMAL_INS ins
+# WHERE NOT EXISTS (
+#     SELECT 1
+#     FROM ANIMAL_OUTS outs
+#     WHERE ins.ANIMAL_ID = outs.ANIMAL_ID
+# )
+# ORDER BY ins.DATETIME
+# LIMIT 3;
 
--- 아직 입양을 못 간 동물들 : ins에는 있지만 outs에는 없는 동물
--- select name, datetime
--- from animal_ins
--- left join animal_outs on ins.animal_id = outs.animal_id
--- where outs.animal_id is null
--- order by datetime desc
--- limit 3
--- 가장 오래 보호소에 있었던 동물 desc datetime
--- 3마리 (limit)
--- 
+-- 아직 입양을 못 간 동물 중, 가장 오래 보호소에 있었던 동물 3마리
+-- 이름과 보호 시작일을 조회
+
+select ains.NAME, ains.DATETIME
+from ANIMAL_INS ains
+left join ANIMAL_OUTS outs on ains.ANIMAL_ID = outs.ANIMAL_ID
+where outs.DATETIME IS NULL
+order by ains.DATETIME
+limit 3;
