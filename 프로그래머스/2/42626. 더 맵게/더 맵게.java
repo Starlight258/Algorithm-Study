f7d9afda89743ef9798a90b61d1ab8856d583313
Mint <78211281+Starlight258@users.noreply.github.com>
2025-11-10 23:50:22 +0900

    [level 2] Title: 더 맵게, Time: 1539.73 ms, Memory: 141 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -1,20 +1,34 @@
 import java.util.*;
+
 class Solution {
     public int solution(int[] scoville, int K) {
         int answer = 0;
         PriorityQueue<Integer> pq = new PriorityQueue<>();
-        for (int s:scoville){
-            pq.add(s);
+        for (int scov:scoville){
+            pq.offer(scov);
         }
         while (!pq.isEmpty()){
-            int e1 = pq.poll();
-            if (e1 >= K) return answer;
-            if (pq.size()==0) return -1; 
-            int e2 = pq.poll();
-            int sc = e1 + e2 * 2;
-            pq.offer(sc);
+            int s1 = pq.poll();
+            if (s1>=K){
+                break;
+            }
+            if (pq.isEmpty()){
+                return -1;
+            }
+            
+            int s2 = pq.poll();
+            int s3 = s1 + s2 * 2;
+            pq.offer(s3);
             answer++;
         }
-        return -1;
+        return answer;
     }
+    // 모든 음식의 스코빌 지수를 K 이상으로
+    // 섞은 음식의 스코빌 지수 = 가장 맵지 않은 음식의 스코빌 지수 + (두 번째로 맵지 않은 음식의 스코빌 지수 * 2)
+    // 모든 음식의 스코빌 지수를 K 이상으로 만들기 위해 섞어야 하는 최소 횟수
+    // K 10억
+    // scoville 100만
+    // 작은거 2개 pop -> 큰거 1개 만들기
+    // 가장 작은게 K 이상이 될때까지 
+    // 정렬 -> 우선순위큐 
 }
