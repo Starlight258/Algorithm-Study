300412e3d10d0dbd0d4ebea05e6f86cfba744701
Mint <78211281+Starlight258@users.noreply.github.com>
2025-11-10 21:30:52 +0900

    [level 2] Title: 방문 길이, Time: 0.56 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -5,38 +5,39 @@ class Solution {
     private static final int[] dy = {-1,0,1,0};
     private static final int[] dx = {0,1,0,-1};
     
-    private boolean[][][] visited;
-    private final Map<Character, Integer> map = Map.of('U', 2, 'D', 0, 'L',3,'R',1);
+    private static Map<Character, Integer> mp = Map.of('U', 0, 'D', 2, 'L', 3, 'R', 1);
     
     public int solution(String dirs) {
         int answer = 0;
+        int n = 11;
         int y = 5;
         int x = 5;
-        visited = new boolean[11][11][4];
-        for (char dir:dirs.toCharArray()){
-            int ny = y, nx = x;
-            int d = map.get(dir);
-            ny += dy[d];
-            nx += dx[d];
-            
-            if (ny<0||nx<0||ny>10||nx>10){
-                continue;
-            } 
-            if (!(visited[y][x][d] || visited[ny][nx][nextDir(d)])){
-                answer++;
-            }
-            visited[y][x][d] = true;
+        
+        boolean[][][] visited = new boolean[n][n][4];
+        
+        for (char d:dirs.toCharArray()){
+            int dir = mp.get(d);
+            int ny = y + dy[dir];
+            int nx = x + dx[dir];
+            if (ny<0||nx<0||ny>=n||nx>=n) continue;
+            int prevY = y;
+            int prevX = x;
             y = ny;
             x = nx;
+            int odir = oppositeDir(dir);
+            if (visited[ny][nx][odir] || visited[prevY][prevX][dir]) continue;
+            visited[ny][nx][odir] = true;
+            visited[prevY][prevX][dir] = true;
+            answer++;
         }
-        
         return answer;
     }
     
-    private int nextDir(int dir){
+    private int oppositeDir(int dir){
         return (dir + 2) % 4;
     }
-    // 방문배열 : visited[y][x][방향]
-    // 방향: 0(위), 1(오른), 2(아래), 3(왼)
-    // 상하좌우 이동, 넘어갈 경우 stop
+    // 움직임
+    // 처음 걸어본 길의 길이 
+    // visited[][][][] : 시작 y,x -> 종료 y,x
+    // 방문시 시작 y,x -> 종료 y,x, 종료 y,x -> 시작 y,x
 }
