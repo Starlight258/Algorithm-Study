f98435340d4fa50559a18c935c1d25134da647b1
Mint <78211281+Starlight258@users.noreply.github.com>
2025-11-11 01:58:24 +0900

    [level 3] Title: 카테고리 별 도서 판매량 집계하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/144855. 카테고리 별 도서 판매량 집계하기/카테고리 별 도서 판매량 집계하기.sql
+++ 프로그래머스/3/144855. 카테고리 별 도서 판매량 집계하기/카테고리 별 도서 판매량 집계하기.sql
@@ -1,14 +1,23 @@
 -- 코드를 입력하세요
-SELECT
-    b.CATEGORY,
-    SUM(s.SALES) AS TOTAL_SALES
-FROM BOOK b
-JOIN BOOK_SALES s
-  ON s.BOOK_ID = b.BOOK_ID
-WHERE s.SALES_DATE >= '2022-01-01'
-  AND s.SALES_DATE <  '2022-02-01'
-GROUP BY b.CATEGORY
-ORDER BY b.CATEGORY;
+# SELECT
+#     b.CATEGORY,
+#     SUM(s.SALES) AS TOTAL_SALES
+# FROM BOOK b
+# JOIN BOOK_SALES s
+#   ON s.BOOK_ID = b.BOOK_ID
+# WHERE s.SALES_DATE >= '2022-01-01'
+#   AND s.SALES_DATE <  '2022-02-01'
+# GROUP BY b.CATEGORY
+# ORDER BY b.CATEGORY;
 
 -- 카테고리별 도서 판매량 합산 SUM()
 -- 카테고리명 기준 오름차순 정렬
+
+-- 2022년 1월의 카테고리 별 도서 판매량을 합산
+-- 카테고리(CATEGORY), 총 판매량(TOTAL_SALES) 리스트를 출력
+select b.CATEGORY, sum(bs.SALES) as TOTAL_SALES
+from BOOK b
+join BOOK_SALES bs on b.BOOK_ID = bs.BOOK_ID
+where bs.SALES_DATE like '2022-01%'
+group by b.CATEGORY
+order by b.CATEGORY
