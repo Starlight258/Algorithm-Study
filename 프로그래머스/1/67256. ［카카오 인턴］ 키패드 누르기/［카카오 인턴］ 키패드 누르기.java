47d57e549157704aaf5b03bc2a89eab80eabd951
Mint <78211281+Starlight258@users.noreply.github.com>
2025-11-10 20:50:47 +0900

    [level 1] Title: [카카오 인턴] 키패드 누르기, Time: 0.36 ms, Memory: 93 MB -BaekjoonHub

--- 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
+++ 프로그래머스/1/67256. ［카카오 인턴］ 키패드 누르기/［카카오 인턴］ 키패드 누르기.java
@@ -1,43 +1,61 @@
 class Solution {
-    public int getDistance(int number1, int number2){
-        int y1 = (number1-1)/3;
-        int x1 = (number1-1)%3;
-        int y2 = (number2-1)/3;
-        int x2 = (number2-1)%3;
-        return Math.abs(y1-y2)+Math.abs(x1-x2);
-    }
+    
+    private static char l = 'L';
+    private static char r = 'R';
+    
     public String solution(int[] numbers, String hand) {
         StringBuilder answer = new StringBuilder();
-        int left = 10; int right=12;
+        boolean isRight = false;
+        if (hand.equals("right")){
+            isRight = true;
+        }
+        
+        // * : 10, #: 12
+        int left = 10, right = 12;
         for (int number:numbers){
-            if (number==1||number==4||number==7){
-                answer.append("L");
+            if (number ==0) number = 11;
+            if (number == 1 || number == 4 || number == 7){
                 left = number;
-            } else if (number==3||number==6||number==9){
-                answer.append("R");
-                right = number;
-            } else {
-                if (number==0) number=11;
-                int leftDistance = getDistance(number, left);
-                int rightDistance = getDistance(number, right);
-                if (leftDistance==rightDistance){
-                    if (hand.equals("right")){
-                        answer.append("R");
-                        right=number;
-                    } else {
-                        answer.append("L");
-                        left = number;
-                    }
-                }
-                else if (leftDistance>rightDistance){
-                    answer.append("R");
-                    right=number;
-                } else {
-                    answer.append("L");
-                    left = number;
-                }
+                answer.append(l);
+                continue;
             }
+            if (number == 3 || number == 6 || number == 9){
+                right = number;
+                answer.append(r);
+                continue;
+            }
+            int curY = (number-1)/3;
+            int curX = (number-1)%3;
+            int rightY = (right-1)/3;
+            int rightX = (right-1)%3;
+            int diffRight = Math.abs(curY - rightY) + Math.abs(curX - rightX);
+            int leftY = (left-1)/3;
+            int leftX = (left-1)%3;
+            int diffLeft = Math.abs(curY - leftY) + Math.abs(curX - leftX);
+            if(diffLeft==diffRight){
+                if (isRight){
+                    right = number;
+                    answer.append(r);
+                } else {
+                    left = number;
+                    answer.append(l);
+                }
+                continue;
+            }
+            if (diffLeft<diffRight){ // diffLeft
+                left = number;
+                answer.append(l);
+            } else {
+                 right = number;
+                answer.append(r);
+            }
+            
         }
         return answer.toString();
     }
+    // 5/3 + 5%3-1
+    // 3 vs 6 : (6-3)/3 = 1
+    // 6: (1,2) , 3: (0,2) : (6-1)/3, (6-1)%3
+    // 10 : (3,1), 6(1,2) -> 2+1 = 3 
+    // (1,2) vs ()
 }
