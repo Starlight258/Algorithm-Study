8dbfa1e67fb549cf2dfc3e30e083c5cee9795ff7
Mint <78211281+Starlight258@users.noreply.github.com>
2025-11-10 17:47:19 +0900

    [level 2] Title: 미로 탈출, Time: 5.24 ms, Memory: 84.9 MB -BaekjoonHub

--- 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
+++ 프로그래머스/2/159993. 미로 탈출/미로 탈출.java
@@ -4,70 +4,92 @@ class Solution {
     
     private static int[] dy = {-1,0,1,0};
     private static int[] dx = {0,1,0,-1};
-    private static int n=0;
-    private static int m=0;
-    private static String[] map;
     
     private int[][] visited;
+    private int n;
+    private int m;
     
-    public int solution(String[] map) {
-        int answer = 0;
-        n = map.length;
-        m = map[0].length();
-        this.map = map;
-        visited = new int[n][m];
-        int startY =0, startX=0;
-        int leverY =0, leverX=0;
-        
-        for (int i=0;i<n;i++){
-            for (int j=0;j<m;j++){
-                if (map[i].charAt(j) =='S'){
-                    startY = i;
-                    startX = j;
-                }
-                if (map[i].charAt(j) =='L'){
-                    leverY = i;
-                    leverX = j;
-                }
-            }
+    class Node {
+        int y;
+        int x;
+        Node(int y, int x){
+            this.y = y;
+            this.x = x;
         }
-        
-        int f = bfs(startY, startX, 'L');
-        if (f==0){
-            return -1;
-        }
-        int l = bfs(leverY, leverX, 'E');
-        if (l==0){
-            return -1;
-        }
-        
-        return f + l;
     }
     
-    private int bfs(int startY, int startX, char target){
+    public int solution(String[] maps) {
+        int answer = 0;
+        n = maps.length;
+        m = maps[0].length();
+        char[][] mp = new char[n][m];
         visited = new int[n][m];
-        visited[startY][startX] = 1;
-        Queue<int[]> q = new ArrayDeque<>();
-        q.offer(new int[]{startY, startX});
+        for (int i=0;i<n;i++){
+            mp[i] = maps[i].toCharArray();
+        }
+        // 시작점 구하기
+        int levY = 0;
+        int levX = 0;
+        int destY = 0;
+        int destX = 0;
+        boolean canMove = false;
+        for (int i=0;i<n;i++){
+            for (int j=0;j<m;j++){
+                if (mp[i][j]=='L'){
+                    levY = i;
+                    levX = j;
+                }
+                if (mp[i][j]=='E'){
+                    destY = i;
+                    destX = j;
+                }
+                if (mp[i][j]=='S'){
+                    visited[i][j] = 1;
+                    canMove = bfs(i, j, mp, 'L');
+                    if (!canMove){
+                        return -1;   
+                    }
+                }
+            }
+        }
+        answer += visited[levY][levX]-1;
+        visited = new int[n][m];
+
+        // bfs 수행
+        canMove = bfs(levY, levX, mp, 'E');
+        if (!canMove){
+            return -1;
+        }
+        answer += visited[destY][destX];
+        return answer;
+    }
+    
+    private boolean bfs(int y, int x, char[][] mp, char goal){
+        Queue<Node> q = new ArrayDeque<>();
+        q.offer(new Node(y, x));
         
         while (!q.isEmpty()){
-            int[] node = q.poll();
-            int y = node[0];
-            int x = node[1];
-            if (map[y].charAt(x) == target){
-                return visited[y][x]-1;
+            Node cur = q.poll();
+            y = cur.y;
+            x = cur.x;
+            if (mp[y][x] == goal){
+                return true;
             }
+
             for (int i=0;i<4;i++){
                 int ny = y + dy[i];
                 int nx = x + dx[i];
-                if (ny<0||nx<0||ny>=n||nx>=m) continue;
-                if (visited[ny][nx]>0 || map[ny].charAt(nx) == 'X'){
-                    continue;
+                if (ny<0||nx<0||ny>=n||nx>=m || visited[ny][nx]>0) continue;
+                if (mp[ny][nx] != 'X'){
+                    visited[ny][nx] = visited[y][x]+1;
+                    q.offer(new Node(ny, nx));
                 }
-                q.offer(new int[]{ny, nx});
-                visited[ny][nx] = visited[y][x]+1;
-            }    
+            }
         }
-        return 0;
+        return false;
     }
+    
+    // 레버
+    // 출발(S) -> 레버(L) -> 출구(E) 
+    // 미로를 탈출하는데 필요한 최소 시간 (탈출할 수 없다면 -1)
 }
