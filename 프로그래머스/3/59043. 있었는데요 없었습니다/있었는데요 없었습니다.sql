22f465900de3112ae0733a6141570eb18f2ac001
Mint <78211281+Starlight258@users.noreply.github.com>
2025-11-11 03:55:56 +0900

    [level 3] Title: 있었는데요 없었습니다, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
+++ 프로그래머스/3/59043. 있었는데요 없었습니다/있었는데요 없었습니다.sql
@@ -1,15 +1,20 @@
 -- 코드를 입력하세요
-SELECT ins.ANIMAL_ID, ins.NAME
-FROM ANIMAL_INS ins
-JOIN ANIMAL_OUTS outs ON ins.ANIMAL_ID = outs.ANIMAL_ID
-WHERE outs.DATETIME < ins.DATETIME
-ORDER BY ins.DATETIME ASC;
+# SELECT ins.ANIMAL_ID, ins.NAME
+# FROM ANIMAL_INS ins
+# WHERE EXISTS (
+#     SELECT 1
+#     FROM ANIMAL_OUTS outs 
+#     WHERE ins.ANIMAL_ID = outs.ANIMAL_ID
+#     AND outs.DATETIME < ins.DATETIME
+# )
+# ORDER BY ins.DATETIME ASC;
 
--- 아이디와 이름 조회
--- 입양일 < 보호 시작일
--- select animal_id, name
--- from animal_ins ins
--- join animal_outs outs on ins.animal_id = outs.animal_id
--- where outs.datetime < ins.datetime
--- order by outs.datetime
--- 보호 시작일이 빠른 순으로 조회
+-- 보호 시작일보다 입양일이 더 빠른 동물
+-- 아이디와 이름을 조회
+--  보호 시작일이 빠른 순으로 조회
+
+select ains.ANIMAL_ID, ains.NAME
+from ANIMAL_INS ains
+join ANIMAL_OUTS aouts on ains.ANIMAL_ID = aouts.ANIMAL_ID
+where aouts.DATETIME < ains.DATETIME
+order by ains.DATETIME;
