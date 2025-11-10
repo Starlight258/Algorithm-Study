20cc1d5c60acd4a61fee0c7f88f38cd75f583c8e
Mint <78211281+Starlight258@users.noreply.github.com>
2025-11-11 02:49:03 +0900

    [level 4] Title: 저자 별 카테고리 별 매출액 집계하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

--- 프로그래머스/4/144856. 저자 별 카테고리 별 매출액 집계하기/저자 별 카테고리 별 매출액 집계하기.sql
+++ 프로그래머스/4/144856. 저자 별 카테고리 별 매출액 집계하기/저자 별 카테고리 별 매출액 집계하기.sql
@@ -1,19 +1,22 @@
 -- 코드를 입력하세요
-SELECT a.AUTHOR_ID, a.AUTHOR_NAME, b.CATEGORY, sum(bs.SALES * b.PRICE) as TOTAL_SALES
-FROM BOOK b
-JOIN AUTHOR a ON b.AUTHOR_ID = a.AUTHOR_ID
-JOIN BOOK_SALES bs ON b.BOOK_ID = bs.BOOK_ID 
-WHERE bs.SALES_DATE >= '2022-01-01' and bs.SALES_DATE < '2022-02-01'
-GROUP BY a.AUTHOR_ID, b.CATEGORY
-ORDER BY a.AUTHOR_ID ASC, b.CATEGORY DESC;
+# SELECT a.AUTHOR_ID, a.AUTHOR_NAME, b.CATEGORY, sum(bs.SALES * b.PRICE) as TOTAL_SALES
+# FROM BOOK b
+# JOIN AUTHOR a ON b.AUTHOR_ID = a.AUTHOR_ID
+# JOIN BOOK_SALES bs ON b.BOOK_ID = bs.BOOK_ID 
+# WHERE bs.SALES_DATE >= '2022-01-01' and bs.SALES_DATE < '2022-02-01'
+# GROUP BY a.AUTHOR_ID, b.CATEGORY
+# ORDER BY a.AUTHOR_ID ASC, b.CATEGORY DESC;
 
--- select a.author_id, a.author_name, b.category, sum(bs.sales * b.price) as total_sales
--- from book as b
--- join author a on b.author_id = a.author_id
--- join book sales bs on bs.book_id = b.book_id
--- where book bs.sales_date >= '2022-01-01' and bs.sales_date > '2022-02-01'
--- group by a.author_id, a.author_name, b.category
--- order by a.author_id asc, b.category desc
--- 판매량 * 판매가
--- sales * price 
--- 저자별, 카테고리별
+-- 2022년 1월의 도서 판매 데이터를 기준으로 
+-- 저자 별, 카테고리 별 매출액(TOTAL_SALES = 판매량 * 판매가) 
+-- 저자 ID(AUTHOR_ID), 저자명(AUTHOR_NAME), 카테고리(CATEGORY), 매출액(SALES) 리스트를 출력
+-- 저자 ID를 오름차순으로, 저자 ID가 같다면 카테고리를 내림차순 정렬
+
+select a.AUTHOR_ID, a.AUTHOR_NAME, b.CATEGORY, sum(bs.SALES * b.PRICE ) as TOTAL_SALES
+from BOOK b
+join AUTHOR a on b.AUTHOR_ID = a.AUTHOR_ID
+join BOOK_SALES bs on b.BOOK_ID = bs.BOOK_ID
+where bs.SALES_DATE like '2022-01%'
+group by a.AUTHOR_ID, b.CATEGORY
+order by a.AUTHOR_ID asc, b.CATEGORY desc;
+
