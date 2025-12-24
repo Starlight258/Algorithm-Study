e492fb6a033ac2bd9491b79c4bf33d3682e50844
Mint <78211281+Starlight258@users.noreply.github.com>
2025-12-24 14:31:26 +0900

    [Silver III] Title: 계단 오르기, Time: 104 ms, Memory: 14232 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -1,41 +1,35 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
 
+    // DP[i][j] : 현재 i칸, 지금까지 j칸(현재칸 포함) = 점수
+    // 세 칸을 연속으로 밟아서는 안된다.
+    // DP[i][1] = Math.max(DP[i-2][1], DP[i-2][2]) + S[i]
+    // DP[i][2] = DP[i-1][1] + S[i]
+    // DP[1][1] = S[1]
+    // DP[1][2] = 0
+    // DP[2][1] = S[2]
+    // DP[2][2] = S[1] + S[2]
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-
-        List<Integer> scores = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            scores.add(Integer.parseInt(br.readLine()));
+        int[][] dp = new int[305][3];
+        int[] s = new int[305];
+        for (int i = 1; i <= n; i++) {
+            s[i] = Integer.parseInt(br.readLine());
+        }
+        dp[1][1] = s[1];
+        dp[1][2] = 0;
+        dp[2][1] = s[2];
+        dp[2][2] = s[1] + s[2];
+        for (int i = 3; i <= n; i++) {
+            dp[i][1] = Math.max(dp[i - 2][1], dp[i - 2][2]) + s[i];
+            dp[i][2] = dp[i - 1][1] + s[i];
         }
 
-        int answer = dp(n, scores);
-        System.out.println(answer);
+        System.out.println(Math.max(dp[n][1], dp[n][2]));
     }
 
-    private static int dp(final int n, final List<Integer> scores) {
-        if (n == 1) {
-            return scores.get(0);
-        }
-        if (n == 2) {
-            return scores.get(0) + scores.get(1);
-        }
-        int[] dp = new int[n];
-        dp[0] = scores.get(0);
-        dp[1] = scores.get(0) + scores.get(1);
-        dp[2] = Math.max(scores.get(0) + scores.get(2), scores.get(1) + scores.get(2));
-
-        for (int i = 3; i < n; i++) {
-            dp[i] = Math.max(
-                    dp[i - 2] + scores.get(i),
-                    dp[i - 3] + scores.get(i - 1) + scores.get(i));
-        }
-        return dp[n - 1];
-    }
 }
