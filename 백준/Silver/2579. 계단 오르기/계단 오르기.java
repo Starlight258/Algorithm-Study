58b3979dc68d9e028936104d56caec18d12d0073
Mint <78211281+Starlight258@users.noreply.github.com>
2026-01-06 15:41:10 +0900

    [Silver III] Title: 계단 오르기, Time: 100 ms, Memory: 14128 KB -BaekjoonHub

--- 백준/Silver/2579. 계단 오르기/계단 오르기.java
+++ 백준/Silver/2579. 계단 오르기/계단 오르기.java
@@ -4,31 +4,46 @@ import java.io.InputStreamReader;
 
 public class Main {
 
-    // DP[i][j] : 현재 i칸, 지금까지 j칸(현재칸 포함) = 점수
-    // 세 칸을 연속으로 밟아서는 안된다.
-    // DP[i][1] = Math.max(DP[i-2][1], DP[i-2][2]) + S[i]
-    // DP[i][2] = DP[i-1][1] + S[i]
-    // DP[1][1] = S[1]
-    // DP[1][2] = 0
-    // DP[2][1] = S[2]
-    // DP[2][2] = S[1] + S[2]
+    /*
+       계단 오르기 게임은 계단 아래 시작점부터 계단 꼭대기에 위치한 도착점까지 가는 게임이다. <그림 1>과 같이 각각의 계단에는 일정한 점수가 쓰여 있는데 계단을 밟으면 그 계단에 쓰여 있는 점수를 얻게 된다.
+       예를 들어 <그림 2>와 같이 시작점에서부터 첫 번째, 두 번째, 네 번째, 여섯 번째 계단을 밟아 도착점에 도달하면 총 점수는 10 + 20 + 25 + 20 = 75점이 된다.
+
+       계단 오르는 데는 다음과 같은 규칙이 있다.
+
+       계단은 한 번에 한 계단씩 또는 두 계단씩 오를 수 있다. 즉, 한 계단을 밟으면서 이어서 다음 계단이나, 다음 다음 계단으로 오를 수 있다.
+       연속된 세 개의 계단을 모두 밟아서는 안 된다. 단, 시작점은 계단에 포함되지 않는다.
+       마지막 도착 계단은 반드시 밟아야 한다.
+       따라서 첫 번째 계단을 밟고 이어 두 번째 계단이나, 세 번째 계단으로 오를 수 있다. 하지만, 첫 번째 계단을 밟고 이어 네 번째 계단으로 올라가거나, 첫 번째, 두 번째, 세 번째 계단을 연속해서 모두 밟을 수는 없다.
+
+       각 계단에 쓰여 있는 점수가 주어질 때 이 게임에서 얻을 수 있는 총 점수의 최댓값을 구하는 프로그램을 작성하시오.
+        */
+        /*
+       s[i] : i칸의 점수
+       dp[i][j] : 현재 i칸, 연속 j 칸(현재 칸 포함)
+
+       정답 : Math.max(dp[n][1], dp[n][2])
+       dp[1][1] = s[1]
+       dp[2][1] = s[2]
+       dp[2][2] = s[1] + s[2]
+       dp[3][1] = dp[1][1] + s[3]
+       dp[3][2] = dp[2][1] + s[3]
+
+       dp[i][1] = dp[i-2][1] + s[i]
+       dp[i][2] = dp[i-1][1] + s[i]
+        */
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        int[][] dp = new int[305][3];
-        int[] s = new int[305];
+        int[] s = new int[n + 1];
         for (int i = 1; i <= n; i++) {
             s[i] = Integer.parseInt(br.readLine());
         }
+        int[][] dp = new int[n + 1][3];
         dp[1][1] = s[1];
-        dp[1][2] = 0;
-        dp[2][1] = s[2];
-        dp[2][2] = s[1] + s[2];
-        for (int i = 3; i <= n; i++) {
+        for (int i = 2; i <= n; i++) {
             dp[i][1] = Math.max(dp[i - 2][1], dp[i - 2][2]) + s[i];
             dp[i][2] = dp[i - 1][1] + s[i];
         }
-
         System.out.println(Math.max(dp[n][1], dp[n][2]));
     }
 
