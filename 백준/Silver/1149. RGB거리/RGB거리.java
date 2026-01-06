4437fbeb646daef4737b982167a43a2077429de4
Mint <78211281+Starlight258@users.noreply.github.com>
2026-01-06 17:18:49 +0900

    [Silver I] Title: RGB거리, Time: 116 ms, Memory: 14600 KB -BaekjoonHub

---
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -0,0 +1,47 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    /*
+    RGB거리에는 집이 N개 있다. 거리는 선분으로 나타낼 수 있고, 1번 집부터 N번 집이 순서대로 있다.
+
+    집은 빨강, 초록, 파랑 중 하나의 색으로 칠해야 한다. 각각의 집을 빨강, 초록, 파랑으로 칠하는 비용이 주어졌을 때, 아래 규칙을 만족하면서 모든 집을 칠하는 비용의 최솟값을 구해보자.
+
+    1번 집의 색은 2번 집의 색과 같지 않아야 한다.
+    N번 집의 색은 N-1번 집의 색과 같지 않아야 한다.
+    i(2 ≤ i ≤ N-1)번 집의 색은 i-1번, i+1번 집의 색과 같지 않아야 한다.
+    dp[i][j] = i번째 집까지 칠하는 비용의 최소값 j번째 색깔
+    R G B
+    R G R
+    dp[1][1] = c[1]
+    dp[2][1] = Math.min(dp[1][2], dp[1][3]) + c[1]
+    dp[2][2] = Math.min(dp[1][1], dp[1][3]) + c[2]
+    dp[2][3] = Math.min(dp[1][1], dp[1][2]) + c[3]
+    answer = Math.min(dp[n][1], dp[n][2], dp[n][3])
+     */
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[][] c = new int[n + 1][4];
+        StringTokenizer st;
+        for (int i = 1; i <= n; i++) {
+            st = new StringTokenizer(br.readLine());
+            c[i][1] = Integer.parseInt(st.nextToken()); // R
+            c[i][2] = Integer.parseInt(st.nextToken()); // G
+            c[i][3] = Integer.parseInt(st.nextToken()); // B
+        }
+        int[][] dp = new int[n + 1][4];
+        dp[1][1] = c[1][1];
+        dp[1][2] = c[1][2];
+        dp[1][3] = c[1][3];
+        for (int i = 2; i <= n; i++) {
+            dp[i][1] = Math.min(dp[i - 1][2], dp[i - 1][3]) + c[i][1];
+            dp[i][2] = Math.min(dp[i - 1][1], dp[i - 1][3]) + c[i][2];
+            dp[i][3] = Math.min(dp[i - 1][1], dp[i - 1][2]) + c[i][3];
+        }
+        System.out.println(Math.min(Math.min(dp[n][1], dp[n][2]), dp[n][3]));
+    }
+}
