61eaf1476c66561606b8f8c8df3bc6464928864b
Mint <78211281+Starlight258@users.noreply.github.com>
2026-01-10 17:45:09 +0900

    [Silver IV] Title: 로프, Time: 272 ms, Memory: 26136 KB -BaekjoonHub

--- 백준/Silver/2217. 로프/로프.java
+++ 백준/Silver/2217. 로프/로프.java
@@ -1,25 +1,37 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
+
+    /*
+    N(1 ≤ N ≤ 100,000)개의 로프가 있다. 이 로프를 이용하여 이런 저런 물체를 들어올릴 수 있다.
+    각각의 로프는 그 굵기나 길이가 다르기 때문에 들 수 있는 물체의 중량이 서로 다를 수도 있다.
+
+    하지만 여러 개의 로프를 병렬로 연결하면 각각의 로프에 걸리는 중량을 나눌 수 있다.
+    k개의 로프를 사용하여 중량이 w인 물체를 들어올릴 때, 각각의 로프에는 모두 고르게 w/k 만큼의 중량이 걸리게 된다.
+
+    각 로프들에 대한 정보가 주어졌을 때, 이 로프들을 이용하여 들어올릴 수 있는 물체의 최대 중량을 구해내는 프로그램을 작성하시오.
+    모든 로프를 사용해야 할 필요는 없으며, 임의로 몇 개의 로프를 골라서 사용해도 된다.
+     */
+    /*
+    로프 최소 무게 * k = 최대
+    가장 무거운 루프부터 역순으로 k를 1씩 증가시켜 최대값 구하기
+     */
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine().trim());
-        int[] w = new int[N];
-        for (int i = 0; i < N; i++) {
-            w[i] = Integer.parseInt(br.readLine().trim());
+        int n = Integer.parseInt(br.readLine());
+        int[] w = new int[n];
+        for (int i = 0; i < n; i++) {
+            w[i] = Integer.parseInt(br.readLine());
         }
-
-        Arrays.sort(w); // 오름차순
-
-        long ans = 0;
-        // 오름차순 기준: i번째를 최약으로 할 때 k = N - i
-        for (int i = 0; i < N; i++) {
-            int k = N - i;
-            long candidate = (long) w[i] * k; // long으로 캐스팅
-            if (candidate > ans) ans = candidate; // 최대 갱신
+        Arrays.sort(w);
+        int answer = 0;
+        for (int i = n - 1; i >= 0; i--) {
+            answer = Math.max(answer, (n - i) * w[i]);
         }
-
-        System.out.println(ans);
+        System.out.println(answer);
     }
+
 }
