c6edadacd06193316a00c19635ef384803a91258
Mint <78211281+Starlight258@users.noreply.github.com>
2026-01-10 17:27:44 +0900

    [Gold V] Title: 회의실 배정, Time: 456 ms, Memory: 46276 KB -BaekjoonHub

--- 백준/Gold/1931. 회의실 배정/회의실 배정.java
+++ 백준/Gold/1931. 회의실 배정/회의실 배정.java
@@ -1,39 +1,50 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
 
+    /*
+    한 개의 회의실이 있는데 이를 사용하고자 하는 N개의 회의에 대하여 회의실 사용표를 만들려고 한다.
+    각 회의 I에 대해 시작시간과 끝나는 시간이 주어져 있고,
+    각 회의가 겹치지 않게 하면서 회의실을 사용할 수 있는 회의의 최대 개수를 찾아보자.
+    단, 회의는 한번 시작하면 중간에 중단될 수 없으며 한 회의가 끝나는 것과 동시에 다음 회의가 시작될 수 있다.
+    회의의 시작시간과 끝나는 시간이 같을 수도 있다.
+    이 경우에는 시작하자마자 끝나는 것으로 생각하면 된다.
+     */
+
+    /*
+    끝나는 시간으로 정렬
+    -> 가장 일찍 끝나는 회의를 선택
+     */
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
-        List<int[]> meetings = new ArrayList<>();
+        int[][] meetings = new int[n][2];
         StringTokenizer st;
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
             int b = Integer.parseInt(st.nextToken());
-            meetings.add(new int[]{a, b});
+            meetings[i] = new int[]{a, b};
         }
-        meetings.sort((o1, o2) -> {
-            if (o1[1] == o2[1]) {
-                return o1[0] - o2[0];
+        Arrays.sort(meetings, (a, b) -> {
+            if (a[1] == b[1]) {
+                return a[0] - b[0];
             }
-            return o1[1] - o2[1];
+            return a[1] - b[1];
         });
-        
-        int endTime = 0;
-        int count = 0;
+        int answer = 0;
+        int cur = 0;
         for (int i = 0; i < n; i++) {
-            if (endTime <= meetings.get(i)[0]){
-                endTime = meetings.get(i)[1];
-                count++;
+            if (cur <= meetings[i][0]) {
+                cur = meetings[i][1];
+                answer++;
             }
         }
-        System.out.println(count);
+        System.out.println(answer);
     }
 
 }
