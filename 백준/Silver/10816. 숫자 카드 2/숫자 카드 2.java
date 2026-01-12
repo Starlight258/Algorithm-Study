19d6c61063d7c80dbafc959bc7a1f9975bd1297c
Mint <78211281+Starlight258@users.noreply.github.com>
2026-01-13 00:46:17 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 1464 ms, Memory: 124496 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.java
@@ -5,60 +5,75 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
+    /*
+    숫자 카드는 정수 하나가 적혀져 있는 카드이다.
+    상근이는 숫자 카드 N개를 가지고 있다.
+    정수 M개가 주어졌을 때, 이 수가 적혀있는 숫자 카드를 상근이가 몇 개 가지고 있는지 구하는 프로그램을 작성하시오.
+    10
+    6 3 2 10 10 10 -10 -10 7 3
+    8
+    10 9 -5 2 3 4 5 -10
+     */
+    /*
+    -10 -10 2 3 3 6 7 10 10 10
+     */
+    private static int n;
+    private static int[] nums;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        int[] numbers = new int[n];
-        String[] split = br.readLine().split(" ");
-        for (int i = 0; i < n; i++) {
-            numbers[i] = Integer.parseInt(split[i]);
-        }
-        Arrays.sort(numbers);
-
-        int m = Integer.parseInt(br.readLine());
+        n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
+        nums = new int[n];
+        for (int i = 0; i < n; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
         StringBuilder sb = new StringBuilder();
+        Arrays.sort(nums);
+        int m = Integer.parseInt(br.readLine());
+        int[] picks = new int[m];
+        st = new StringTokenizer(br.readLine());
         for (int i = 0; i < m; i++) {
-            int target = Integer.parseInt(st.nextToken());
-            int count = upperBound(numbers, target) - lowerBound(numbers, target);
-            sb.append(count).append(" ");
+            picks[i] = Integer.parseInt(st.nextToken());
+        }
+        for (int i = 0; i < m; i++) {
+            int pick = picks[i];
+            int lower = lowerBound(pick);
+            int upper = upperBound(pick);
+            sb.append(upper - lower).append(" ");
         }
         System.out.println(sb);
     }
 
-
-    // 값을 초과하는 처음 위치
-    private static int upperBound(final int[] numbers, final int target) {
+    // 처음으로 해당 값이 존재하는 인덱스
+    private static int lowerBound(int target) {
         int left = 0;
-        int right = numbers.length;
-
+        int right = n;
         while (left < right) {
             int mid = (right - left) / 2 + left;
-            if (numbers[mid] <= target) {
+            // 2 2 3 3 4 5 6
+            if (nums[mid] < target) { // 값이 작으면 update
                 left = mid + 1;
             } else {
-                right = mid;
+                right = mid; // 값이 크거나 같으면 pass
             }
         }
         return left;
     }
 
-    // 값이 존재하는 처음 위치
-    private static int lowerBound(final int[] numbers, final int target) {
+    // 처음으로 해당 값이 오르는 인덱스
+    private static int upperBound(int target) {
         int left = 0;
-        int right = numbers.length;
-
+        int right = n;
         while (left < right) {
             int mid = (right - left) / 2 + left;
-            if (numbers[mid] < target) {
+            // 2 2 3 3 4 5 6
+            if (nums[mid] <= target) { // 값이 작거나 같으면
                 left = mid + 1;
-            } else {
+            } else { // 값이 크면
                 right = mid;
             }
         }
         return left;
     }
-
-
 }
