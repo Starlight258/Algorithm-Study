1907c8f596340c46d7f30d66be5d51369972a9d3
Mint <78211281+Starlight258@users.noreply.github.com>
2026-01-12 22:18:06 +0900

    [Gold IV] Title: N-Queen, Time: 2696 ms, Memory: 14812 KB -BaekjoonHub

--- 백준/Gold/9663. N－Queen/N－Queen.java
+++ 백준/Gold/9663. N－Queen/N－Queen.java
@@ -3,43 +3,57 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-
+    /**
+     * N-Queen 문제는 크기가 N × N인 체스판 위에 퀸 N개를 서로 공격할 수 없게 놓는 문제이다.
+     *
+     * N이 주어졌을 때, 퀸을 놓는 방법의 수를 구하는 프로그램을 작성하시오.
+     */
+    /**
+     * N=3 : 2 * 3 * 3 = 18
+     * 3 * 3 - (3+2+2)
+     * O O O
+     * O O O
+     * O O O
+     * N=4 : 6 * 4 * 4
+     * 4 * 4 - (4+3+3)
+     * O O O O (0,0) (0,1) (0,2)
+     * O O O O (1,0) (1,1) (1,2)
+     * O O O O (2,0) (2,1) (2,2)
+     * O O O O (3,0) (3,1) (3,2)
+     */
     private static int n;
-    private static int[] map;
     private static int answer = 0;
+    private static boolean[] colVisited;
+    private static boolean[] diagonalVisited;
+    private static boolean[] reverseDiagonalVisited;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         n = Integer.parseInt(br.readLine());
-        map = new int[n];
-
-        nQueenMethod(0);
+        colVisited = new boolean[n + 1]; // x
+        diagonalVisited = new boolean[n * 2+1]; // x+y
+        reverseDiagonalVisited = new boolean[n * 2+1]; // y-x+n-1=5
+        backtracking(1);
         System.out.println(answer);
     }
 
-    private static void nQueenMethod(final int depth) {
-        if (depth == n) {
+    private static void backtracking(int y) {
+        if (y == n + 1) {
             answer++;
             return;
         }
-
-        for (int i = 0; i < n; i++) {
-            map[depth] = i;
-            if (canQueen(depth)) {
-                nQueenMethod(depth + 1);
+        for (int x = 1; x <= n; x++) {
+            if (colVisited[x] || diagonalVisited[y + x] || reverseDiagonalVisited[y - x + n - 1]) {
+                continue;
             }
+            colVisited[x] = true;
+            diagonalVisited[y + x] = true;
+            reverseDiagonalVisited[y - x + n - 1] = true;
+            backtracking(y + 1);
+            colVisited[x] = false;
+            diagonalVisited[y + x] = false;
+            reverseDiagonalVisited[y - x + n - 1] = false;
         }
-    }
 
-    private static boolean canQueen(final int depth) {
-        for (int i = 0; i < depth; i++) {
-            if (map[i] == map[depth]) {
-                return false;
-            }
-            if (Math.abs(i - depth) == Math.abs(map[i] - map[depth])) {
-                return false;
-            }
-        }
-        return true;
     }
 }
