ada5723b79bade0bb7aafa861f57c6d711bb0353
Mint <78211281+Starlight258@users.noreply.github.com>
2026-01-14 01:16:16 +0900

    [Gold IV] Title: 부분합, Time: 240 ms, Memory: 23992 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -4,37 +4,49 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+    /*
+    10,000 이하의 자연수로 이루어진 길이 N짜리 수열이 주어진다.
+    이 수열에서 연속된 수들의 부분합 중에 그 합이 S 이상이 되는 것 중, 가장 짧은 것의 길이를 구하는 프로그램을 작성하시오.
+     */
+    /*
+    10 15
+    5 1 3 5 10 7 4 9 2 8
+
+    0 5 6 9 14
+    i~j : prefix[j] - prefix[i-1]
+    투포인터
+     */
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
-        int s = Integer.parseInt(st.nextToken());
-        int[] arr = new int[n + 1];
+        int s = Integer.parseInt(st.nextToken()); // 합
+        int[] prefix = new int[n + 1];
+        int[] nums = new int[n];
         st = new StringTokenizer(br.readLine());
         for (int i = 0; i < n; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+            nums[i] = Integer.parseInt(st.nextToken());
         }
-
-        // 2. 합 구하기
-        int left = 0, right = 0;
-        int answer = Integer.MAX_VALUE;
-        int sum = 0;
-        while (left <= n && right <= n) {
-            if (sum >= s) {
-                if (sum >= s && right - left < answer) {
-                    answer = right - left;
-                }
-                sum -= arr[left++];
+        for (int i = 1; i <= n; i++) {
+            prefix[i] += prefix[i-1] + nums[i - 1];
+        }
+        int left = 1;
+        int right = 1;
+        int length = Integer.MAX_VALUE;
+        while (left <= right && right <= n) {
+            int diff = prefix[right] - prefix[left - 1];
+            if (diff >= s) {
+                length = Math.min(right - left + 1, length);
+                left++;
             } else {
-                sum += arr[right++];
+                right++;
             }
         }
-
-        // 3. 정답 출력R
-        if (answer == Integer.MAX_VALUE) {
+        if (length == Integer.MAX_VALUE) {
             System.out.println(0);
         } else {
-            System.out.println(answer);
+            System.out.println(length);
         }
     }
+
 }
