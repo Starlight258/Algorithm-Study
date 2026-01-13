6a50344ae8e6c1acc6d6e1db0acac9ea77fffdd3
Mint <78211281+Starlight258@users.noreply.github.com>
2026-01-13 18:46:51 +0900

    [Silver II] Title: 나무 자르기, Time: 500 ms, Memory: 119376 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.java
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.java
@@ -4,29 +4,43 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+    /*
+    상근이는 나무 M미터가 필요하다. 근처에 나무를 구입할 곳이 모두 망해버렸기 때문에, 정부에 벌목 허가를 요청했다.
+    정부는 상근이네 집 근처의 나무 한 줄에 대한 벌목 허가를 내주었고, 상근이는 새로 구입한 목재절단기를 이용해서 나무를 구할것이다.
 
-    private static int[] lines;
+    목재절단기는 다음과 같이 동작한다. 먼저, 상근이는 절단기에 높이 H를 지정해야 한다. 높이를 지정하면 톱날이 땅으로부터 H미터 위로 올라간다.
+    그 다음, 한 줄에 연속해있는 나무를 모두 절단해버린다. 따라서, 높이가 H보다 큰 나무는 H 위의 부분이 잘릴 것이고, 낮은 나무는 잘리지 않을 것이다. 예를 들어, 한 줄에 연속해있는 나무의 높이가 20, 15, 10, 17이라고 하자. 상근이가 높이를 15로 지정했다면, 나무를 자른 뒤의 높이는 15, 15, 10, 15가 될 것이고, 상근이는 길이가 5인 나무와 2인 나무를 들고 집에 갈 것이다. (총 7미터를 집에 들고 간다) 절단기에 설정할 수 있는 높이는 양의 정수 또는 0이다.
 
+    상근이는 환경에 매우 관심이 많기 때문에, 나무를 필요한 만큼만 집으로 가져가려고 한다.
+    이때, 적어도 M미터의 나무를 집에 가져가기 위해서 절단기에 설정할 수 있는 높이의 최댓값을 구하는 프로그램을 작성하시오.
+     */
+    /*
+    n : 4 m : 7
+    20 15 10 17
+    답 : 15
+    자르는 길이 : mid, 잘라서 얻은 값 총 합 >= m
+    mid 최대값
+     */
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        long left = 1;
-        long right = 1;
-        lines = new int[n];
+        int[] trees = new int[n];
         st = new StringTokenizer(br.readLine());
+        int maxLine = 0;
         for (int i = 0; i < n; i++) {
-            lines[i] = Integer.parseInt(st.nextToken());
-            right = Math.max(right, lines[i]);
+            trees[i] = Integer.parseInt(st.nextToken());
+            maxLine = Math.max(trees[i], maxLine);
         }
-
-        long answer = 0;
+        int left = 1;
+        int right = maxLine;
+        int answer = 0;
         while (left <= right) {
-            long mid = (left + right) >>> 1;
-            if (calculateTree(mid) >= m) {
-                answer = mid;
+            int mid = (right - left) / 2 + left;
+            if (solve(mid, trees) >= m) {
                 left = mid + 1;
+                answer = Math.max(answer, mid);
             } else {
                 right = mid - 1;
             }
@@ -34,17 +48,12 @@ public class Main {
         System.out.println(answer);
     }
 
-    private static long calculateTree(final long mid) {
-        long answer = 0;
-        for (int line : lines) {
-            if (line >= mid) {
-                answer += line - mid;
-            }
+    private static long solve(int mid, final int[] trees) {
+        long total = 0;
+        for (int tree : trees) {
+            total += Integer.max(0, tree - mid);
         }
-        return answer;
+        return total;
     }
 
-    // 나무 m미터 필요, 높이 h 지정하여 자름
-    // 높이의 최대값
-    // long
 }
