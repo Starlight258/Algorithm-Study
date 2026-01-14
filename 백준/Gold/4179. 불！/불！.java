93b7f4ea0a8a8a9700e104f32fedbedf537566f8
Mint <78211281+Starlight258@users.noreply.github.com>
2026-01-14 14:57:09 +0900

    [Gold III] Title: 불!, Time: 500 ms, Memory: 55404 KB -BaekjoonHub

--- 백준/Gold/4179. 불！/불！.java
+++ 백준/Gold/4179. 불！/불！.java
@@ -2,6 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Queue;
 import java.util.StringTokenizer;
 
@@ -34,108 +35,78 @@ public class Main {
         StringTokenizer st = new StringTokenizer(br.readLine());
         int n = Integer.parseInt(st.nextToken());
         int m = Integer.parseInt(st.nextToken());
-        int[][] board = new int[n][m];
-        boolean[][] fvisited = new boolean[n][m];
-        boolean[][] jvisited = new boolean[n][m];
-        int jy = 0, jx = 0;
+        char[][] board = new char[n][m];
+        int[][] fireTime = new int[n][m];
+        int[][] jTime = new int[n][m];
+        int INF = Integer.MAX_VALUE;
+        for (int i = 0; i < n; i++) {
+            Arrays.fill(fireTime[i], INF);
+            Arrays.fill(jTime[i], INF);
+        }
 
+        int jy = 0, jx = 0;
         Queue<int[]> fq = new ArrayDeque<>();
         for (int i = 0; i < n; i++) {
             String line = br.readLine();
             for (int j = 0; j < m; j++) {
                 char c = line.charAt(j);
-                if (c == '#') {
-                    board[i][j] = -1;
-                } else if (c == '.') {
-                    board[i][j] = 0;
-                } else if (c == 'J') {
-                    board[i][j] = 0;
+                board[i][j] = c;
+                if (c == 'J') {
                     jy = i;
                     jx = j;
+                    jTime[jy][jx] = 0;
                 } else if (c == 'F') {
-                    board[i][j] = 0;
+                    fireTime[i][j] = 0;
                     fq.offer(new int[]{i, j});
-                    fvisited[i][j] = true;
                 }
             }
         }
 
-        boolean canExit = false;
+        // 불 이동
+        while (!fq.isEmpty()) {
+            int[] cur = fq.poll();
+            int y = cur[0], x = cur[1];
+            for (int i = 0; i < 4; i++) {
+                int ny = y + dy[i];
+                int nx = x + dx[i];
+                if (ny < 0 || nx < 0 || ny >= n || nx >= m) {
+                    continue;
+                }
+                if (board[ny][nx] == '#' || fireTime[ny][nx] != INF) {
+                    continue;
+                }
+                fq.offer(new int[]{ny, nx});
+                fireTime[ny][nx] = fireTime[y][x] + 1;
+            }
+        }
 
+        // 지훈 이동
         Queue<int[]> jq = new ArrayDeque<>();
         jq.offer(new int[]{jy, jx});
-        jvisited[jy][jx] = true;
-        int time = 0;
-        if (isEdge(jy, n, jx, m)) {
-            System.out.println(1);
-            return;
-        }
-
-        while (true) {
-            // 불 이동
-            int size = fq.size();
-            while (size-- > 0) {
-                int[] cur = fq.poll();
-                int y = cur[0];
-                int x = cur[1];
-                for (int i = 0; i < 4; i++) {
-                    int ny = y + dy[i];
-                    int nx = x + dx[i];
-                    if (ny < 0 || nx < 0 || ny >= n || nx >= m) {
-                        continue;
-                    }
-                    if (fvisited[ny][nx] || board[ny][nx] == -1) {
-                        continue;
-                    }
-                    fvisited[ny][nx] = true;
-                    fq.offer(new int[]{ny, nx});
+        while (!jq.isEmpty()) {
+            int[] curJ = jq.poll();
+            jy = curJ[0];
+            jx = curJ[1];
+            int t = jTime[jy][jx];
+            // 지훈 이동
+            for (int i = 0; i < 4; i++) {
+                int njy = jy + dy[i];
+                int njx = jx + dx[i];
+                if (njy < 0 || njx < 0 || njy >= n || njx >= m) {
+                    System.out.println(t + 1);
+                    return;
                 }
-            }
-
-            if (jq.isEmpty()) {
-                break;
-            }
-            size = jq.size();
-            while (size-- > 0) {
-                int[] curJ = jq.poll();
-                jy = curJ[0];
-                jx = curJ[1];
-                // 지훈 이동
-                for (int i = 0; i < 4; i++) {
-                    int njy = jy + dy[i];
-                    int njx = jx + dx[i];
-                    if (njy < 0 || njx < 0 || njy >= n || njx >= m) {
-                        continue;
-                    }
-                    if (fvisited[njy][njx] || jvisited[njy][njx] || board[njy][njx] == -1) {
-                        continue;
-                    }
-                    if (isEdge(njy, n, njx, m)) {
-                        canExit = true;
-                        break;
-                    }
-                    jq.offer(new int[]{njy, njx});
-                    jvisited[njy][njx] = true;
+                if (jTime[njy][njx] != INF || board[njy][njx] == '#') {
+                    continue;
                 }
-                if (canExit) {
-                    break;
+                int now = t + 1;
+                if (fireTime[njy][njx] <= now) {
+                    continue;
                 }
-            }
-            time++;
-            if (canExit) {
-                break;
+                jq.offer(new int[]{njy, njx});
+                jTime[njy][njx] = now;
             }
         }
-        if (canExit) {
-            System.out.println(time + 1);
-        } else {
-            System.out.println("IMPOSSIBLE");
-        }
-
+        System.out.println("IMPOSSIBLE");
     }
-
-    private static boolean isEdge(final int y, final int n, final int x, final int m) {
-        return y == 0 || y == n - 1 || x == 0 || x == m - 1;
-    }
-
 }
