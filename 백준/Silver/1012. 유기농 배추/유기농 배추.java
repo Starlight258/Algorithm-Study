097ef28d9c52952e5daff41a28e6d7df7027b50c
Mint <78211281+Starlight258@users.noreply.github.com>
2026-01-14 15:24:28 +0900

    [Silver II] Title: 유기농 배추, Time: 148 ms, Memory: 15932 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -1,55 +1,80 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
+    /*
+    입력의 첫 줄에는 테스트 케이스의 개수 T가 주어진다. 그 다음 줄부터 각각의 테스트 케이스에 대해 첫째 줄에는 배추를 심은 배추밭의 가로길이 M(1 ≤ M ≤ 50)과 세로길이 N(1 ≤ N ≤ 50),
+    그리고 배추가 심어져 있는 위치의 개수 K(1 ≤ K ≤ 2500)이 주어진다.
+    그 다음 K줄에는 배추의 위치 X(0 ≤ X ≤ M-1), Y(0 ≤ Y ≤ N-1)가 주어진다.
+    두 배추의 위치가 같은 경우는 없다.
+     */
+    private static final int[] dy = {-1, 0, 1, 0};
+    private static final int[] dx = {0, 1, 0, -1};
 
-    static int[] dy = {-1, 0, 1, 0};
-    static int[] dx = {0, 1, 0, -1};
-
-    static void dfs(int y, int x, int[][] map, boolean[][] visited) {
-        visited[y][x] = true;
-
-        for (int i = 0; i < 4; i++) {
-            int ny = y + dy[i];
-            int nx = x + dx[i];
-            if (ny < 0 || nx < 0 || ny >= map.length || nx >= map[0].length) {
-                continue;
-            }
-            if (!visited[ny][nx] && map[ny][nx] == 1) {
-                dfs(ny, nx, map, visited);
-            }
-        }
-    }
+    private static boolean[][] board;
+    private static boolean[][] visited;
+    private static int n;
+    private static int m;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int t = Integer.parseInt(br.readLine());
-        while (t-- > 0) {
-            String[] input = br.readLine().split(" ");
-            int m = Integer.parseInt(input[0]);
-            int n = Integer.parseInt(input[1]);
-            int k = Integer.parseInt(input[2]);
+        StringTokenizer st;
 
-            int[][] map = new int[n][m];
-            boolean[][] visited = new boolean[n][m];
+        while (t-- > 0) {
+            st = new StringTokenizer(br.readLine());
+            m = Integer.parseInt(st.nextToken());
+            n = Integer.parseInt(st.nextToken());
+            int k = Integer.parseInt(st.nextToken());
+            board = new boolean[n][m];
+            visited = new boolean[n][m];
             for (int i = 0; i < k; i++) {
-                String[] pos = br.readLine().split(" ");
-                int x = Integer.parseInt(pos[0]);
-                int y = Integer.parseInt(pos[1]);
-                map[y][x] = 1;
+                st = new StringTokenizer(br.readLine());
+                int x = Integer.parseInt(st.nextToken());
+                int y = Integer.parseInt(st.nextToken());
+                board[y][x] = true;
             }
 
-            int result = 0;
+            // bfs 확인
+            int answer = 0;
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < m; j++) {
-                    if (!visited[i][j] && map[i][j] == 1) {
-                        dfs(i, j, map, visited);
-                        result++;
+                    if (board[i][j] && !visited[i][j]) {
+                        bfs(i, j);
+                        answer++;
                     }
                 }
             }
-            System.out.println(result);
+            System.out.println(answer);
         }
     }
+
+    private static void bfs(int y, int x) {
+        Queue<int[]> q = new ArrayDeque<>();
+        q.offer(new int[]{y, x});
+        visited[y][x] = true;
+
+        while (!q.isEmpty()) {
+            int[] cur = q.poll();
+            y = cur[0];
+            x = cur[1];
+            for (int i = 0; i < 4; i++) {
+                int ny = y + dy[i];
+                int nx = x + dx[i];
+                if (ny < 0 || nx < 0 || ny >= n || nx >= m) {
+                    continue;
+                }
+                if (visited[ny][nx] || !board[ny][nx]) {
+                    continue;
+                }
+                q.offer(new int[]{ny, nx});
+                visited[ny][nx] = true;
+            }
+        }
+    }
+
 }
